daed103b5dac00e6ac1a1378983a74b03d7ec627
Olivier Corradi <olivier.corradi@gmail.com>
2016-06-05 18:05:19 +0200

    Modularize countrymap

---
+++ scripts/countrymap.js
@@ -0,0 +1,76 @@
+function CountryMap(selector) {
+    this.root = d3.select(selector);
+    this.graticule = this.root
+        .append('path')
+        .attr('class', 'graticule');
+    this.land = this.root.append('g');
+}
+
+CountryMap.prototype.render = function() {
+    var computedMapWidth = this.root.node().getBoundingClientRect().width,
+        computedMapHeight = this.root.node().getBoundingClientRect().height;
+
+    this.projection = d3.geo.mercator()
+        .center([3, 48])
+        .translate([0.6 * computedMapWidth, 0.6 * computedMapHeight])
+        .scale(700);
+
+    this.path = d3.geo.path()
+        .projection(this.projection);
+        
+    var graticuleData = d3.geo.graticule()
+        .step([5, 5]);
+        
+    this.graticule
+        .datum(graticuleData)
+        .attr('d', this.path);
+}
+
+CountryMap.prototype.projection = function(arg) {
+    if (!arg) return this.projection;
+    else this.projection = arg;
+};
+
+CountryMap.prototype.onCountryClick = function(arg) {
+    if (!arg) return this.countryClickHandler;
+    else this.countryClickHandler = arg;
+};
+
+CountryMap.prototype.onCountryMouseOver = function(arg) {
+    if (!arg) return this.countryMouseOverHandler;
+    else this.countryMouseOverHandler = arg;
+};
+
+CountryMap.prototype.onCountryMouseOut = function(arg) {
+    if (!arg) return this.countryMouseOutHandler;
+    else this.countryMouseOutHandler = arg;
+};
+
+CountryMap.prototype.data = function(data) {
+    if (!data) {
+        return this.data;
+    } else {
+        this.data = data;
+        var that = this;
+        this.land.selectAll('.country')
+            .data(data)
+            .enter()
+                .append('path')
+                .attr('class', 'country')
+                .attr('d', this.path)
+                .attr('stroke', 'black')
+                .attr('stroke-width', 0.3)
+                .attr('fill', function (d, i) { 
+                    return d.meta.co2 ? co2color(d.meta.co2) : 'gray';
+                })
+                .on('mouseover', function (d, i) {
+                    return that.countryMouseOverHandler.call(this, d, i);
+                })
+                .on('mouseout', function (d, i) {
+                    return that.countryMouseOutHandler.call(this, d, i);
+                })
+                .on('click', function (d, i) {
+                    return that.countryClickHandler.call(this, d, i);
+                });
+    }
+};
