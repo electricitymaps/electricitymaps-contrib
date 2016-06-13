e217e84c2a4bcac48c5688e946f5686c0fb93e26
Olivier Corradi <olivier.corradi@gmail.com>
2016-06-13 21:20:47 +0200

    Tentatively add solar

--- backend/parsers/solar.py
+++ backend/parsers/solar.py
@@ -1,6 +1,7 @@
 import json, pygrib, gzip
 with pygrib.open('flxf2016061000.01.2016061000.grb2') as f:
-    grb = f.select(name='Downward long-wave radiation flux', level=0)[0]
+    grb_LW = f.select(name='Downward long-wave radiation flux', level=0)[-1]
+    grb_SW = f.select(name='Downward short-wave radiation flux', level=0)[-1]
     # obj = {
     #     'lonlats': [],
     #     'DLWRF': []
@@ -11,9 +12,11 @@ with pygrib.open('flxf2016061000.01.2016061000.grb2') as f:
     #         obj['lonlats'].append([grb['longitudes'][n], grb['latitudes'][n]])
     #         obj['DLWRF'].append(grb['values'][i, j])
     obj = {
-        'lonlats': [grb['longitudes'].tolist(), grb['latitudes'].tolist()],
-        'values': grb['values'].tolist()
+        'lonlats': [grb_LW['longitudes'].tolist(), grb_LW['latitudes'].tolist()],
+        'DLWRF': grb_LW['values'].tolist(),
+        'DSWRF': grb_SW['values'].tolist()
     }
 
-with gzip.open('backend/data/solar.json.gz', 'w') as f:
+#with gzip.open('backend/data/solar.json.gz', 'w') as f:
+with open('backend/data/solar.json', 'w') as f:
     json.dump(obj, f)
