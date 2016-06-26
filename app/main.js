2425b29cdae4b3fece1b0e591ce33f300bf4a13f
Olivier Corradi <olivier.corradi@gmail.com>
2016-06-26 19:53:27 +0200

    Add initial text

--- app/main.js
+++ app/main.js
@@ -1,5 +1,4 @@
-//var ENDPOINT = 'http://localhost:8000';
-var ENDPOINT = 'http://52.19.179.81:8000';
+var ENDPOINT = document.domain == 'localhost' ? 'http://localhost:8000' : 'http://52.19.179.81:8000';
 
 var co2color = d3.scale.linear()
     .domain([0, 250, 500])
