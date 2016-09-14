6e439565ab2e53cac963a447368f5ce41f631c0c
Olivier Corradi <olivier.corradi@gmail.com>
2016-09-14 10:18:45 +0200

    Browser check is now a redirect

--- server.py
+++ server.py
@@ -74,6 +74,18 @@ def co2_GET():
     # http://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452
     return flask.jsonify({'error': 'NotImplementedYet'}), 400
 
+@app.route('/health', methods=['GET', 'OPTIONS'])
+@statsd.StatsdTimer.wrap('health_GET')
+def health_GET():
+    statsd.increment('health_GET')
+    # Check last data point obtained
+    EXPIRATION_SECONDS = 30 * 60.0
+    result = list(col.find(sort=[('datetime', pymongo.DESCENDING)], limit=1))
+    if not len(result) or (arrow.now() - arrow.get(result[0]['datetime'])).total_seconds() > EXPIRATION_SECONDS:
+        return flask.jsonify({'error': 'Database is empty or last measurement is too old.'}), 400
+    return flask.jsonify({'status': 'ok'})
+
+# ** Static routes
 @app.route('/solar', methods=['GET', 'OPTIONS'])
 @statsd.StatsdTimer.wrap('solar_GET')
 def solar_GET():
@@ -96,46 +108,15 @@ def data_GET(path):
     statsd.increment('data_GET')
     return flask.send_from_directory('public/data', path)
 
-@app.route('/')
-@statsd.StatsdTimer.wrap('index_GET')
-def index_GET():
-    statsd.increment('index_GET')
-    return flask.send_from_directory('public', 'index.html')
-
-@app.route('/style.css')
-def style_GET():
-    return flask.send_from_directory('public', 'style.css')
-
-@app.route('/favicon-32x32.png')
-def favicon_GET():
-    return flask.send_from_directory('public', 'favicon-32x32.png')
-
-@app.route('/tomorrow_logo_open_source.svg')
-def logo_GET():
-    return flask.send_from_directory('public', 'tomorrow_logo_open_source.svg')
-
-@app.route('/chromelogo-highres.png')
-def chrome_logo_GET():
-    return flask.send_from_directory('public', 'chromelogo-highres.png')    
-
 @app.route('/vendor/<path:path>', methods=['GET', 'OPTIONS'])
-def vendor_GET(path):
-    return flask.send_from_directory('public/vendor', path)
-
+def vendor_GET(path): return flask.send_from_directory('public/vendor', path)
 @app.route('/app/<path:path>', methods=['GET', 'OPTIONS'])
-def app_GET(path):
-    return flask.send_from_directory('public/app', path)
+def app_GET(path): return flask.send_from_directory('public/app', path)
+@app.route('/<path:filename>')
+def public_GET(filename): return flask.send_from_directory('public', filename)
 
-@app.route('/health', methods=['GET', 'OPTIONS'])
-@statsd.StatsdTimer.wrap('health_GET')
-def health_GET():
-    statsd.increment('health_GET')
-    # Check last data point obtained
-    EXPIRATION_SECONDS = 30 * 60.0
-    result = list(col.find(sort=[('datetime', pymongo.DESCENDING)], limit=1))
-    if not len(result) or (arrow.now() - arrow.get(result[0]['datetime'])).total_seconds() > EXPIRATION_SECONDS:
-        return flask.jsonify({'error': 'Database is empty or last measurement is too old.'}), 400
-    return flask.jsonify({'status': 'ok'})
+@app.route('/')
+def index_GET(): return flask.send_from_directory('public', 'index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=PORT)
