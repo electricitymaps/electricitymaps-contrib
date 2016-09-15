6a4f60b12d2c54e39c6ace61a5061a6f81e25d49
Olivier Corradi <olivier.corradi@gmail.com>
2016-09-15 09:54:19 +0200

    w/ co2 api

--- api/server.js
+++ api/server.js
@@ -1,8 +1,11 @@
 var express = require('express');
+var http = require('http');
 var app = express();
-var http = require('http').Server(app);
+var server = http.Server(app);
+var d3 = require('d3');
 var statsd = require('node-statsd');
 var MongoClient = require('mongodb').MongoClient;
+var co2calc = require('./static/app/co2eq').Co2eqCalculator();
 
 // * Database
 var mongoCollection;
@@ -14,7 +17,7 @@ MongoClient.connect(process.env['MONGO_URL'], function(err, db) {
     mongoCollection.createIndexes(
         [
             { datetime: -1 },
-            { countryCode: 1},
+            { countryCode: 1 },
             { datetime: -1, countryCode: 1 }
         ],
         null,
@@ -23,7 +26,7 @@ MongoClient.connect(process.env['MONGO_URL'], function(err, db) {
             else console.log('Database indexes created');
         }
     );
-    http.listen(8000, function() {
+    server.listen(8000, function() {
         console.log('Listening on *:8000');
     });
 });
@@ -33,6 +36,71 @@ var statsdClient = new statsd.StatsD();
 statsdClient.post = 8125;
 statsdClient.host = process.env['STATSD_HOST'];
 statsdClient.prefix = 'electricymap_api';
+
+// * Database methods
+function queryLastValues(callback) {
+    return mongoCollection.aggregate([
+        {'$sort': {'countryCode': 1, 'datetime': -1}},
+        {'$group': {'_id': '$countryCode', 'lastDocument': {'$first': '$$CURRENT'}}}
+    ], callback);
+}
+function queryAndCalculateCo2(countryCode, callback) {
+    queryLastValues(function (err, result) {
+        if (err) {
+            callback(err, result);
+        } else {
+            countries = {};
+            result.forEach(function(d) {
+                countries[d['_id']] = {
+                    data: {
+                        production: d.lastDocument.production,
+                        exchange: d.lastDocument.exchange,
+                        countryCode: d['_id']
+                    }
+                };
+            });
+            // Average out import-exports between commuting pairs
+            d3.keys(countries).forEach(function(country1, i) {
+                d3.keys(countries).forEach(function(country2, j) {
+                    if (i < j) return;
+                    var o = country1.countryCode;
+                    var d = country2.countryCode;
+                    var netFlows = [
+                        countries[d] ? countries[d].data.exchange[o] : undefined,
+                        countries[o] ? -countries[o].data.exchange[d] : undefined
+                    ];
+                    var netFlow = d3.mean(netFlows);
+                    if (netFlow == undefined)
+                        return;
+                    countries[o].data.exchange[d] = -netFlow;
+                    countries[d].data.exchange[o] = netFlow;
+                });
+            });
+            // Compute aggregates
+            d3.entries(countries).forEach(function (entry) {
+                country = entry.value;
+                // Add extra data
+                country.data.maxCapacity = 
+                    d3.max(d3.values(country.data.capacity));
+                country.data.maxProduction = 
+                    d3.max(d3.values(country.data.production));
+                country.data.totalProduction = 
+                    d3.sum(d3.values(country.data.production));
+                country.data.totalNetExchange = 
+                    d3.sum(d3.values(country.data.exchange));
+                country.data.maxExport = 
+                    -Math.min(d3.min(d3.values(country.data.exchange)), 0) || 0;
+            });
+            co2calc.compute(countries);
+            callback(err, {
+                status: 'ok',
+                countryCode: countryCode,
+                data: co2calc.assignments[countryCode],
+                unit: 'gCo2eq/kWh'
+            });
+        }
+    });
+}
 
 // * Routes
 app.use(express.static('static'));
@@ -50,10 +118,7 @@ app.get('/v1/solar', function(req, res) {
 app.get('/v1/production', function(req, res) {
     statsdClient.increment('production_GET');
     var t0 = new Date().getTime();
-    mongoCollection.aggregate([
-        {'$sort': {'countryCode': 1, 'datetime': -1}},
-        {'$group': {'_id': '$countryCode', 'lastDocument': {'$first': '$$CURRENT'}}}
-    ], function (err, result) {
+    queryLastValues(function (err, result) {
         if (err) {
             statsdClient.increment('production_GET_ERROR');
             console.error(err);
@@ -62,9 +127,54 @@ app.get('/v1/production', function(req, res) {
             obj = {}
             result.forEach(function(d) { obj[d['_id']] = d.lastDocument; });
             res.json({status: 'ok', data: obj});
-            statsdClient.timing('production', new Date().getTime() - t0);
+            statsdClient.timing('production_GET', new Date().getTime() - t0);
         }
     });
+});
+app.get('/v1/co2', function(req, res) {
+    statsdClient.increment('co2_GET');
+    var t0 = new Date().getTime();
+
+    function onCo2Computed(err, result) {
+        if (err) {
+            statsdClient.increment('co2_GET_ERROR');
+            console.error(err);
+            res.status(500).json({error: 'Unknown error'});
+        } else {
+            res.json(result);
+            statsdClient.timing('co2_GET', new Date().getTime() - t0);
+        }
+    }
+
+    if ((req.query.lon && req.query.lat) || req.query.countryCode) {
+        if (!req.query.countryCode) {
+            // Geocode
+            http.get(
+                'http://maps.googleapis.com/maps/api/geocode/json?latlng=' + req.query.lat + ',' + req.query.lon,
+                function (geocoderResponse) {
+                    var body = '';
+                    geocoderResponse.on('data', function(chunk) { body += chunk; });
+                    geocoderResponse.on('end', function() {
+                        var obj = JSON.parse(body).results[0].address_components
+                            .filter(function(d) { return d.types.indexOf('country') != -1; });
+                        if (obj.length)
+                            queryAndCalculateCo2(obj[0].short_name, onCo2Computed);
+                        else {
+                            console.error('Geocoder returned no usable results');
+                            res.status(500).json({error: 'Error while geocoding'});
+                        }
+                    });
+                }
+            ).on('error', (e) => {
+                console.error(`Error while geocoding: ${e.message}`);
+                res.status(500).json({error: 'Error while geocoding'});
+            });
+        } else {
+            queryAndCalculateCo2(req.query.countryCode, onCo2Computed);
+        }
+    } else {
+        res.status(400).json({'error': 'Missing arguments "lon" and "lat" or "countryCode"'})
+    }
 });
 app.get('/health', function(req, res) {
     statsdClient.increment('health_GET');
---
+++ api/static/app/co2eq.js
@@ -0,0 +1,115 @@
+function Co2eqCalculator() {
+    // Node dependencies
+    if (typeof require != 'undefined') {
+        d3 = require('d3');
+        math = require('mathjs');
+    }
+
+    this.defaultCo2eqFootprint = {
+        'biomass': 740,
+        'coal': 820,
+        'gas': 490,
+        'hydro': 24,
+        'nuclear': 12,
+        'oil': 650,
+        'solar': 45,
+        'wind': 12,
+    }; // in gCo2eq/kWh
+
+    this.countryCo2eqFootprint = {
+        'DE': function (productionMode) {
+            return productionMode == 'other' ? 700 : null;
+        },
+        'DK': function (productionMode) {
+            return productionMode == 'other' ? 700 : null;
+        },
+        'FI': function (productionMode) {
+            return productionMode == 'other' ? 700 : null;
+        },
+        'GB': function (productionMode) {
+            return productionMode == 'other' ? 300 : null;
+        },
+        'NO': function (productionMode) {
+            return productionMode == 'other' ? 700 : null;
+        },
+        'SE': function (productionMode) {
+            return productionMode == 'other' ? 700 : null;
+        }
+    };
+
+    this.footprintOf = function(productionMode, countryKey) {
+        var defaultFootprint = this.defaultCo2eqFootprint[productionMode];
+        var countryFootprint = this.countryCo2eqFootprint[countryKey] || function () { };
+        return countryFootprint(productionMode) || defaultFootprint;
+    };
+
+    this.compute = function(countries) {
+        var validCountries = d3.entries(countries)
+            .map(function(d) { return d.value.data })
+            .filter(function (d) {
+                return d.countryCode !== undefined;
+            });
+        var validCountryKeys = validCountries.map(function (d) { return d.countryCode });
+
+        // x_i: unknown co2 (consumption) footprint of i-th country
+        // f_ij: known co2 footprint of j-th system of i-th country
+        // v_ij: power volume of j-th system of i-th country
+        // CO2 mass flow balance equation for each country i:
+        // x_i * (sum_j_intern(v_ij) + sum_j_import(v_ij) - sum_j_export(v_ij)) = 
+        //     sum_j_intern(f_ij * v_ij)
+        //   + sum_j_import(x_j * v_ij)
+        //   - x_i * sum_j_export(v_ij)
+        
+        // We wish to solve Ax = b
+        var n = validCountries.length;
+        var A = math.sparse().resize([n, n]);
+        var b = math.zeros(n);
+
+        var that = this;
+
+        validCountries.forEach(function (country, i) {
+            A.set([i, i], -country.totalProduction - country.totalNetExchange);
+            // Intern
+            d3.entries(country.production).forEach(function (production) {
+                var footprint = that.footprintOf(production.key, country.countryCode);
+                if (footprint === undefined) {
+                    console.warn(country.countryCode + ' CO2 footprint of ' + production.key + ' is unknown');
+                    return;
+                }
+                // Accumulate
+                b.set([i], b.get([i]) - footprint * production.value);
+            });
+            // Exchanges
+            d3.entries(country.exchange).forEach(function (exchange) {
+                var j = validCountryKeys.indexOf(exchange.key);
+                if (j < 0) {
+                    console.warn(country.countryCode + ' neighbor ' + exchange.key + ' has no data');
+                    return;
+                }
+                if (exchange.value > 0) {
+                    // Import
+                    A.set([i, j], exchange.value);
+                } else {
+                    // Accumulate export
+                    A.set([i, i], A.get([i, i]) - Math.abs(exchange.value));
+                }
+            });
+        });
+
+        // Solve
+        var x = math.lusolve(A, b);
+        this.assignments = {};
+        x.toArray().forEach(function (x, i) {
+            that.assignments[validCountries[i].countryCode] = x[0];
+        });
+
+        return this;
+    }
+
+    return this;
+}
+
+if (typeof module != 'undefined')
+    module.exports = {
+        Co2eqCalculator: Co2eqCalculator
+    }
