c4cd09083175cb7a5f0d8f2e23457640c581e66b
ThierryOllivero <thierry.ollivero@gmail.com>
2016-09-20 12:44:00 +0200

    Update RO.py with exchanges

--- feeder/parsers/RO.py
+++ feeder/parsers/RO.py
@@ -17,7 +17,13 @@ def fetch_RO():
         'unknown': float(data['CONS'])
     }
     # According to http://www.transelectrica.ro/widget/web/tel/sen-harta/-/harta_WAR_SENOperareHartaportlet
+    # BALT and UCRS (for Baltic and Ukraine South) are categorized under Bulgary on transelectrica website. We did the same here.
     obj['exchange'] = {
+        'BG': float(data['VARN']) + float(data['DOBR']) + float(data['KOZL1']) + float(data['KOZL2']) + float(data['BALT']) + float(data['UCRS'])
+        'HU': float(data['SAND']) + float(data['BEKE1']) + float(data['BEKE2'])
+        'MD': float(data['COSE']) + float(data['UNGE']) + float(data['CIOA']) + float(data['GOTE'])
+        'RS': float(data['DJER']) + float(data['PAN1']) + float(data['PAN2']) + float(data['KUSJ']) + float(data['SIP_']) + float(data['KIKI'])
+        'UA': float(data['VULC']) + float(data['MUKA']) + float(data['COD1'])
     }
     obj['production'] = {
         'biomass': float(data['BMASA']),
