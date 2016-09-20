183ce63f0bec47ef4bf411a00019abfaef8f587a
Olivier Corradi <olivier.corradi@gmail.com>
2016-09-20 11:51:23 +0200

    Initial RO commit

---
+++ feeder/parsers/RO.py
@@ -0,0 +1,30 @@
+import arrow
+import requests
+
+COUNTRY_CODE = 'RO'
+
+def fetch_RO():
+    url = 'http://www.transelectrica.ro/sen-filter'
+    data = requests.get(url).json()
+
+    obj = {
+        'countryCode': COUNTRY_CODE,
+        'datetime': arrow.get(data['row1_HARTASEN_DATA'], "YY/M/D HH:mm:ss").replace(tzinfo=dateutil.tz.gettz('Europe/Bucharest'))
+    }
+    obj['consumption'] = {}
+    obj['exchange'] = {
+    }
+    obj['production'] = {
+        'biomass': float(data['BMASA']),
+        'coal': float(data['CARB']),
+        'gas': float(data['GAZE']),
+        'hydro': float(data['APE']),
+        'nuclear': float(data['NUCL']),
+        'solar': float(data['FOTO']),
+        'wind': float(data['EOLINA']),
+    }
+
+    return obj
+
+if __name__ == '__main__':
+    print fetch_RO()
