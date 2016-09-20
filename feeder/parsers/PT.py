918c7b47eb69616410307dcbd5efed5a64bb7d1c
Guilherme Coelho <me@iamguico.com>
2016-09-20 20:42:18 +0200

    Add PT parser

---
+++ feeder/parsers/PT.py
@@ -0,0 +1,44 @@
+import requests, dateutil, arrow
+from bs4 import BeautifulSoup
+
+COUNTRY_CODE = 'PT'
+
+def fetch_PT():
+
+	r = requests.get('http://www.centrodeinformacao.ren.pt/EN/InformacaoExploracao/Pages/EstatisticaDiaria.aspx')
+	soup = BeautifulSoup(r.text, 'html.parser')
+
+	trs = soup.find_all("tr", { "class" : "grid_row" })
+	daily_values = []
+
+	for tr in trs:
+		value = tr.find_all("td")[2].string # Daily values are in column 3
+		value = float(value) # str -> float
+		daily_values.append(value)
+
+	date_str = soup.find(id="ctl00_m_g_5e80321e_76aa_4894_8c09_4e392fc3dc7d_txtDatePicker_foo")['value']
+	date = arrow.get(date_str, "DD-MM-YYYY").replace(tzinfo=dateutil.tz.gettz('Europe/Lisbon')).datetime
+
+
+	data = {
+		'countryCode': COUNTRY_CODE,
+		'datetime': date, # UTC
+		'production': {
+		    'wind': daily_values[9],
+		    'solar': daily_values[10],
+		    'hydro': daily_values[0] + daily_values[7], # There are 2 different production regimes
+		    'coal': daily_values[1] + daily_values[8] # There are 2 different production regimes
+		},
+		'consumption': {
+		    'unknown': daily_values[13]
+		},
+		'exchange':{
+			'ES': daily_values[3] - daily_values[4]
+		}
+
+	}
+
+	return data
+
+if __name__ == '__main__':
+    print fetch_PT()
