898d965a74f46b1ec558ff7e29d90cf3abe6784c
apant <a.pantkowski@gmail.com>
2016-10-17 21:53:03 +0200

    current

--- feeder/parsers/PL.py
+++ feeder/parsers/PL.py
@@ -1,7 +1,9 @@
 
 from bs4 import BeautifulSoup
-import urllib.request
+import requests
 import arrow
+
+COUNTRY_CODE = 'PL'
 
 def fetchValue(params):
 
@@ -11,11 +13,13 @@ def fetchValue(params):
     periodEnd = str(end.format('YYYYMMDDHH00'))
     periodStart = str(start.format('YYYYMMDDHH00'))
 
+
+
     parameters = '&psrType=' + params + '&documentType=A75&processType=A16&in_Domain=10YPL-AREA-----S&periodStart=' + periodStart + '&periodEnd=' + periodEnd
     link = 'https://transparency.entsoe.eu/api?securityToken=7466690c-c66a-4a00-8e21-2cb7d538f380' + parameters
 
-    content = urllib.request.urlopen(link).read()
-    soup = BeautifulSoup(content, "lxml")
+    content = requests.get(link)
+    soup = BeautifulSoup(content.text, "html.parser")
 
     quantity = []
 
@@ -31,7 +35,7 @@ def fetchValue(params):
     return float(value)
 
 
-def fetchCountry():
+def fetch_PL():
 
     parameters = ["B01", "B02", "B03", "B04", "B05", "B06", "B10", "B11", "B12", "B19"]
 
@@ -40,7 +44,7 @@ def fetchCountry():
         output_array.append(fetchValue(place))
 
     data = {
-        'countryCode': "PL",
+        'countryCode': COUNTRY_CODE,
         'production': {
             'wind': output_array[9],
             'solar': 0,
@@ -57,7 +61,7 @@ def fetchCountry():
     return data
 
 if __name__ == '__main__':
-    print(fetchCountry())
+    print(fetch_PL())
 
 
 
