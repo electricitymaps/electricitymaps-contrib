590ced915c94d906b50ec125f824ca6d44c22e0f
Olivier Corradi <olivier.corradi@gmail.com>
2016-11-08 19:14:57 +0100

    Fix ENTSOE date formatting and add NL (Fixes #78)

---
+++ feeder/parsers/NL.py
@@ -0,0 +1,9 @@
+from ENTSOE import fetch_ENTSOE
+
+COUNTRY_CODE = 'NL'
+ENTSOE_DOMAIN = '10YNL----------L'
+
+def fetch_NL(session=None): 
+    return fetch_ENTSOE(ENTSOE_DOMAIN, COUNTRY_CODE, session)
+
+if __name__ == '__main__': print(fetch_NL())
