9f3a9a53b863a64030fef4812d44cdf128da5a5b
Olivier Corradi <olivier.corradi@gmail.com>
2016-11-08 22:02:08 +0100

    Switch FI to ENTSOE

--- feeder/parsers/FI.py
+++ feeder/parsers/FI.py
@@ -1,32 +1,9 @@
-import arrow
-import requests
+from ENTSOE import fetch_ENTSOE
 
 COUNTRY_CODE = 'FI'
+ENTSOE_DOMAIN = '10YFI-1--------U'
 
 def fetch_FI(session=None):
-    url = 'http://driftsdata.statnett.no/restapi/ProductionConsumption/GetLatestDetailedOverview'
-    data = (session or requests).get(url).json()
-    countries = map(lambda x: x['value'], data['Headers'])
-    i = countries.index(COUNTRY_CODE)
+    return fetch_ENTSOE(ENTSOE_DOMAIN, COUNTRY_CODE, session)
 
-    obj = {
-        'countryCode': COUNTRY_CODE,
-        'datetime': arrow.get(data['MeasuredAt'] / 1000).datetime # time given in UTC
-    }
-    obj['consumption'] = {
-        'unknown': float(data['ConsumptionData'][i]['value'].replace(u'\xa0', '').replace(' ', '').replace('-', '0'))
-    }
-    obj['exchange'] = {
-    }
-    obj['production'] = {
-        'unknown': float(data['ThermalData'][i]['value'].replace(u'\xa0', '').replace(' ', '').replace('-', '0')) + 
-            float(data['NotSpecifiedData'][i]['value'].replace(u'\xa0', '').replace(' ', '').replace('-', '0')),
-        'wind': float(data['WindData'][i]['value'].replace(u'\xa0', '').replace(' ', '').replace('-', '0')),
-        'nuclear': float(data['NuclearData'][i]['value'].replace(u'\xa0', '').replace(' ', '').replace('-', '0')),
-        'hydro': float(data['HydroData'][i]['value'].replace(u'\xa0', '').replace(' ', '').replace('-', '0')),
-    }
-
-    return obj
-
-if __name__ == '__main__':
-    fetch_FI()
+if __name__ == '__main__': print(fetch_FI())
