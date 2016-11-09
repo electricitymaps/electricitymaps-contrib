29a4eb97793b9c18c287c88752b26540e335c24e
apant <a.pantkowski@gmail.com>
2016-11-09 18:44:26 +0100

    removed BG parser

--- feeder/parsers/BG.py
+++
@@ -1,9 +0,0 @@
-from ENTSOE import fetch_ENTSOE
-
-COUNTRY_CODE = 'BG'
-ENTSOE_DOMAIN = '10YCA-BULGARIA-R'
-
-def fetch_BG(session=None):
-    return fetch_ENTSOE(ENTSOE_DOMAIN, COUNTRY_CODE, session)
-
-if __name__ == '__main__': print(fetch_BG())
