4beda3157aae48f6d8378007b3b00a4e18033f68
apant <a.pantkowski@gmail.com>
2016-11-09 18:43:43 +0100

    deleted SI.py

--- feeder/parsers/SI.py
+++
@@ -1,9 +0,0 @@
-from ENTSOE import fetch_ENTSOE
-
-COUNTRY_CODE = 'SI'
-ENTSOE_DOMAIN = '10YSI-ELES-----O'
-
-def fetch_SI(session=None):
-    return fetch_ENTSOE(ENTSOE_DOMAIN, COUNTRY_CODE, session)
-
-if __name__ == '__main__': print(fetch_SI())
