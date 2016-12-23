18a41001da46fcdd68ecb548790b3264599d8905
Olivier Corradi <olivier.corradi@gmail.com>
2016-12-23 09:52:14 +0100

    Serve dev bundle in dev mode

--- web/server.js
+++ web/server.js
@@ -33,7 +33,8 @@ app.disable('etag'); // Disable etag generation (except for static)
 // * Static and templating
 app.use(express.static(__dirname + '/public', {etag: true, maxAge: '4h'}));
 app.set('view engine', 'ejs');
-var BUNDLE_HASH = JSON.parse(fs.readFileSync('public/dist/manifest.json')).hash;
+var BUNDLE_HASH = !isProduction ? 'dev' : 
+    JSON.parse(fs.readFileSync('public/dist/manifest.json')).hash;
 
 // * Cache
 var memcachedClient = new Memcached(process.env['MEMCACHED_HOST']);
