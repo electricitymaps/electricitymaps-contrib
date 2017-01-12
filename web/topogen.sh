84fface72189b39531f1244cf1cdbaff7b9a1116
Olivier Corradi <olivier.corradi@gmail.com>
2017-01-12 21:49:27 +0100

    Sugar

--- web/topogen.sh
+++ web/topogen.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 set -eu -o pipefail
 
+FILENAME=ne_50m_admin_0_map_subunits
+#Check ne_50m_admin_1_states_provinces_lakes for US & Canada provinces.
+
 # To install tools:
 # npm install -g d3-geo-projection ndjson-cli shapefile topojson
 
 mkdir -p build
-curl -z build/ne_50m_admin_0_map_subunits.zip -o build/ne_50m_admin_0_map_subunits.zip http://naciscdn.org/naturalearth/50m/cultural/ne_50m_admin_0_map_subunits.zip
-unzip -od build build/ne_50m_admin_0_map_subunits.zip
-chmod a-x build/ne_50m_admin_0_map_subunits.*
+curl -z build/${FILENAME}.zip -o build/${FILENAME}.zip http://naciscdn.org/naturalearth/50m/cultural/${FILENAME}.zip
+unzip -od build build/${FILENAME}.zip
+chmod a-x build/${FILENAME}.*
 
 geo2topo -q 1e5 -n countries=<( \
-  shp2json -n build/ne_50m_admin_0_map_subunits.shp \
+  shp2json -n build/${FILENAME}.shp \
     | ndjson-map '(d.id = d.properties.adm0_a3, d.subid = d.properties.su_a3, delete d.properties, d)' \
     | geostitch -n) \
 | topomerge land=countries \
 > app/world_50m.json
-shp2json -n build/ne_50m_admin_0_map_subunits.shp \
+
+shp2json -n build/${FILENAME}.shp \
 | ndjson-map 'd.properties' \
 > world_50m_props.json
 rm -rvf build
