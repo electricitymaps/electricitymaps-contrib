a93ff5242c08c99b88d42af26ea86282fc797753
Martin Daniel <martin.daniel@airbnb.com>
2017-01-13 10:53:41 -0800

    fix space in parameters. Change function name

--- datascience/utils.py
+++ datascience/utils.py
@@ -5,7 +5,7 @@ import requests
 
 r = requests.session()
 
-def date_range(start_date,end_date,delta):
+def date_range(start_date, end_date, delta):
     start, end = [
         arrow.get(start_date),
         arrow.get(end_date)
@@ -28,17 +28,17 @@ def fetch_production(country_code, t, delta):
     if not obj: return
     return obj if (t - arrow.get(obj['datetime'])).total_seconds() < delta * 60.0 else None
 
-def df_production(countries,start_date,end_date,delta):
+def get_production(countries, start_date, end_date, delta):
     df = pd.DataFrame(columns=['country','timestamp','sources','production'])
-    time_span = date_range(start_date,end_date,delta)
+    time_span = date_range(start_date, end_date,delta)
     for country in countries:
         print 'Fetching country %s..' % country
         for t in time_span:
             print 'Fetching time %s..' % t
             o = fetch_production(country, t, delta)
             p = pd.DataFrame({'country': country,
-                                'timestamp':t,
-                                'sources':o['production'].keys(),
-                                'production':o['production'].values()})
+                              'timestamp': t ,
+                              'sources': o['production'].keys(),
+                              'production': o['production'].values()})
             df = df.append(p)
     return df
