7cbe44a7ebd05a1253b984c7efb08686ce3b2499
Olivier Corradi <olivier.corradi@gmail.com>
2017-02-07 08:53:50 +0100

    Fix #249

--- web/app/exchangeconfig.js
+++ web/app/exchangeconfig.js
@@ -292,6 +292,10 @@ exports.addExchangesConfiguration = function(exchanges) {
         lonlat: [20.614102, 49.359467],
         rotation: 180
     };
+    exchanges['PL->UA'] = {
+        lonlat: [24.097224, 50.664587],
+        rotation: 90
+    }
     // RO
     exchanges['RO->UA'] = {
         lonlat: [24.821959, 47.768595],
