898b7df51a57b807d8af856087f39f51a38b0410
Olivier Corradi <olivier.corradi@gmail.com>
2017-03-07 11:48:58 +0100

    Bugfix

--- datascience/utils.py
+++ datascience/utils.py
@@ -29,7 +29,7 @@ def fetch_production(country_code, t, delta):
         'countryCode': country_code,
         'datetime': t.isoformat()
     }
-    obj = r.get(url, params=params, headers={'ELECTRICITYMAP_TOKEN', ELECTRICITYMAP_TOKEN}).json()
+    obj = r.get(url, params=params, headers={'ELECTRICITYMAP_TOKEN': ELECTRICITYMAP_TOKEN}).json()
     if not obj: return
     return obj if (t - arrow.get(obj['datetime'])).total_seconds() < delta * 60.0 else None
 
@@ -61,7 +61,7 @@ def fetch_exchange(country_code, t):
         'datetime': t.isoformat(),
         'countryCode': country_code
     }
-    obj = r.get(url, params=params, headers={'ELECTRICITYMAP_TOKEN', ELECTRICITYMAP_TOKEN}).json()
+    obj = r.get(url, params=params, headers={'ELECTRICITYMAP_TOKEN': ELECTRICITYMAP_TOKEN}).json()
     return obj['data']
 
 def get_exchange(countries, start_date, end_date, delta):
@@ -101,7 +101,7 @@ def fetch_state(t, delta):
     params = {
         'datetime': t.isoformat()
     }
-    obj = r.get(url, params=params, headers={'ELECTRICITYMAP_TOKEN', ELECTRICITYMAP_TOKEN}).json()
+    obj = r.get(url, params=params, headers={'ELECTRICITYMAP_TOKEN': ELECTRICITYMAP_TOKEN}).json()
     if not obj: return
     return obj['data']
 
@@ -117,11 +117,12 @@ def get_state(countries, start_date, end_date, delta):
             if not 'datetime' in d: continue
             p = pd.DataFrame(
                 data={
-                    'timestamp': pd.Timestamp(arrow.get(d['datetime']).datetime),
+                    'timestamp': pd.Timestamp(t.datetime),
                     'country': countryCode,
                     'co2intensity': d.get('co2intensity', None),
                 },
                 index=[0])
+            print(p)
             if df is not None: df = df.append(p)
             else: df = p
     return df
@@ -134,7 +135,7 @@ def fetch_price(country_code, t, delta):
         'countryCode': country_code,
         'datetime': t.isoformat()
     }
-    obj = r.get(url, params=params, headers={'ELECTRICITYMAP_TOKEN', ELECTRICITYMAP_TOKEN}).json()
+    obj = r.get(url, params=params, headers={'ELECTRICITYMAP_TOKEN': ELECTRICITYMAP_TOKEN}).json()
     if not obj: return
     return obj if (t - arrow.get(obj['datetime'])).total_seconds() < delta * 60.0 else None
 
