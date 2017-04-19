26ec2e1f3ac6672af7294e06634a64c6ea87eea6
Olivier Corradi <olivier.corradi@gmail.com>
2017-04-19 09:31:43 +0200

    Add languages for mobile app, fix #478

--- mobileapp/build.sh
+++ mobileapp/build.sh
@@ -2,15 +2,17 @@
 set -eu -o pipefail
 
 # Build assets
-rm -rf www || true
-rm -rf locales || true
 pushd ..
 docker-compose build web
 popd
 CONTAINER_ID=$(docker create electricitymap_web)
-docker cp $CONTAINER_ID:/home/web/public/ .
-mv public www
+
+rm -rf www/electricitymap || true
+docker cp $CONTAINER_ID:/home/web/public/ www/electricitymap
+
+rm -rf locales || true
 docker cp $CONTAINER_ID:/home/web/locales/ .
+
 docker rm $CONTAINER_ID
 
 # Run node in order to build index.html
--- mobileapp/generate-index.js
+++ mobileapp/generate-index.js
@@ -2,33 +2,62 @@ var ejs = require('ejs');
 var fs = require('fs');
 var i18n = require('i18n');
 
-var BUNDLE_HASH = JSON.parse(fs.readFileSync('www/dist/manifest.json')).hash;
+var BUNDLE_HASH = JSON.parse(fs.readFileSync('www/electricitymap/dist/manifest.json')).hash;
+
+// TODO:
+// Currently, those variables are duplicated from server.js
+// We should instead have a central configuration file in the `config` folder
+var locales = ['da', 'de', 'en', 'es', 'fr', 'it', 'nl', 'sv'];
+var LOCALE_TO_FB_LOCALE = {
+    'da': 'da_DK',
+    'de': 'de_DE',
+    'en': 'en_US',
+    'es': 'es_ES',
+    'fr': 'fr_FR',
+    'it': 'it_IT',
+    'nl': 'nl_NL',
+    'sv': 'sv_SE'
+};
+var SUPPORTED_FB_LOCALES = [
+    'da_DK',
+    'de_DE',
+    'es_ES',
+    'es_LA',
+    'es_MX',
+    'en_GB',
+    'en_PI',
+    'en_UD',
+    'en_US',
+    'fr_CA',
+    'fr_FR',
+    'it_IT',
+    'nl_BE',
+    'nl_NL',
+    'sv_SE'
+];
 
 // * i18n
 i18n.configure({
     // where to store json files - defaults to './locales' relative to modules directory
-    locales: ['de', 'en', 'es', 'fr', 'it', 'nl', 'sv'],
+    locales: locales,
     directory: __dirname + '/locales',
     defaultLocale: 'en',
     queryParameter: 'lang',
     objectNotation: true,
     updateFiles: false // whether to write new locale information to disk - defaults to true
 });
-i18n.setLocale('en');
 
-var template = ejs.compile(fs.readFileSync('../web/views/pages/index.ejs', 'utf8'));
-var html = template({
-  bundleHash: BUNDLE_HASH,
-  locale: 'en',
-  FBLocale: 'en_US',
-  supportedFBLocales: ['en_US'],
-  '__': i18n.__
+locales.forEach(function(locale) {
+    i18n.setLocale(locale);
+    var template = ejs.compile(fs.readFileSync('../web/views/pages/index.ejs', 'utf8'));
+    var html = template({
+      bundleHash: BUNDLE_HASH,
+      isCordova: true,
+      locale: locale,
+      FBLocale: LOCALE_TO_FB_LOCALE[locale],
+      supportedFBLocales: SUPPORTED_FB_LOCALES,
+      '__': i18n.__
+    });
+
+    fs.writeFileSync('www/electricitymap/index_' + locale + '.html', html);
 });
-
-fs.writeFile('www/index.html', html, function(err) {
-  if(err) {
-    throw err;
-  } else {
-    process.exit();
-  }
-})
