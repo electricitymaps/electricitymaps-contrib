40d77a4581296d77b1bd34ae0fb9c6fe9842e50d
Olivier Corradi <olivier.corradi@gmail.com>
2017-04-20 14:48:48 +0200

    Add support for Canadian states

--- web/topogen.sh
+++ web/topogen.sh
@@ -5,7 +5,7 @@ COUNTRIES_FILENAME=ne_50m_admin_0_map_subunits
 # COUNTRIES_FILTER='...'
 
 STATES_FILENAME=ne_50m_admin_1_states_provinces_lakes
-STATES_FILTER='AUS'
+STATES_FILTER='AUS\|CAN'
 
 # To install tools:
 # npm install -g d3-geo-projection ndjson-cli shapefile topojson
@@ -22,13 +22,13 @@ unzip -od build build/${STATES_FILENAME}.zip
 # Parse countries
 (shp2json -n build/${COUNTRIES_FILENAME}.shp \
 | ndjson-map '(d.id = d.properties.adm0_a3, d.oldProps = d.properties, d.properties = {}, d.properties.subid = d.oldProps.su_a3, d.properties.code_hasc = d.oldProps.code_hasc, delete d.oldProps, d)' \
-#| grep $COUNTRIES_FILTER
+#| grep "$COUNTRIES_FILTER"
 )>> build/tmp.json
 
 # Parse states
 (shp2json -n build/${STATES_FILENAME}.shp \
 | ndjson-map '(d.id = d.properties.sr_adm0_a3, d.oldProps = d.properties, d.properties = {}, d.properties.subid = d.oldProps.su_a3, d.properties.code_hasc = d.oldProps.code_hasc, delete d.oldProps, d)' \
-| grep $STATES_FILTER
+| grep "$STATES_FILTER"
 )>> build/tmp.json
 
 # Merge
@@ -38,11 +38,11 @@ geo2topo -q 1e5 -n countries=<(cat build/tmp.json | geostitch -n) \
 
 (shp2json -n build/${STATES_FILENAME}.shp \
   | ndjson-map 'd.properties' \
-  | grep $STATES_FILTER \
+  | grep "$STATES_FILTER" \
 )> world_states_50m_props.json
 (shp2json -n build/${COUNTRIES_FILENAME}.shp \
   | ndjson-map 'd.properties' \
-  #| grep $COUNTRIES_FILTER \
+  #| grep "$COUNTRIES_FILTER" \
 )> world_countries_50m_props.json
 
 rm -rvf build
