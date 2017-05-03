1aeda232ad18f0fd81055293ff07e5886e969225
Jarek Piórkowski <jarek@piorkowski.ca>
2017-05-03 22:43:06 +0200

    include information on imports for Nova Scotia

--- parsers/CA_NS.py
+++ parsers/CA_NS.py
@@ -6,6 +6,46 @@ import requests
 import re
 
 
+def _get_info(requests_obj, country_code):
+    mix_url = 'http://www.nspower.ca/system_report/today/currentmix.json'
+    mix_data = requests_obj.get(mix_url).json()
+
+    load_url = 'http://www.nspower.ca/system_report/today/currentload.json'
+    load_data = requests_obj.get(load_url).json()
+
+    data = []
+    for mix in mix_data:
+        corresponding_load = [load_period for load_period in load_data
+                              if load_period['datetime'] == mix['datetime']]
+
+        # in mix_data, the values are expressed as percentages,
+        # e.g. "Solid Fuel":  45.76 meaning 45.76%
+        # Divide load by 100.0 to simplify calculations later on.
+        if corresponding_load:
+            load = corresponding_load[0]['Base Load'] / 100.0
+        else:
+            # if not found, assume 1244 MW, based on average yearly electricity available for use
+            # in 2014 and 2015 (Statistics Canada table Table 127-0008 for Nova Scotia)
+            load = 1244 / 100.0
+
+        data.append({
+            'countryCode': country_code,
+            'datetime': arrow.get(
+                int(re.search('\d+', mix['datetime']).group(0)) / 1000.0).datetime,
+            'production': {
+                'coal': (mix['Solid Fuel'] * load),
+                'gas': ((mix['HFO/Natural Gas'] + mix['CT\'s'] + mix['LM 6000\'s']) * load),
+                'biomass': (mix['Biomass'] * load),
+                'hydro': (mix['Hydro'] * load),
+                'wind': (mix['Wind'] * load)
+            },
+            'import': (mix['Imports'] * load),
+            'source': 'nspower.ca',
+        })
+
+    return data
+
+
 def fetch_production(country_code='CA-NS', session=None):
     """Requests the last known production mix (in MW) of a given country
 
@@ -37,40 +77,39 @@ def fetch_production(country_code='CA-NS', session=None):
     }
     """
     r = session or requests.session()
-    mix_url = 'http://www.nspower.ca/system_report/today/currentmix.json'
-    mix_data = r.get(mix_url).json()
 
-    load_url = 'http://www.nspower.ca/system_report/today/currentload.json'
-    load_data = r.get(load_url).json()
+    return _get_info(r, country_code)
 
-    data = []
-    for mix in mix_data:
-        corresponding_load = [load_period for load_period in load_data
-                              if load_period['datetime'] == mix['datetime']]
 
-        # in mix_data, the values are expressed as percentages,
-        # e.g. "Solid Fuel":  45.76 meaning 45.76%
-        # Divide load by 100.0 to simplify calculations later on.
-        if corresponding_load:
-            load = corresponding_load[0]['Base Load'] / 100.0
-        else:
-            # if not found, assume 1500 MW, based on total available capacity of around 2500 MW
-            load = 1500 / 100.0
+def fetch_exchange(country_code1, country_code2, session=None):
+    """Requests the last known power exchange (in MW) between two regions.
 
-        data.append({
-            'countryCode': country_code,
-            'datetime': arrow.get(
-                int(re.search('\d+', mix['datetime']).group(0)) / 1000.0).datetime,
-            'production': {
-                'coal': (mix['Solid Fuel'] * load),
-                'gas': ((mix['HFO/Natural Gas'] + mix['CT\'s'] + mix['LM 6000\'s']) * load),
-                'biomass': (mix['Biomass'] * load),
-                'hydro': (mix['Hydro'] * load),
-                'wind': (mix['Wind'] * load)
-            },
-            'storage': {},
-            'source': 'nspower.ca',
-        })
+    Note: As of early 2017, Nova Scotia only has an exchange with New Brunswick (CA-NB).
+    (An exchange with Newfoundland, "Maritime Link", is scheduled to open in "late 2017").
+
+    The API for Nova Scotia only specifies imports. When NS is exporting energy,
+    the API returns 0.
+    """
+    sorted_country_codes = '->'.join(sorted([country_code1, country_code2]))
+
+    if sorted_country_codes != 'CA-NB->CA-NS':
+        raise NotImplementedError('This exchange pair is not implemented')
+
+    requests_obj = session or requests.session()
+    full_data = _get_info(requests_obj, 'CA-NS')
+
+    latest_data = full_data[-1]
+
+    # In this source, imports are positive. In the expected result for CA-NB->CA-NS,
+    # "net" represents a flow from NB to NS, that is, an import to NS.
+    # So the value can be used directly.
+
+    data = {
+        'sortedCountryCodes': sorted_country_codes,
+        'datetime': latest_data['datetime'],
+        'netFlow': latest_data['import'],
+        'source': latest_data['source']
+    }
 
     return data
 
@@ -78,5 +117,8 @@ def fetch_production(country_code='CA-NS', session=None):
 if __name__ == '__main__':
     """Main method, never used by the Electricity Map backend, but handy for testing."""
 
-    print 'fetch_production() ->'
-    print fetch_production()
+    print('fetch_production() ->')
+    print(fetch_production())
+
+    print('fetch_exchange("CA-NS", "CA-NB") ->')
+    print(fetch_exchange("CA-NS", "CA-NB"))
