d822035bee3d9952ebd2bf044cd43b7a72dd965d
Olivier Corradi <olivier.corradi@gmail.com>
2017-06-13 17:14:46 +0200

    Edit TW

--- parsers/TW.py
+++ parsers/TW.py
@@ -36,27 +36,25 @@ def fetch_production(country_code='TW', session=None):
     coal_production = production.ix['Coal'].output + production.ix['IPP-Coal'].output
     gas_production = production.ix['LNG'].output + production.ix['IPP-LNG'].output
     hydro_production = production.ix['Pumping Gen'].output - production.ix['Pumping Load'].output
-    oil_production = production.ix['Oil'].output - production.ix['Diesel'].output
-    production = production.drop(['LNG','IPP-LNG','IPP-Coal','Pumping Gen','Pumping Load','Diesel'])
-    production.ix['Coal'].output = coal_production
-    production.ix['Gas'] = [0,gas_production]
-    production.ix['Hydro'] = [0,hydro_production]
+    oil_production = production.ix['Oil'].output + production.ix['Diesel'].output
 
     returndata = {
-		'countryCode': country_code,
-    'datetime': str(dumpDate.format('YYYY-MM-DDTHH:mm:ssZZ')),
-		'production': {
-			'coal': round(production.ix['Coal'].output,1),
-            'gas': round(production.ix['Gas'].output,1),
-            'oil': round(production.ix['Oil'].output,1),
-        'hydro' : round(production.ix['Hydro'].output,1),
-            'nuclear': round(production.ix['Nuclear'].output,1),
-            'solar': round(production.ix['Solar'].output,1),
-        'wind': round(production.ix['Wind'].output,1),
-        'other': round(production.ix['Co-Gen'].output,1)
+    		'countryCode': country_code,
+        'datetime': dumpDate.datetime,
+    		'production': {
+    			  'coal': coal_production,
+            'gas': gas_production,
+            'oil': oil_production,
+            'hydro' : hydro_production,
+            'nuclear': production.ix['Nuclear'].output,
+            'solar': production.ix['Solar'].output,
+            'wind': production.ix['Wind'].output,
+            'other': production.ix['Co-Gen'].output
         },
-        'storage': {},
-        'source': 'http://www.taipower.com.tw/'
+        'storage': {
+            'hydro': production.ix['Pumping Load'].output
+        },
+        'source': 'taipower.com.tw'
     }
 
     return returndata
