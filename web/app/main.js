82a26fbf691ac82322a53a469472ee45274dc6ae
Olivier Corradi <olivier.corradi@gmail.com>
2017-07-09 10:24:50 +0200

    f

--- web/app/main.js
+++ web/app/main.js
@@ -207,6 +207,7 @@ if (!isLocalhost) {
       m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
       })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
     ga('create', 'UA-79729918-1', 'auto');
+    ga('set', 'bundleHash', bundleHash);
 }
 
 if (!isLocalhost) {
