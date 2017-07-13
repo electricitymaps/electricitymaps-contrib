e3b196f572f2bd0b6fca2242eb98999b4de2395c
blackleg <hectorespertpardo@gmail.com>
2017-07-13 16:45:59 +0200

    Added ES-CN parser

---
+++ parsers/ES-CN.py
@@ -0,0 +1,81 @@
+# The arrow library is used to handle datetimes
+from arrow import get, utcnow
+# The request library is used to fetch content through HTTP
+from requests import Session
+from reescraper import CanaryIslands, NoDataException, TimestampException
+
+
+def fetch_consumption(country_code='ES-CN', session=None):
+    if not session:
+        session = Session
+
+    try:
+        response = CanaryIslands(session).get()
+    except NoDataException:
+        response = None
+    except TimestampException:
+        response = None
+
+    if not response:
+        datetime = utcnow().datetime
+        consumption = None
+    else:
+        datetime = get(response.timestamp).datetime
+        consumption = response.demand
+
+    data = {
+        'countryCode': country_code,
+        'datetime': datetime,
+        'consumption': consumption,
+        'source': 'https://demanda.ree.es/movil'
+    }
+
+    return data
+
+
+def fetch_production(country_code='ES-CN', session=None):
+
+    if not session:
+        session = Session
+
+    try:
+        response = CanaryIslands(session).get()
+    except NoDataException:
+        response = None
+    except TimestampException:
+        response = None
+
+    if not response:
+        datetime = utcnow().datetime
+    else:
+        datetime = get(response.timestamp).datetime
+
+    data = {
+        'countryCode': country_code,
+        'datetime': datetime,
+        'production': {},
+        'storage': {},
+        'source': 'https://demanda.ree.es/movil',
+    }
+
+    if response:
+        data['production']['gas'] = response.gas + response.combined
+        data['production']['solar'] = response.solar
+        data['production']['oil'] = response.vapor + response.diesel
+        data['production']['wind'] = response.wind
+
+        hidro = response.hydraulic
+        if hidro >= 0.0:
+            data['production']['hydro'] = hidro
+            data['storage']['hydro'] = 0.0
+        else:
+            data['production']['hydro'] = 0.0
+            data['storage']['hydro'] = abs(hidro)
+
+    return data
+
+
+if __name__ == '__main__':
+    session = Session
+    print fetch_consumption('ES-CN', session)
+    print fetch_production('ES-CN', session)
