15e1af621b36a1eee9b03f95670ae84a87057797
igorarduin <arduin.igor@gmail.com>
2017-08-21 17:24:55 +0200

    Fixes #671

--- parsers/BO.py
+++ parsers/BO.py
@@ -104,7 +104,7 @@ def fetch_production(country_code='BO', session=None):
     return data
     
 
-def fetch_hourly_generation_forecast(country_code, obj, hour, date):          
+def fetch_hourly_generation_forecast(country_code, obj, hour, date):
     #output frame
     data = {
         'countryCode': country_code,
@@ -112,50 +112,37 @@ def fetch_hourly_generation_forecast(country_code, obj, hour, date):
         'source': 'cndc.bo',
     }
     
-    #Fill datetime variable
-    data['datetime'] = arrow.get(date, 'YYYY-MM-DD').replace(tzinfo=tz_bo, hour=hour).datetime   
-    #Datetime are recorded from hour 1 to 24 in the web service
-    if hour == 0:
-        hour = 24
-    #Fill production types
+    #Fill forecasted value
     data['value'] = obj['Gen.Prevista'][obj.hour == hour].iloc[0]
+    
+    #Fill datetime variable - changing format if midnight (datetime are recorded from hour 1 to 24 in the webservice)
+    if hour == 24:
+        hour = 0
+        date = arrow.get(date, 'YYYY-MM-DD').shift(days=+1).format('YYYY-MM-DD')
+    data['datetime'] = arrow.get(date, 'YYYY-MM-DD').replace(tzinfo=tz_bo, hour=hour).datetime   
     
     return data
 
 
 def fetch_generation_forecast(country_code = 'BO', session=None):
-	
     #Define actual and last day (for midnight data)
-    now = arrow.now(tz=tz_bo)
-    formatted_date = now.format('YYYY-MM-DD')
-    past_formatted_date = arrow.get(formatted_date, 'YYYY-MM-DD').shift(days=-1).format('YYYY-MM-DD')
-
-    #Define output frame    
-    actual_hour = now.hour
-    data = [dict() for h in range(actual_hour+1)]
+    formatted_date = arrow.now(tz=tz_bo).format('YYYY-MM-DD')
+    
+    #Define output frame
+    data = [dict() for h in range(24)]
 
     #initial path for url to request
     url_init = 'http://www.cndc.bo/media/archivos/graf/gene_hora/despacho_diario.php?fechag=' 
+    url = url_init + formatted_date
     
-    #Start with data for midnight
-    url = url_init + past_formatted_date
     #Request and rearange in DF
     r = session or requests.session()
     response = r.get(url)
     obj = webparser(response)
-    data_temp = fetch_hourly_generation_forecast(country_code, obj, 0, formatted_date)
-    data[0] = data_temp
     
-    #Fill data for the other hours until actual hour
-    if actual_hour>1:
-        url = url_init + formatted_date
-        #Request and rearange in DF
-        r = session or requests.session()
-        response = r.get(url)
-        obj = webparser(response)
-        for h in range(1, actual_hour+1):
-            data_temp = fetch_hourly_generation_forecast(country_code, obj, h, formatted_date)
-            data[h] = data_temp
+    for h in range(1,25):
+        data_temp = fetch_hourly_generation_forecast('BO', obj, h, formatted_date)
+        data[h-1] = data_temp
 
     return data
 
