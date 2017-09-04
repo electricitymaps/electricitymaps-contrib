df1cb70c6e0f1a3b09f68a6b566e3ea49c6226fd
blackleg <hectorespertpardo@gmail.com>
2017-09-04 14:52:10 +0200

    Split centrals

--- parsers/IN_KA.py
+++ parsers/IN_KA.py
@@ -21,22 +21,59 @@ def fetch_production(country_code='IN-KA', session=None):
     # NCEP Production
     ncep_value = india.read_value_from_span_id(html, 'lblncep')
 
-    unknown_value = round(cgs_value + ncep_value + state_value, 2)
+    # RTPS Production: https://en.wikipedia.org/wiki/Raichur_Thermal_Power_Station
+    rtps_value = india.read_value_from_span_id(html, 'lblrtptot')
+
+    # BTPS Production: https://en.wikipedia.org/wiki/Bellary_Thermal_Power_station
+    btps_value = india.read_value_from_span_id(html, 'lblbtptot')
+
+    # YTPS Production: https://en.wikipedia.org/wiki/Yermarus_Thermal_Power_Station
+    ytps_value = india.read_value_from_span_id(html, 'ytptot')
+
+    # Coal Production
+    coal_value = rtps_value + btps_value + ytps_value
+
+    # Sharavati Production
+    sharavati_value = india.read_value_from_span_id(html, 'lblshvytot')
+
+    # Nagjhari Production
+    nagjhari_value = india.read_value_from_span_id(html, 'lblngjtot')
+
+    # Varahi Production
+    varahi_value = india.read_value_from_span_id(html, 'lblvrhtot')
+
+    # Kodsalli Production
+    kodsalli_value = india.read_value_from_span_id(html, 'lblkdsltot')
+
+    # Kadra Production
+    kadra_value = india.read_value_from_span_id(html, 'lblkdrtot')
+
+    # GERUSOPPA production
+    gerusoppa_value = india.read_value_from_span_id(html, 'lblgrsptot')
+
+    # JOG production
+    jog_value = india.read_value_from_span_id(html, 'lbljogtot')
+
+    # Hydro production
+    hydro_value = sharavati_value + nagjhari_value + varahi_value + kodsalli_value + kadra_value + gerusoppa_value + jog_value
+
+    # Unknown production
+    unknown_value = state_value + cgs_value + ncep_value - coal_value - hydro_value
 
     data = {
         'countryCode': country_code,
         'datetime': india_date_time.datetime,
         'production': {
             'biomass': 0.0,
-            'coal': 0.0,
+            'coal': round(coal_value, 2),
             'gas': 0.0,
-            'hydro': 0.0,
+            'hydro': round(hydro_value, 2),
             'nuclear': 0.0,
             'oil': 0.0,
             'solar': 0.0,
             'wind': 0.0,
             'geothermal': 0.0,
-            'unknown': unknown_value
+            'unknown': round(unknown_value, 2)
         },
         'storage': {
             'hydro': 0.0
