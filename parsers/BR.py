5c2e3e589a576dc4cad9f0a157e912930ef440b7
Alexandre Magno <alz@tradeshift.com>
2017-09-12 16:00:55 +0200

    adding exchange rate between Brazil and Uruguay

--- parsers/BR.py
+++ parsers/BR.py
@@ -96,9 +96,39 @@ def fetch_production(country_code = 'BR', session = None):
 
     return datapoint
 
+def fetch_exchange(country_code1='BR', country_code2='UY', session=None):
+    """Requests the last known power exchange (in MW) between two regions
+    Arguments:
+    country_code1           -- the first country code
+    country_code2           -- the second country code; order of the two codes in params doesn't matter
+    session (optional)      -- request session passed in order to re-use an existing session
+    Return:
+    A dictionary in the form:
+    {
+      'sortedCountryCodes': 'DK->NO',
+      'datetime': '2017-01-01T00:00:00Z',
+      'netFlow': 0.0,
+      'source': 'mysource.com'
+    }
+    where net flow is from DK into NO
+    """
+
+    gd = get_data()
+
+    data = {
+        'datetime': gd['Data'],
+        'sortedCountryCodes': 'BR->UY',
+        'netFlow': gd['internacional']['uruguai'],
+        'source': 'ons.org.br'
+    }
+
+    return data
 
 if __name__ ==  '__main__':
     """Main method, never used by the Electricity Map backend, but handy for testing."""
 
     print('fetch_production() ->')
     print(fetch_production())
+
+    print('fetch_exchange() ->')
+    print(fetch_exchange())
