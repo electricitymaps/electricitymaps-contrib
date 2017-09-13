345bf6e06368b4763e1c8c69f8cde712e3241648
Olivier Corradi <olivier.corradi@gmail.com>
2017-09-13 13:13:59 +0200

    Update tests

--- parsers/test/test_IN_PB.py
+++ parsers/test/test_IN_PB.py
@@ -67,7 +67,7 @@ class TestINPB(unittest.TestCase):
 
     def test_date_time_strings_to_kolkata_date(self):
         date_text = "09/06/2017"
-        date_format = "MM/DD/YYYY"
+        date_format = "DD/MM/YYYY"
         time_text = "13:33:59"
         time_format = "HH:mm:ss"
         date_time = IN_PB.date_time_strings_to_kolkata_date(date_text, date_format, time_text, time_format)
@@ -76,12 +76,12 @@ class TestINPB(unittest.TestCase):
         self.assertEquals(date_time, expected)
 
     def test_time_string_to_kolkata_date(self):
-        utc_actual = get(datetime(2017, 6, 9, 12, 30, 0), 'UTC')
+        utc_actual = get(datetime(2017, 9, 6, 12, 30, 0), 'UTC')
         time_text = "12:25:30"
         time_format = "HH:mm:ss"
         date_time = IN_PB.time_string_to_kolkata_date(utc_actual, time_text, time_format)
         self.assertIsNotNone(date_time)
-        expected = get(datetime(2017, 6, 9, 12, 25, 30), 'Asia/Kolkata')
+        expected = get(datetime(2017, 9, 6, 12, 25, 30), 'Asia/Kolkata')
         self.assertEquals(date_time, expected)
 
         utc_actual = get(datetime(2017, 9, 7, 0, 5, 0), 'UTC')
@@ -89,7 +89,7 @@ class TestINPB(unittest.TestCase):
         time_format = "HH:mm:ss"
         date_time = IN_PB.time_string_to_kolkata_date(utc_actual, time_text, time_format)
         self.assertIsNotNone(date_time)
-        expected = get(datetime(2017, 6, 9, 23, 55, 30), 'Asia/Kolkata')
+        expected = get(datetime(2017, 9, 6, 23, 55, 30), 'Asia/Kolkata')
         self.assertEquals(date_time, expected)
 
 
