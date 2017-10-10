846552c3f0a0eff47262411335d13c8a0b099d1d
Olivier Corradi <olivier.corradi@gmail.com>
2017-10-10 11:20:19 +0200

    PT coal

--- parsers/ENTSOE.py
+++ parsers/ENTSOE.py
@@ -311,6 +311,10 @@ def validate_production(datapoint):
         p = datapoint['production']
         return p.get('coal', None) is not None and \
                p.get('gas', None) is not None
+    elif datapoint['countryCode'] == 'PT':
+        p = datapoint['production']
+        return p.get('coal', None) is not None and \
+               p.get('gas', None) is not None
     else: return True
 
 def get_biomass(values):
