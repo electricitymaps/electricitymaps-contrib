060d526c4672ee4ee462019c9cc3d29093606abb
systemcatch <30196510+systemcatch@users.noreply.github.com>
2017-11-22 06:33:34 +0000

    Extend TR parser (#863)
    *Fetch whole day rather than only last hour.
    *Add docstring.

--- parsers/TR.py
+++ parsers/TR.py
@@ -47,33 +47,73 @@ def get_last_data_idx(productions):
 
 
 def fetch_production(country_code='TR', session=None):
+    """
+    Requests the last known production mix (in MW) of a given country
+    Arguments:
+    country_code (optional) -- used in case a parser is able to fetch multiple countries
+    session (optional)      -- request session passed in order to re-use an existing session
+    Return:
+    A list of dictionaries in the form:
+    {
+      'countryCode': 'FR',
+      'datetime': '2017-01-01T00:00:00Z',
+      'production': {
+          'biomass': 0.0,
+          'coal': 0.0,
+          'gas': 0.0,
+          'hydro': 0.0,
+          'nuclear': null,
+          'oil': 0.0,
+          'solar': 0.0,
+          'wind': 0.0,
+          'geothermal': 0.0,
+          'unknown': 0.0
+      },
+      'storage': {
+          'hydro': -10.0,
+      },
+      'source': 'mysource.com'
+    }
+    """
     session = None # Explicitely make a new session to avoid caching from their server...
     r = session or requests.session()
     tr_datetime = arrow.now().to('Europe/Istanbul').floor('day')
-    data = {
-      'countryCode': country_code,
-      'production': {},
-      'storage': {},
-      'source': 'ytbs.teias.gov.tr',
-      'datetime': tr_datetime.datetime
-    }
     response = r.get(URL, verify=False)
     str_data = re.search(SEARCH_DATA, response.text)
+
+    production_by_hour = []
     if str_data:
         productions = json.loads(str_data.group('data'), object_hook=as_float)
         last_data_index = get_last_data_idx(productions)
+        valid_production = productions[:last_data_index + 1]
         if last_data_index != EMPTY_DAY:
-            last_prod = productions[last_data_index]
-            data['production'] = dict(zip(MAP_GENERATION.values(), [0] * len(MAP_GENERATION)))
-            for prod_type, prod_val in last_prod.items():
-                if prod_type in MAP_GENERATION.keys():
-                    data['production'][MAP_GENERATION[prod_type]] += prod_val
-            data['datetime'] = tr_datetime.replace(hour=last_data_index).datetime
+            for datapoint in valid_production:
+                data = {
+                  'countryCode': country_code,
+                  'production': {},
+                  'storage': {},
+                  'source': 'ytbs.teias.gov.tr',
+                  'datetime': None
+                }
+                data['production'] = dict(zip(MAP_GENERATION.values(), [0] * len(MAP_GENERATION)))
+                for prod_type, prod_val in datapoint.items():
+                    if prod_type in MAP_GENERATION.keys():
+                        data['production'][MAP_GENERATION[prod_type]] += prod_val
+
+                try:
+                    data['datetime'] = tr_datetime.replace(hour = int(datapoint['saat'])).datetime
+                except ValueError:
+                    #24 is not a valid hour!
+                    data['datetime'] = tr_datetime.datetime
+
+                production_by_hour.append(data)
     else:
         raise Exception('Extracted data was None')
-    return data
+
+    return production_by_hour
 
 if __name__ == '__main__':
-    res = fetch_production()
-    print res
-    print sum(res['production'].values())
+    """Main method, never used by the Electricity Map backend, but handy for testing."""
+
+    print('fetch_production() ->')
+    print(fetch_production())
