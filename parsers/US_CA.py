db910190b22733ae072ea4e95791d1d61a7e7b66
7hibault <https://7hibault@github.com>
2017-11-26 18:03:12 +0100

    Fetch data for every hour

--- parsers/US_CA.py
+++ parsers/US_CA.py
@@ -36,65 +36,40 @@ def fetch_production(country_code='FR', session=None):
       'source': 'mysource.com'
     }
     """
+    # caiso.com provides daily data until the day before today
     s = requests.session()
     yesterday = datetime.now() - timedelta(days=1)
     url = 'https://content.caiso.com/green/renewrpt/' + yesterday.strftime('%Y%m%d')  +'_DailyRenewablesWatch.txt'
-    print url
+
     response = s.get(url)
     obj = response.text.splitlines()
 
-    res = {}
-    res['countryCode'] = 'US_CA'
-    res['datetime'] = datetime.now()
-    res['production'] = {}
-    res['production']['biomass'] = int(obj[2].split('\t')[5])
-    res['production']['gas'] = int(obj[2].split('\t')[7])
-    # hydro + small hydro
-    res['production']['hydro'] = int(obj[2].split('\t')[9]) + int(obj[30].split('\t')[11])
-    res['production']['nuclear'] = int(obj[30].split('\t')[5])
-    # solar PV + thermal
-    res['production']['solar'] = int(obj[2].split('\t')[13]) + int(obj[2].split('\t')[15])
-    res['production']['wind'] = int(obj[2].split('\t')[11])
-    res['production']['geothermal'] = int(obj[2].split('\t')[3])
-    res['production']['unknown'] = int(obj[2].split('\t')[5])
+    dailyData = []
 
-    print res
+    for i in range(0, 24):
+        data = {
+            'countryCode': country_code,
+            'production': {},
+            'storage': {},
+            'source': 'content.caiso.com',
+        }
+        data['countryCode'] = 'US_CA'
+        data['datetime'] = datetime.now()
+        data['production'] = {}
+        data['production']['biomass'] = int(obj[i+2].split('\t')[5])
+        data['production']['gas'] = int(obj[i+2].split('\t')[7])   
+        data['production']['hydro'] = int(obj[i+2].split('\t')[9]) + int(obj[i+30].split('\t')[11]) # hydro + small hydro
+        data['production']['nuclear'] = int(obj[i+30].split('\t')[5])
+        data['production']['solar'] = int(obj[i+2].split('\t')[13]) + int(obj[i+2].split('\t')[15]) # solar PV + thermal
+        data['production']['wind'] = int(obj[i+2].split('\t')[11])
+        data['production']['geothermal'] = int(obj[i+2].split('\t')[3])
+        data['production']['unknown'] = int(obj[i+2].split('\t')[5])
+        # set the date at the end of the hour and convert UTC-8 (California) to UTC
+        dataTime = yesterday.replace(hour = i, minute = 59, second = 0)- timedelta(hours=8)
+        data['datetime'] = dataTime.strftime('%Y-%m-%dT%H:%M:%SZ') 
+        dailyData.append(data)
 
-      # item # 2 -> 26
-      #- GEOTHERMAL: 3
-      #- BIOMASS: 5
-      #- BIOGAS: 7
-      #- SMALL HYDRO: 9
-      #- WIND TOTAL: 11
-      #- SOLAR PV: 13
-      #- SOLAR THERMAL: 15 
-      # item # 30 -> 55 
-      #x RENEWABLES : 3 
-      #- NUCLEAR : 5
-      #x THERMAL : 7
-      #x IMPORTS : 9
-      #- HYDRO : 11
-
-
-    data = {
-        'countryCode': country_code,
-        'production': {},
-        'storage': {},
-        'source': 'someservice.com',
-    }
-    for item in obj['productionMix']:
-        # All production values should be >= 0
-        data['production'][item['key']] = item['value'] # Should be a floating point value
-
-    for item in obj['storage']:
-        # Positive storage means energy is stored
-        # Negative storage means energy is generated from the storage system
-        data['storage'][item['key']] = item['value'] # Should be a floating point value
-
-    # Parse the datetime and return a python datetime object
-    data['datetime'] = arrow.get(obj['datetime']).datetime
-
-    return data
+    return dailyData
 
 
 
@@ -115,22 +90,7 @@ def fetch_price(country_code='FR', session=None):
       'source': 'mysource.com'
     }
     """
-
-    r = session or requests.session()
-    url = 'https://api.someservice.com/v1/price/latest'
-    response = r.get(url)
-    obj = response.json()
-
-    data = {
-        'countryCode': country_code,
-        'currency': 'EUR',
-        'price': obj['price'],
-        'source': 'someservice.com',
-    }
-
-    # Parse the datetime and return a python datetime object
-    data['datetime'] = arrow.get(obj['datetime']).datetime
-
+    data = {}
     return data
 
 
@@ -150,31 +110,7 @@ def fetch_exchange(country_code1='DK', country_code2='NO', session=None):
       'source': 'mysource.com'
     }
     """
-
-    r = session or requests.session()
-    url = 'https://api.someservice.com/v1/exchange/latest?from=%s&to=%s' % (country_code1, country_code2)
-    response = r.get(url)
-    obj = response.json()
-
-    data = {
-        'sortedCountryCodes': '->'.join(sorted([country_code1, country_code2])),
-        'source': 'someservice.com',
-    }
-
-    # Country codes are sorted in order to enable easier indexing in the database
-    sorted_country_codes = sorted([country_code1, country_code2])
-    # Here we assume that the net flow returned by the api is the flow from 
-    # country1 to country2. A positive flow indicates an export from country1
-    # to country2. A negative flow indicates an import.
-    netFlow = obj['exchange']
-    # The net flow to be reported should be from the first country to the second
-    # (sorted alphabetically). This is NOT necessarily the same direction as the flow
-    # from country1 to country2
-    data['netFlow'] = netFlow if country_code1 == sorted_country_codes[0] else -1 * netFlow
-
-    # Parse the datetime and return a python datetime object
-    data['datetime'] = arrow.get(obj['datetime']).datetime
-
+    data = {}
     return data
 
 
