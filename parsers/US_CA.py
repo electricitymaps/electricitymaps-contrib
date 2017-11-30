fc45545d6b7c8becc921d4f2f158572b0925a73f
Thibault Rihet <thibault.rihet@trialog.com>
2017-11-30 12:13:51 +0100

    Use arrow instead of datetime

--- parsers/US_CA.py
+++ parsers/US_CA.py
@@ -38,8 +38,8 @@ def fetch_production(country_code='FR', session=None):
     """
     # caiso.com provides daily data until the day before today
     s = requests.session()
-    yesterday = datetime.now() - timedelta(days=1)
-    url = 'https://content.caiso.com/green/renewrpt/' + yesterday.strftime('%Y%m%d')  +'_DailyRenewablesWatch.txt'
+    yesterday = arrow.utcnow().to('US/Pacific').shift(days=-1)
+    url = 'http://content.caiso.com/green/renewrpt/' + yesterday.format('YYYYMMDD')  +'_DailyRenewablesWatch.txt'
 
     response = s.get(url)
     obj = response.text.splitlines()
@@ -57,61 +57,19 @@ def fetch_production(country_code='FR', session=None):
         data['datetime'] = datetime.now()
         data['production'] = {}
         data['production']['biomass'] = int(obj[i+2].split('\t')[5])
-        data['production']['gas'] = int(obj[i+2].split('\t')[7])   
+        data['production']['gas'] = int(obj[i+2].split('\t')[7])
         data['production']['hydro'] = int(obj[i+2].split('\t')[9]) + int(obj[i+30].split('\t')[11]) # hydro + small hydro
         data['production']['nuclear'] = int(obj[i+30].split('\t')[5])
         data['production']['solar'] = int(obj[i+2].split('\t')[13]) + int(obj[i+2].split('\t')[15]) # solar PV + thermal
         data['production']['wind'] = int(obj[i+2].split('\t')[11])
         data['production']['geothermal'] = int(obj[i+2].split('\t')[3])
         data['production']['unknown'] = int(obj[i+2].split('\t')[5])
-        # set the date at the end of the hour and convert UTC-8 (California) to UTC
-        dataTime = yesterday.replace(hour = i, minute = 59, second = 0)- timedelta(hours=8)
-        data['datetime'] = dataTime.strftime('%Y-%m-%dT%H:%M:%SZ') 
+        # set the date at the end of the hour
+        dataTime = yesterday.replace(hour = i, minute = 59, second = 0)
+        data['datetime'] = dataTime.isoformat()
         dailyData.append(data)
 
     return dailyData
-
-
-
-def fetch_price(country_code='FR', session=None):
-    """Requests the last known power price of a given country
-
-    Arguments:
-    country_code (optional) -- used in case a parser is able to fetch multiple countries
-    session (optional)      -- request session passed in order to re-use an existing session
-
-    Return:
-    A dictionary in the form:
-    {
-      'countryCode': 'FR',
-      'currency': EUR,
-      'datetime': '2017-01-01T00:00:00Z',
-      'price': 0.0,
-      'source': 'mysource.com'
-    }
-    """
-    data = {}
-    return data
-
-
-def fetch_exchange(country_code1='DK', country_code2='NO', session=None):
-    """Requests the last known power exchange (in MW) between two countries
-
-    Arguments:
-    country_code (optional) -- used in case a parser is able to fetch multiple countries
-    session (optional)      -- request session passed in order to re-use an existing session
-
-    Return:
-    A dictionary in the form:
-    {
-      'sortedCountryCodes': 'DK->NO',
-      'datetime': '2017-01-01T00:00:00Z',
-      'netFlow': 0.0,
-      'source': 'mysource.com'
-    }
-    """
-    data = {}
-    return data
 
 
 if __name__ == '__main__':
@@ -119,7 +77,3 @@ if __name__ == '__main__':
 
     print 'fetch_production() ->'
     print fetch_production()
-    print 'fetch_price() ->'
-    print fetch_price()
-    print 'fetch_exchange(DK, NO) ->'
-    print fetch_exchange('DK', 'NO')
