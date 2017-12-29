dd763933473a0283fa7c421034c5ee5646711cac
Anirudh Ramesh <anirudh-ramesh@users.noreply.github.com>
2017-12-29 15:28:15 +0530

    Add fixes for data sources for India (#954)
    * Edit comments in parsers/IN_KA.py
    
    * Fix natural gas-fuelled generation in Delhi, IN
    
    * Identify garbage-to-electricity sources in Delhi, IN
    
    * Mark garbage-to-electricity as biomass, in Delhi, IN
    
    * Edit comments in parsers/IN_DL.py

--- parsers/IN_DL.py
+++ parsers/IN_DL.py
@@ -38,32 +38,32 @@ def fetch_production(country_code='IN-DL', session=None):
     # BTPS https://en.wikipedia.org/wiki/Badarpur_Thermal_Power_Station
     btps = read_value(prod_rows[1])
 
-    # CCGT https://en.wikipedia.org/wiki/Pragati-III_Combined_Cycle_Power_Plant
+    # CCGT https://en.wikipedia.org/wiki/Pragati-III_Combined_Cycle_Power_Plant = Pragati-3
     ccgt = read_value(prod_rows[2])
 
-    # DMSWSL Unknown
+    # DMSWSL (Delhi Municipal Solid Waste Solutions Limited): Garbage-to-electricity
     dmswsl = read_value(prod_rows[3])
 
-    # EDWPL Unknown
+    # EDWPL (East Delhi Waste Processing Company Limited): Garbage-to-electricity
     edwpl = read_value(prod_rows[4])
 
-    # GT Unknown
+    # GT (Gas Turbine) https://en.wikipedia.org/wiki/IPGCL_Gas_Turbine_Power_Station
     gt = read_value(prod_rows[5])
 
-    # Pragati
+    # Pragati https://en.wikipedia.org/wiki/Pragati-I_Combined_Cycle_Gas_Power_Station = Pragati-1
     pragati = read_value(prod_rows[6])
 
-    # TOWMP Waste?
+    # TOWMP (Timarpur Okhla Waste Management Company Pvt. Ltd.): Garbage-to-electricity
     towmp = read_value(prod_rows[7])
 
     # Coal production
     coal = btps
 
     # Gas production
-    gas = ccgt + pragati
+    gas = ccgt + pragati + gt
 
     # Unknown production
-    unknown_value = dmswsl + edwpl + gt + towmp
+    garbage = dmswsl + edwpl + towmp
 
     data = {
         'countryCode': country_code,
@@ -71,7 +71,7 @@ def fetch_production(country_code='IN-DL', session=None):
         'production': {
             'coal': coal,
             'gas': gas,
-            'unknown': unknown_value
+            'biomass': garbage
         },
         'source': 'delhisldc.org',
     }
