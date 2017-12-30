9b22cd9349eb9ee9dc7a7b6311d6cf2a84eea08f
DeOldSax <DeOldSax@users.noreply.github.com>
2017-12-30 21:38:31 +0100

    Add search bar #909 (#945)
    * Add seach bar (#909)
    
    * Make use of ES6, adapt font and wording

--- web/app/main.js
+++ web/app/main.js
@@ -1111,6 +1111,24 @@ function dataLoaded(err, clientVersion, argCallerLocation, state, argSolar, argW
         countryTooltip.hide();
     });
 
+    // Add search bar handler
+    d3.select('.country-search-bar input')
+        .on("keyup", (obj, i, nodes) => {
+            const query = nodes[i].value.toLowerCase()
+
+            d3.select('.country-picker-container p')
+                .selectAll('a').each((obj, i, nodes) => {
+                    const countryName = obj.shortname.toLowerCase()
+                    const listItem = d3.select(nodes[i])
+
+                    if (countryName.startsWith(query)) {
+                        listItem.style('display', '')
+                    } else {
+                        listItem.style('display', 'none')
+                    }
+                })
+        })
+
     // Re-render country table if it already was visible
     if (selectedCountryCode)
         countryTable.data(countries[selectedCountryCode]).render()
