069698e3c79f08e37f661400374819dd37cd8470
cclauss <cclauss@bluewin.ch>
2017-12-31 03:27:47 +0100

    undo parsers/test/test_ESIOS.py json

--- parsers/test/test_ESIOS.py
+++ parsers/test/test_ESIOS.py
@@ -15,10 +15,8 @@ class TestESIOS(unittest.TestCase):
         self.session.mount('https://', self.adapter)
 
     def test_fetch_exchange(self):
-        # json_data = resource_string("parsers.test.mocks", "ESIOS_ES_MA.json")
-        # self.adapter.register_uri(ANY, ANY, json=loads(str(json_data)))
-        response_text = resource_string("parsers.test.mocks", "ESIOS_ES_MA.json")
-        self.adapter.register_uri(ANY, ANY, text=str(response_text))
+        json_data = resource_string("parsers.test.mocks", "ESIOS_ES_MA.json")
+        self.adapter.register_uri(ANY, ANY, json=loads(str(json_data)))
         try:
             data_list = ESIOS.fetch_exchange('ES', 'MA', self.session, 'ESIOS_MOCK_TOKEN')
             self.assertIsNotNone(data_list)
