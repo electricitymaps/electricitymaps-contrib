4a7250ac6dceabcd6b8abf7520483ad0134a274c
Olivier Corradi <olivier.corradi@gmail.com>
2018-01-05 14:07:43 +0100

    Fix statnett

--- parsers/statnett.py
+++ parsers/statnett.py
@@ -133,7 +133,7 @@ def _fetch_exchanges_from_sorted_bidding_zones(sorted_bidding_zones, session=Non
 def _sum_of_exchanges(exchanges):
     exchange_list = list(exchanges)
     return {
-        'netFlow': sum(e['netFlow'] for e in exchanges),
+        'netFlow': sum(e['netFlow'] for e in exchange_list),
         'datetime': exchange_list[0]['datetime'],
         'source': exchange_list[0]['source']
     }
