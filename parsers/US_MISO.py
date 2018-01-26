3fd84d58761d4ff76cf8c0d3e34f0155f78a237f
Chris <30196510+systemcatch@users.noreply.github.com>
2018-01-26 12:21:04 +0000

    Add US (MISO) (#1024)
    * Create US-MISO production parser
    
    * Complete US-MISO parser
    
    * Update readme with US-MISO info
    
    * Create US-MISO zone
    
    Disabling states of which MISO is roughly composed. Eventually this will
    be replaced by a correct shapefile.
    
    * Fix bug in update_state that prevents production parsers running
    
    If one zone was passed as an argument this resulted in the zone
    name being assigned as the first letter only. Also now that the
    script can take 'all' as an argument the looping logic assumed
    that 'zone_config' was the entire file but for a single zone
    only a small portion of the file was assigned. This resulted in
    a keyerror with no error message displayed.
    
    The solution is not to use list() to create zone_name and to
    ensure the whole zone file is assigned to zone_config. Also
    provide a message if zone has no production parser.
    
    * Update mockserver state
    
    * Change USA formatting and remove pyiso reference

---
+++ parsers/US_MISO.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python3
+
+"""Parser for the MISO area of the United States."""
+
+from dateutil import parser, tz
+import json
+import requests
+
+mix_url = 'https://api.misoenergy.org/MISORTWDDataBroker/DataBrokerServices.asmx?messageType=getfuelmix&returnType=json'
+
+mapping = {'Coal': 'coal',
+           'Natural Gas': 'gas',
+           'Nuclear': 'nuclear',
+           'Wind': 'wind',
+           'Other': 'unknown'}
+
+# To quote the MISO data source;
+# "The category listed as “Other” is the combination of Hydro, Pumped Storage Hydro, Diesel, Demand Response Resources,
+# External Asynchronous Resources and a varied assortment of solid waste, garbage and wood pulp burners".
+
+# Timestamp reported by data source is in format 23-Jan-2018 - Interval 11:45 EST
+# Unsure exactly why EST is used, possibly due to operational connections with PJM.
+
+
+def get_json_data(session = None):
+    """Returns 5 minute generation data in json format."""
+
+    s = session or requests.session()
+    json_data = s.get(mix_url).json()
+
+    return json_data
+
+
+def add_default_tz(timestamp):
+    """Adds EST timezone to datetime object if tz = None."""
+
+    EST = tz.gettz('America/New_York')
+    modified_timestamp = timestamp.replace(tzinfo=timestamp.tzinfo or EST)
+
+    return modified_timestamp
+
+
+def data_processer(json_data):
+    """
+    Identifies any unknown fuel types and logs a warning.
+    Returns a tuple containing datetime object and production dictionary.
+    """
+
+    generation = json_data['Fuel']['Type']
+
+    production = {}
+    for fuel in generation:
+        try:
+            k = mapping[fuel['CATEGORY']]
+        except KeyError as e:
+            print("Key \"{}\" is missing from the MISO fuel mapping.".format(fuel['CATEGORY']))
+            k = 'unknown'
+        v = float(fuel['ACT'])
+        production[k] = production.get(k, 0.0) + v
+
+    # Remove unneeded parts of timestamp to allow datetime parsing.
+    timestamp = json_data['RefId']
+    split_time = timestamp.split(" ")
+    time_junk = set([1,2])
+    useful_time_parts = [v for i, v in enumerate(split_time) if i not in time_junk]
+
+    if useful_time_parts[-1] != 'EST':
+        raise ValueError('Timezone reported for US-MISO has changed.')
+
+    time_data = " ".join(useful_time_parts)
+    dt_naive = parser.parse(time_data)
+    dt = add_default_tz(dt_naive)
+
+    return dt, production
+
+
+def fetch_production(country_code = 'US-MISO', session = None):
+    """
+    Requests the last known production mix (in MW) of a given country
+    Arguments:
+    country_code (optional) -- used in case a parser is able to fetch multiple countries
+    session (optional)      -- request session passed in order to re-use an existing session
+    Return:
+    A dictionary in the form:
+    {
+      'countryCode': 'FR',
+      'datetime': '2017-01-01T00:00:00Z',
+      'production': {
+          'biomass': 0.0,
+          'coal': 0.0,
+          'gas': 0.0,
+          'hydro': 0.0,
+          'nuclear': null,
+          'oil': 0.0,
+          'solar': 0.0,
+          'wind': 0.0,
+          'geothermal': 0.0,
+          'unknown': 0.0
+      },
+      'storage': {
+          'hydro': -10.0,
+      },
+      'source': 'mysource.com'
+    }
+    """
+
+    json_data = get_json_data(session = None)
+    processed_data = data_processer(json_data)
+
+    data = {
+      'countryCode': country_code,
+      'datetime': processed_data[0],
+      'production': processed_data[1],
+      'storage': {'hydro': None},
+      'source': 'misoenergy.org'
+    }
+
+    return data
+
+
+if __name__ == '__main__':
+    print('fetch_production() ->')
+    print(fetch_production())
