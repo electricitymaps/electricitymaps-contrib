36d1a1812d0a950628962cea1885174102402e02
blackleg <hectorespertpardo@gmail.com>
2018-01-26 19:12:00 +0100

    Added Gran Canaria, Gomera, La Palma and Tenerife.

--- parsers/ES_CN.py
+++ parsers/ES_CN.py
@@ -16,6 +16,30 @@ def fetch_island_data(country_code, session):
             raise ParserException(country_code, "LanzaroteFuerteventura not response")
         else:
             return lanzarote_fuerteventura_data
+    elif country_code == 'ES-CN-GC':
+        gran_canaria_data = GranCanaria(session).get_all()
+        if not gran_canaria_data:
+            raise ParserException(country_code, "GranCanaria not response")
+        else:
+            return
+    elif country_code == 'ES-CN-IG':
+        gomera_data = Gomera(session).get_all()
+        if not gomera_data:
+            raise ParserException(country_code, "Gomera not response")
+        else:
+            return gomera_data
+    elif country_code == 'ES-CN-LP':
+        la_palma_data = LaPalma(session).get_all()
+        if not la_palma_data:
+            raise ParserException(country_code, "LaPalma not response")
+        else:
+            return la_palma_data
+    elif country_code == 'ES-CN-TE':
+        tenerife_data = Tenerife(session).get_all()
+        if not tenerife_data:
+            raise ParserException(country_code, "Tenerife not response")
+        else:
+            return tenerife_data
     else:
         raise ParserException(country_code, 'Can\'t read this country code {0}'.format(country_code))
 
@@ -73,5 +97,18 @@ def fetch_production(country_code, session=None):
 
 if __name__ == '__main__':
     session = Session
+    print("# ES-CN-FVLZ")
     print(fetch_consumption('ES-CN-FVLZ', session))
     print(fetch_production('ES-CN-FVLZ', session))
+    print("# ES-CN-GC")
+    print(fetch_consumption('ES-CN-GC', session))
+    print(fetch_production('ES-CN-GC', session))
+    print("# ES-CN-IG")
+    print(fetch_consumption('ES-CN-IG', session))
+    print(fetch_production('ES-CN-IG', session))
+    print("# ES-CN-LP")
+    print(fetch_consumption('ES-CN-LP', session))
+    print(fetch_production('ES-CN-LP', session))
+    print("# ES-CN-TE")
+    print(fetch_consumption('ES-CN-TE', session))
+    print(fetch_production('ES-CN-TE', session))
