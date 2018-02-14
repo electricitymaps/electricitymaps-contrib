0f8913fcf09ac51ab898fd795794c1ae23c07577
Jake Pierzinski <pierzinskij@gmail.com>
2018-02-13 22:52:55 -0600

    Add new ocean.json. Add new function to get ocean coordinates in countrytopos

--- web/src/countrytopos.js
+++ web/src/countrytopos.js
@@ -1,23 +1,28 @@
 const topo = require('./world.json');
 const topojson = require('topojson');
+const oceanjson = require('./ocean.json')
 
 var exports = module.exports = {};
 
 exports.addCountryTopos = (countries) => {
   Object.keys(topo.objects).forEach((k) => {
     if (!topo.objects[k].arcs) { return; }
-    let geo;
-    // Do merge inner arcs for those
-    if (['US'].indexOf(k.split('-')[0]) !== -1) {
-      geo = topojson.feature(topo, topo.objects[k]);
-    } else {
-      geo = { geometry: topojson.merge(topo, [topo.objects[k]]) };
-    }
-    // Exclude countries with null geometries.
-    if (geo.geometry) {
-      countries[k] = geo;
-    }
-  });
+  let geo;
+  // Do merge inner arcs for those
+  if (['US'].indexOf(k.split('-')[0]) !== -1) {
+    geo = topojson.feature(topo, topo.objects[k]);
+  } else {
+    geo = { geometry: topojson.merge(topo, [topo.objects[k]]) };
+  }
+  // Exclude countries with null geometries.
+  if (geo.geometry) {
+    countries[k] = geo;
+  }
+});
 
   return countries;
+};
+
+exports.addOceanTopos = function() {
+  return oceanjson;
 };
