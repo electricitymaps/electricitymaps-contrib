07dcdb53258a2f9ae1cd46323ce278503f01986c
Jake Pierzinski <pierzinskij@gmail.com>
2018-02-13 22:55:43 -0600

    Fix indentation

--- web/src/countrytopos.js
+++ web/src/countrytopos.js
@@ -7,17 +7,17 @@ var exports = module.exports = {};
 exports.addCountryTopos = (countries) => {
   Object.keys(topo.objects).forEach((k) => {
     if (!topo.objects[k].arcs) { return; }
-  let geo;
-  // Do merge inner arcs for those
-  if (['US'].indexOf(k.split('-')[0]) !== -1) {
-    geo = topojson.feature(topo, topo.objects[k]);
-  } else {
-    geo = { geometry: topojson.merge(topo, [topo.objects[k]]) };
-  }
-  // Exclude countries with null geometries.
-  if (geo.geometry) {
-    countries[k] = geo;
-  }
+    let geo;
+    // Do merge inner arcs for those
+    if (['US'].indexOf(k.split('-')[0]) !== -1) {
+      geo = topojson.feature(topo, topo.objects[k]);
+    } else {
+      geo = { geometry: topojson.merge(topo, [topo.objects[k]]) };
+    }
+    // Exclude countries with null geometries.
+    if (geo.geometry) {
+      countries[k] = geo;
+    }
 });
 
   return countries;
