ea86b63cab1b7ba880c37eebbcc41bce3fcb292b
Jake Pierzinski <pierzinskij@gmail.com>
2018-02-14 20:08:34 -0600

    Fix indentation take 2

--- web/src/main.js
+++ web/src/main.js
@@ -803,13 +803,13 @@ function mapMouseOver(lonlat) {
   if (getState().application.windEnabled && wind && lonlat && typeof windLayer !== 'undefined') {
     const now = getState().application.customDate ?
       moment(getState().application.customDate) : (new Date()).getTime();
-      if (!windLayer.isExpired(now, wind.forecasts[0], wind.forecasts[1])) {
-        const u = grib.getInterpolatedValueAtLonLat(lonlat,
-          now, wind.forecasts[0][0], wind.forecasts[1][0]);
-        const v = grib.getInterpolatedValueAtLonLat(lonlat,
-          now, wind.forecasts[0][1], wind.forecasts[1][1]);
-        if (!getState().application.selectedCountryCode) {
-          windColorbar.currentMarker(Math.sqrt(u * u + v * v));
+    if (!windLayer.isExpired(now, wind.forecasts[0], wind.forecasts[1])) {
+      const u = grib.getInterpolatedValueAtLonLat(lonlat,
+        now, wind.forecasts[0][0], wind.forecasts[1][0]);
+      const v = grib.getInterpolatedValueAtLonLat(lonlat,
+        now, wind.forecasts[0][1], wind.forecasts[1][1]);
+      if (!getState().application.selectedCountryCode) {
+        windColorbar.currentMarker(Math.sqrt(u * u + v * v));
       }
     }
   } else {
