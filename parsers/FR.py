8b12e29140bc4d8bd559bdf54697718d367a1287
Olivier Corradi <olivier.corradi@gmail.com>
2018-03-14 10:36:38 +0100

    FR parser fetches whole day and is able to go back in time

--- parsers/FR.py
+++ parsers/FR.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import arrow
+import logging
 import requests
 import xml.etree.ElementTree as ET
 
@@ -20,65 +21,69 @@ MAP_STORAGE = {
 }
 
 
-def fetch_production(zone_key='FR', session=None, target_datetime=None, logger=None):
-    if target_datetime:
-        raise NotImplementedError('This parser is not yet able to parse past dates')
-    
+def fetch_production(zone_key='FR', session=None, target_datetime=None, logger=logging.getLogger(__name__)):
+    now = arrow.get(target_datetime, 'Europe/Paris') if target_datetime else arrow.now(tz='Europe/Paris')
+
     r = session or requests.session()
-    formatted_date = arrow.now(tz='Europe/Paris').format('DD/MM/YYYY')
-    url = 'http://www.rte-france.com/getEco2MixXml.php?type=mix&&dateDeb={}&dateFin={}&mode=NORM'.format(formatted_date, formatted_date)
+    formatted_from = now.shift(days=-1).format('DD/MM/YYYY')
+    formatted_to = now.format('DD/MM/YYYY')
+    url = 'http://www.rte-france.com/getEco2MixXml.php?type=mix&&dateDeb={}&dateFin={}&mode=NORM'.format(formatted_from, formatted_to)
     response = r.get(url)
     obj = ET.fromstring(response.content)
     mixtr = obj[7]
-    data = {
-        'zoneKey': zone_key,
-        'production': {},
-        'storage': {},
-        'source': 'rte-france.com',
-    }
+    date = arrow.get(arrow.get(obj[1].text).datetime, 'Europe/Paris')
+    datas = []
+
+    # Iterate over mix
     for item in mixtr.getchildren():
         key = item.get('v')
         granularite = item.get('granularite')
         value = None
+        # Iterate over time
         for value in item.getchildren():
-            pass
-        if key == 'Hydraulique':
-            # Hydro is a special case!
-            if granularite == 'Global':
-                continue
-            elif granularite in ['FEE', 'LAC']:
-                if not MAP_GENERATION[key] in data['production']:
-                    data['production'][MAP_GENERATION[key]] = 0
-                # Run of the river or conventional
-                data['production'][MAP_GENERATION[key]] += float(value.text)
-            elif granularite == 'STT':
-                if not MAP_STORAGE[key] in data['storage']:
-                    data['storage'][MAP_STORAGE[key]] = 0
-                # Pumped storage generation
-                data['storage'][MAP_STORAGE[key]] += -1 * float(value.text)
-        elif granularite == 'Global':
-            if key in MAP_GENERATION:
-                data['production'][MAP_GENERATION[key]] = float(value.text)
-            elif key in MAP_STORAGE:
-                if not MAP_STORAGE[key] in data['storage']:
-                    data['storage'][MAP_STORAGE[key]] = 0
-                data['storage'][MAP_STORAGE[key]] += -1 * float(value.text)
+            # Figure out time index
+            i = int(value.attrib['periode'])
+            if i >= len(datas):
+                datas.append({
+                    'zoneKey': zone_key,
+                    'datetime': date.replace(minutes=+(i * 15.0)).datetime,
+                    'production': {},
+                    'storage': {},
+                    'source': 'rte-france.com',
+                })
+            data = datas[i]
 
-    data['datetime'] = arrow.get(arrow.get(obj[1].text).datetime,
-        'Europe/Paris').replace(minutes=+(int(value.attrib['periode']) * 15.0)).datetime
+            if key == 'Hydraulique':
+                # Hydro is a special case!
+                if granularite == 'Global':
+                    continue
+                elif granularite in ['FEE', 'LAC']:
+                    if not MAP_GENERATION[key] in data['production']:
+                        data['production'][MAP_GENERATION[key]] = 0
+                    # Run of the river or conventional
+                    data['production'][MAP_GENERATION[key]] += float(value.text)
+                elif granularite == 'STT':
+                    if not MAP_STORAGE[key] in data['storage']:
+                        data['storage'][MAP_STORAGE[key]] = 0
+                    # Pumped storage generation
+                    data['storage'][MAP_STORAGE[key]] += -1 * float(value.text)
+            elif granularite == 'Global':
+                if key in MAP_GENERATION:
+                    data['production'][MAP_GENERATION[key]] = float(value.text)
+                elif key in MAP_STORAGE:
+                    if not MAP_STORAGE[key] in data['storage']:
+                        data['storage'][MAP_STORAGE[key]] = 0
+                    data['storage'][MAP_STORAGE[key]] += -1 * float(value.text)
 
-    return data
+    return datas
 
 
-def fetch_price(zone_key, session=None, from_date=None, to_date=None, target_datetime=None,
-                logger=None):
-    if target_datetime is not None:
-        raise NotImplementedError('This parser is not yet able to parse past dates')
+def fetch_price(zone_key, session=None, target_datetime=None, logger=logging.getLogger(__name__)):
+    now = arrow.get(target_datetime, 'Europe/Paris') if target_datetime else arrow.now(tz='Europe/Paris')
 
     r = session or requests.session()
-    dt_now = arrow.now(tz='Europe/Paris')
-    formatted_from = from_date or dt_now.format('DD/MM/YYYY')
-    formatted_to = to_date or dt_now.format('DD/MM/YYYY')
+    formatted_from = now.shift(days=-1).format('DD/MM/YYYY')
+    formatted_to = now.format('DD/MM/YYYY')
 
     url = 'http://www.rte-france.com/getEco2MixXml.php?type=donneesMarche&dateDeb={}&dateFin={}&mode=NORM'.format(formatted_from, formatted_to)
     response = r.get(url)
@@ -101,18 +106,16 @@ def fetch_price(zone_key, session=None, from_date=None, to_date=None, target_dat
             if value.text == 'ND':
                 continue
             datetime = date.replace(hours=+int(value.attrib['periode'])).datetime
-            if datetime > dt_now:
-                continue
             datetimes.append(datetime)
             prices.append(float(value.text))
 
-    data = {
+    data = [{
         'zoneKey': zone_key,
         'currency': 'EUR',
-        'datetime': datetimes[-1],
-        'price': prices[-1],
+        'datetime': datetimes[i],
+        'price': prices[i],
         'source': 'rte-france.com',
-    }
+    } for i in range(len(prices))]
     return data
 
 
