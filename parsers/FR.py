c3c1eda7b319dcb41be4a9c223447bfe98d3627d
Olivier Corradi <olivier.corradi@gmail.com>
2018-03-17 09:13:25 +0100

    Fix FR parser

--- parsers/FR.py
+++ parsers/FR.py
@@ -30,58 +30,63 @@ def fetch_production(zone_key='FR', session=None, target_datetime=None,
 
     r = session or requests.session()
     formatted_from = now.shift(days=-1).format('DD/MM/YYYY')
-    formatted_to = now.shift(days=+1).format('DD/MM/YYYY')
-    url = 'http://www.rte-france.com/getEco2MixXml.php?type=mix&&dateDeb={}&' \
+    formatted_to = now.format('DD/MM/YYYY')
+    url = 'http://www.rte-france.com/getEco2MixXml.php?type=mix&dateDeb={}&' \
           'dateFin={}&mode=NORM'.format(formatted_from, formatted_to)
     response = r.get(url)
     obj = ET.fromstring(response.content)
+    datas = {}
     mixtr = obj[7]
-    date = arrow.get(arrow.get(obj[1].text).datetime, 'Europe/Paris')
-    datas = []
 
-    # Iterate over mix
-    for item in mixtr.getchildren():
-        key = item.get('v')
-        granularite = item.get('granularite')
-        value = None
-        # Iterate over time
-        for value in item.getchildren():
-            # Figure out time index
-            i = int(value.attrib['periode'])
-            if i >= len(datas):
-                datas.append({
-                    'zoneKey': zone_key,
-                    'datetime': date.replace(minutes=+(i * 15.0)).datetime,
-                    'production': {},
-                    'storage': {},
-                    'source': 'rte-france.com',
-                })
-            data = datas[i]
+    for mixtr in obj:
+        if mixtr.tag != 'mixtr':
+            continue
 
-            if key == 'Hydraulique':
-                # Hydro is a special case!
-                if granularite == 'Global':
-                    continue
-                elif granularite in ['FEE', 'LAC']:
-                    if not MAP_GENERATION[key] in data['production']:
-                        data['production'][MAP_GENERATION[key]] = 0
-                    # Run of the river or conventional
-                    data['production'][MAP_GENERATION[key]] += float(
-                        value.text)
-                elif granularite == 'STT':
-                    if not MAP_STORAGE[key] in data['storage']:
-                        data['storage'][MAP_STORAGE[key]] = 0
-                    # Pumped storage generation
-                    data['storage'][MAP_STORAGE[key]] += -1 * float(value.text)
-            elif granularite == 'Global':
-                if key in MAP_GENERATION:
-                    data['production'][MAP_GENERATION[key]] = float(value.text)
-                elif key in MAP_STORAGE:
-                    if not MAP_STORAGE[key] in data['storage']:
-                        data['storage'][MAP_STORAGE[key]] = 0
-                    data['storage'][MAP_STORAGE[key]] += -1 * float(value.text)
+        start_date = arrow.get(arrow.get(mixtr.attrib['date']).datetime, 'Europe/Paris')
 
-    return datas
+        # Iterate over mix
+        for item in mixtr:
+            key = item.get('v')
+            granularite = item.get('granularite')
+            value = None
+            # Iterate over time
+            for value in item:
+                period = int(value.attrib['periode'])
+                datetime = start_date.replace(minutes=+(period * 15.0)).datetime
+                if not datetime in datas:
+                    datas[datetime] = {
+                        'zoneKey': zone_key,
+                        'datetime': datetime,
+                        'production': {},
+                        'storage': {},
+                        'source': 'rte-france.com',
+                    }
+                data = datas[datetime]
+
+                if key == 'Hydraulique':
+                    # Hydro is a special case!
+                    if granularite == 'Global':
+                        continue
+                    elif granularite in ['FEE', 'LAC']:
+                        if not MAP_GENERATION[key] in data['production']:
+                            data['production'][MAP_GENERATION[key]] = 0
+                        # Run of the river or conventional
+                        data['production'][MAP_GENERATION[key]] += float(
+                            value.text)
+                    elif granularite == 'STT':
+                        if not MAP_STORAGE[key] in data['storage']:
+                            data['storage'][MAP_STORAGE[key]] = 0
+                        # Pumped storage generation
+                        data['storage'][MAP_STORAGE[key]] += -1 * float(value.text)
+                elif granularite == 'Global':
+                    if key in MAP_GENERATION:
+                        data['production'][MAP_GENERATION[key]] = float(value.text)
+                    elif key in MAP_STORAGE:
+                        if not MAP_STORAGE[key] in data['storage']:
+                            data['storage'][MAP_STORAGE[key]] = 0
+                        data['storage'][MAP_STORAGE[key]] += -1 * float(value.text)
+
+    return datas.values()
 
 
 def fetch_price(zone_key, session=None, target_datetime=None,
@@ -93,42 +98,44 @@ def fetch_price(zone_key, session=None, target_datetime=None,
 
     r = session or requests.session()
     formatted_from = now.shift(days=-1).format('DD/MM/YYYY')
-    formatted_to = now.shift(days=+1).format('DD/MM/YYYY')
+    formatted_to = now.format('DD/MM/YYYY')
 
     url = 'http://www.rte-france.com/getEco2MixXml.php?type=donneesMarche&da' \
           'teDeb={}&dateFin={}&mode=NORM'.format(formatted_from, formatted_to)
+    print(url)
     response = r.get(url)
     obj = ET.fromstring(response.content)
-    mixtr = obj[5]
+    datas = {}
 
-    prices = []
-    datetimes = []
+    for donnesMarche in obj:
+        if donnesMarche.tag != 'donneesMarche':
+            continue
 
-    date_str = mixtr.get('date')
-    date = arrow.get(arrow.get(date_str).datetime, 'Europe/Paris')
-    for country_item in mixtr.getchildren():
-        if country_item.get('granularite') != 'Global':
-            continue
-        country_c = country_item.get('perimetre')
-        if zone_key != country_c:
-            continue
-        value = None
-        for value in country_item.getchildren():
-            if value.text == 'ND':
+        start_date = arrow.get(arrow.get(donnesMarche.attrib['date']).datetime, 'Europe/Paris')
+
+        for item in donnesMarche:
+            if item.get('granularite') != 'Global':
                 continue
-            datetime = date.replace(hours=+int(value.attrib['periode'])
-                                    ).datetime
-            datetimes.append(datetime)
-            prices.append(float(value.text))
+            country_c = item.get('perimetre')
+            if zone_key != country_c:
+                continue
+            value = None
+            for value in item:
+                if value.text == 'ND':
+                    continue
+                period = int(value.attrib['periode'])
+                datetime = start_date.replace(hours=+period).datetime
+                if not datetime in datas:
+                    datas[datetime] = {
+                        'zoneKey': zone_key,
+                        'currency': 'EUR',
+                        'datetime': datetime,
+                        'source': 'rte-france.com',
+                    }
+                data = datas[datetime]
+                data['price'] = float(value.text)
 
-    data = [{
-        'zoneKey': zone_key,
-        'currency': 'EUR',
-        'datetime': datetimes[i],
-        'price': prices[i],
-        'source': 'rte-france.com',
-    } for i in range(len(prices))]
-    return data
+    return datas.values()
 
 
 if __name__ == '__main__':
