53c41cccc76fb55293bb6e41df4b7a4a3c5ee55f
systemcatch <30196510+systemcatch@users.noreply.github.com>
2018-03-20 10:58:04 +0000

    Improvements based on comments

--- parsers/US_SPP.py
+++ parsers/US_SPP.py
@@ -2,19 +2,17 @@
 
 """Parser for the Southwest Power Pool area of the United States."""
 
-from dateutil import parser, tz
+from dateutil import parser
 from io import StringIO
 from logging import getLogger
 import pandas as pd
 import requests
 
-generation_url = 'https://marketplace.spp.org/public-data-api/gen-mix/asFile'
+GENERATION_URL = 'https://marketplace.spp.org/public-data-api/gen-mix/asFile'
 
-exchange_url = 'https://marketplace.spp.org/public-data-api/interchange-trend/asFile'
+EXCHANGE_URL = 'https://marketplace.spp.org/public-data-api/interchange-trend/asFile'
 
-mapping = {'coal': 'coal',
-           'unknown': 'unknown',
-           'Wind': 'wind',
+MAPPING = {'Wind': 'wind',
            'Nuclear': 'nuclear',
            'Hydro': 'hydro',
            'Solar': 'solar',
@@ -23,7 +21,7 @@ mapping = {'coal': 'coal',
            'Waste Disposal Services': 'biomass'
             }
 
-tie_mapping = {'US-MISO->US-SPP': ['AMRN', 'DPC', 'GRE', 'MDU', 'MEC', 'NSP', 'OTP']}
+TIE_MAPPING = {'US-MISO->US-SPP': ['AMRN', 'DPC', 'GRE', 'MDU', 'MEC', 'NSP', 'OTP']}
 
 # NOTE
 # Data sources return timestamps in GMT.
@@ -52,21 +50,20 @@ def data_processor(df, logger):
     # Remove leading whitespace in column headers.
     df.columns = df.columns.str.strip()
 
-    keys_to_remove = ['Coal Market', 'Coal Self', 'GMT MKT Interval', 'Average Actual Load',
-                      'Other', 'Waste Heat']
+    keys_to_remove = {'Coal Market', 'Coal Self', 'GMT MKT Interval', 'Average Actual Load',
+                      'Other', 'Waste Heat'}
 
     # Check for new generation columns.
-    known_keys = list(mapping.keys()) + keys_to_remove
-    column_headers = list(df)
+    known_keys = MAPPING.keys() | keys_to_remove
+    column_headers = set(df.columns)
 
-    unknown_keys = []
-    for heading in column_headers:
-        if heading not in known_keys:
-            logger.warning('New column \'{}\' present in US-SPP data source.'.format(
-                heading), extra={'key': 'US-SPP'})
-            unknown_keys.append(heading)
+    unknown_keys = column_headers - known_keys
 
-    keys_to_remove += unknown_keys
+    for heading in unknown_keys:
+        logger.warning('New column \'{}\' present in US-SPP data source.'.format(
+            heading), extra={'key': 'US-SPP'})
+
+    keys_to_remove = keys_to_remove | unknown_keys
 
     processed_data = []
     for index, row in df.iterrows():
@@ -81,7 +78,7 @@ def data_processor(df, logger):
         for k in keys_to_remove:
             production.pop(k, None)
 
-        mapped_production = {mapping.get(k,k):v for k,v in production.items()}
+        mapped_production = {MAPPING.get(k,k):v for k,v in production.items()}
 
         processed_data.append((dt_aware, mapped_production))
 
@@ -121,7 +118,7 @@ def fetch_production(zone_key = 'US-SPP', session=None, target_datetime=None, lo
     if target_datetime is not None:
         raise NotImplementedError('This parser is not yet able to parse past dates')
 
-    raw_data = get_data(generation_url, session=session)
+    raw_data = get_data(GENERATION_URL, session=session)
     processed_data = data_processor(raw_data, logger)
 
     data = []
@@ -160,13 +157,13 @@ def fetch_exchange(zone_key1, zone_key2, session=None, target_datetime=None, log
     if target_datetime:
         raise NotImplementedError('This parser is not yet able to parse past dates')
 
-    raw_data = get_data(exchange_url, session=session)
+    raw_data = get_data(EXCHANGE_URL, session=session)
     sorted_codes = '->'.join(sorted([zone_key1, zone_key2]))
 
     try:
-        exchange_ties = tie_mapping[sorted_codes]
+        exchange_ties = TIE_MAPPING[sorted_codes]
     except KeyError as e:
-        raise NotImplementedError('This exchange pair is not implemented')
+        raise NotImplementedError('The exchange {} is not implemented'.format(sorted_codes))
 
     # TODO check glossary for flow direction.
 
