15b750533ad6777d4b1a49e7dbeadd90e629c0f6
Maxime Bellec <maxbellec@gmail.com>
2018-04-25 12:12:16 +0200

    cleanup

--- parsers/IN_GJ.py
+++ parsers/IN_GJ.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
 
 import re
-from ast import literal_eval
-from arrow import get
-from requests import Session
-from .lib import zonekey
-from .lib import web
-from .lib import IN
+import collections
+from operator import itemgetter
+import arrow
+import requests
+from .lib import zonekey, web, IN
 from .lib.validation import validate
 from logging import getLogger
-from operator import itemgetter
 
 
 station_map = {
@@ -43,6 +41,9 @@ station_map = {
 
 
 def fetch_data(zone_key, session=None, logger=None):
+    session = session or requests.session()
+
+    values = collections.Counter()
     zonekey.assert_zone_key(zone_key, 'IN-GJ')
 
     solar_html = web.get_response_soup(
@@ -50,40 +51,33 @@ def fetch_data(zone_key, session=None, logger=None):
     wind_html = web.get_response_soup(
         zone_key, 'https://www.sldcguj.com/RealTimeData/wind.php', session)
 
-    india_date = get(solar_html.find_all('tr')[0].text.split('\t')[-1].strip() + ' Asia/Kolkata', 'D-MM-YYYY H:mm:ss ZZZ')
+    india_date = arrow.get(
+        solar_html.find_all('tr')[0].text.split('\t')[-1].strip()
+        + ' Asia/Kolkata', 'D-MM-YYYY H:mm:ss ZZZ')
 
-    solar_value = float(literal_eval(solar_html.find_all('tr')[-1].find_all('td')[-1].text.strip()))
-    wind_value = float(literal_eval(wind_html.find_all('tr')[-1].find_all('td')[-1].text.strip()))
-
-    hydro_value = thermal_value = gas_value = coal_value = nuclear_value = unknown = 0.0
-
-    value_map = {
-        "date": india_date.datetime,
-        "solar": solar_value,
-        "hydro": hydro_value,
-        "thermal": thermal_value,
-        "wind": wind_value,
-        "gas": gas_value,
-        "coal": coal_value,
-        "nuclear": nuclear_value,
-        "unknown": unknown
-    }
+    values['solar'] = float(
+        solar_html.find_all('tr')[-1].find_all('td')[-1].text.strip())
+    values['wind'] = float(
+        wind_html.find_all('tr')[-1].find_all('td')[-1].text.strip())
 
     cookies_params = {
         'ASPSESSIONIDSUQQQTRD': 'ODMNNHADJFGCMLFFGFEMOGBL',
         'PHPSESSID': 'a301jk6p1p8d50dduflceeg6l1'
     }
 
-    rows = web.get_response_soup(zone_key, 'https://www.sldcguj.com/RealTimeData/RealTimeDemand.php',
-                                 session).find_all('tr')
+    rows = web.get_response_soup(
+        zone_key,
+        'https://www.sldcguj.com/RealTimeData/RealTimeDemand.php',
+        session).find_all('tr')
 
     for row in rows:
         elements = row.find_all('td')
         if len(elements) > 3:  # will find production rows
-            v1, v2 = (re.sub(r'\s+', r'', x.text) for x in itemgetter(*[0, 3])(elements))
+            v1, v2 = (re.sub(r'\s+', r'', x.text)
+                      for x in itemgetter(*[0, 3])(elements))
             energy_type = [k for k, v in station_map.items() if v1 in v]
             if len(energy_type) > 0:
-                value_map[energy_type[0]] += float(literal_eval(v2))
+                values[energy_type[0]] += float(v2)
             else:
                 if 'StationName' in (v1, v2):  # meta data row
                     continue
@@ -91,22 +85,27 @@ def fetch_data(zone_key, session=None, logger=None):
                     continue
                 else:
                     try:
-                        logger.warning('Unknown fuel for station name: {}'.format(v1),
+                        logger.warning(
+                            'Unknown fuel for station name: {}'.format(v1),
                             extra={'key': zone_key})
-                        value_map['unknown'] += float(literal_eval(v2))
+                        values['unknown'] += float(v2)
                     except (SyntaxError, ValueError) as e:
-                        #handle eval failures which are likely to occur for new types
-                        logger.warning('literal_eval failed on: {}'.format(v2), extra={'key': zone_key})
+                        # handle float failures
+                        logger.warning(
+                            "couldn't convert {} to float".format(v2),
+                            extra={'key': zone_key})
                         continue
         elif len(elements) == 3:  # will find consumption row
-            v1, v2 = (re.sub(r'\s+', r'', x.text) for x in itemgetter(*[0, 2])(elements))
+            v1, v2 = (re.sub(r'\s+', r'', x.text)
+                      for x in itemgetter(*[0, 2])(elements))
             if v1 == 'GujaratCatered':
-                value_map['total consumption'] = float(literal_eval(v2.split('MW')[0]))
+                values['total consumption'] = float(v2.split('MW')[0])
 
-    return value_map
+    return values
 
 
-def fetch_production(zone_key='IN-GJ', session=None, target_datetime=None, logger=getLogger('IN-GJ')):
+def fetch_production(zone_key='IN-GJ', session=None, target_datetime=None,
+                     logger=getLogger('IN-GJ')):
     """
     Requests the last known production mix (in MW) of a given country
     Arguments:
@@ -138,8 +137,10 @@ def fetch_production(zone_key='IN-GJ', session=None, target_datetime=None, logge
       'source': 'mysource.com'
     }
     """
+    session = session or requests.session()
     if target_datetime:
-        raise NotImplementedError('This parser is not yet able to parse past dates')
+        raise NotImplementedError(
+            'This parser is not yet able to parse past dates')
 
     value_map = fetch_data(zone_key, session, logger=logger)
 
@@ -148,15 +149,15 @@ def fetch_production(zone_key='IN-GJ', session=None, target_datetime=None, logge
         'datetime': value_map['date'],
         'production': {
             'biomass': None,
-            'coal': value_map['coal'],
-            'gas': value_map['gas'],
-            'hydro': value_map['hydro'],
-            'nuclear': value_map['nuclear'],
+            'coal': value_map.get('coal', 0),
+            'gas': value_map.get('gas', 0),
+            'hydro': value_map.get('hydro', 0),
+            'nuclear': value_map.get('nuclear', 0),
             'oil': None,
-            'solar': value_map['solar'],
-            'wind': value_map['wind'],
+            'solar': value_map.get('solar', 0),
+            'wind': value_map.get('wind', 0),
             'geothermal': None,
-            'unknown': value_map['unknown']
+            'unknown': value_map.get('unknown', 0)
         },
         'storage': {
             'hydro': None
@@ -169,15 +170,18 @@ def fetch_production(zone_key='IN-GJ', session=None, target_datetime=None, logge
     return valid_data
 
 
-def fetch_consumption(zone_key='IN-GJ', session=None, target_datetime=None, logger=getLogger('IN-GJ')):
+def fetch_consumption(zone_key='IN-GJ', session=None, target_datetime=None,
+                      logger=getLogger('IN-GJ')):
     """
     Method to get consumption data of Gujarat
     :param zone_key:
     :param session:
     :return:
     """
+    session = session or requests.session()
     if target_datetime:
-        raise NotImplementedError('This parser is not yet able to parse past dates')
+        raise NotImplementedError(
+            'This parser is not yet able to parse past dates')
 
     value_map = fetch_data(zone_key, session, logger=logger)
 
@@ -192,6 +196,6 @@ def fetch_consumption(zone_key='IN-GJ', session=None, target_datetime=None, logg
 
 
 if __name__ == '__main__':
-    session = Session()
+    session = requests.Session()
     print(fetch_production('IN-GJ', session))
     print(fetch_consumption('IN-GJ', session))
