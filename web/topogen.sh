d5b67ad6aa58b38f5a48d62344a17c769abee6ca
systemcatch <30196510+systemcatch@users.noreply.github.com>
2018-05-15 14:01:52 +0100

    Change generation of US zones

--- web/topogen.sh
+++ web/topogen.sh
@@ -6,7 +6,7 @@ RESOLUTION=10m
 COUNTRIES_FILENAME=ne_${RESOLUTION}_admin_0_map_subunits
 
 STATES_FILENAME=ne_${RESOLUTION}_admin_1_states_provinces_lakes
-STATES_FILTER="['AUS', 'BIH', 'BRA', 'CAN', 'CHL', 'DNK', 'GRC', 'IND', 'MYS', 'RUS', 'USA']"
+STATES_FILTER="['AUS', 'BIH', 'BRA', 'CAN', 'CHL', 'DNK', 'GRC', 'IND', 'MYS', 'RUS']"
 
 NODE_MODULES_PATH="node_modules/.bin"
 
@@ -37,16 +37,20 @@ echo "Extract metadata"
   | "$NODE_MODULES_PATH/ndjson-map" 'd.properties' \
 )> build/world_states_props.json
 
-("$NODE_MODULES_PATH/shp2json" -n build/Canary_Islands.shp \
+("$NODE_MODULES_PATH/shp2json" -n build/US_Control_Areas.shp \
   | "$NODE_MODULES_PATH/ndjson-map" 'd.properties' \
 )> build/third_party_props.json
 
+("$NODE_MODULES_PATH/shp2json" -n build/Canary_Islands.shp \
+  | "$NODE_MODULES_PATH/ndjson-map" 'd.properties' \
+)>> build/third_party_props.json
+
 # We could use a country filter here to remove unneeded natural earth shapes.
-# Parse countries
+# Parse countries but exclude continental USA.
 echo 'Parsing countries..'
 ("$NODE_MODULES_PATH/shp2json" -n build/${COUNTRIES_FILENAME}.shp \
   | "$NODE_MODULES_PATH/ndjson-map" '(d.id = (d.properties.ISO_A3 != "-99") ? d.properties.ISO_A3 : d.properties.ADM0_A3, d.oldProps = d.properties, d.properties = {}, d.properties.subid = d.oldProps.SU_A3, d.properties.code_hasc = d.oldProps.code_hasc, delete d.oldProps, d)' \
-  | "$NODE_MODULES_PATH/ndjson-filter" "d.id" \
+  | "$NODE_MODULES_PATH/ndjson-filter" 'd.id && d.id != "USB"' \
 )> build/tmp_countries.json
 
 # Parse states
@@ -57,11 +61,18 @@ echo 'Parsing states..'
 )> build/tmp_states.json
 
 # Parse 3rd party
-echo 'Parsing 3rd party..'
-("$NODE_MODULES_PATH/shp2json" -n build/Canary_Islands.shp \
+echo 'Parsing USA..'
+("$NODE_MODULES_PATH/shp2json" -n build/US_Control_Areas.shp \
   | "$NODE_MODULES_PATH/ndjson-map" 'd.id = d.properties.NAME, d' \
   | "$NODE_MODULES_PATH/ndjson-filter" "d.id" \
 )> build/tmp_thirdparty.json
+
+# Parse 3rd party
+echo 'Parsing Canaries..'
+("$NODE_MODULES_PATH/shp2json" -n build/Canary_Islands.shp \
+  | "$NODE_MODULES_PATH/ndjson-map" 'd.id = d.properties.NAME, d' \
+  | "$NODE_MODULES_PATH/ndjson-filter" "d.id" \
+)>> build/tmp_thirdparty.json
 
 # Generate final geometries
 node generate-geometries.js
