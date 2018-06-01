3b4602327e91dfa6b1448fd48fb2f80f1db4391d
lorrieq <lorrieq@hotmail.com>
2018-06-01 18:48:39 +0100

    Use B1620 report in fetch_production
    * Create reports dictionary with report variables e.g. expected fields
    
    * Remove GB-NIR -> IE exchange from test run area

--- parsers/ELEXON.py
+++ parsers/ELEXON.py
@@ -17,23 +17,36 @@ import arrow
 import logging
 import requests
 import datetime as dt
+import pandas as pd
+from StringIO import StringIO
 
 ELEXON_ENDPOINT = 'https://api.bmreports.com/BMRS/{}/v1'
 
-FUELS = {
-    'production': {
-        'gas': [4, 11],
-        'oil': 5,
-        'coal': 6,
-        'nuclear': 7,
-        'wind': 8,
-        'hydro': 10,
-        'unknown': 12,
-        'biomass': 17
+REPORT_META = {
+    'B1620': {
+        'expected_fields': 13,
+        'skiprows': 5
     },
-    'storage': {
-        'hydro': 9
+    'INTERFUELHH': {
+        'expected_fields': 7,
+        'skiprows': 0
     }
+}
+
+# 'hydro' key is for hydro production
+# 'hydro storage' key is for hydro storage
+RESOURCE_TYPE_TO_FUEL = {
+    'Biomass': 'biomass',
+    'Fossil Gas': 'gas',
+    'Fossil Hard coal': 'coal',
+    'Fossil Oil': 'oil',
+    'Hydro Pumped Storage': 'hydro storage',
+    'Hydro Run-of-river and poundage': 'hydro',
+    'Nuclear': 'nuclear',
+    'Solar': 'solar',
+    'Wind Onshore': 'wind',
+    'Wind Offshore': 'wind',
+    'Other': 'unknown'
 }
 
 EXCHANGES = {
@@ -43,25 +56,8 @@ EXCHANGES = {
     'GB->IE': 6
 }
 
-EXPECTED_FIELDS = {
-    'FUELINST': 18,
-    'INTERFUELHH': 7
-}
 
-
-def query_ELEXON(report, session, params, target_datetime=None,
-                 span=(-48, 24)):
-    if target_datetime is None:
-        target_datetime = arrow.utcnow().to('Europe/London')
-    else:
-        # make sure we have an arrow object
-        target_datetime = arrow.get(target_datetime).to('Europe/London')
-
-    params['FromDate'] = target_datetime.replace(
-        hours=span[0]).format('YYYY-MM-DD')
-    params['ToDate'] = target_datetime.replace(
-        hours=span[1]).format('YYYY-MM-DD')
-
+def query_ELEXON(report, session, params):
     if 'ELEXON_TOKEN' not in os.environ:
         raise Exception('No ELEXON_TOKEN found! Please add it to secrets.env!')
     params['APIKey'] = os.environ['ELEXON_TOKEN']
@@ -69,18 +65,31 @@ def query_ELEXON(report, session, params, target_datetime=None,
 
 
 def query_exchange(session, target_datetime=None):
+    if target_datetime is None:
+        target_datetime = dt.date.today()
+
+    from_date = (target_datetime - dt.timedelta(days=1)).strftime('%Y-%m-%d')
+    to_date = target_datetime.strftime('%Y-%m-%d')
+
     params = {
+        'FromDate': from_date,
+        'ToDate': to_date,
         'ServiceType': 'csv'
     }
-    response = query_ELEXON('INTERFUELHH', session, params, target_datetime)
+    response = query_ELEXON('INTERFUELHH', session, params)
     return response.text
 
 
 def query_production(session, target_datetime=None):
+    if target_datetime is None:
+        target_datetime = dt.date.today()
+
     params = {
+        'SettlementDate': target_datetime.strftime('%Y-%m-%d'),
+        'Period': '*',
         'ServiceType': 'csv'
     }
-    response = query_ELEXON('FUELINST', session, params, target_datetime)
+    response = query_ELEXON('B1620', session, params)
     return response.text
 
 
@@ -89,18 +98,19 @@ def parse_exchange(zone_key1, zone_key2, csv_text, target_datetime=None,
     if not csv_text:
         return None
 
-    sorted_zone_keys = sorted([zone_key1, zone_key2])
+    report = REPORT_META['INTERFUELHH']
 
+    sorted_zone_keys = sorted([zone_key1, zone_key2])
     exchange = '->'.join(sorted_zone_keys)
     data_points = list()
     lines = csv_text.split('\n')
 
     # check field count in report is as expected
     field_count = len(lines[1].split(','))
-    if field_count != EXPECTED_FIELDS['INTERFUELHH']:
+    if field_count != report['expected_fields']:
         raise ValueError(
             'Expected {} fields in INTERFUELHH report, got {}'.format(
-                EXPECTED_FIELDS['INTERFUELHH'], field_count))
+                report['expected_fields'], field_count))
 
     for line in lines[1:-1]:
         fields = line.split(',')
@@ -129,42 +139,60 @@ def parse_production(csv_text, target_datetime=None,
     if not csv_text:
         return None
 
-    data_points = list()
-    lines = csv_text.split('\n')
+    report = REPORT_META['B1620']
+
+    # create DataFrame from slice of CSV rows
+    df = pd.read_csv(StringIO(csv_text), skiprows=report['skiprows'] - 1)
 
     # check field count in report is as expected
-    field_count = len(lines[1].split(','))
-    if field_count != EXPECTED_FIELDS['FUELINST']:
+    field_count = len(df.columns)
+    if field_count != report['expected_fields']:
         raise ValueError(
-            'Expected {} fields in FUELINST report, got {}'.format(
-                EXPECTED_FIELDS['FUELINST'], field_count))
+            'Expected {} fields in B1620 report, got {}'.format(
+                report['expected_fields'], len(df.columns)))
 
-    for line in lines[1:-1]:
-        fields = line.split(',')
+    # filter out undesired columns
+    df = df.iloc[:-1, [7, 8, 9, 4]]
 
-        # all timestamps are always GMT in ELEXON.
-        # the publish time of a datapoint is five minutes after that
-        # datapoint e.g. datapoint of 09:00 has a publish time of 09:05
-        publish_time = fields[3]
-        date_obj = dt.datetime.strptime(publish_time, '%Y%m%d%H%M%S')
-        datetime = arrow.get(date_obj).replace(minutes=-5).to('Europe/London')
+    df['datetime'] = df.apply(lambda x: datetime_from_date_sp(
+        x['Settlement Date'], x['Settlement Period']), axis=1)
 
-        data = {
+    # map from report fuel names to electricitymap fuel names
+    fuel_column = 'Power System Resource  Type'
+    df[fuel_column] = df[fuel_column].apply(lambda x: RESOURCE_TYPE_TO_FUEL[x])
+
+    # loop through unique datetimes and create each data point
+    data_points = list()
+    for time in pd.unique(df['datetime']):
+        time_df = df[df['datetime'] == time]
+
+        data_point = {
             'zoneKey': 'GB',
-            'datetime': datetime.datetime,
-            'production': {},
-            'storage': {},
-            'source': 'bmreports.com'
+            'datetime': time,
+            'source': 'bmreports.com',
+            'production': dict(),
+            'storage': dict()
         }
 
-        # read the fuel values into the datapoint
-        for _type, fuels in FUELS.items():
-            for fuel, field_nums in fuels.items():
-                if not isinstance(field_nums, list):
-                    field_nums = [field_nums, ]
-                data[_type][fuel] = sum([float(fields[i]) for i in field_nums])
+        for row in time_df.iterrows():
+            fields = row[1].to_dict()
+            fuel = fields[fuel_column]
+            quantity = fields['Quantity']
 
-        data_points.append(data)
+            # check if storage value and if so correct key
+            if 'storage' in fuel:
+                fuel_key = fuel.replace('storage', '').strip()
+                data_point['storage'][fuel_key] = quantity
+            else:
+                # if/else structure allows summation of multiple quantities
+                # e.g. 'Wind Onshore' and 'Wind Offshore' both have the
+                # key 'wind' here.
+                if fuel in data_point['production'].keys():
+                    data_point['production'][fuel] += quantity
+                else:
+                    data_point['production'][fuel] = quantity
+
+        data_points.append(data_point)
 
     return data_points
 
@@ -183,7 +211,7 @@ def fetch_exchange(zone_key1, zone_key2, session=None, target_datetime=None,
     return data
 
 
-def fetch_production(session=None, target_datetime=None,
+def fetch_production(zone_key='GB', session=None, target_datetime=None,
                      logger=logging.getLogger(__name__)):
     session = session or requests.session()
     response = query_production(session, target_datetime)
@@ -206,6 +234,3 @@ if __name__ == '__main__':
 
     print('fetch_exchange(GB, NL) ->')
     print(fetch_exchange('GB', 'NL'))
-
-    print('fetch_exchange(GB-NIR, IE) ->')
-    print(fetch_exchange('GB-NIR', 'IE'))
