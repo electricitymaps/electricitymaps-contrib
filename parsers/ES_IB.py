6bb9c5d7e2c9a5350da4780a8a545210c4b69350
Alex <alixunderplatz@users.noreply.github.com>
2018-06-10 00:12:13 +0200

    Add ES_IB split parser
    ES-IB-MA: Mallorca (connected with ES, ES-IB-ME and ES-IB-IZ
    ES-IB-ME: Menorca (connected with ES-IB-MA, power cable currently broken
    ES-IB-IZ: Ibiza (connected with ES-IB-MA and ES-IB-FO)
    ES-IB-FO: Formentera (connected with ES-IB-IZ)

--- parsers/ES_IB.py
+++ parsers/ES_IB.py
@@ -1,108 +1,188 @@
 #!/usr/bin/env python3
 
-# The arrow library is used to handle datetimes
+import logging
 from arrow import get
-# The request library is used to fetch content through HTTP
 from requests import Session
-from reescraper import BalearicIslands
-from .lib.exceptions import ParserException
+from ree import (Formentera, Ibiza,
+                 Mallorca, Menorca,
+                 BalearicIslands, IberianPeninsula)
+
+## I had to comment these two due to local issues:
+
+#from .lib.exceptions import ParserException
+#from .lib.validation import validate
+
+## Guess we'll need to figure these out later?! Adapted from ES-CN:
+
+# Minimum valid zone demand. This is used to eliminate some cases
+# where generation for one or more modes is obviously missing.
+#FLOORS = {
+#'ES-IB-FO': 0,
+#'ES-IB-IZ': 0,
+#'ES-IB-MA': 0,
+#'ES-IB-ME': 0,
+#}
 
 
-def fetch_consumption(zone_key='ES-IB', session=None, target_datetime=None, logger=None):
+def fetch_island_data(zone_key, session):
+    if zone_key == 'ES-IB-FO':
+        formentera_data = Formentera(session, verify=False).get_all()
+        if not formentera_data:
+            raise ParserException(zone_key, "Formentera doesn't respond")
+        else:
+            return formentera_data
+    elif zone_key == 'ES-IB-IZ':
+        ibiza_data = Ibiza(session, verify=False).get_all()
+        if not ibiza_data:
+            raise ParserException(zone_key, "Party is over, Ibiza doesn't respond")
+        else:
+            return ibiza_data
+    elif zone_key == 'ES-IB-MA':
+        mallorca_data = Mallorca(session, verify=False).get_all()
+        if not mallorca_data:
+            raise ParserException(zone_key, "Mallorca doesn't respond")
+        else:
+            return mallorca_data
+    elif zone_key == 'ES-IB-ME':
+        menorca_data = Menorca(session, verify=False).get_all()
+        if not menorca_data:
+            raise ParserException(zone_key, "Menorca doesn't respond")
+        else:
+            return menorca_data
+    else:
+        raise ParserException(zone_key, 'Can\'t read this country code {0}'.format(zone_key))
+
+
+def fetch_consumption(zone_key, session=None, target_datetime=None, logger=None):
     if target_datetime:
         raise NotImplementedError('This parser is not yet able to parse past dates')
-    
+
     ses = session or Session()
-    responses = BalearicIslands(ses).get_all()
-    if not responses:
-        raise ParserException("ES-IB", "No response")
-    else:
-        data = []
+    island_data = fetch_island_data(zone_key, ses)
+    data = []
+    for response in island_data:
+        response_data = {
+            'zoneKey': zone_key,
+            'datetime': get(response.timestamp).datetime,
+            'consumption': response.demand,
+            'source': 'demanda.ree.es'
+        }
 
-        for response in responses:
-            response_data = {
-                'zoneKey': zone_key,
-                'datetime': get(response.timestamp).datetime,
-                'consumption': response.demand,
-                'source': 'demanda.ree.es'
-            }
+        data.append(response_data)
 
-            data.append(response_data)
-
-        return data
+    return data
 
 
-def fetch_production(zone_key='ES-IB', session=None, target_datetime=None, logger=None):
+def fetch_production(zone_key, session=None, target_datetime=None, logger=None):
     if target_datetime:
         raise NotImplementedError('This parser is not yet able to parse past dates')
 
     ses = session or Session()
-    responses = BalearicIslands(ses).get_all()
+    island_data = fetch_island_data(zone_key, ses)
+    data = []
 
-    if not responses:
-        raise ParserException("ES-IB", "No response")
-    else:
+## biomass could probably be added as response.biomass in ree, including value['resid'] which I assume is for residuos=waste. Atm it seems unincluded.
+## I saw generation from "genAux" (generacion auxiliar) today on Formentera, which should probably be added to response.other in ree
+## Formentera mostly only has some solar generation during the day, importing from Ibiza all of the time, which probably has to be considered for response.production() > 0: ?
 
-        data = []
-
-        for response in responses:
+    for response in island_data:
+        if response.production() > 0:
             response_data = {
                 'zoneKey': zone_key,
                 'datetime': get(response.timestamp).datetime,
                 'production': {
-                    'coal': response.carbon,
-                    'gas': round(response.gas + response.combined, 2),
-                    'solar': response.solar,
-                    'oil': round(response.vapor + response.diesel, 2),
-                    'wind': response.wind,
-                    'hydro': response.hydraulic,
-                    'biomass': 0.0,
-                    'nuclear': 0.0,
-                    'geothermal': 0.0,
-                    'unknown': response.unknown()
-                },
-                'storage': {
-                    'hydro': 0.0,
-                    'battery': 0.0
-                },
-                'source': 'demanda.ree.es',
-            }
+                'coal': round(response.carbon, 2),
+                'gas': round(response.gas + response.combined, 2),
+                'solar': round(response.solar, 2),
+                'oil': round(response.vapor + response.diesel, 2),
+                'wind': round(response.wind, 2),
+                'hydro': round(response.hydraulic, 2),
+                'biomass': 0.0,
+                'nuclear': 0.0,
+                'geothermal': 0.0,
+                'unknown': round(response.other, 2)
+            },
+            'storage': {
+                'hydro': 0.0,
+                'battery': 0.0
+            },
+            'source': 'demanda.ree.es',
+        }
 
-            data.append(response_data)
+## I had to comment the validation due to local issues and the commented FLOORS from above:
 
-        return data
+#                response_data = validate(response_data, logger,
+#                                         floor=FLOORS[zone_key])
+#
+#                if response_data:
+#                    # append if valid
+#                    data.append(response_data)
+            data.append(response_data) ## delete this line when you uncomment the lines right above
+
+    return data
 
 
-def fetch_exchange(zone_key1='ES', zone_key2='ES-IB', session=None, target_datetime=None, logger=None):
+def fetch_exchange(zone_key1, zone_key2, session=None, target_datetime=None, logger=None):
+
     if target_datetime:
         raise NotImplementedError('This parser is not yet able to parse past dates')
 
+    sortedZoneKeys = '->'.join(sorted([zone_key1, zone_key2]))
+
     ses = session or Session()
-    responses = BalearicIslands(ses).get_all()
-    if not responses:
-        raise ParserException("ES-IB", "No response")
+    response_ma = Mallorca(ses, verify=False).get_all()
+    response_fo = Formentera(ses, verify=False).get_all()
+
+    if not response_ma:
+        raise ParserException("ES-IB-MA", "No response")
+    elif not response_fo:
+        raise ParserException("ES-IB-IZ", "No response")        
     else:
 
-        data = []
-        for response in responses:
+        if sortedZoneKeys == 'ES->ES-IB-MA':
+            for response in response_ma:
+                netflow = response.link['pe_ma']
+        elif sortedZoneKeys == 'ES-IB-MA->ES-IB-ME':
+            for response in response_ma:
+                netflow = response.link['ma_me']
+        elif sortedZoneKeys == 'ES-IB-IZ->ES-IB-MA':
+            for response in response_ma:
+                netflow = response.link['ma_ib']
+        elif sortedZoneKeys == 'ES-IB-FO->ES-IB-IZ':
+            for response in response_fo:
+                netflow = -1 * response.link['ib_fo']
+        else:
+            raise NotImplementedError('This exchange pair is not implemented')
 
-            sorted_zone_keys = sorted([zone_key1, zone_key2])
-            net_flow = response.link['pe_ma']
+        exchange = {
+            'sortedZoneKeys': sortedZoneKeys,
+            'datetime': get(response.timestamp).datetime,
+            'netFlow': netflow,
+            'source': 'demanda.ree.es'
+        }
 
-            response_data = {
-                'sortedZoneKeys': '->'.join(sorted_zone_keys),
-                'datetime': get(response.timestamp).datetime,
-                'netFlow': net_flow if zone_key1 == sorted_zone_keys[0] else -1 * net_flow,
-                'source': 'demanda.ree.es',
-            }
+    return exchange
 
-            data.append(response_data)
+session = Session
+   
+print("# ES-IB-FO")
+print(fetch_consumption('ES-IB-FO', session))
+print(fetch_production('ES-IB-FO', session))
 
-        return data
+print("# ES-IB-IZ")
+print(fetch_consumption('ES-IB-IZ', session))
+print(fetch_production('ES-IB-IZ', session))
 
+print("# ES-IB-MA")
+print(fetch_consumption('ES-IB-MA', session))
+print(fetch_production('ES-IB-MA', session))
 
-if __name__ == '__main__':
-    session = Session
-    print(fetch_consumption('ES-IB', session))
-    print(fetch_production('ES-IB', session))
-    print(fetch_exchange('ES', 'ES-IB', session))
+print("# ES-IB-ME")
+print(fetch_consumption('ES-IB-ME', session))
+print(fetch_production('ES-IB-ME', session))
+
+print("# exchanges")
+print(fetch_exchange('ES', 'ES-IB-MA', session))
+print(fetch_exchange('ES-IB-MA', 'ES-IB-ME', session))
+print(fetch_exchange('ES-IB-MA', 'ES-IB-IZ', session))
+print(fetch_exchange('ES-IB-IZ', 'ES-IB-FO', session)) 
