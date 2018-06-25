55621b91cdf6685f35c1f105a4d40abf758b117a
systemcatch <30196510+systemcatch@users.noreply.github.com>
2018-06-25 11:09:13 +0100

    Move fetching of data into a separate function

--- parsers/IQ.py
+++ parsers/IQ.py
@@ -10,9 +10,8 @@ import datetime
 url = 'http://109.224.53.139:8080/api.php'
 
 
-def fetch_production(zone_key='IQ', session=None, target_datetime=None, logger=None):
-    if target_datetime:
-        raise NotImplementedError('This parser is not yet able to parse past dates')
+def fetch_data(session=None):
+    """Returns a tuple containing a json response & arrow object."""
 
     r = session or requests.session()
     response = r.get(url)
@@ -20,18 +19,25 @@ def fetch_production(zone_key='IQ', session=None, target_datetime=None, logger=N
     iraq_data = iraq_json['d']
     iraq_time = iraq_json['lastmodified']
 
-    # Time of dataset shown as "Last Update"
-    
     local_date_time = datetime.datetime.strptime(iraq_json['lastmodified'], "%I:%M:%S %p %d-%m-%Y")
     zone_date_time = arrow.Arrow.fromdatetime(local_date_time, 'Asia/Baghdad')
 
+    return iraq_data, zone_date_time
+
+
+def fetch_production(zone_key='IQ', session=None, target_datetime=None, logger=None):
+    if target_datetime:
+        raise NotImplementedError('This parser is not yet able to parse past dates')
+
+    iraq_data, zone_date_time = fetch_data(session=session)
+
     # Summarized data for Iraq
-    
+
     thermal = iraq_data['d_1218']
     gas = iraq_data['d_1219']
     hydro = iraq_data['d_1220']
-    diesel = iraq_data['d_1221'] 
-  
+    diesel = iraq_data['d_1221']
+
     production = {
             'zoneKey': zone_key,
             'datetime': zone_date_time.datetime,
@@ -50,39 +56,27 @@ def fetch_production(zone_key='IQ', session=None, target_datetime=None, logger=N
 def fetch_consumption(zone_key = 'IQ', session=None, target_datetime=None, logger=None):
     if target_datetime:
         raise NotImplementedError('This parser is not yet able to parse past dates')
-    
-    r = session or requests.session()
-    response = r.get(url)
-    iraq_json = response.json()
-    iraq_consumption = iraq_json['d']['d_1234']
-    iraq_time = iraq_json['lastmodified']
 
-    local_date_time = datetime.datetime.strptime(iraq_json['lastmodified'], "%I:%M:%S %p %d-%m-%Y")
-    zone_date_time = arrow.Arrow.fromdatetime(local_date_time, 'Asia/Baghdad')
-        
+    iraq_data, zone_date_time = fetch_data(session=session)
+
     consumption = {
             'zoneKey': zone_key,
             'datetime': zone_date_time.datetime,
-            'consumption': iraq_consumption,
+            'consumption': iraq_data['d_1234'],
             'source': 'Iraqi Power System'
             }
 
     return consumption
-    
 
 
 def fetch_exchange(zone_key1, zone_key2, session=None, target_datetime=None, logger=None):
-   
+
     if target_datetime:
         raise NotImplementedError('This parser is not yet able to parse past dates')
 
     sortedZoneKeys = '->'.join(sorted([zone_key1, zone_key2]))
 
-    r = session or requests.session()
-    response = r.get(url)
-    iraq_json = response.json()
-    iraq_exch = iraq_json['d']
-    iraq_time = iraq_json['lastmodified']
+    iraq_exch, zone_date_time = fetch_data(session=session)
 
     #    tie_line_mapping = {
     #                'IQ->IR':{'d_1226 = MERSAD',
@@ -91,7 +85,7 @@ def fetch_exchange(zone_key1, zone_key2, session=None, target_datetime=None, log
     #                          'd_1229 = SBZB'},
     #                'IQ->IQ-KUR':'d_1230 = KURDISTAN'
     #                }
-    
+
     if sortedZoneKeys == 'IQ->IR':
         netflow = -1 * (iraq_exch['d_1226'] +
                         iraq_exch['d_1227'] +
@@ -103,9 +97,6 @@ def fetch_exchange(zone_key1, zone_key2, session=None, target_datetime=None, log
     else:
         raise NotImplementedError('This exchange pair is not implemented')
 
-    local_date_time = datetime.datetime.strptime(iraq_time, "%I:%M:%S %p %d-%m-%Y")
-    zone_date_time = arrow.Arrow.fromdatetime(local_date_time, 'Asia/Baghdad')
-
     exchange = {
         'sortedZoneKeys': sortedZoneKeys,
         'datetime': zone_date_time.datetime,
@@ -115,7 +106,7 @@ def fetch_exchange(zone_key1, zone_key2, session=None, target_datetime=None, log
 
     return exchange
 
-        
+
 print(fetch_production())
 print(fetch_consumption())
 print(fetch_exchange('IQ', 'IR'))
