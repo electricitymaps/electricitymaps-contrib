d95113300ad9db7cab4181b757f82a2a04595a3a
tmslaine <tms.laine@gmail.com>
2018-10-29 19:45:37 +0200

    Add JP forecasts(#1550) (#1643)
    * Add JP forecasts
    
    Adds a consumption forecast parser to JP areas
    Corrects sources: JP-CG and JP-SK were mixed
    
    * Add JP forecasts
    
    Adds exchange forecasts to Occtonet parser
    Adds function to solve net generation/demand for high-voltage grid nodes in Japanese areas
    
    * Add JP forecasts
    
    Adds JP exchange forecasts
    
    * Add JP forecasts
    
    Add JP consumption forecasts
    
    * Update occtonet.py
    
    Raise NotImplementedError for querying future date(local time) exchange forecasts
    
    * Update JP.py
    
    Raise NotImplementedError for querying future date(local time) consumption forecasts. Coerce prices to integer values.
    
    * Update JP.py
    
    Remove unneeded copying of dataframes

--- parsers/JP.py
+++ parsers/JP.py
@@ -19,6 +19,18 @@ from . import occtonet
 # JP-ON  : Okinawa
 # JP-CG  : Chūgoku
 
+sources = {
+        'JP-HKD':'denkiyoho.hepco.co.jp',
+        'JP-TH':'setsuden.tohoku-epco.co.jp',
+        'JP-TK':'www.tepco.co.jp',
+        'JP-CB':'denki-yoho.chuden.jp',
+        'JP-HR':'www.rikuden.co.jp/denki-yoho',
+        'JP-KN':'www.kepco.co.jp',
+        'JP-SK':'www.yonden.co.jp',
+        'JP-CG':'www.energia.co.jp',
+        'JP-KY':'www.kyuden.co.jp/power_usages/pc.html',
+        'JP-ON':'www.okiden.co.jp/denki/'
+        }
 
 def fetch_production(zone_key='JP-TK', session=None, target_datetime=None,
                      logger=logging.getLogger(__name__)):
@@ -53,20 +65,11 @@ def fetch_production(zone_key='JP-TK', session=None, target_datetime=None,
         else:
             df['imports'] = df['imports']-df[exchname]
     df['prod'] = df['cons']-df['imports']
-    df = df[['datetime', 'prod']]
     # add a row to production for each entry in the dictionary:
-    sources = {
-        'JP-HKD':'denkiyoho.hepco.co.jp',
-        'JP-TH':'setsuden.tohoku-epco.co.jp',
-        'JP-TK':'www.tepco.co.jp',
-        'JP-CB':'denki-yoho.chuden.jp',
-        'JP-HR':'www.rikuden.co.jp/denki-yoho',
-        'JP-KN':'www.kepco.co.jp',
-        'JP-SK':'www.energia.co.jp',
-        'JP-CG':'www.yonden.co.jp'
-        }
+    
     datalist = []
-    for i in range(df.shape[0]):
+    #for i in range(df.shape[0]):
+    for i in df.index:
         data = {
             'zoneKey': zone_key,
             'datetime': df.loc[i, 'datetime'].to_pydatetime(),
@@ -103,7 +106,9 @@ def fetch_consumption_df(zone_key='JP-TK', target_datetime=None,
         'JP-CB': 'http://denki-yoho.chuden.jp/denki_yoho_content_data/juyo_cepco003.csv',
         'JP-KN': 'http://www.kepco.co.jp/yamasou/juyo1_kansai.csv',
         'JP-CG': 'http://www.energia.co.jp/jukyuu/sys/juyo_07_{}.csv'.format(datestamp),
-        'JP-SK': 'http://www.yonden.co.jp/denkiyoho/juyo_shikoku.csv'
+        'JP-SK': 'http://www.yonden.co.jp/denkiyoho/juyo_shikoku.csv',
+        'JP-KY': 'http://www.kyuden.co.jp/power_usages/csv/juyo-hourly-{}.csv'.format(datestamp),
+        'JP-ON': 'https://www.okiden.co.jp/denki/juyo_10_{}.csv'.format(datestamp)
         }
     # First roughly 40 rows of the consumption files have hourly data,
     # the parser skips to the rows with 5-min actual values 
@@ -111,7 +116,7 @@ def fetch_consumption_df(zone_key='JP-TK', target_datetime=None,
         startrow = 44
     else:
         startrow = 42
-    df = pd.read_csv(consumption_url[zone_key], skiprows=list(range(startrow)),
+    df = pd.read_csv(consumption_url[zone_key], skiprows=startrow,
                      encoding='shift-jis')
     df.columns = ['Date', 'Time', 'cons']
     # Convert 万kW to MW
@@ -121,6 +126,67 @@ def fetch_consumption_df(zone_key='JP-TK', target_datetime=None,
     df = df[['datetime', 'cons']]
     return df
 
+def fetch_consumption_forecast(zone_key='JP-KY', target_datetime=None,
+                      logger=logging.getLogger(__name__)):
+    """
+    Gets consumption forecast for specified zone.
+    Returns a list of dictionaries.
+    """
+    # Currently past dates not implemented for areas with no date in their demand csv files
+    if target_datetime and (zone_key in ['JP-TK', 'JP-TH', 'JP-CB', 'JP-KN', 'JP-SK']):
+        raise NotImplementedError(
+            "Past dates not yet implemented for selected region")
+    datestamp = arrow.get(target_datetime).to('Asia/Tokyo').strftime('%Y%m%d')
+    # Forecasts ahead of current date are not available
+    if datestamp > arrow.get().to('Asia/Tokyo').strftime('%Y%m%d'):
+        raise NotImplementedError(
+            "Future dates(local time) not implemented for selected region")
+        
+    consumption_url = {
+                   'JP-HKD': 'http://denkiyoho.hepco.co.jp/area/data/juyo_01_{}.csv'.format(datestamp),
+                   'JP-TH': 'http://setsuden.tohoku-epco.co.jp/common/demand/juyo_02_{}.csv'.format(datestamp),
+                   'JP-TK': 'http://www.tepco.co.jp/forecast/html/images/juyo-j.csv',
+                   'JP-HR': 'http://www.rikuden.co.jp/denki-yoho/csv/juyo_05_{}.csv'.format(datestamp),
+                   'JP-CB': 'http://denki-yoho.chuden.jp/denki_yoho_content_data/juyo_cepco003.csv',
+                   'JP-KN': 'http://www.kepco.co.jp/yamasou/juyo1_kansai.csv',
+                   'JP-CG': 'http://www.energia.co.jp/jukyuu/sys/juyo_07_{}.csv'.format(datestamp),
+                   'JP-SK': 'http://www.yonden.co.jp/denkiyoho/juyo_shikoku.csv',
+                   'JP-KY': 'http://www.kyuden.co.jp/power_usages/csv/juyo-hourly-{}.csv'.format(datestamp),
+                   'JP-ON': 'https://www.okiden.co.jp/denki/juyo_10_{}.csv'.format(datestamp)
+                   }
+    # Skip non-tabular data at the start of source files
+    if zone_key == 'JP-KN':
+        startrow = 9
+    else:
+        startrow = 7
+    # Read the 24 hourly values
+    df = pd.read_csv(consumption_url[zone_key],skiprows=startrow, nrows = 24, encoding = 'shift-jis')
+    if zone_key == 'JP-KN':
+        df = df[['DATE', 'TIME', '予想値(万kW)']]
+    else:
+        try:
+            df = df[['DATE', 'TIME', '予測値(万kW)']]
+        except KeyError:
+            df = df[['DATE', 'TIME', '予測値（万kW)']]
+    df.columns = ['Date', 'Time', 'fcst']
+
+    df['datetime'] = df.apply(parse_dt, axis=1)
+
+    # convert from 万kW to MW
+    df['fcst'] = 10*df['fcst']
+    # validate
+    df = df.loc[df['fcst']>0]
+    # return format
+    data = []
+    #for i in range(df.shape[0]):
+    for i in df.index:
+            data.append({
+                'zoneKey': zone_key,
+                'datetime': df.loc[i,'datetime'].to_pydatetime(),
+                'value': df.loc[i,'fcst'],
+                'source': sources[zone_key]
+            })
+    return data
 
 def fetch_price(zone_key='JP-TK', session=None, target_datetime=None,
                 logger=logging.getLogger(__name__)):
@@ -155,7 +221,7 @@ def fetch_price(zone_key='JP-TK', session=None, target_datetime=None,
             'zoneKey': zone_key,
             'currency': 'JPY',
             'datetime': row[1]['datetime'].datetime,
-            'price': 1000*row[1][zone_key],# Convert from JPY/kWh to JPY/MWh
+            'price': round(int(1000*row[1][zone_key]),-1),# Convert from JPY/kWh to JPY/MWh
             'source': 'jepx.org'
         })
 
@@ -177,3 +243,19 @@ if __name__ == '__main__':
     print(fetch_production())
     print('fetch_price() ->')
     print(fetch_price())
+    print('fetch_consumption_forecast() ->')
+    print(fetch_consumption_forecast())
+    print('fetch_consumption_forecast(JP-TH) ->')
+    print(fetch_consumption_forecast('JP-TH'))
+    print('fetch_consumption_forecast(JP-TK) ->')
+    print(fetch_consumption_forecast('JP-TK'))
+    print('fetch_consumption_forecast(JP-CB) ->')
+    print(fetch_consumption_forecast('JP-CB'))
+    print('fetch_consumption_forecast(JP-KN) ->')
+    print(fetch_consumption_forecast('JP-KN'))
+    print('fetch_consumption_forecast(JP-SK) ->')
+    print(fetch_consumption_forecast('JP-SK'))
+    print('fetch_consumption_forecast(JP-KY) ->')
+    print(fetch_consumption_forecast('JP-KY'))
+    print('fetch_consumption_forecast(JP-ON) ->')
+    print(fetch_consumption_forecast('JP-ON'))
