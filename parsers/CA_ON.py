394f2b52ced9458513ed9dea3cc8515a05f3430e
Jarek Piórkowski <jarek@piorkowski.ca>
2019-02-20 10:14:09 -0500

    CA-ON: allow fetching past 3 months of data

--- parsers/CA_ON.py
+++ parsers/CA_ON.py
@@ -2,9 +2,14 @@
 
 from collections import defaultdict
 import datetime
+import logging
+import xml.etree.ElementTree as ET
 
 # The arrow library is used to handle datetimes
 import arrow
+
+# BeautifulSoup processes markup, this could be migrated to use xml library
+from bs4 import BeautifulSoup
 
 # pytz gets tzinfo objects
 import pytz
@@ -12,8 +17,14 @@ import pytz
 # The request library is used to fetch content through HTTP
 import requests
 
-from bs4 import BeautifulSoup
+# pandas processes tabular data
+import pandas as pd
 
+
+timezone = 'Canada/Eastern'
+tz_obj = pytz.timezone(timezone)
+
+# fuel types used by IESO
 MAP_GENERATION = {
     'BIOFUEL': 'biomass',
     'GAS': 'gas',
@@ -23,21 +34,26 @@ MAP_GENERATION = {
     'WIND': 'wind'
 }
 
-timezone = 'Canada/Eastern'
-tz_obj = pytz.timezone(timezone)
+PRODUCTION_URL = 'http://reports.ieso.ca/public/GenOutputCapability/PUB_GenOutputCapability_{YYYYMMDD}.xml'
+
+XML_NS_TEXT = '{http://www.theIMO.com/schema}'
 
 
-def fetch_production(zone_key='CA-ON', session=None, target_datetime=None, logger=None):
-    """Requests the last known production mix (in MW) of a given country
+def fetch_production(zone_key='CA-ON', session=None, target_datetime=None,
+                     logger=logging.getLogger(__name__)):
+    """Requests the last known production mix (in MW) of a given region
 
     Arguments:
-    zone_key (optional) -- used in case a parser is able to fetch multiple countries
-    session (optional)      -- request session passed in order to re-use an existing session
+    zone_key (optional): ignored here, only information for CA-ON is returned
+    session (optional): request session passed in order to re-use an existing session
+    target_datetime: the datetime for which we want production data. If not provided, we should
+      default it to now. The provided target_datetime is timezone-aware in UTC.
+    logger: an instance of a `logging.Logger`; all raised exceptions are also logged automatically
 
     Return:
-    A dictionary in the form:
+    A list of dictionaries in the form:
     {
-      'zoneKey': 'FR',
+      'zoneKey': 'CA-ON',
       'datetime': '2017-01-01T00:00:00Z',
       'production': {
           'biomass': 0.0,
@@ -57,39 +73,71 @@ def fetch_production(zone_key='CA-ON', session=None, target_datetime=None, logge
       'source': 'mysource.com'
     }
     """
-    if target_datetime:
-        raise NotImplementedError('This parser is not yet able to parse past dates')
-    
+
+    dt = arrow.get(target_datetime).to(tz_obj).replace(hour=0, minute=0, second=0, microsecond=0)
+    filename = dt.format('YYYYMMDD')
+
     r = session or requests.session()
-    url = 'http://www.ieso.ca/-/media/files/ieso/uploaded/chart/generation_fuel_type_multiday.xml?la=en'
+    url = PRODUCTION_URL.format(YYYYMMDD=filename)
     response = r.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
 
-    data = {}
+    if not response.ok:
+        # Data is generally available for past 3 months. Requesting files older than this
+        # returns an HTTP 404 error.
+        logger.info('CA-ON: failed getting requested data for datetime {} from IESO server'.format(dt))
+        return []
 
-    start_datetime = arrow.get(
-        arrow.get(soup.find_all('startdate')[0].contents[0]).datetime, timezone)
+    xml = ET.fromstring(response.text)
 
-    # Iterate over all datasets (production types)
-    for item in soup.find_all('dataset'):
-        key = item.attrs['series']
-        for rowIndex, row in enumerate(item.find_all('value')):
-            if not len(row.contents):
-                continue
-            if rowIndex not in data:
-                data[rowIndex] = {
-                    'datetime': start_datetime.replace(hours=+rowIndex).datetime,
-                    'zoneKey': zone_key,
-                    'production': {
-                        'coal': 0
-                    },
-                    'storage': {},
-                    'source': 'ieso.ca',
-                }
-            data[rowIndex]['production'][MAP_GENERATION[key]] = \
-                float(row.contents[0])
+    generators = xml\
+        .find(XML_NS_TEXT + 'IMODocBody')\
+        .find(XML_NS_TEXT + 'Generators')\
+        .findall(XML_NS_TEXT + 'Generator')
 
-    return [data[k] for k in sorted(data.keys())]
+    def production_or_zero(output):
+        # Sometimes the XML data has no "EnergyMW" tag for a given plant at a given hour.
+        # The report XSL formats this as "N/A" - we return 0
+        tag = output.find(XML_NS_TEXT + 'EnergyMW')
+        if tag is not None:
+            return tag.text
+        else:
+            return 0
+
+    # flat iterable of per-generator-plant productions per time from the XML data
+    all_productions = (
+        {
+            'name': generator.find(XML_NS_TEXT + 'GeneratorName').text,
+            'fuel': MAP_GENERATION[
+                generator.find(XML_NS_TEXT + 'FuelType').text
+            ],
+            'dt': dt.replace(hours=+int(
+                output.find(XML_NS_TEXT + 'Hour').text
+            )).datetime,
+            'production': float(production_or_zero(output))
+        }
+        for generator in generators
+        for output in generator.find(XML_NS_TEXT + 'Outputs').findall(XML_NS_TEXT + 'Output')
+    )
+
+    df = pd.DataFrame(all_productions)
+
+    by_fuel = df.groupby(['dt', 'fuel']).sum().unstack()
+    # to debug, you can `print(by_fuel)` here, which gives a very pretty table
+
+    by_fuel_dict = by_fuel['production'].to_dict('index')
+
+    data = [
+        {
+            'datetime': time,
+            'zoneKey': zone_key,
+            'production': productions,
+            'storage': {},
+            'source': 'ieso.ca',
+        }
+        for time, productions in by_fuel_dict.items()
+    ]
+
+    return data
 
 
 def fetch_price(zone_key='CA-ON', session=None, target_datetime=None, logger=None):
@@ -145,7 +193,8 @@ def fetch_price(zone_key='CA-ON', session=None, target_datetime=None, logger=Non
     return data
 
 
-def fetch_exchange(zone_key1, zone_key2, session=None, target_datetime=None, logger=None):
+def fetch_exchange(zone_key1, zone_key2, session=None, target_datetime=None,
+                   logger=logging.getLogger(__name__)):
     """Requests the last known power exchange (in MW) between two countries
 
     Arguments:
@@ -251,14 +300,28 @@ def fetch_exchange(zone_key1, zone_key2, session=None, target_datetime=None, log
 if __name__ == '__main__':
     """Main method, never used by the Electricity Map backend, but handy for testing."""
 
+    now = arrow.utcnow()
+
     print('fetch_production() ->')
     print(fetch_production())
+
+    print('we expect correct results when time in UTC and Ontario differs')
+    print('data should be for {}'.format(now.replace(hour=2).to(tz_obj).format('YYYY-MM-DD')))
+    print('fetch_production("CA-ON", target_datetime=now.replace(hour=2)) ->')
+    print(fetch_production("CA-ON", target_datetime=now.replace(hour=2)))
+
+    print('we expect results for 2 months ago')
+    print('fetch_production(target_datetime=now.shift(months=-2).datetime)) ->')
+    print(fetch_production(target_datetime=now.shift(months=-2).datetime))
+
+    print('there are likely no results for 2 years ago')
+    print('fetch_production(target_datetime=now.shift(years=-2).datetime)) ->')
+    print(fetch_production(target_datetime=now.shift(years=-2).datetime))
+
     print('fetch_price() ->')
     print(fetch_price())
     print('fetch_exchange("CA-ON", "US-NY") ->')
     print(fetch_exchange("CA-ON", "US-NY"))
-
-    now = arrow.utcnow()
 
     print('fetch_exchange("CA-ON", "CA-QC", target_datetime=now.datetime)) ->')
     print(fetch_exchange("CA-ON", "CA-QC", target_datetime=now.datetime))
