04dd4d6c85bf3e84f2f1892402092b9fafa3a3a8
systemcatch <30196510+systemcatch@users.noreply.github.com>
2019-03-02 17:09:31 +0000

    Create basic fetch_wind_forecast function

--- parsers/US_MISO.py
+++ parsers/US_MISO.py
@@ -14,6 +14,7 @@ mapping = {'Coal': 'coal',
            'Wind': 'wind',
            'Other': 'unknown'}
 
+wind_forecast_url = 'https://api.misoenergy.org/MISORTWDDataBroker/DataBrokerServices.asmx?messageType=getWindForecast&returnType=json'
 
 # To quote the MISO data source;
 # "The category listed as “Other” is the combination of Hydro, Pumped Storage Hydro, Diesel, Demand Response Resources,
@@ -113,6 +114,27 @@ def fetch_production(zone_key='US-MISO', session=None, target_datetime=None, log
     return data
 
 
+def fetch_wind_forecast(zone_key='US-MISO', session=None, target_datetime=None, logger=None):
+    req = requests.get(wind_forecast_url)
+    raw_json = req.json()
+    raw_data = raw_json['Forecast']
+
+    data = []
+    for item in raw_data:
+        dt = parser.parse(item['DateTimeEST']).replace(tzinfo=tz.gettz('America/New_York'))
+        value = float(item['Value'])
+
+        datapoint = {'datetime': dt,
+                     'value': value,
+                     'source': 'misoenergy.org',
+                     'zoneKey': zone_key}
+        data.append(datapoint)
+
+    return data
+
+
 if __name__ == '__main__':
     print('fetch_production() ->')
     print(fetch_production())
+    print('fetch_wind_forecast() ->')
+    #print(fetch_wind_forecast())
