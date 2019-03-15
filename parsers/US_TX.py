638033af20d88bb2c184659d5bf3bab7ed6b3eb4
Tyler Busby <TylerBusby333@gmail.com>
2019-03-15 11:39:59 -0500

    add unknown production calculation and capitalize constants

--- parsers/US_TX.py
+++ parsers/US_TX.py
@@ -13,10 +13,21 @@ from lxml import html
 
 from parsers.lib.exceptions import ParserException
 
-solar_realtime_directory_url = 'http://mis.ercot.com/misapp/GetReports.do?reportTypeId=13484'
-wind_realtime_directory_url = 'http://mis.ercot.com/misapp/GetReports.do?reportTypeId=13071'
-realtime_demand_url = 'http://www.ercot.com/content/cdr/html/real_time_system_conditions.html'
-base_zip_url = 'http://mis.ercot.com/'
+# This xpath gets the second cell in a row which has a cell that contains parameterized text
+REAL_TIME_DATA_XPATH = "//tr[td[contains(text(),'{}')]]/td[2]/text()"
+
+SOLAR_REAL_TIME_DIRECTORY_URL = 'http://mis.ercot.com/misapp/GetReports.do?reportTypeId=13484'
+WIND_REAL_TIME_DIRECTORY_URL = 'http://mis.ercot.com/misapp/GetReports.do?reportTypeId=13071'
+REAL_TIME_URL = 'http://www.ercot.com/content/cdr/html/real_time_system_conditions.html'
+BASE_ZIP_URL = 'http://mis.ercot.com/'
+
+ELECTRICAL_TIES = [
+    "DC_E",
+    "DC_L",
+    "DC_N",
+    "DC_R",
+    "DC_S"
+]
 
 
 def get_zipped_csv_data(logger, directory_url, session=None):
@@ -28,7 +39,7 @@ def get_zipped_csv_data(logger, directory_url, session=None):
         raise ParserException('US-TX', 'Response code: {0}'.format(response.status_code))
     html_tree = html.fromstring(response.content)
     # This xpath gets the first row to contain 'csv' and then the zip link
-    most_recent_csv_zip_url = base_zip_url + html_tree.xpath("//tr[td[contains(text(),'csv')]]/td/div/a/@href")[0]
+    most_recent_csv_zip_url = BASE_ZIP_URL + html_tree.xpath("//tr[td[contains(text(),'csv')]]/td/div/a/@href")[0]
 
     response = s.get(most_recent_csv_zip_url)
     if response.status_code != 200 or not response.content:
@@ -40,15 +51,22 @@ def get_zipped_csv_data(logger, directory_url, session=None):
     return arrow.get(arrow.get(row[0], 'MM/DD/YYYY HH:mm').datetime, 'US/Central').datetime, float(row[1])
 
 
-def get_demand_data(logger, session=None):
+def get_realtime_data(logger, session=None):
     s = session or requests.session()
-    response = s.get(realtime_demand_url)
+    response = s.get(REAL_TIME_URL)
     if response.status_code != 200 or not response.content:
         raise ParserException('US-TX', 'Response code: {0}'.format(response.status_code))
     html_tree = html.fromstring(response.content)
-    demand = float(html_tree.xpath("//tr[td[contains(text(),'Actual System Demand')]]/td[2]/text()")[0])
+
+    demand = float(html_tree.xpath(REAL_TIME_DATA_XPATH.format("Actual System Demand"))[0])
+
+    tie_dict = {}
+    for tie in ELECTRICAL_TIES:
+        tie_dict[tie] = float(html_tree.xpath(REAL_TIME_DATA_XPATH.format(tie))[0])
+
+    # This xpath gets the text from the timestamp
     date_string = str(html_tree.xpath("//div[contains(@class,'schedTime')]/text()")[0]).replace("Last Updated: ", "")
-    return arrow.get(arrow.get(date_string, 'MMM DD, YYYY HH:mm:ss').datetime, 'US/Central').datetime, demand
+    return arrow.get(arrow.get(date_string, 'MMM DD, YYYY HH:mm:ss').datetime, 'US/Central').datetime, demand, tie_dict
 
 
 def fetch_production(zone_key='US-TX', session=None, target_datetime=None, logger=logging.getLogger(__name__)):
@@ -86,22 +104,25 @@ def fetch_production(zone_key='US-TX', session=None, target_datetime=None, logge
     if target_datetime:
         raise NotImplementedError('This parser is not yet able to parse past dates')
 
-    wind_datetime, wind = get_zipped_csv_data(logger, wind_realtime_directory_url, session=session)
-    solar_datetime, solar = get_zipped_csv_data(logger, solar_realtime_directory_url, session=session)
+    wind_datetime, wind = get_zipped_csv_data(logger, WIND_REAL_TIME_DIRECTORY_URL, session=session)
+    solar_datetime, solar = get_zipped_csv_data(logger, SOLAR_REAL_TIME_DIRECTORY_URL, session=session)
 
     wind_solar_timedelta = (wind_datetime - solar_datetime).total_seconds() / 60
     if abs(wind_solar_timedelta) > 4:  # in case one was grabbed before the other was posted
         if wind_solar_timedelta > 0:  # if solar came earlier, poll it again
-            solar_datetime, solar = get_zipped_csv_data(logger, solar_realtime_directory_url, session=session)
+            solar_datetime, solar = get_zipped_csv_data(logger, SOLAR_REAL_TIME_DIRECTORY_URL, session=session)
         else:  # if wind came earlier poll it again
-            wind_datetime, wind = get_zipped_csv_data(logger, wind_realtime_directory_url, session=session)
+            wind_datetime, wind = get_zipped_csv_data(logger, WIND_REAL_TIME_DIRECTORY_URL, session=session)
+
+    realtime_datetime, demand, ties = get_realtime_data(logger, session=session)
 
     data = {
         'zoneKey': zone_key,
         'datetime': wind_datetime,
         'production': {
             'solar': solar,
-            'wind': wind
+            'wind': wind,
+            'unknown': demand - solar - wind - sum(ties.values())
         },
         'storage': {},
         'source': 'ercot.com'
@@ -114,11 +135,11 @@ def fetch_consumption(zone_key='US-TX', session=None, target_datetime=None,
                       logger=logging.getLogger(__name__)):
     """Gets consumption for a specified zone, returns a dictionary."""
 
-    demand_datetime, demand = get_demand_data(logger, session=session)
+    realtime_datetime, demand, ties = get_realtime_data(logger, session=session)
 
     data = {
         'zoneKey': zone_key,
-        'datetime': demand_datetime,
+        'datetime': realtime_datetime,
         'consumption': demand,
         'source': 'ercot.eu'
     }
