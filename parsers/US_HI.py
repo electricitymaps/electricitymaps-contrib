90c51c588134f8624940a9bdf12f5a1af0c19574
kepiej <kepiej@users.noreply.github.com>
2019-07-31 10:10:46 +0200

    Update zone id to US-HI-OA + small fixes

--- parsers/US_HI.py
+++ parsers/US_HI.py
@@ -11,7 +11,7 @@ import requests
 # requirement is to limit your line length to 79 characters.
 
 
-def fetch_production(zone_key='US-HI', session=None,
+def fetch_production(zone_key='US-HI-OA', session=None,
                      target_datetime: datetime.datetime = None,
                      logger: logging.Logger = logging.getLogger(__name__)):
     """Requests the last known production mix (in MW) of a given country
@@ -64,109 +64,41 @@ def fetch_production(zone_key='US-HI', session=None,
     """
     r = session or requests.session()
     if target_datetime is None:
+        url_date = arrow.get()
         url = 'https://www.islandpulse.org/api/mix?limit=1'
     else:
         # WHEN HISTORICAL DATA IS AVAILABLE
         # convert target datetime to local datetime
-        # url_date = arrow.get(target_datetime).to("HST")
-        # url = 'https://www.islandpulse.org/api/mix?date={}'.format(
-        #     url_date)
+        #url_date = arrow.get(target_datetime).to("Pacific/Honolulu")
+        #url = 'https://www.islandpulse.org/api/mix?date={}'.format(url_date.date())
 
         # WHEN HISTORICAL DATA IS NOT AVAILABLE
         raise NotImplementedError(
             'This parser is not yet able to parse past dates')
 
     res = r.get(url)
-    assert res.status_code == 200, 'Exception when fetching production for ' \
-                                   '{}: error when calling url={}'.format(
-                                       zone_key, url)
 
     obj = res.json()
+	raw_data = obj[0]
 
     data = {
         'zoneKey': zone_key,
         'production': {
-          'biomass': float(obj[0]['Waste2Energy'] + obj[0]['BioFuel']),
-          'coal': float(obj[0]['Coal']),
-          'oil': float(obj[0]['Fossil_Fuel']),
-          'solar': float(obj[0]['Solar']),
-          'wind': float(obj[0]['WindFarm'])
+          'biomass': float(raw_data['Waste2Energy'] + raw_data['BioFuel']),
+          'coal': float(raw_data['Coal']),
+          'oil': float(raw_data['Fossil_Fuel']),
+          'solar': float(raw_data['Solar']),
+          'wind': float(raw_data['WindFarm'])
           },
         'storage': {},
         'source': 'islandpulse.org',
     }
 
     # Parse the datetime and return a python datetime object
-    data['datetime'] = arrow.get(obj[0]['dateTime'], 'YYYY-MM-DDTHH:mm:ss').to(tz="Pacific/Honolulu").datetime
+    data['datetime'] = arrow.get(raw_data['dateTime'], 'YYYY-MM-DDTHH:mm:ss').to(tz="Pacific/Honolulu").datetime
 
     return data
-
-
-# def fetch_price(zone_key='US-HI', session=None, target_datetime=None,
-#                 logger=logging.getLogger(__name__)):
-#     """Requests the last known power price of a given country
-
-#     Arguments:
-#     ----------
-#     zone_key: used in case a parser is able to fetch multiple countries
-#     session: request session passed in order to re-use an existing session
-#     target_datetime: the datetime for which we want production data. If not
-#       provided, we should default it to now. If past data is not available,
-#       raise a NotImplementedError. Beware that the provided target_datetime is
-#       UTC. To convert to local timezone, you can use
-#       `target_datetime = arrow.get(target_datetime).to('America/New_York')`.
-#       Note that `arrow.get(None)` returns UTC now.
-#     logger: an instance of a `logging.Logger` that will be passed by the
-#       backend. Information logged will be publicly available so that correct
-#       execution of the logger can be checked. All Exceptions will automatically
-#       be logged, so when something's wrong, simply raise an Exception (with an
-#       explicit text). Use `logger.warning` or `logger.info` for information
-#       that can useful to check if the parser is working correctly. A default
-#       logger is used so that logger output can be seen when coding / debugging.
-
-#     Returns:
-#     --------
-#     If no data can be fetched, any falsy value (None, [], False) will be
-#       ignored by the backend. If there is no data because the source may have
-#       changed or is not available, raise an Exception.
-
-#     A dictionary in the form:
-#     {
-#       'zoneKey': 'FR',
-#       'currency': EUR,
-#       'datetime': '2017-01-01T00:00:00Z',
-#       'price': 0.0,
-#       'source': 'mysource.com'
-#     }
-#     """
-#     if target_datetime:
-#         raise NotImplementedError(
-#             'This parser is not yet able to parse past dates')
-
-#     r = session or requests.session()
-#     assert r.status_code == 200
-#     url = 'https://api.someservice.com/v1/price/latest'
-
-#     response = r.get(url)
-#     obj = response.json()
-
-#     data = {
-#         'zoneKey': zone_key,
-#         'currency': 'EUR',
-#         'price': obj['price'],
-#         'source': 'someservice.com',
-#     }
-
-#     # Parse the datetime and return a python datetime object
-#     data['datetime'] = arrow.get(obj['datetime']).datetime
-
-#     return data
 
 if __name__ == '__main__':
     """Main method, never used by the Electricity Map backend, but handy
     for testing."""
-
-    print('fetch_production() ->')
-    print(fetch_production())
-    #print('fetch_price() ->')
-    #print(fetch_price())
