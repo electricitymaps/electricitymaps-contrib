05ad02280efacf8683d02326b7de7e035c2d946f
Paul Cornelissen <pcornelissen@gmail.com>
2019-09-17 14:53:50 +0200

    Add production and consumption parsers for Croatia
    Configured zones.json to use ENTSOE parsers for consumption and consumptionForecast
    Wrote parser for HOPS json output according to issue 1950
    Added ENTSOE capacity figures to zones.json

--- parsers/HOPS.py
+++ parsers/HOPS.py
@@ -1,87 +1,49 @@
 #!/usr/bin/env python3
 
-"""Parser for Croatian exchanges."""
+"""Parser for power production in Croatia"""
 
 import arrow
-from pprint import pprint
 import requests
-import xml.etree.ElementTree as ET
+import logging
+import pandas as pd
 
-xml_link = 'https://www.hops.hr/resources/razmjena.xml'
+url = "https://www.hops.hr/Home/PowerExchange"
 
+def fetch_production(zone_key='HR', session=None, target_datetime=None,
+                     logger=logging.getLogger(__name__)):
 
-def get_xml_data(session=None):
-    """Returns a request response body as bytes."""
+    r = session or requests.session()
+    response = r.get(url)
+    obj = response.json()
 
-    s = session or requests.Session()
-    req = s.get(xml_link)
+    # We assume the timezone of the time returned is local time and convert to UTC
+    date_time = arrow.get(obj['updateTime']).replace(tzinfo='Europe/Belgrade').to('utc')
 
-    return req.content
+    # The json returned contains a list of values
+    # 0 - 9 are individual exchanges with neighbouring countries
+    # 10 - 13 are cumulative exchanges with neigbouring countries
+    # 14 grid frequency
+    # 15 total load of Croatia
+    # 16 'Ukupna proizvodnja'  total generation
+    # 17 'Proizvodnja VE'      wind generation
+    df = pd.DataFrame.from_dict(obj['resources']).set_index('sourceName')
 
+    # Get the wind power generation
+    wind = df['value'].loc['Proizvodnja VE']
 
-def xml_processor(raw_xml):
-    """Returns a tuple containing a list of dictionaries and a arrow object."""
+    # Get the total power generation and substract wind power
+    unknown = df['value'].loc['Ukupna proizvodnja'] - wind
 
-    xml_full = ET.fromstring(raw_xml)
-
-    timestamp = xml_full.attrib['updateTime']
-    dt_naive = arrow.get(timestamp, 'YYYY-MM-DD HH:mm:ss')
-    dt_aware = dt_naive.replace(tzinfo='Europe/Belgrade')
-
-    xml_values = []
-    for child in xml_full:
-        val = child.attrib
-        xml_values.append(val)
-
-    exchange_keys = ('Mađarska', 'Slovenija', 'Bosna i Hercegovina', 'Srbija i Crna Gora')
-    exchange_data = [item for item in xml_values if item['key'] in exchange_keys]
-
-    return exchange_data, dt_aware
-
-
-def fetch_exchange(zone_key1, zone_key2, session=None, logger=None):
-    """Requests the last known power exchange (in MW) between two countries
-    Arguments:
-    zone_key (optional) -- used in case a parser is able to fetch multiple countries
-    session (optional)      -- request session passed in order to re-use an existing session
-    Return:
-    A dictionary in the form:
-    {
-      'sortedZoneKeys': 'DK->NO',
-      'datetime': '2017-01-01T00:00:00Z',
-      'netFlow': 0.0,
-      'source': 'mysource.com'
-    }
-    """
-
-    # HOPS assigns negative values to exports.
-    gxd = get_xml_data(session=None)
-    processed_data = xml_processor(gxd)
-
-    sorted_zone_keys = "->".join(sorted([zone_key1, zone_key2]))
-
-    if sorted_zone_keys == 'BA->HR':
-        ba_exchange = next(
-            item for item in processed_data[0] if item['key'] == "Bosna i Hercegovina")
-        net_flow = float(ba_exchange['value'])
-    elif sorted_zone_keys == 'HR->SI':
-        si_exchange = next(item for item in processed_data[0] if item['key'] == "Slovenija")
-        net_flow = -1 * float(si_exchange['value'])
-    else:
-        raise NotImplementedError('This exchange pair is not implemented')
-
-    exchange = {
-        'sortedZoneKeys': sorted_zone_keys,
-        'datetime': processed_data[1].datetime,
-        'netFlow': net_flow,
-        'source': 'hops.hr'
-    }
-
-    return exchange
-
+    return  [{
+                'zoneKey': 'HR',
+                'datetime': date_time.datetime,
+                'production': {
+                    'wind' : wind,
+                    'unknown' : unknown
+                },
+                'source': 'hops.hr'
+            }]
 
 if __name__ == '__main__':
-    print('fetch_exchange(BA, HR)->')
-    print(fetch_exchange('BA', 'HR'))
-    print('fetch_exchange(HR, SI)->')
-    print(fetch_exchange('HR', 'SI'))
+    print('fetch_oroduction(HR)->')
+    print(fetch_production('HR'))
