cc7748c8f4cc4db16e8f347b439dc2d46ee34708
Johannes Vass <johannes@familie-vass.at>
2019-10-09 18:03:34 +0200

    Implement the expected "interface" for an exchange parser

--- parsers/AT.py
+++ parsers/AT.py
@@ -7,7 +7,7 @@ import datetime
 import time
 
 from io import StringIO
-
+from dateutil import tz
 
 APG_API_BASE="https://transparency.apg.at/transparency-api/api/v1"
 EXCHANGE_API="{}/Download/CBPF/German/M15".format(APG_API_BASE)
@@ -23,20 +23,69 @@ COL_CH_AT="CH>AT Leistung [MW]"
 COL_DE_AT="DE>AT Leistung [MW]"
 EXCHANGE_COLS = [COL_CZ_AT, COL_HU_AT, COL_SI_AT, COL_IT_AT, COL_CH_AT, COL_DE_AT]
 
+# for each interconnector a function which retrieves the desired value from a dict or Series
+# when the direction changes, we need to multiply by -1
+EXCHANGES = {"AT->CH": lambda d: -1 * d[COL_CH_AT],
+             "AT->CZ": lambda d: -1 * d[COL_CZ_AT],
+             "AT->DE": lambda d: -1 * d[COL_DE_AT],
+             "AT->IT": lambda d: -1 * d[COL_IT_AT],
+             "AT->HU": lambda d: -1 * d[COL_HU_AT],
+             "AT->SI": lambda d: -1 * d[COL_SI_AT],}
+
 
 def format_date(date):
+    """
+    Format a date as expected by the APG api
+    Return: the date object as formatted string
+    """
     return "{}T000000".format(date.isoformat())
 
 
-def fetch_exchange_data(date=None):
-    if date is None:
+def fetch_exchange(zone_key1, zone_key2, session=None, target_datetime=None, logger=None):
+    if target_datetime is None:
         date = datetime.date.today()
+    else:
+        date = target_datetime.date
 
+    # TODO do we really need to fetch the document for each and every interconnector separately?
+    exchange_data_of_day = fetch_exchanges_for_date(date, session=session)
+
+    if target_datetime is None:
+        # last row which is not NaN
+        best_row = exchange_data_of_day.dropna().iloc[-1]
+    else:
+        # TODO also here: can we cache the results somehow?
+        raise NotImplementedError("Selecting a specific time in the past is not yet implemented")
+
+    exchange = '->'.join(sorted([zone_key1, zone_key2]))
+    if exchange not in EXCHANGES:
+        raise ValueError("The requested exchange {} is not supported by the AT parser".format(exchange))
+
+
+    flow = EXCHANGES[exchange](best_row)        
+    end_time = arrow.get(best_row[COL_TO], tz.gettz('Europe/Vienna'))
+    exchange = {'sortedZoneKeys': exchange,
+                'datetime': end_time.datetime,
+                'netFlow': flow,
+                'source': 'transparency.apg.at'}
+
+    return exchange
+
+
+def fetch_exchanges_for_date(date, session=None):
+    """
+    Fetches all Austrian exchanges for a given date.
+    Return: the exchange values of the requested day as a pandas DataFrame 
+    """
+    # build the request url
     from_date = format_date(date)
     to_date = format_date(date + datetime.timedelta(days=1))
-
     export_request_url = "{}/{}/{}".format(EXCHANGE_API, from_date, to_date)
-    resp = requests.get(export_request_url)
+
+    s = session or requests.Session()
+
+    # perform the request which creates the export file
+    resp = s.get(export_request_url)
     resp.raise_for_status()
 
     try:
@@ -46,8 +95,9 @@ def fetch_exchange_data(date=None):
     except:
         raise ValueError("The response to the request for generating the csv file has an unexpected format.")
 
+    # fetch the csv file which was created by the previous request
     csv_url = "{}/{}/{}".format(export_request_url, cache_id, filename)
-    csv_response = requests.get(csv_url)
+    csv_response = s.get(csv_url)
     csv_response.raise_for_status()
 
     # override encoding by real educated guess as provided by chardet
@@ -59,9 +109,9 @@ def fetch_exchange_data(date=None):
     except:
         raise ValueError("The downloaded csv file has an unexpected format")
 
-    # parse float values
+    # parse float values (AT locale uses ',' instead of '.')
     csv_data[EXCHANGE_COLS] = csv_data[EXCHANGE_COLS].replace(to_replace=",", value='.', regex=True).apply(pd.to_numeric)
-        
+
     return csv_data
 
 
@@ -74,7 +124,7 @@ def check_delay():
             time.sleep(sleep_time)
 
         try:
-            exchange_data = fetch_exchange_data()
+            exchange_data = fetch_exchanges_for_date(datetime.date.today())
 
             last_row = exchange_data.dropna().iloc[-1]
             end_date = last_row[COL_TO]
@@ -97,13 +147,9 @@ STORAGE = {}
 STORAGE_FILE = "delay_storage.json"
 
 
-def main():
-    if os.path.isfile(STORAGE_FILE):
-        with open(STORAGE_FILE) as f:
-            STORAGE = json.load(f) 
-
-    check_delay()
-
 
 if __name__ == "__main__":
-    main()
+    print("Fetching exchange AT->DE")
+    print(fetch_exchange("AT", "DE"))
+    print("Fetching exchange AT->CH")
+    print(fetch_exchange("AT", "CH"))
