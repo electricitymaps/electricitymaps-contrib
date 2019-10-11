dc0f7a5e87dc53cb54fe991b020cc5af65758436
@amrit1207! <jainamrit233@gmail.com>
2019-10-11 10:55:30 +0530

    changes made in server file

--- mockserver/server.py
+++ mockserver/server.py
@@ -4,12 +4,12 @@
 
 from http.server import SimpleHTTPRequestHandler, HTTPServer, test
 
-class CORSRequestHandler(SimpleHTTPRequestHandler):
-    def do_OPTIONS(self):
+class CORSRequest(SimpleHTTPRequestHandler):
+    def doop(self):
         self.send_response(200, 'OK')
         self.end_headers()
 
-    def end_headers(self):
+    def endheaders(self):
         self.send_header('Access-Control-Allow-Origin', '*')
         self.send_header('Access-Control-Allow-Headers', 'x-request-timestamp, x-signature, electricitymap-token')
         self.send_header('Cache-Control', 'no-cache, no-store, must-revalidate')
@@ -18,4 +18,4 @@ class CORSRequestHandler(SimpleHTTPRequestHandler):
         SimpleHTTPRequestHandler.end_headers(self)
 
 if __name__ == '__main__':
-    test(CORSRequestHandler, HTTPServer)
+    test(CORSRequest, HTTPServer)
