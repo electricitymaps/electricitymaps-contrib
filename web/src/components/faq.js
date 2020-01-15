06b01c760d892cafb22b0a1e097dd1e64fb55f72
Filip Barl <filip.barl@gmail.com>
2020-01-15 18:27:27 +0100

    Migrated FAQ component to React

--- web/src/components/faq.js
+++ web/src/components/faq.js
@@ -1,12 +1,12 @@
+import React, { useState } from 'react';
+
+import { __ } from '../helpers/translation';
+import { co2Sub } from '../helpers/formatting';
+
 const d3 = Object.assign(
   {},
   require('d3-selection'),
 );
-
-const translation = require('../helpers/translation');
-const formatting = require('../helpers/formatting');
-
-const faq = {};
 
 const orderings = [
   {
@@ -74,66 +74,66 @@ const orderings = [
   },
 ];
 
-orderings.forEach((e) => {
-  faq[e.groupKey] = {
-    groupName: translation.translate(`${e.groupKey}.groupName`),
-    entries: {},
+const FAQ = ({ className }) => {
+  const [answersVisible, setAnswersVisible] = useState({});
+
+  const toggleQuestion = (entryKey, linkToEntryKey) => {
+    setAnswersVisible(Object.assign({}, answersVisible, {
+      [entryKey]: !answersVisible[entryKey],
+    }));
+    if (linkToEntryKey) {
+      setTimeout(() => {
+        d3.selectAll(`#${entryKey}`).selectAll('.entry-link').on('click', () => {
+          setTimeout(() => {
+            setAnswersVisible(Object.assign({}, answersVisible, {
+              [entryKey]: true,
+              [linkToEntryKey]: true,
+            }));
+          }, 50);
+        });
+      }, 50);
+    }
   };
-  e.entryOrder.forEach((f) => {
-    faq[e.groupKey].entries[f] = {
-      question: translation.translate(`${e.groupKey}.${f}-question`),
-      answer: translation.translate(`${e.groupKey}.${f}-answer`),
-    };
-  });
-});
 
-export default class FAQ {
-  constructor(selectorId) {
-    this.selector = d3.select(selectorId);
-    this._setup();
-  }
 
-  _setup() {
-    this.container = this.selector.append('div').attr('class', 'faq-container');
-    this.domElements = [];
-    orderings.forEach((questionGroup) => {
-      this._createQuestionGroup(questionGroup.groupKey, questionGroup.entryOrder, questionGroup.entryLinks);
-    });
-  }
+  return (
+    <div className={className}>
+      <div className="faq-container">
+        {orderings.map(({ groupKey, entryOrder, entryLinks }) => (
+          <div className="question-group-container" key={groupKey}>
+            <div className="question-group-header title">
+              {__(`${groupKey}.groupName`)}
+            </div>
+            {entryOrder.map((entryKey) => {
+              const answerVisible = answersVisible[entryKey];
+              const linkToEntryKey = entryLinks[entryKey];
+              return (
+                <div className="question-answer-container" id={entryKey} key={entryKey}>
+                  <div
+                    className="question"
+                    onClick={() => toggleQuestion(entryKey, linkToEntryKey)}
+                  >
+                    <i className="material-icons">
+                      {answerVisible ? 'expand_less' : 'expand_more'}
+                    </i>
+                    <span>{__(`${groupKey}.${entryKey}-question`)}</span>
+                  </div>
+                  {answerVisible && (
+                    <div
+                      className="answer"
+                      dangerouslySetInnerHTML={{
+                        __html: co2Sub(__(`${groupKey}.${entryKey}-answer`))
+                      }}
+                    />
+                  )}
+                </div>
+              );
+            })}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-  _createQuestionGroup(groupKey, groupEntryOrder, groupEntryLinks) {
-    const groupContainer = this.container.append('div').attr('class', 'question-group-container');
-    const groupHeader = groupContainer.append('div').attr('class', 'question-group-header title');
-    groupHeader.text(faq[groupKey].groupName);
-
-    groupEntryOrder.forEach((entryKey) => {
-      const questionEntryTexts = faq[groupKey].entries[entryKey];
-      const answerLinkTargetEntryId = groupEntryLinks && groupEntryLinks[entryKey];
-      this._createQuestionAndAnswer(groupContainer, questionEntryTexts, entryKey, answerLinkTargetEntryId);
-    });
-  }
-
-  _createQuestionAndAnswer(groupContainer, questionEntryTexts, entryKey, answerLinkTargetEntryId) {
-    const entryContainer = groupContainer.append('div').attr('class', 'question-answer-container').attr('id', entryKey);
-    const questionContainer = entryContainer.append('div').attr('class', 'question');
-    questionContainer.append('i').attr('class', 'material-icons').text('expand_more');
-    questionContainer.append('span').text(questionEntryTexts.question);
-    const answer = entryContainer.append('div').attr('class', 'answer').html(formatting.co2Sub(questionEntryTexts.answer));
-    if (answerLinkTargetEntryId) {
-      answer.selectAll('.entry-link').on('click', () => {
-        this._toggleAnswer(answerLinkTargetEntryId);
-      });
-    }
-    questionContainer.on('click', () => this._toggleAnswer(entryKey));
-  }
-
-  _toggleAnswer(id) {
-    const entry = d3.selectAll(`#${id}`);
-    if (entry.nodes().length) {
-      const answer = entry.selectAll('.answer');
-      const answerVisible = !answer.classed('visible');
-      answer.classed('visible', answerVisible);
-      entry.selectAll('.material-icons').text(answerVisible ? 'expand_less' : 'expand_more');
-    }
-  }
-}
+export default FAQ;
