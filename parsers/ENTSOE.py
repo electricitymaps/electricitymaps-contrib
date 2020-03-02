44039a2e9d9c8aa34ad2ae684b0dc5bfb96e5ab2
Roberta Hunt <rehunt@ualberta.ca>
2020-03-02 17:09:44 +0100

    Added possibility to use second token

--- parsers/ENTSOE.py
+++ parsers/ENTSOE.py
@@ -407,7 +407,13 @@ def query_ENTSOE(session, params, target_datetime=None, span=(-48, 24)):
     params['periodEnd'] = target_datetime.shift(hours=span[1]).format('YYYYMMDDHH00')
     if 'ENTSOE_TOKEN' not in os.environ:
         raise Exception('No ENTSOE_TOKEN found! Please add it into secrets.env!')
-    params['securityToken'] = os.environ['ENTSOE_TOKEN']
+    tokens = [os.environ['ENTSOE_TOKEN']]
+
+    # Due to rate limiting, we need to spread our requests across two different tokens
+    if 'ENTSOE_TOKEN_2' in os.environ:
+        tokens += [os.environ['ENTSOE_TOKEN_2']]
+    params['securityToken'] = np.random.choice(tokens)
+    
     return session.get(ENTSOE_ENDPOINT, params=params)
 
 
