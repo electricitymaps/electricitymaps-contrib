de52c22d41e58a726c79eb864eaa0a0c3aa42ac9
Caitlin White <caitlinwhite@gmail.com>
2020-05-26 16:46:34 +1000

    New mock data for hrote.json
    - Update test assertions to match the new data
    - Update date stamps in tests and other mock data files to match

--- parsers/test/test_HOPS.py
+++ parsers/test/test_HOPS.py
@@ -22,7 +22,7 @@ class TestHOPS(unittest.TestCase):
                                   content=response_text)
         response_text = resource_string("parsers.test.mocks", "hrote.json")
         self.adapter.register_uri("GET", "https://files.hrote.hr/files/EKO_BG/FORECAST/SOLAR/FTP/TEST_DRIVE/"
-                                         "3.11.2019.json", content=response_text)
+                                         "21.5.2020.json", content=response_text)
 
         data = HOPS.fetch_production('HR', self.session)
         self.assertIsNotNone(data)
@@ -32,8 +32,8 @@ class TestHOPS(unittest.TestCase):
         self.assertIsNotNone(data['datetime'])
         self.assertIsNotNone(data['production'])
         self.assertEqual(data['production']['wind'], 504.0)
-        self.assertEqual(data['production']['solar'], 0.0)
-        self.assertEqual(data['production']['unknown'], 1494.0)
+        self.assertEqual(data['production']['solar'], 19.5)
+        self.assertEqual(data['production']['unknown'], 1474.5)
 
     def test_fetch_production_no_solar(self):
         response_text = resource_string("parsers.test.mocks", "hops.json")
@@ -43,13 +43,12 @@ class TestHOPS(unittest.TestCase):
                                   content=response_text)
         response_text = resource_string("parsers.test.mocks", "hrote_no_solar_on_that_hour.json")
         self.adapter.register_uri("GET", "https://files.hrote.hr/files/EKO_BG/FORECAST/SOLAR/FTP/TEST_DRIVE/"
-                                         "3.11.2019.json", content=response_text)
+                                         "21.5.2020.json", content=response_text)
 
         with LogCapture() as log:
             data = HOPS.fetch_production('HR', self.session)
-
         self.assertTrue(('parsers.HOPS', 'WARNING', 'No value for Solar power production on '
-                                                 '2019-11-03 18:00:00+01:00') in log.actual())
+                                                 '2020-05-21 18:00:00+02:00') in log.actual())
         self.assertIsNotNone(data)
         data = data[0]
         self.assertEqual(data['zoneKey'], 'HR')
