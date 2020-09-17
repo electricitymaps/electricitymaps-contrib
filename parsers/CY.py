0eab5e7d16d30ae594e63b09caf7507d1fb6269b
Paul Georgiou <veqtrus@users.noreply.github.com>
2020-09-17 15:45:56 +0100

    Update CY parser to fetch historical data (#2669)
    Co-authored-by: Felix Qvist <felix.qvist@gmail.com>

--- parsers/CY.py
+++ parsers/CY.py
@@ -2,6 +2,7 @@
 import logging
 import datetime
 import re
+import sys
 
 # The arrow library is used to handle datetimes
 import arrow
@@ -11,6 +12,38 @@ import requests
 # please try to write PEP8 compliant code (use a linter). One of PEP8's
 # requirement is to limit your line length to 79 characters.
 
+
+def append_datum(data, time_str, prods_str):
+    time_str = time_str[0] + ' ' + time_str[1] + ':' + time_str[2]
+    time_value = arrow.get(time_str).replace(tzinfo='Asia/Nicosia').datetime
+
+    prods_value = {
+        'oil': float(prods_str[3]),
+        'solar': float(prods_str[1]),
+        'wind': float(prods_str[0]),
+        'biomass': 0.0
+    }
+
+    # Because solar is explicitly listed as "Solar PV" (so no thermal with energy storage) and there
+    # is zero sunlight in the middle of the night (https://www.timeanddate.com/sun/cyprus/nicosia),
+    # we use the biomass+solar generation reported at 0:00 to determine the portion of biomass+solar
+    # which constitutes biomass
+    nocturnal_biomass_generation = data[0]['production']['biomass'] if len(data) != 0 else prods_value['solar']
+    prods_value['solar'] -= nocturnal_biomass_generation
+    prods_value['biomass'] += nocturnal_biomass_generation
+    if prods_value['solar'] < 0.0:
+        # if there is (next to) no sunlight and biomass is lower than at midnight
+        prods_value['solar'] = 0.0
+
+    data.append({
+        'zoneKey': 'CY',
+        'production': prods_value,
+        'storage': {},
+        'source': 'tsoc.org.cy',
+        'datetime': time_value
+    })
+
+
 def parse_html(html):
     html = html.replace('\n', ' ').replace('\r', ' ')
 
@@ -18,44 +51,21 @@ def parse_html(html):
         '''data\.addColumn\("number", "([^"]*)"\);''', html)]
     assert columns == ['Συνολική Προβλεπόμενη Ζήτηση', 'Αιολική Παραγωγή',
         'Εκτίμηση Διεσπαρμένης Παραγωγής (Φωτοβολταϊκά και Βιομάζα)',
-        'Συνολική Ζήτηση', 'Συμβατική Παραγωγή']
+        'Συνολική Ζήτηση', 'Συμβατική Παραγωγή'], 'Source format changed'
 
     times = (m.group(1, 2, 3) for m in re.finditer(
         '''var dateStr = "([^"]*)";\s+var hourStr = "(\d+)";\s+var minutesStr = "(\d+)";''', html))
     prods = (m.group(1, 2, 3, 4) for m in re.finditer(
         '''\[\s*dateStrFormat,\s*(\d+|null),\s*(\d+|null),\s*(\d+|null),\s*(\d+|null)\s*\]''', html))
 
-    last_datum = None
-    #Because solar is explicitly listed as "Solar PV" (so no thermal with energy storage) and there
-    #is zero sunlight in the middle of the night (https://www.timeanddate.com/sun/cyprus/nicosia),
-    #we use the biomass+solar generation reported at 0:00 to determine the portion of biomass+solar
-    #which constitutes biomass
-    nocturnal_biomass_generation = None
+    data = []
     for t, p in zip(times, prods):
-        if(nocturnal_biomass_generation is None):
-          nocturnal_biomass_generation = float(p[1])
-        # find last datum without null values
-        # this is the current as null is used for where the chart should show estimates
         if any(e == 'null' for e in p):
             break
-        last_datum = (t, p)
+        append_datum(data, t, p)
 
-    if last_datum is None:
-        return None
-    last_time, last_prods = last_datum
+    return data
 
-    last_time = last_time[0] + ' ' + last_time[1] + ':' + last_time[2]
-
-    last_prods = {
-        'oil': float(last_prods[3]),
-        'solar': float(last_prods[1]) - nocturnal_biomass_generation,
-        'wind': float(last_prods[0]),
-        'biomass': nocturnal_biomass_generation # estimate based on the reported solar+biomass production at midnight
-    }
-    if (last_prods['solar'] < 0.0):#If there is (next to) no sunlight and biomass is lower than at 0:00AM
-        last_prods['solar'] = 0.0
-
-    return last_time, last_prods
 
 def fetch_production(zone_key='CY', session=None,
                      target_datetime: datetime.datetime = None,
@@ -114,33 +124,20 @@ def fetch_production(zone_key='CY', session=None,
     if target_datetime is None:
         url = 'https://tsoc.org.cy/total-daily-system-generation-on-the-transmission-system/'
     else:
-        # WHEN HISTORICAL DATA IS AVAILABLE
         # convert target datetime to local datetime
-        # url_date = arrow.get(target_datetime).to(
-        #     "America/Argentina/Buenos_Aires")
-        # url = 'https://api.someservice.com/v1/productionmix/{}'.format(
-        #     url_date)
-
-        # WHEN HISTORICAL DATA IS NOT AVAILABLE
-        raise NotImplementedError(
-            'This parser is not yet able to parse past dates')
+        url_date = arrow.get(target_datetime).to('Asia/Nicosia')
+        url = 'https://tsoc.org.cy/archive-total-daily-system-generation-on-the-transmission-system/?startdt={}&enddt=%2B1days'.format(
+            url_date.format('DD-MM-YYYY'))
 
     res = r.get(url)
     assert res.status_code == 200, 'Exception when fetching production for ' \
                                    '{}: error when calling url={}'.format(
                                        zone_key, url)
 
-    last_time, last_prods = parse_html(res.text)
+    data = parse_html(res.text)
 
-    last_time = arrow.get(last_time).replace(tzinfo='Asia/Nicosia')
-
-    data = {
-        'zoneKey': zone_key,
-        'production': last_prods,
-        'storage': {},
-        'source': 'tsoc.org.cy',
-        'datetime': last_time.datetime
-    }
+    if len(data) == 0:
+        raise Warning('No data returned')
 
     return data
 
@@ -149,5 +146,10 @@ if __name__ == '__main__':
     """Main method, never used by the Electricity Map backend, but handy
     for testing."""
 
+    target_datetime = None
+    if len(sys.argv) == 4:
+        target_datetime = datetime.datetime(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]))
+
     print('fetch_production() ->')
-    print(fetch_production())
+    for datum in fetch_production(target_datetime=target_datetime):
+        print(datum)
