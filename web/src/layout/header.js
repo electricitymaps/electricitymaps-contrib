d6af28687825c7c35724cee6765028e8d57be57b
Erica Pramer <epramer@godaddy.com>
2020-10-24 23:21:44 -0700

    start to add styled components for header

--- web/src/layout/header.js
+++ web/src/layout/header.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import styled from 'styled-components';
 
 // TODO: Move all styles from styles.css to here
+const Wrapper = styled.div`
+  background-color: transparent;
+  z-index: 2;
+  position: fixed;
+  right: 0;
+  top: 10px;
+  color: #FFFFFF;
+  min-height: 50px; /* required for old Safari */
+
+  &.brightmode {
+    color: #000000; /* TODO: define variable */
+  }
+`;
 
 const mapStateToProps = state => ({
   brightModeEnabled: state.application.brightModeEnabled,
 });
 
 export default connect(mapStateToProps)(props => (
-  <div id="header">
+  <Wrapper>
     <div id="header-content" className={props.brightModeEnabled ? 'brightmode' : null}>
       <div className="logo">
         <div className="image" id="electricitymap-logo" />
@@ -19,5 +33,5 @@ export default connect(mapStateToProps)(props => (
         </span>
       </div>
     </div>
-  </div>
+  </Wrapper>
 ));
