d80d7c50d2d925d40510eee9157d571578c66827
Erica Pramer <epramer@godaddy.com>
2020-10-24 23:39:33 -0700

    add variables and breakpoints

--- web/src/layout/header.js
+++ web/src/layout/header.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import styled from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
+import { theme, breakpoints } from '../scss/theme';
 
 // TODO: Move all styles from styles.css to here
-const Wrapper = styled.div`
+const Header = styled.div`
   background-color: transparent;
   z-index: 2;
   position: fixed;
   right: 0;
   top: 10px;
-  color: #FFFFFF;
+  color: ${props => props.theme.white};
   min-height: 50px; /* required for old Safari */
 
   &.brightmode {
-    color: #000000; /* TODO: define variable */
+    color: ${props => props.theme.black};
+  }
+
+  @media ${breakpoints.small} {
+    display:none;
+    min-height: 50px; /* required for old Safari */
+    left: 0;
+    top: 0;
+    z-index: 2;
   }
 `;
 
@@ -22,16 +31,18 @@ const mapStateToProps = state => ({
 });
 
 export default connect(mapStateToProps)(props => (
-  <Wrapper>
-    <div id="header-content" className={props.brightModeEnabled ? 'brightmode' : null}>
-      <div className="logo">
-        <div className="image" id="electricitymap-logo" />
-        <span className="maintitle small-screen-hidden">
-          <span className="live" style={{ fontWeight: 'bold' }}>Live</span>
-          · <a href="https://api.electricitymap.org?utm_source=electricitymap.org&utm_medium=referral">API</a>
-          · <a href="https://www.tmrow.com/blog/tags/electricitymap?utm_source=electricitymap.org&utm_medium=referral">Blog</a>
-        </span>
+  <ThemeProvider theme={ theme }>
+    <Header>
+      <div id="header-content" className={props.brightModeEnabled ? 'brightmode' : null}>
+        <div className="logo">
+          <div className="image" id="electricitymap-logo" />
+          <span className="maintitle small-screen-hidden">
+            <span className="live" style={{ fontWeight: 'bold' }}>Live</span>
+            · <a href="https://api.electricitymap.org?utm_source=electricitymap.org&utm_medium=referral">API</a>
+            · <a href="https://www.tmrow.com/blog/tags/electricitymap?utm_source=electricitymap.org&utm_medium=referral">Blog</a>
+          </span>
+        </div>
       </div>
-    </div>
-  </Wrapper>
+    </Header>
+  </ThemeProvider>
 ));
