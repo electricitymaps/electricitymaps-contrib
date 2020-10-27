0c2fe18f21dea7f89d1a9fcb7f4ddedf8c1ba3fb
Mila Grigoryeva <mila.grigoryeva@nextfour.com>
2020-10-27 14:14:31 +0200

    Remove typo in MD.py

--- parsers/MD.py
+++ parsers/MD.py
@@ -28,7 +28,7 @@ def get_data(session=None):
     #In order for the data url to return data, cookies from the display url must be obtained then reused.
     response = s.get(display_url)
     data_response = s.get(data_url)
-    raw_data = data_response.textg
+    raw_data = data_response.text
     data = [float(i) for i in raw_data.split(',')]
 
     return data
