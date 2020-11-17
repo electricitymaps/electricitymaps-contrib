electricitymaps/electricitymaps-contrib

commit 97865b8d33fbfe643aeba2fcff53ecb8b727786c

Ignore consumption values when fetching production to fix negative unknown production in NL (#2676)
Co-authored-by: Felix Qvist <felix.qvist@gmail.com>

parsers/ENTSOE.py
@@ -619,20 +619,19 @@ def parse_production(xml_text):
         datetime_start = arrow.get(timeseries.find_all('start')[0].contents[0])
         is_production = len(timeseries.find_all('inBiddingZone_Domain.mRID'.lower())) > 0
         psr_type = timeseries.find_all('mktpsrtype')[0].find_all('psrtype')[0].contents[0]
-        for entry in timeseries.find_all('point'):
-            quantity = float(entry.find_all('quantity')[0].contents[0])
-            position = int(entry.find_all('position')[0].contents[0])
-            datetime = datetime_from_position(datetime_start, position, resolution)
-            try:
-                i = datetimes.index(datetime)
-                if is_production:
+
+        if len(timeseries.find_all('inBiddingZone_Domain.mRID'.lower())) > 0:
+            for entry in timeseries.find_all('point'):
+                quantity = float(entry.find_all('quantity')[0].contents[0])
+                position = int(entry.find_all('position')[0].contents[0])
+                datetime = datetime_from_position(datetime_start, position, resolution)
+                try:
+                    i = datetimes.index(datetime)
                     productions[i][psr_type] += quantity
-                else:
-                    productions[i][psr_type] -= quantity
-            except ValueError:  # Not in list
-                datetimes.append(datetime)
-                productions.append(defaultdict(lambda: 0))
-                productions[-1][psr_type] = quantity if is_production else -1 * quantity
+                except ValueError:  # Not in list
+                    datetimes.append(datetime)
+                    productions.append(defaultdict(lambda: 0))
+                    productions[-1][psr_type] = quantity if is_production else -1 * quantity
     return productions, datetimes
 
 

commit b80c5adda89b5984cfdbe7957524c7187603b3de

Fix missing ENTSOE hydro storage (fixes #2800)
97865b8 introduced this bug by removing all consumption data from the
ENTSOE parser, however it was not intended to remove hydro storage,
only power plant self-consumption.

This commit reverts the earlier change, and adds a check so that
only hydro storage consumption data will be included, rather than
no consumption data at all.

parsers/ENTSOE.py
@@ -620,18 +620,22 @@ def parse_production(xml_text):
         is_production = len(timeseries.find_all('inBiddingZone_Domain.mRID'.lower())) > 0
         psr_type = timeseries.find_all('mktpsrtype')[0].find_all('psrtype')[0].contents[0]
 
-        if len(timeseries.find_all('inBiddingZone_Domain.mRID'.lower())) > 0:
-            for entry in timeseries.find_all('point'):
-                quantity = float(entry.find_all('quantity')[0].contents[0])
-                position = int(entry.find_all('position')[0].contents[0])
-                datetime = datetime_from_position(datetime_start, position, resolution)
-                try:
-                    i = datetimes.index(datetime)
+        for entry in timeseries.find_all('point'):
+            quantity = float(entry.find_all('quantity')[0].contents[0])
+            position = int(entry.find_all('position')[0].contents[0])
+            datetime = datetime_from_position(datetime_start, position, resolution)
+            try:
+                i = datetimes.index(datetime)
+                if is_production:
                     productions[i][psr_type] += quantity
-                except ValueError:  # Not in list
-                    datetimes.append(datetime)
-                    productions.append(defaultdict(lambda: 0))
-                    productions[-1][psr_type] = quantity if is_production else -1 * quantity
+                elif psr_type in ENTSOE_PARAMETER_GROUPS['storage']['hydro storage']:
+                    # Only include consumption if it's for hydro storage. In other cases
+                    # it is power plant self-consumption which should be ignored.
+                    productions[i][psr_type] -= quantity
+            except ValueError:  # Not in list
+                datetimes.append(datetime)
+                productions.append(defaultdict(lambda: 0))
+                productions[-1][psr_type] = quantity if is_production else -1 * quantity
     return productions, datetimes
 
 