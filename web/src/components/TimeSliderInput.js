99f190980b3e8129aa1231731325f8a204d05c94
Mads Nedergaard <nedergaardmads@gmail.com>
2021-03-15 10:39:46 +0100

    Creates new time-slider with night-time gradients

---
+++ web/src/components/TimeSliderInput.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import styled from 'styled-components';
+import { sortBy } from 'lodash';
+
+const COLORS = {
+  DAY: '#EFEFEF',
+  NIGHT: '#666', // TODO: Find proper color
+};
+
+const getStyling = sets => sortBy(sets, 'end')
+  .map(
+    ({ start, end }) => `${COLORS.DAY} ${start}%, ${COLORS.NIGHT} ${start}%, ${COLORS.NIGHT} ${end}%, ${COLORS.DAY} ${end}%`,
+  )
+  .join(',\n');
+
+export const StyledInput = styled.input`
+  --time-gradient: linear-gradient(
+    90deg,
+    ${props => getStyling(props.nightSets)}
+  );
+`;
+
+// TODO: Test performance
+export const TimeSliderInput = ({
+  onChange, value, nightTimeSets, min, max,
+}) => {
+  const isValueInNight = true; // TODO: Make this dynamic
+  const timeClass = isValueInNight ? 'night' : 'day';
+  return (
+    <StyledInput
+      type="range"
+      className={`time-slider-input ${timeClass}`}
+      nightSets={nightTimeSets}
+      onChange={onChange}
+      onInput={onChange}
+      value={value}
+      min={min}
+      max={max}
+    />
+  );
+};
--- web/src/components/timeslider.js
+++ web/src/components/timeslider.js
@@ -1,12 +1,6 @@
-import React, {
-  useMemo,
-  useState,
-} from 'react';
+import React, { useMemo, useState } from 'react';
 import {
-  first,
-  last,
-  sortedIndex,
-  isNumber,
+  first, last, sortedIndex, isNumber,
 } from 'lodash';
 import { scaleTime } from 'd3-scale';
 import moment from 'moment';
@@ -14,19 +8,27 @@ import moment from 'moment';
 import TimeAxis from './graph/timeaxis';
 import { useRefWidthHeightObserver } from '../hooks/viewport';
 import { useCurrentNightTimes } from '../hooks/redux';
+import { TimeSliderInput } from './TimeSliderInput';
 
 const AXIS_HORIZONTAL_MARGINS = 12;
 
-const getTimeScale = (width, datetimes, startTime, endTime) => scaleTime()
+const getTimeScale = (rangeEnd, datetimes, startTime, endTime) => scaleTime()
   .domain([
     startTime ? moment(startTime).toDate() : first(datetimes),
     endTime ? moment(endTime).toDate() : last(datetimes),
   ])
-  .range([0, width]);
+  .range([0, rangeEnd]);
 
-const createChangeAndInputHandler = (datetimes, onChange, setAnchoredTimeIndex) => (ev) => {
+const createChangeAndInputHandler = (
+  datetimes,
+  onChange,
+  setAnchoredTimeIndex,
+) => (ev) => {
   const value = parseInt(ev.target.value, 10);
-  let index = sortedIndex(datetimes.map(t => t.valueOf()), value);
+  let index = sortedIndex(
+    datetimes.map(t => t.valueOf()),
+    value,
+  );
   // If the slider is past the last datetime, we set index to null in order to use the scale end time.
   if (index >= datetimes.length) {
     index = null;
@@ -46,6 +48,7 @@ const TimeSlider = ({
   endTime,
 }) => {
   const { ref, width } = useRefWidthHeightObserver(2 * AXIS_HORIZONTAL_MARGINS);
+  // const { ref: ref2 } = useRefWidthHeightObserver(2 * AXIS_HORIZONTAL_MARGINS);
 
   const [anchoredTimeIndex, setAnchoredTimeIndex] = useState(null);
   const nightTimes = useCurrentNightTimes();
@@ -55,6 +58,22 @@ const TimeSlider = ({
     [width, datetimes, startTime, endTime],
   );
 
+  const startTimeValue = timeScale.domain()[0].valueOf();
+  const endTimeValue = timeScale.domain()[1].valueOf();
+
+  // Creating a scale for the night-time background gradients
+  const gradientScale = useMemo(
+    () => getTimeScale(100, nightTimes, startTimeValue, endTimeValue),
+    [nightTimes, timeScale],
+  );
+
+  const nightTimeSets = nightTimes.flatMap(([start, end]) => [
+    {
+      start: Math.max(0, gradientScale(start)),
+      end: Math.min(100, gradientScale(end)),
+    },
+  ]);
+
   const handleChangeAndInput = useMemo(
     () => createChangeAndInputHandler(datetimes, onChange, setAnchoredTimeIndex),
     [datetimes, onChange, setAnchoredTimeIndex],
@@ -62,19 +81,19 @@ const TimeSlider = ({
 
   if (!datetimes || datetimes.length === 0) return null;
 
-  const selectedTimeValue = isNumber(selectedTimeIndex) ? datetimes[selectedTimeIndex].valueOf() : null;
-  const anchoredTimeValue = isNumber(anchoredTimeIndex) ? datetimes[anchoredTimeIndex].valueOf() : null;
-  const startTimeValue = timeScale.domain()[0].valueOf();
-  const endTimeValue = timeScale.domain()[1].valueOf();
+  const selectedTimeValue = isNumber(selectedTimeIndex)
+    ? datetimes[selectedTimeIndex].valueOf()
+    : null;
+  const anchoredTimeValue = isNumber(anchoredTimeIndex)
+    ? datetimes[anchoredTimeIndex].valueOf()
+    : null;
 
   return (
     <div className={className}>
-      <input
-        type="range"
-        className="time-slider-input"
+      <TimeSliderInput
         onChange={handleChangeAndInput}
-        onInput={handleChangeAndInput}
         value={selectedTimeValue || anchoredTimeValue || endTimeValue}
+        nightTimeSets={nightTimeSets}
         min={startTimeValue}
         max={endTimeValue}
       />
