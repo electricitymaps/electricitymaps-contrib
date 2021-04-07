384271cab80bd5cf050d5f8a28145e2c9780648f
Yuval Aboulafia <yuval.abou@gmail.com>
2021-04-07 14:38:21 +0300

    change price

--- parsers/IL.py
+++ parsers/IL.py
@@ -7,12 +7,14 @@
 # Shares of Electricity production in 2018: 65.6% oil, 34.4% gas (source: IEA; https://www.iea.org/data-and-statistics?country=ISRAEL&fuel=Electricity%20and%20heat&indicator=Electricity%20generation%20by%20source)
 
 
+import re
+
 import arrow
 import requests
-import re
 from bs4 import BeautifulSoup
 
 IEC_PRODUCTION = 'https://www.iec.co.il/_layouts/iec/applicationpages/lackmanagment.aspx'
+PRICE = '50.66' # Price is determined yearly
 
 def fetch_production(zone_key='IL', session=None, logger=None):
     first = requests.get(IEC_PRODUCTION)
@@ -32,7 +34,7 @@ def fetch_production(zone_key='IL', session=None, logger=None):
         'datetime': arrow.now('Asia/Jerusalem').datetime,
         'production': production,
         'source': 'iec.co.il',
-        'price': '50.66' # Price is determined yearly
+        'price': PRICE
     }
 
     return datapoint
