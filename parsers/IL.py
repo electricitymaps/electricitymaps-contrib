b7ea785f12367cf2163efdbea162d3d5d03844a7
Yuval Aboulafia <yuval.abou@gmail.com>
2021-04-12 07:53:40 +0300

    Remove comments

--- parsers/IL.py
+++ parsers/IL.py
@@ -22,10 +22,8 @@ PRICE = "50.66"  # Price is determined yearly
 
 def flatten_list(_2d_list):
     flat_list = []
-    # Iterate through the outer list
     for element in _2d_list:
         if type(element) is list:
-            # If the element is of type list, iterate through the sublist
             for item in element:
                 flat_list.append(item)
         else:
