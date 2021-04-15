db24289128b1fa9729bde1cd0a72dc6219ace60e
Yuval Aboulafia <yuval.abou@gmail.com>
2021-04-15 15:36:42 +0300

    IL refactor

--- parsers/IL.py
+++ parsers/IL.py
@@ -1,43 +1,69 @@
-#!/usr/bin/env python3
-# coding=utf-8
+"""
+Israel's electricity system load parser.
 
-# This parser returns Israel's electricity system load (assumed to be equal to electricity production)
-# Source: Israel Electric Corporation
-# URL: https://www.iec.co.il/en/pages/default.aspx
-# Shares of Electricity production in 2018: 65.6% oil, 34.4% gas (source: IEA; https://www.iea.org/data-and-statistics?country=ISRAEL&fuel=Electricity%20and%20heat&indicator=Electricity%20generation%20by%20source)
+Source: Israel Electric Corporation
+URL: https://www.iec.co.il/en/pages/default.aspx
 
+Shares of Electricity production in 2018: 65.6% oil, 34.4% gas
+(source: IEA; https://www.iea.org/data-and-statistics?country=ISRAEL&fuel=Electricity%20and%20heat&indicator=Electricity%20generation%20by%20source)
+"""
+
+import re
 
 import arrow
-import requests
-import re
+from requests import get
 from bs4 import BeautifulSoup
 
+IEC_URL = "www.iec.co.il"
+IEC_PRODUCTION = (
+    "https://www.iec.co.il/_layouts/iec/applicationpages/lackmanagment.aspx"
+)
+PRICE = "50.66"  # Price is determined yearly
+TZ = "Asia/Jerusalem"
 
-def fetch_production(zone_key='IL', session=None, logger=None):
-    first = requests.get('https://www.iec.co.il/_layouts/iec/applicationpages/lackmanagment.aspx')
+
+def flatten_list(_2d_list) -> list:
+    flat_list = []
+    for element in _2d_list:
+        if type(element) is list:
+            for item in element:
+                flat_list.append(item)
+        else:
+            flat_list.append(element)
+    return flat_list
+
+
+def fetch_production(zone_key="IL", session=None, logger=None) -> dict:
+    first = get(IEC_PRODUCTION)
     first.cookies
-    second = requests.get('https://www.iec.co.il/_layouts/iec/applicationpages/lackmanagment.aspx', cookies=first.cookies)
+    second = get(IEC_PRODUCTION, cookies=first.cookies)
+    soup = BeautifulSoup(second.content, "lxml")
 
-    soup = BeautifulSoup(second.content, 'lxml')
+    values: list = soup.find_all("span", class_="statusVal")
+    del values[1]
 
-    span = soup.find("span", attrs={"class": "statusVal"})
-    value = re.findall("\d+", span.text.replace(",", ""))
-    load = int(value[0])
-    production = {}
-    production['unknown'] = load
-    
-    datapoint = {
-        'zoneKey': zone_key,
-        'datetime': arrow.now('Asia/Jerusalem').datetime,
-        'production': production,
-        'source': 'iec.co.il'
+    cleaned_list = []
+    for value in values:
+        value = re.findall("\d+", value.text.replace(",", ""))
+        cleaned_list.append(value)
+
+    cleaned_list = flatten_list(cleaned_list)
+
+    production = [float(item) for item in cleaned_list]
+
+    # all mapped to unknown as there is no available breakdown
+    return {
+        "zoneKey": zone_key,
+        "datetime": arrow.now(TZ).datetime,
+        "production": {
+            "unknown": production[0] + production[1]
+        },
+        "source": IEC_URL,
+        "price": PRICE,
     }
 
-    return datapoint
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     """Main method, never used by the Electricity Map backend, but handy for testing."""
-    
-    print('fetch_production() ->')
+    print("fetch_production() ->")
     print(fetch_production())
