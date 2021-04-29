93100551d23423d3889b8884b80e7f42ecb01f4a
Yuval Aboulafia <yuval.abou@gmail.com>
2021-04-29 22:35:51 +0300

    Format .lib folder (#3122)

--- parsers/lib/IN.py
+++ parsers/lib/IN.py
@@ -1,28 +1,30 @@
 from arrow import get, utcnow
 
+TZ = "Asia/Kolkata"
+
 
 def read_datetime_from_span_id(html, span_id, format):
-    """Read date time from span with id"""
-    date_time_span = html.find('span', {'id': span_id})
-    india_date_time = date_time_span.text + ' Asia/Kolkata'
-    return get(india_date_time, format + ' ZZZ')
+    date_time_span = html.find("span", {"id": span_id})
+    india_date_time = date_time_span.text + " Asia/Kolkata"
+    return get(india_date_time, format + " ZZZ")
+
 
 def read_text_from_span_id(html, span_id):
-    """Read text from span with id"""
-    return html.find('span', {'id': span_id}).text
+    return html.find("span", {"id": span_id}).text
 
 
 def read_value_from_span_id(html, span_id):
-    """Read value from span with id"""
     html_span = read_text_from_span_id(html, span_id)
     return float(html_span)
 
 
 def read_datetime_with_only_time(time_string, time_format, now=utcnow()):
-    utc = now.floor('hour')
-    india_now = utc.to('Asia/Kolkata')
+    utc = now.floor("hour")
+    india_now = utc.to(TZ)
     time = get(time_string, time_format)
-    india_date_time = india_now.replace(hour=time.hour, minute=time.minute, second=time.second)
+    india_date_time = india_now.replace(
+        hour=time.hour, minute=time.minute, second=time.second
+    )
     if india_date_time > india_now:
         india_date_time.shift(days=-1)
     return india_date_time
--- parsers/lib/web.py
+++ parsers/lib/web.py
@@ -1,36 +1,36 @@
-from requests import Session
 from bs4 import BeautifulSoup
+from requests import Session
 
 from .exceptions import ParserException
 
 
 def get_response(zone_key, url, session=None):
-    """Get response"""
     ses = session or Session()
     response = ses.get(url)
     if response.status_code != 200:
-        raise ParserException(zone_key, 'Response code: {0}'.format(response.status_code))
+        raise ParserException(
+            zone_key, "Response code: {0}".format(response.status_code)
+        )
     return response
 
 
 def get_response_with_params(zone_key, url, session=None, params=None):
-    """Get response"""
     ses = session or Session()
-    response = ses.get(url, params= params)
+    response = ses.get(url, params=params)
     if response.status_code != 200:
-        raise ParserException(zone_key, 'Response code: {0}'.format(response.status_code))
+        raise ParserException(
+            zone_key, "Response code: {0}".format(response.status_code)
+        )
     return response
 
 
 def get_response_text(zone_key, url, session=None):
-    """Get text response"""
     response = get_response(zone_key, url, session)
     if not response.text:
-        raise ParserException(zone_key, 'Response empty')
+        raise ParserException(zone_key, "Response empty")
     return response.text
 
 
 def get_response_soup(zone_key, url, session=None):
-    """Get BeautifulSoup response"""
     response_text = get_response_text(zone_key, url, session)
-    return BeautifulSoup(response_text, 'html.parser')
+    return BeautifulSoup(response_text, "html.parser")
