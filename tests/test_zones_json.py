58ed04cd2d889959c56be8459c301254bbd97142
Kenneth Skovhus <skovhus@users.noreply.github.com>
2021-06-14 13:21:31 +0200

    Ensure all zones are defined in zones.json (#3182)
    * Test that all sub zones have an entry
    
    * Test that exchanges are known
    
    * Add missing zones
    
    * Update bounding boxes

--- tests/test_zones_json.py
+++ tests/test_zones_json.py
@@ -1,22 +1,24 @@
-import json
 import unittest
-from pathlib import Path
 
-CONFIG_DIR = Path(__file__).parent.parent.joinpath('config').resolve()
+from electricitymap.contrib.config import ZONES_CONFIG
+
+ZONE_KEYS = ZONES_CONFIG.keys()
 
 
 class ZonesJsonTestcase(unittest.TestCase):
-    def setUp(self):
-        with open(CONFIG_DIR.joinpath('zones.json')) as zc:
-            self.zones_config = json.load(zc)
-
     def test_bounding_boxes(self):
-        for zone, values in self.zones_config.items():
-            bbox = values.get('bounding_box')
+        for zone, values in ZONES_CONFIG.items():
+            bbox = values.get("bounding_box")
             if bbox:
                 self.assertLess(bbox[0][0], bbox[1][0])
                 self.assertLess(bbox[0][1], bbox[1][1])
 
+    def test_sub_zones(self):
+        for zone, values in ZONES_CONFIG.items():
+            sub_zones = values.get("subZoneNames", [])
+            for sub_zone in sub_zones:
+                self.assertIn(sub_zone, ZONE_KEYS)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main(buffer=True)
