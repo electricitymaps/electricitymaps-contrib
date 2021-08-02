8d778ae3f07c1dc8b94ac1e05dd8dd662091abb9
P Jakobsen <pjakobsen@gmail.com>
2021-08-01 22:51:25 -0400

    New information on capacities and implemented special case for gas production from ticket #3218 and #3254

--- parsers/CA_QC.py
+++ parsers/CA_QC.py
@@ -25,8 +25,11 @@ def fetch_production(
             "thermique": "thermal",
             "solaire": "solar",
             "eolien": "wind",
-            "autres": "unknown",
+            #autres is all renewable, and mostly biomass.  See Github    #3218
+            "autres": "biomass",
             "valeurs": "values",
+            "biomass":0.0
+
         }
         english = {v: k for k, v in english.items()}
         try:
@@ -42,15 +45,22 @@ def fetch_production(
                 "zoneKey": zone_key,
                 "datetime":  arrow.get(elem["date"], tzinfo=timezone_id).datetime,
                 "production": {
-                    "biomass": 0.0,
+                    "biomass": if_exists(elem, "biomass"),
                     "coal": 0.0,
-                    "gas": 0.0,
                     "hydro": if_exists(elem, "hydro"),
                     "nuclear": 0.0,
                     "oil": 0.0,
                     "solar": if_exists(elem, "solar"),
                     "wind": if_exists(elem, "wind"),
-                    "geothermal": if_exists(elem, "geothermal"),
+                    #3218, Québec's thermal generation is at Bécancour gas turbine.
+                    #It is reported with a delay, and data source returning 0.0 can indicate either no generation or not-yet-reported generation.
+                    #To handle this, if reported value is 0.0, overwrite it to None, so that backend can know this is not entirely reliable and might be updated later.
+                    # thermal is not a key recognized by Electricity Map.
+                    # Everything connected to Hydro-Québec grid described as "thermal" is fueled by natural gas
+                    # (Bécancour station), so we use "gas" as Electricity Map key for what Hydro-Québec calls "thermal".
+                    "gas": None if if_exists(elem, "thermal") == 0 else if_exists(elem, "thermal"),
+                    #There are no geothermal electricity generation stations in Québec (and all of Canada for that matter).
+                    "geothermal": 0.0,
                     "unknown": if_exists(elem, "unknown"),
                 },
                 "source": "hydroquebec.com",
