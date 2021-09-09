edbdbb677c86ac091a20c62e292673b247b8c35a
H Boisgibault <h.boisgibault@gmail.com>
2021-09-09 13:51:18 +0200

    Adding language param to canonical URL

--- web/server.js
+++ web/server.js
@@ -158,7 +158,10 @@ app.use('/', (req, res) => {
       res.setLocale(lr[0]);
     }
     const { locale } = res;
-    const fullUrl = `https://www.electricitymap.org${req.baseUrl + req.path}`;
+    let canonicalUrl = `https://www.electricitymap.org${req.baseUrl + req.path}`;
+    if(req.query.lang) {
+      canonicalUrl += `?lang=${req.query.lang}`;
+    }
 
     // basic auth for premium access
     if (process.env.BASIC_AUTH_CREDENTIALS) {
@@ -182,7 +185,10 @@ app.use('/', (req, res) => {
     }
     res.render('pages/index', {
       alternateUrls: locales.map((l) => {
-        return `${fullUrl}?lang=${l}`;
+        if (canonicalUrl.indexOf('lang') !== -1) {
+          return canonical.replace(`lang=${req.query.lang}`, `lang=${l}`);
+        }
+        return `${canonicalUrl}?lang=${l}`;
       }),
       bundleHash: getHash('bundle', 'js', manifest),
       vendorHash: getHash('vendor', 'js', manifest),
@@ -200,7 +206,7 @@ app.use('/', (req, res) => {
           // Note we here point to static hosting in order to make
           // sure we can serve older bundle versions
           `https://static.electricitymap.org/public_web/${relativePath}`,
-      fullUrl,
+      canonicalUrl,
       locale,
       locales: { en: localeConfigs.en, [locale]: localeConfigs[locale] },
       supportedLocales: locales,
