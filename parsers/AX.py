9e8520fe1eb3f306ac5c3171765fa97a3c509c15
Yuval Aboulafia <yuval.abou@gmail.com>
2021-09-22 22:37:07 +0300

    formatting improvements

--- parsers/AX.py
+++ parsers/AX.py
@@ -1,10 +1,13 @@
-
 import arrow
+
 # Numpy and PIL are used to process the image
 import numpy as np
 import requests
 from PIL import Image
 
+URL = "http://194.110.178.135/grafik/stamnat.php"
+SOURCE = "kraftnat.aland.fi"
+TZ = "Europe/Mariehamn"
 
 def _get_masks(session=None):
     Minus = np.array([[[255, 255, 255],[255, 255, 255],[255, 255, 255],[255, 255, 255],[255, 255, 255],[255, 255, 255]],
@@ -146,6 +149,7 @@ def _get_masks(session=None):
 
 
 def _fetch_data(session=None) -> dict:
+    """Return usable data from source."""
     # Load masks for reading numbers from the image
     # Create a dictionary of symbols and their pixel masks
     mapping = _get_masks(session)
@@ -153,18 +157,17 @@ def _fetch_data(session=None) -> dict:
     # Download the updating image from Kraftnät Åland
     r = session or requests.session()
 
-    url = 'http://194.110.178.135/grafik/stamnat.php'
-
-    im = Image.open(r.get(url, stream=True).raw)
+    im = Image.open(r.get(URL, stream=True).raw)
     # Get timestamp
-    fetchtime = arrow.utcnow().floor('second').to('Europe/Mariehamn')
+    fetchtime = arrow.utcnow().floor("second").to(TZ)
 
     # "data" is a height x width x 3 RGB numpy array
     data = np.array(im)
-    #red, green, blue, alpha = data.T # Temporarily unpack the bands for readability
+    # red, green, blue, alpha = data.T
+    # Temporarily unpack the bands for readability
     red, green, blue = data.T
     # Color non-blue areas in the image with white
-    blue_areas = ((red == 0) & (green == 0) & (blue == 255))
+    blue_areas = (red == 0) & (green == 0) & (blue == 255)
     data[~blue_areas.T] = (255, 255, 255)
     # Color blue areas in the image with black
     data[blue_areas.T] = (0, 0, 0)
@@ -175,38 +178,36 @@ def _fetch_data(session=None) -> dict:
     shorts = mapping.keys()
 
     # check import from Sweden
-    SE3Flow = []
-    for x in range(80, 130-6):
+    se_3_flow = []
+    for x in range(80, 130 - 6):
         for abr in shorts:
-            im1 = im.crop((x, 443, x+6, 452))
+            im1 = im.crop((x, 443, x + 6, 452))
             if im1 == mapping[abr]:
-                SE3Flow.append(abr)
-    SE3Flow = "".join(SE3Flow)
-    SE3Flow = round(float(SE3Flow),1)
+                se_3_flow.append(abr)
+    se_3_flow = "".join(se_3_flow)
+    se_3_flow = round(float(se_3_flow), 1)
 
     # export Åland-Finland(Kustavi/Gustafs)
-
-    GustafsFlow=[]
-    for x in range(780, 825-6):
+    gustafs_flow = []
+    for x in range(780, 825 - 6):
         for abr in shorts:
-            im1 = im.crop((x, 43, x+6, 52))
+            im1 = im.crop((x, 43, x + 6, 52))
             if im1 == mapping[abr]:
-                GustafsFlow.append(abr)
-    GustafsFlow = "".join(GustafsFlow)
-    GustafsFlow = round(float(GustafsFlow),1)
+                gustafs_flow.append(abr)
+    gustafs_flow = "".join(gustafs_flow)
+    gustafs_flow = round(float(gustafs_flow), 1)
 
     # Reserve cable import Naantali-Åland
     # Åland administration does not allow export
     # to Finland through this cable
-    FIFlow = []
-    for x in range(760, 815-6):
+    fin_flow = []
+    for x in range(760, 815 - 6):
         for abr in shorts:
-            im1 = im.crop((x, 328, x+6, 337))
+            im1 = im.crop((x, 328, x + 6, 337))
             if im1 == mapping[abr]:
-                FIFlow.append(abr)
-    FIFlow = "".join(FIFlow)
-    FIFlow = round(float(FIFlow),1)
-
+                fin_flow.append(abr)
+    fin_flow = "".join(fin_flow)
+    fin_flow = round(float(fin_flow), 1)
 
     # The shown total consumption is not reliable according to the TSO
     # Consumption
@@ -220,136 +221,140 @@ def _fetch_data(session=None) -> dict:
     # Cons = round(float(Cons),1)
 
     # Wind production
-    WProd = []
-    for x in range(650, 700-6):
+    wind = []
+    for x in range(650, 700 - 6):
         for abr in shorts:
-            im1 = im.crop((x, 576, x+6, 585))
+            im1 = im.crop((x, 576, x + 6, 585))
             if im1 == mapping[abr]:
-                WProd.append(abr)
-    WProd = "".join(WProd)
-    WProd = round(float(WProd),1)
+                wind.append(abr)
+    wind = "".join(wind)
+    wind = round(float(wind), 1)
 
     # Fossil fuel production
-    FProd = []
-    for x in range(650, 700-6):
+    fossil = []
+    for x in range(650, 700 - 6):
         for abr in shorts:
-            im1 = im.crop((x, 588, x+6, 597))
+            im1 = im.crop((x, 588, x + 6, 597))
             if im1 == mapping[abr]:
-                FProd.append(abr)
-    FProd = "".join(FProd)
-    FProd = round(float(FProd),1)
+                fossil.append(abr)
+    fossil = "".join(fossil)
+    fossil = round(float(fossil), 1)
 
     # Both are confirmed to be import from Finland by the TSO
-    FIFlow = FIFlow+GustafsFlow
+    fin_flow = fin_flow + gustafs_flow
 
     # Calculate sum of exchanges
-    SumExchanges = SE3Flow+FIFlow
-
+    sum_exchanges = se_3_flow + fin_flow
     # Calculate total production
-    TotProd = FProd+WProd
-
+    total_production = fossil + wind
     # Calculate total consumption
-    Cons = round(TotProd + SumExchanges,1)
+    consumption = round(total_production + sum_exchanges, 1)
 
     # The production that is not fossil fuel or wind based is unknown
     # Impossible to estimate with current data
     # UProd = TotProd - WProd - FProd
     return {
-        'production': TotProd,
-        'consumption': Cons,
-        'wind': WProd,
-        'fossil': FProd,
-        'SE3->AX': SE3Flow,
-        'FI->AX': FIFlow,
-        'fetchtime': fetchtime
-        }
+        "production": total_production,
+        "consumption": consumption,
+        "wind": wind,
+        "fossil": fossil,
+        "SE3->AX": se_3_flow,
+        "FI->AX": fin_flow,
+        "fetchtime": fetchtime,
+    }
 
 
-def fetch_production(zone_key='AX', session=None, target_datetime=None, logger=None) -> dict:
+def fetch_production(
+    zone_key="AX", session=None, target_datetime=None, logger=None
+) -> dict:
     """Requests the last known production mix (in MW) of a given country."""
     if target_datetime:
-        raise NotImplementedError('This parser is not yet able to parse past dates')
+        raise NotImplementedError("This parser is not yet able to parse past dates")
 
     obj = _fetch_data(session)
 
     return {
-        'zoneKey': zone_key,
-        'production': {},
-        'storage': {},
-        'source': 'kraftnat.aland.fi',
-        'datetime': arrow.get(obj['fetchtime']).datetime,
-        'production': {
-            'biomass': None,
-            'coal': 0,
-            'gas': 0,
-            'hydro': None,
-            'nuclear': 0,
-            'oil': obj['fossil'],
-            'solar': None,
-            'wind': obj['wind'],
-            'geothermal': None,
-            'unknown': None,
-        }
+        "zoneKey": zone_key,
+        "production": {
+            "biomass": None,
+            "coal": 0,
+            "gas": 0,
+            "hydro": None,
+            "nuclear": 0,
+            "oil": obj["fossil"],
+            "solar": None,
+            "wind": obj["wind"],
+            "geothermal": None,
+            "unknown": None,
+        },
+        "storage": {},
+        "source": SOURCE,
+        "datetime": arrow.get(obj["fetchtime"]).datetime,
     }
 
 
-def fetch_consumption(zone_key='AX', session=None, target_datetime=None, logger=None) -> dict:
+def fetch_consumption(
+    zone_key="AX", session=None, target_datetime=None, logger=None
+) -> dict:
     if target_datetime:
-        raise NotImplementedError('This parser is not yet able to parse past dates')
+        raise NotImplementedError("This parser is not yet able to parse past dates")
 
     obj = _fetch_data(session)
 
     return {
-        'zoneKey': zone_key,
-        'datetime': arrow.get(obj['fetchtime']).datetime,
-        'consumption': obj['consumption'],
-        'source': 'kraftnat.aland.fi'
+        "zoneKey": zone_key,
+        "datetime": arrow.get(obj["fetchtime"]).datetime,
+        "consumption": obj["consumption"],
+        "source": SOURCE,
     }
 
 
-def fetch_exchange(zone_key1, zone_key2, session=None, target_datetime=None, logger=None) -> dict:
+def fetch_exchange(
+    zone_key1, zone_key2, session=None, target_datetime=None, logger=None
+) -> dict:
     """Requests the last known power exchange (in MW) between two countries."""
     if target_datetime:
-        raise NotImplementedError('This parser is not yet able to parse past dates')
+        raise NotImplementedError("This parser is not yet able to parse past dates")
 
     obj = _fetch_data(session)
 
+    # Country codes are sorted in order to enable easier indexing in the database
+    sorted_zone_keys = "->".join(sorted([zone_key1, zone_key2]))
+
     data = {
-        'sortedZoneKeys': '->'.join(sorted([zone_key1, zone_key2])),
-        'source': 'kraftnat.aland.fi',
-        'datetime': arrow.get(obj['fetchtime']).datetime
+        "sortedZoneKeys": sorted_zone_keys,
+        "source": SOURCE,
+        "datetime": arrow.get(obj["fetchtime"]).datetime,
     }
 
-    # Country codes are sorted in order to enable easier indexing in the database
-    sorted_zone_keys = sorted([zone_key1, zone_key2])
     # Here we assume that the net flow returned by the api is the flow from
     # country1 to country2. A positive flow indicates an export from country1
     # to country2. A negative flow indicates an import.
 
-    if '->'.join(sorted([zone_key1, zone_key2])) in ['AX->SE', 'AX->SE-SE3']:
-        netFlow = obj['SE3->AX']
+    if sorted_zone_keys in ["AX->SE", "AX->SE-SE3"]:
+        net_flow = obj["SE3->AX"]
 
-    elif '->'.join(sorted([zone_key1, zone_key2]))== 'AX->FI':
-        netFlow = obj['FI->AX'] # Import is positive
+    elif sorted_zone_keys == "AX->FI":
+        net_flow = obj["FI->AX"]  # Import is positive
 
     # The net flow to be reported should be from the first country to the second
     # (sorted alphabetically). This is NOT necessarily the same direction as the flow
     # from country1 to country2
 
     #  AX is before both FI and SE
-    data['netFlow'] =  round(-1 * netFlow, 1)
+    data["netFlow"] = round(-1 * net_flow, 1)
 
     return data
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     """Main method, never used by the Electricity Map backend, but handy for testing."""
 
-    print('fetch_production() ->')
+    print("fetch_production() ->")
     print(fetch_production())
-    print('fetch_consumption() ->')
+    print("fetch_consumption() ->")
     print(fetch_consumption())
-    print('fetch_exchange(AX, FI) ->')
-    print(fetch_exchange('FI', 'AX'))
-    print('fetch_exchange(AX, SE) ->')
-    print(fetch_exchange('SE', 'AX'))
+    print("fetch_exchange(AX, FI) ->")
+    print(fetch_exchange("FI", "AX"))
+    print("fetch_exchange(AX, SE) ->")
+    print(fetch_exchange("SE", "AX"))
