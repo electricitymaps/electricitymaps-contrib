5e7889d8b812ffa3b8f1dd7d7d0a89d589e13789
Yuval Aboulafia <yuval.abou@gmail.com>
2021-10-03 21:32:42 +0300

    black

--- parsers/IN_CT.py
+++ parsers/IN_CT.py
@@ -2,61 +2,65 @@ from requests import Session
 
 from .lib import IN, web, zonekey
 
-ZONE_KEY = 'IN-CT'
-URL = 'http://117.239.199.203/csptcl/GEN.aspx'
-SOURCE = 'cspc.co.in'
-TIME_FORMAT = 'hh:m DD-MM-YY'
+ZONE_KEY = "IN-CT"
+URL = "http://117.239.199.203/csptcl/GEN.aspx"
+SOURCE = "cspc.co.in"
+TIME_FORMAT = "hh:m DD-MM-YY"
 
-def fetch_consumption(zone_key=ZONE_KEY, session=None, target_datetime=None, logger=None) -> dict:
+
+def fetch_consumption(
+    zone_key=ZONE_KEY, session=None, target_datetime=None, logger=None
+) -> dict:
     """Fetch Chhattisgarh consumption"""
     if target_datetime:
-        raise NotImplementedError('This parser is not yet able to parse past dates')
+        raise NotImplementedError("This parser is not yet able to parse past dates")
 
     zonekey.assert_zone_key(zone_key, ZONE_KEY)
 
     html = web.get_response_soup(zone_key, URL, session)
 
-    india_date_time = IN.read_datetime_from_span_id(html, 'L34', TIME_FORMAT)
-    demand_value = IN.read_value_from_span_id(html, 'L26')
+    india_date_time = IN.read_datetime_from_span_id(html, "L34", TIME_FORMAT)
+    demand_value = IN.read_value_from_span_id(html, "L26")
 
     return {
-        'zoneKey': zone_key,
-        'datetime': india_date_time.datetime,
-        'consumption': demand_value,
-        'source': SOURCE
+        "zoneKey": zone_key,
+        "datetime": india_date_time.datetime,
+        "consumption": demand_value,
+        "source": SOURCE,
     }
 
 
-def fetch_production(zone_key=ZONE_KEY, session=None, target_datetime=None, logger=None) -> dict:
+def fetch_production(
+    zone_key=ZONE_KEY, session=None, target_datetime=None, logger=None
+) -> dict:
     """Fetch Chhattisgarh production"""
 
     if target_datetime:
-        raise NotImplementedError('This parser is not yet able to parse past dates')
+        raise NotImplementedError("This parser is not yet able to parse past dates")
 
     zonekey.assert_zone_key(zone_key, ZONE_KEY)
 
     html = web.get_response_soup(zone_key, URL, session)
 
-    india_date_time = IN.read_datetime_from_span_id(html, 'L34', 'hh:m DD-MM-YY')
-    korba_east_value = IN.read_value_from_span_id(html, 'L3')
-    korba_west_value = IN.read_value_from_span_id(html, 'L9')
-    dsmp_value = IN.read_value_from_span_id(html, 'L12')
-    marwa_value = IN.read_value_from_span_id(html, 'L19')
-    coal_value = round(korba_east_value + korba_west_value + dsmp_value + marwa_value, 2)
-    bango_value = IN.read_value_from_span_id(html, 'L16')
+    india_date_time = IN.read_datetime_from_span_id(html, "L34", "hh:m DD-MM-YY")
+    korba_east_value = IN.read_value_from_span_id(html, "L3")
+    korba_west_value = IN.read_value_from_span_id(html, "L9")
+    dsmp_value = IN.read_value_from_span_id(html, "L12")
+    marwa_value = IN.read_value_from_span_id(html, "L19")
+    coal_value = round(
+        korba_east_value + korba_west_value + dsmp_value + marwa_value, 2
+    )
+    bango_value = IN.read_value_from_span_id(html, "L16")
 
     return {
-        'zoneKey': zone_key,
-        'datetime': india_date_time.datetime,
-        'production': {
-            'coal': coal_value,
-            'hydro': bango_value
-        },
-        'source': SOURCE,
+        "zoneKey": zone_key,
+        "datetime": india_date_time.datetime,
+        "production": {"coal": coal_value, "hydro": bango_value},
+        "source": SOURCE,
     }
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     session = Session()
     print(fetch_production(ZONE_KEY, session))
     print(fetch_consumption(ZONE_KEY, session))
