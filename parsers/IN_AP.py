69c5eaaa4c56546ae9507987e67342671bea4f82
Yuval Aboulafia <yuval.abou@gmail.com>
2021-10-03 21:22:50 +0300

    use constants

--- parsers/IN_AP.py
+++ parsers/IN_AP.py
@@ -1,19 +1,19 @@
-#!/usr/bin/env python3
-
 from requests import Session
 from .lib import zonekey, IN, web
 
-
-def fetch_production(zone_key='IN-AP', session=None, target_datetime=None, logger=None):
+URL = "https://core.ap.gov.in/CMDashBoard/UserInterface/Power/PowerReport.aspx"
+ZONE_KEY = 'IN-AP'
+TIME_FORMAT = 'DD-MM-YYYY HH:mm'
+SOURCE = 'core.ap.gov.in'
+def fetch_production(zone_key=ZONE_KEY, session=None, target_datetime=None, logger=None):
     """Fetch Andhra Pradesh  production"""
     if target_datetime:
         raise NotImplementedError('This parser is not yet able to parse past dates')
 
-    zonekey.assert_zone_key(zone_key, 'IN-AP')
+    zonekey.assert_zone_key(zone_key, ZONE_KEY)
 
-    html = web.get_response_soup(zone_key,
-                                 'https://core.ap.gov.in/CMDashBoard/UserInterface/Power/PowerReport.aspx', session)
-    india_date = IN.read_datetime_from_span_id(html, 'MainContent_lblPowerStatusDate', 'DD-MM-YYYY HH:mm')
+    html = web.get_response_soup(zone_key, URL, session)
+    india_date = IN.read_datetime_from_span_id(html, 'MainContent_lblPowerStatusDate', TIME_FORMAT)
 
     hydro_value = IN.read_value_from_span_id(html, 'MainContent_lblHydel')
     gas_value = IN.read_value_from_span_id(html, 'MainContent_lblGas')
@@ -45,22 +45,21 @@ def fetch_production(zone_key='IN-AP', session=None, target_datetime=None, logge
         'storage': {
             'hydro': 0.0
         },
-        'source': 'core.ap.gov.in',
+        'source': SOURCE,
     }
 
     return data
 
 
-def fetch_consumption(zone_key='IN-AP', session=None, target_datetime=None, logger=None):
+def fetch_consumption(zone_key=ZONE_KEY, session=None, target_datetime=None, logger=None):
     """Fetch Andhra Pradesh consumption"""
     if target_datetime:
         raise NotImplementedError('This parser is not yet able to parse past dates')
 
-    zonekey.assert_zone_key(zone_key, 'IN-AP')
+    zonekey.assert_zone_key(zone_key, ZONE_KEY)
 
-    html = web.get_response_soup(zone_key,
-                                 'https://core.ap.gov.in/CMDashBoard/UserInterface/Power/PowerReport.aspx', session)
-    india_date = IN.read_datetime_from_span_id(html, 'MainContent_lblPowerStatusDate', 'DD-MM-YYYY HH:mm')
+    html = web.get_response_soup(zone_key, URL, session)
+    india_date = IN.read_datetime_from_span_id(html, 'MainContent_lblPowerStatusDate', TIME_FORMAT)
 
     demand_value = IN.read_value_from_span_id(html, 'MainContent_lblGridDemand')
 
@@ -68,7 +67,7 @@ def fetch_consumption(zone_key='IN-AP', session=None, target_datetime=None, logg
         'zoneKey': zone_key,
         'datetime': india_date.datetime,
         'consumption': demand_value,
-        'source': 'core.ap.gov.in'
+        'source': SOURCE
     }
 
     return data
@@ -76,5 +75,5 @@ def fetch_consumption(zone_key='IN-AP', session=None, target_datetime=None, logg
 
 if __name__ == '__main__':
     session = Session()
-    print(fetch_production('IN-AP', session))
-    print(fetch_consumption('IN-AP', session))
+    print(fetch_production(ZONE_KEY, session))
+    print(fetch_consumption(ZONE_KEY, session))
