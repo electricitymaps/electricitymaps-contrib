8377885d0f2c1ed69252403552edd9084b751639
Markus Killendahl <kill@itu.dk>
2021-10-13 19:47:50 +0200

    added skeleton

---
+++ web/geo/updateworld.js
@@ -0,0 +1,18 @@
+function validateGeometry() {
+    // find line intersections
+    // find overlaps
+    // find gaps
+    // find complexity
+    // ensure ids
+}
+
+function detectChanges() {
+    // get added
+    // get removed
+    // all zonekeys affected
+}
+
+function generateTopojson() {
+
+}
+
