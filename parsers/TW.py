0bda91d78b8623fc02cafe712c5ec32ff91d6b63
Guillaume Picard <gwpicard@gmail.com>
2021-10-13 13:56:37 +0200

    adds comment to assert

--- parsers/TW.py
+++ parsers/TW.py
@@ -39,7 +39,7 @@ def fetch_production(zone_key='TW', session=None, target_datetime=None, logger=N
 
     # check output values coincide with total capacity by fuel type
     check_values = production.output <= production.capacity
-    assert check_values.loc[~check_values.index.isin(["Co-Gen"])].all() # HACK: Co-Gen capacity is underestimated
+    assert check_values.loc[~check_values.index.isin(["Co-Gen"])].all(), "output > capacity" # HACK: Co-Gen capacity is underestimated
 
     coal_capacity = production.loc['Coal'].capacity + production.loc['IPP-Coal'].capacity
     gas_capacity = production.loc['LNG'].capacity + production.loc['IPP-LNG'].capacity
