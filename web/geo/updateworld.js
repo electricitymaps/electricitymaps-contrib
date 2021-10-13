9f7e9481aa6ec53005ebbaf9b32cbed06dc2a894
Markus Killendahl <kill@itu.dk>
2021-10-13 20:35:57 +0200

    add world.geojson

--- web/geo/updateworld.js
+++ web/geo/updateworld.js
@@ -1,3 +1,14 @@
+const turf = require("@turf/turf")
+const fs = require("fs");
+const newFileCallback = fileName => console.log("Created new file: " + fileName);
+const getJSON = (fileName, encoding = "utf8", callBack = () => {}) =>
+  typeof fileName === "string" ?
+  JSON.parse(fs.readFileSync(fileName, encoding, () => callBack())) :
+  fileName;
+
+const writeJSON = (fileName, obj, callBack = newFileCallback, encoding = 'utf8') =>
+  fs.writeFile(fileName, JSON.stringify(obj), encoding, () => callBack(fileName));
+
 function validateGeometry() {
     // find line intersections
     // find overlaps
@@ -16,3 +27,14 @@ function generateTopojson() {
 
 }
 
+
+// create world.geojson
+const data = getJSON("../src/world.json")
+Object.keys(data.objects).forEach(key => {
+    data.objects[key]['id'] = key;
+    data.objects[key]["properties"] = {"zoneName": key}
+})
+
+writeJSON("./world.json", data);
+
+console.log(data.objects["DK-DK1"]);
