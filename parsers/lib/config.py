0995defaba6e448b016daefaaeab65e5bdef59df
Felix Qvist <felix.qvist@gmail.com>
2021-11-17 14:57:35 +0100

    Added parser config decorator

---
+++ parsers/lib/config.py
@@ -0,0 +1,20 @@
+from datetime import timedelta
+
+
+def refetch_frequency(frequency: timedelta):
+    """Specifies the refetch frequency of a parser.
+    The refetch frequency is used to determine the how much data is returned by the parser.
+
+    i.e. if we refetch from d1 to d2 and the frequency is timedelta(days=1), then we will only
+    call the function once every day between d1 and d2.
+    """
+    assert isinstance(frequency, timedelta)
+
+    def wrap(f):
+        def wrapped_f(*args, **kwargs):
+            f(*args, **kwargs)
+
+        wrapped_f.REFETCH_FREQUENCY = frequency
+        return wrapped_f
+
+    return wrap
