dd21cb6937745eb4df9eaae50a737ee97cb07b17
drc38 <20024196+drc38@users.noreply.github.com>
2021-11-24 11:43:12 +1300

    Update NZ.py
    The changes may need refining as I am not sure how the 'NZ' zone key will be handled, see #3562 for discussion on the NZ parser being offline

--- parsers/NZ.py
+++ parsers/NZ.py
@@ -71,7 +71,7 @@ def fetch_price(zone_key='NZ-NZN', session=None, target_datetime=None, logger=No
         'zoneKey': zone_key
     }
 
-def fetch_production(zone_key=None, session=None, target_datetime=None, logger=None) -> dict:
+def fetch_production(zone_key='NZ', session=None, target_datetime=None, logger=None) -> dict:
     """Requests the last known production mix (in MW) of a given zone."""
     if target_datetime:
         raise NotImplementedError('This parser is not able to retrieve data for past dates')
@@ -84,6 +84,8 @@ def fetch_production(zone_key=None, session=None, target_datetime=None, logger=N
         region_key = 'North Island'
     elif zone_key == 'NZ-NZS':
         region_key = 'South Island'
+    elif zone_key == 'NZ':
+        region_key = 'New Zealand'
     else:
         raise NotImplementedError('Unsupported zone_key %s' % zone_key)
 
@@ -93,23 +95,26 @@ def fetch_production(zone_key=None, session=None, target_datetime=None, logger=N
         'zoneKey': zone_key,
         'datetime': datetime,
         'production': {
-            'coal': productions.get('Gas/Coal', {'generation': 0.0})['generation'],
-            'oil': productions.get('Diesel/Oil', {'generation': 0.0})['generation'],
+            'coal': productions.get('Coal', {'generation': 0.0})['generation'],
+            'oil': productions.get('Liquid', {'generation': 0.0})['generation'],
             'gas': productions.get('Gas', {'generation': 0.0})['generation'],
             'geothermal': productions.get('Geothermal', {'generation': 0.0})['generation'],
             'wind': productions.get('Wind', {'generation': 0.0})['generation'],
             'hydro': productions.get('Hydro', {'generation': 0.0})['generation'],
+            'battery': productions.get('Battery', {'generation': 0.0})['generation'],
             'unknown': productions.get('Co-Gen', {'generation': 0.0})['generation'],
             'nuclear': 0  # famous issue in NZ politics
         },
         'capacity': {
-            'coal': productions.get('Gas/Coal', {'capacity': 0.0})['capacity'],
-            'oil': productions.get('Diesel/Oil', {'capacity': 0.0})['capacity'],
+            'coal': productions.get('Coal', {'capacity': 0.0})['capacity'],
+            'oil': productions.get('Liquid', {'capacity': 0.0})['capacity'],
             'gas': productions.get('Gas', {'capacity': 0.0})['capacity'],
             'geothermal': productions.get('Geothermal', {'capacity': 0.0})['capacity'],
             'wind': productions.get('Wind', {'capacity': 0.0})['capacity'],
             'hydro': productions.get('Hydro', {'capacity': 0.0})['capacity'],
-            'unknown': productions.get('Co-Gen', {'capacity': 0.0})['capacity']
+            'battery': productions.get('Battery', {'capacity': 0.0})['capacity'],
+            'unknown': productions.get('Co-Gen', {'capacity': 0.0})['capacity'],
+            'nuclear': 0  # famous issue in NZ politics
         },
         'storage': {},
         'source': 'transpower.co.nz',
@@ -152,5 +157,7 @@ if __name__ == '__main__':
     print(fetch_production('NZ-NZN'))
     print('fetch_production(NZ-NZS) ->')
     print(fetch_production('NZ-NZS'))
+    print('fetch_production(NZ) ->')
+    print(fetch_production('NZ'))
     print('fetch_exchange() ->')
     print(fetch_exchange())
