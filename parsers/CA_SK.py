91e0ee53eed979a3c63d63dbd470c43d27713e8d
Reid Miller <reid@reidmiller.name>
2021-12-06 22:59:55 -0500

    Added CA-SK current consumption
    The following page shows a System Load widget in megawatts:
    https://www.saskpower.com/Our-Power-Future/Our-Electricity/Electrical-System/System-Map
    
    It makes an AJAX call to:
    https://www.saskpower.com/ignitionapi/Content/GetNetLoad
    
    That endpoint simply returns a number as a string. I've interpreted
    this to be the current consumption based solely on the website's
    language surrounding this number, "system load" and "net load".
    
    fetch_consumption(...) has been implemented accordingly.

---
+++ parsers/CA_SK.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+
+import arrow
+import requests
+
+from parsers.lib.exceptions import ParserException
+
+CONSUMPTION_URL = 'https://www.saskpower.com/ignitionapi/Content/GetNetLoad'
+SOURCE = 'saskpower.com'
+ZONE_KEY = 'CA-SK'
+
+
+def fetch_consumption(zone_key=ZONE_KEY, session=None, target_datetime=None, logger=None):
+    if target_datetime:
+        raise NotImplementedError('This parser is not yet able to parse past dates')
+
+    request = session or requests.session()
+    response = request.get(url=CONSUMPTION_URL)
+    if response.ok:
+        decoded_content = response.content.decode(response.encoding)
+        consumption = float(decoded_content.strip('"'))
+        date_header = response.headers.get('date')
+        consumption_date = arrow.get(date_header, 'ddd, DD MMM YYYY HH:mm:ss ZZZ')
+
+        return {
+            'zoneKey': zone_key,
+            'datetime': consumption_date.isoformat(),
+            'consumption': consumption,
+            'source': SOURCE
+        }
+    else:
+        raise ParserException('CA_SK.py', 'HTTP request for current consumption failed', zone_key)
+
+
+if __name__ == '__main__':
+    """Main method, never used by the Electricity Map backend, but handy for testing."""
+
+    print('fetch_consumption() ->')
+    print(fetch_consumption())
