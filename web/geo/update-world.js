23da724c6d429441bb85df335386493abbda1499
Markus Killendahl <markus.killendahl@tmrow.com>
2021-12-09 12:42:46 +0100

    Update update-world.js

--- web/geo/update-world.js
+++ web/geo/update-world.js
@@ -1,18 +1,18 @@
-const { validateGeometry } = require("./validate");
-const { detectChanges } = require("./detect-changes")
-const { getJSON } = require("./utilities")
-const { generateTopojson } = require("./generate-topojson")
+const { validateGeometry } = require('./validate');
+const { detectChanges } = require('./detect-changes');
+const { getJSON } = require('./utilities');
+const { generateTopojson } = require('./generate-topojson');
 
 const config = {
-    WORLD_PATH: "./world.geojson",
-    OUT_PATH: "../src/world.json",
-    MIN_AREA_HOLES: 600000000,
-    MAX_CONVEX_DEVIATION: 0.708
-}
+  WORLD_PATH: './geo/test/world.geojson',
+  OUT_PATH: './src/world2.json',
+  ERROR_PATH: './geo',
+  MIN_AREA_HOLES: 600000000,
+  MAX_CONVEX_DEVIATION: 0.708,
+  MIN_AREA_INTERSECTION: 500000
+};
 
 const fc = getJSON(config.WORLD_PATH);
 validateGeometry(fc, config);
 detectChanges(fc, config);
 generateTopojson(fc, config);
-
-
