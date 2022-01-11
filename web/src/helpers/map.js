8be44bc5632c26c7a1a6b7b6a1de7601d71671da
Mads Nedergaard <nedergaardmads@gmail.com>
2022-01-11 11:41:07 +0100

    Fixes crash on specific, small zones  (#3658)

--- web/src/helpers/map.js
+++ web/src/helpers/map.js
@@ -1,4 +1,4 @@
-import { max, min, mean } from 'lodash';
+import { max, min, mean, isArray } from 'lodash';
 
 export function getCenteredLocationViewport([longitude, latitude]) {
   return {
@@ -13,12 +13,15 @@ export function getCenteredLocationViewport([longitude, latitude]) {
 // If the panel is open the zoom doesn't appear perfectly centered because
 // it centers on the whole window and not just the visible map part, which
 // is something one could fix in the future.
+// TODO: this could be done build time instead of runtime
 export function getCenteredZoneViewport(zone) {
   const longitudes = [];
   const latitudes = [];
 
   zone.geometry.coordinates.forEach((geojson) => {
-    geojson[0].forEach(([longitude, latitude]) => {
+    // There seems to be an inconsistency in the generate-topojson script:
+    const data = isArray(geojson[0][0]) ? geojson[0] : geojson;
+    data.forEach(([longitude, latitude]) => {
       longitudes.push(longitude);
       latitudes.push(latitude);
     });
