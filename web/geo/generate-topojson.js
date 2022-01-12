05c033f2c7b035b1b1ea5fc024ccd5aec43b8100
Kenneth Skovhus <skovhus@users.noreply.github.com>
2022-01-12 14:50:43 +0100

    Pre-compute zone center compile time (#3660)
    * Precompute center for all zones
    
    * Use pre-computed center
    
    This also removes the US specific hack that I cannot see does anything
    
    * Update world.json

--- web/geo/generate-topojson.js
+++ web/geo/generate-topojson.js
@@ -1,20 +1,56 @@
+const { max, min, mean } = require("lodash");
 const { topology } = require("topojson-server");
-const { getJSON, writeJSON } = require("./utilities")
+const { getJSON, writeJSON } = require("./utilities");
+const turf = require("@turf/turf");
 
+function getCenter(geojson, zoneName) {
+  const geojsonFeatures = geojson.features.filter(
+    (f) => f.properties.zoneName === zoneName
+  );
+  if (geojsonFeatures.length !== 1) {
+    console.error(
+      `ERROR: Found ${geojsonFeatures.length} features matching zoneName ${zoneName}`
+    );
+    process.exit(1);
+  }
 
-function generateTopojson(fc, {OUT_PATH, verifyNoUpdates}) {
+  const longitudes = [];
+  const latitudes = [];
+
+  turf.explode(geojsonFeatures[0].geometry).features.forEach(({ geometry }) => {
+    const [longitude, latitude] = geometry.coordinates;
+    longitudes.push(longitude);
+    latitudes.push(latitude);
+  });
+
+  if (longitudes.length === 0 || latitudes.length === 0) {
+    console.error(
+      `ERROR: Found ${longitudes.length} longitudes and ${latitudes} latitudes for zoneName ${zoneName}`
+    );
+    process.exit(1);
+  }
+
+  return [
+    mean([min(longitudes), max(longitudes)]),
+    mean([min(latitudes), max(latitudes)]),
+  ];
+}
+
+function generateTopojson(fc, { OUT_PATH, verifyNoUpdates }) {
   console.log("Generating new world.json");
   const topo = topology({
     objects: fc,
   });
 
   // We do the following to match the specific format needed for visualization
-  const geoms = topo.objects.objects.geometries
-  const newObjects = {}
-  geoms.forEach(geo => {
-    newObjects[geo.properties.zoneName] = geo
+  const newObjects = {};
+  topo.objects.objects.geometries.forEach((geo) => {
+    // Precompute center for enable centering on the zone
+    geo.properties.center = getCenter(fc, geo.properties.zoneName);
+
+    newObjects[geo.properties.zoneName] = geo;
   });
-  topo.objects = newObjects
+  topo.objects = newObjects;
 
   const currentTopo = getJSON(OUT_PATH);
   if (JSON.stringify(currentTopo) === JSON.stringify(topo)) {
@@ -23,11 +59,13 @@ function generateTopojson(fc, {OUT_PATH, verifyNoUpdates}) {
   }
 
   if (verifyNoUpdates) {
-    console.error('Did not expect any updates to world.json. Please run "yarn update-world"');
+    console.error(
+      'Did not expect any updates to world.json. Please run "yarn update-world"'
+    );
     process.exit(1);
   }
 
-  writeJSON(OUT_PATH, topo)
+  writeJSON(OUT_PATH, topo);
 }
 
 module.exports = { generateTopojson };
