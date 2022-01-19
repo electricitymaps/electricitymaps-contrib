7dfee48174743388242ea02e47fbfd66f232ee6e
Nico1320 <brandon.gesterkamp@gmail.com>
2022-01-18 22:09:25 -0400

    Update parsers/AW.py
    Co-authored-by: jarek <jarek@piorkowski.ca>

--- parsers/AW.py
+++ parsers/AW.py
@@ -24,12 +24,17 @@ def fetch_production(zone_key='AW', session=None, target_datetime=None, logger=N
     # biogas live value is 0 MW all the time (2021)
     biogas = top_data['total_bio_gas']
     total = top_data['TotalPower']
-    # if "unknown" exceeds total power, it becomes 0
-    unknwon_check = float(total['value']) - float(fossil['value']) - float(wind['value']) - float(solar['value']) - float(biogas['value'])
-    if unknwon_check > 0:
-        unknown = unknwon_check
-    else:
-        unknown = float(0)
+    # "unknown" is when data reported in the categories above is less than total reported.
+    # If categories sum up to more than total, accept the datapoint, but only if it's less than 2% of total.
+    # This helps avoid missing data when it's a little bit off, due to rounding or reporting
+    reported_total = float(total['value'])
+    sources_total = float(fossil['value']) + float(wind['value']) + float(solar['value']) + float(biogas['value'])
+
+    if (sources_total / reported_total) > 1.02:
+        raise RuntimeError(f'AW parser reports fuel sources add up to {sources_total} but total generation {reported_total} is lower')
+
+    missing_from_total = reported_total - sources_total
+    unknown = missing_from_total if missing_from_total > 0 else 0
     # We're using Fossil data to get timestamp in correct time zone
     local_date_time = datetime.datetime.strptime(fossil['timestamp'], "%Y-%m-%d %H:%M:%S.%f")
     zone_date_time = arrow.Arrow.fromdatetime(local_date_time, 'America/Aruba')
