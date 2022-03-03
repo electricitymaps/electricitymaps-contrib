e6ae352c5cfa325a339100b1e23f1a0a4c79afc1
Mads Nedergaard <nedergaardmads@gmail.com>
2022-03-03 15:41:47 +0100

    Creates new useTranslation hook that handles interpolation

--- web/src/helpers/translation.js
+++ web/src/helpers/translation.js
@@ -1,24 +1,25 @@
-/* eslint-disable prefer-rest-params */
-/* eslint-disable prefer-spread */
-// TODO: re-enable rules
-// TODO: move to LinguiJS or react-intl that doesn't depend on the global
-// object or a node.js process running.
-
 import { vsprintf } from 'sprintf-js';
+import i18next from './i18n';
+import { useTranslation as useTranslationHook } from 'react-i18next';
 
-function translateWithLocale(locale, keyStr) {
-  const keys = keyStr.split('.');
-  let result = window.locales[locale];
-  for (let i = 0; i < keys.length; i += 1) {
-    if (result == null) { break; }
-    result = result[keys[i]];
+// Todo: We should get rid of vsprintf and use i18next interpolation instead
+const translateWithTranslator = (translator, key, ...args) => {
+  const translation = translator(key, '');
+  if (args.length > 0) {
+    return vsprintf(translation, args);
   }
-  if (locale !== 'en' && !result) {
-    return translateWithLocale('en', keyStr);
-  }
-  const formatArgs = Array.prototype.slice.call(arguments).slice(2); // remove 2 first
-  return result && vsprintf(result, formatArgs);
-}
+  return translation;
+};
+
+// Used for translation outside of React components
+export const translate = (key, ...args) => translateWithTranslator(i18next.t, key, ...args);
+
+// Hook for translations inside React components
+export const useTranslation = () => {
+  const { t } = useTranslationHook();
+  const __ = (key, ...args) => translateWithTranslator(t, key, ...args);
+  return { __ };
+};
 
 export function translate() {
   const args = Array.prototype.slice.call(arguments);
