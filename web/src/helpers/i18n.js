a257d4bb35d523a811d16bc6206b49d26cdd8cca
Mads Nedergaard <nedergaardmads@gmail.com>
2022-03-03 13:37:59 +0100

    Disables debug mode in production

--- web/src/helpers/i18n.js
+++ web/src/helpers/i18n.js
@@ -2,6 +2,7 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
 import HttpApi from 'i18next-http-backend';
+import { isProduction } from './environment';
 //import LanguageDetector from 'i18next-browser-languagedetector';
 
 // Init localisation package
@@ -12,7 +13,7 @@ await i18n
   .use(initReactI18next)
   .init({
     fallbackLng: 'en',
-    debug: true,
+    debug: isProduction() ? false : true,
 
     backend: {
       loadPath: '/locales/{{lng}}.json',
