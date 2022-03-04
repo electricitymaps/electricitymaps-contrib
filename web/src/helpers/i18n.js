9c97fc29222eccc82b84da9f47cdca3f03b854b4
Mads Nedergaard <nedergaardmads@gmail.com>
2022-03-04 08:15:40 +0100

    Adds language detection (and saving choice as cookie)

--- web/src/helpers/i18n.js
+++ web/src/helpers/i18n.js
@@ -1,8 +1,8 @@
 import moment from 'moment';
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-
 import HttpApi from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import { isProduction } from './environment';
 //import LanguageDetector from 'i18next-browser-languagedetector';
 import { history } from './router';
@@ -19,16 +19,19 @@ function hideLanguageSearchParam() {
 i18n
   .use(HttpApi)
   // detect user language
-  // .use(LanguageDetector)
+  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     fallbackLng: 'en',
     debug: isProduction() ? false : true,
-
     backend: {
       loadPath: '/locales/{{lng}}.json',
     },
-
+    detection: {
+      order: ['querystring', 'cookie', 'localStorage', 'sessionStorage', 'navigator', 'htmlTag'],
+      lookupQuerystring: 'lang',
+      caches: ['localStorage', 'cookie'],
+    },
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
