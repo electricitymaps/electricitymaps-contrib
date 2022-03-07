5a70fb102574f0facccf0ecbd2ad4077ec0b5eb4
Mads Nedergaard <nedergaardmads@gmail.com>
2022-03-07 14:48:14 +0100

    Adds request function to support cross-platform

--- web/src/helpers/i18n.js
+++ web/src/helpers/i18n.js
@@ -15,6 +15,43 @@ function hideLanguageSearchParam() {
   history.replace(`?${searchParams.toString()}`);
 }
 
+// This function is copied and slightly adjusted from https://github.com/i18next/i18next-http-backend/blob/master/lib/request.js
+// The changes are done in order to make it work cross-platform (Cordova did not support file://)
+// See source for these changes: https://github.com/i18next/i18next-http-backend/issues/23#issuecomment-718929822
+function requestWithXmlHttpRequest(options, url, payload, callback) {
+  try {
+    const x = new XMLHttpRequest();
+    x.open('GET', url, 1);
+    if (!options.crossDomain) {
+      x.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    }
+    x.withCredentials = Boolean(options.withCredentials);
+    if (payload) {
+      x.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    }
+    if (x.overrideMimeType) {
+      x.overrideMimeType('application/json');
+    }
+    let h = options.customHeaders;
+    h = typeof h === 'function' ? h() : h;
+    if (h) {
+      for (var i in h) {
+        x.setRequestHeader(i, h[i]);
+      }
+    }
+    x.onreadystatechange = () => {
+      x.readyState > 3 &&
+        callback(x.status >= 400 ? x.statusText : null, {
+          status: x.status || 200,
+          data: x.responseText,
+        }); // in android webview loading a file is status status 0
+    };
+    x.send(payload);
+  } catch (e) {
+    console && console.log(e);
+  }
+}
+
 // Init localisation package
 i18n
   .use(HttpApi)
@@ -26,7 +63,8 @@ i18n
     debug: isProduction() ? false : true,
     backend: {
       loadPath: 'locales/{{lng}}.json',
-      crossDomain: true
+      crossDomain: true,
+      request: requestWithXmlHttpRequest,
     },
     detection: {
       order: ['querystring', 'cookie', 'localStorage', 'sessionStorage', 'navigator', 'htmlTag'],
@@ -36,11 +74,12 @@ i18n
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
-  }).then(() => {
+  })
+  .then(() => {
     hideLanguageSearchParam();
   });
 
-i18n.on('languageChanged', function(lng) {
+i18n.on('languageChanged', function (lng) {
   moment.locale(lng);
   document.documentElement.setAttribute('lang', lng);
 });
