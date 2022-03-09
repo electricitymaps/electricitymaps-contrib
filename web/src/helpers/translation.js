0f90f41d2bc0650e906293a95af0f2b726a44a88
Mads Nedergaard <nedergaardmads@gmail.com>
2022-03-09 15:31:21 +0100

    Replaces translate functionality and adds custom hook

--- web/src/helpers/translation.js
+++ web/src/helpers/translation.js
@@ -1,31 +1,29 @@
-/* eslint-disable prefer-rest-params */
-/* eslint-disable prefer-spread */
-// TODO: re-enable rules
-// TODO: move to LinguiJS or react-intl that doesn't depend on the global
-// object or a node.js process running.
-
 import { vsprintf } from 'sprintf-js';
+import { useTranslation as useTranslationHook } from 'react-i18next';
+import i18next from './i18n';
 
-function translateWithLocale(locale, keyStr) {
-  const keys = keyStr.split('.');
-  let result = window.locales[locale];
-  for (let i = 0; i < keys.length; i += 1) {
-    if (result == null) { break; }
-    result = result[keys[i]];
+// Todo: We should get rid of vsprintf and use i18next interpolation instead
+const translateWithTranslator = (translator, key, ...args) => {
+  const translation = translator(key, '');
+  if (args.length > 0) {
+    return vsprintf(translation, args);
   }
-  if (locale !== 'en' && !result) {
-    return translateWithLocale('en', keyStr);
-  }
-  const formatArgs = Array.prototype.slice.call(arguments).slice(2); // remove 2 first
-  return result && vsprintf(result, formatArgs);
-}
+  return translation;
+};
 
-export function translate() {
-  const args = Array.prototype.slice.call(arguments);
-  // Prepend locale
-  args.unshift(window.locale);
-  return translateWithLocale.apply(null, args);
-}
+// Used for translation outside of React components
+export const translate = (key, ...args) => translateWithTranslator(i18next.t, key, ...args);
+
+// Hook for translations inside React components
+export const useTranslation = () => {
+  const { t, i18n } = useTranslationHook();
+  const __ = (key, ...args) => translateWithTranslator(t, key, ...args);
+  return { __, i18n };
+};
+
+export const translateIfExists = (key) => {
+  return i18next.exists(key) ? i18next.t(key) : '';
+};
 
 
 export const getZoneName = (zoneCode) => translate(`zoneShortName.${zoneCode}.zoneName`);
@@ -71,4 +69,3 @@ export function getShortenedZoneNameWithCountry(zoneCode, limit = 40) {
   return `${zoneName} (${countryName})`;
 }
 
-export const __ = translate;
