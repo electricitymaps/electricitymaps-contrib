b7d625a8f2e71d3c1cc737174a2078fa5b231821
Mads Nedergaard <nedergaardmads@gmail.com>
2022-03-14 17:03:57 +0100

    Ignores eslint-rule to avoid changing anything here

--- web/translation-helper.js
+++ web/translation-helper.js
@@ -8,10 +8,10 @@ const languages = fs.readdirSync(LOCALES_PATH)
   .filter(f => f !== 'en.json' && f.endsWith('.json'))
   .map(f => f.replace('.json', ''));
 
-console.log('Languages you can translate: ' + languages.join(', ').green);
+console.log(`Languages you can translate: ${languages.join(', ').green}`);
 const language = readline.question('Which language do you want to translate: ');
 if (!languages.includes(language)) {
-  console.error((language + ' is not a translatable language!').red);
+  console.error((`${language} is not a translatable language!`).red);
   return;
 }
 
@@ -23,13 +23,15 @@ const other = JSON.parse(fs.readFileSync(`${LOCALES_PATH}/${language}.json`));
 function checkUntranslated(e, o, p) {
   if (typeof e === 'string') {
     if (o === undefined) {
-      o = readline.question(p.slice(0, -1).blue + ' [' + e.green + ']: ');
+      // eslint-disable-next-line no-param-reassign
+      o = readline.question(`${p.slice(0, -1).blue} [${e.green}]: `);
     }
   } else {
     Object.keys(e).forEach(key => {
-      const t = checkUntranslated(e[key], o !== undefined ? o[key]: undefined, p + key + '.');
+      const t = checkUntranslated(e[key], o !== undefined ? o[key]: undefined, `${p + key}.`);
       if (t !== '') {
         if (o === undefined) {
+          // eslint-disable-next-line no-param-reassign
           o = {};
         }
         o[key] = t;
