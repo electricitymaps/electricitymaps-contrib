72ea903c9c70821de8a86477e9a23665147ae045
Markus Killendahl <markus.killendahl@tmrow.com>
2022-03-15 14:47:33 +0100

    Adds twitter track

--- web/src/services/thirdparty/twitter.js
+++ web/src/services/thirdparty/twitter.js
@@ -25,11 +25,7 @@ class TwitterThirdParty {
             this.inst.events.bind('click', function (event) {
                 // event.region is {tweet,follow}
                 const thirdPartyService = require('../thirdparty');
-                thirdPartyService.trackEvent(event.region);
-                thirdPartyService.ga('event', event.region, {
-                  event_category: 'social',
-                  event_label: 'twitter',
-                });
+                thirdPartyService.trackEvent(event.region, {event_category: "social", event_label: "twitter"});
             });
         });
     }
