kruschk/electricitymap-contrib

Switches to hook-based approach in all components

web/src/components/tooltips/countrypanelexchangetooltip.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { __, getFullZoneName } from '../../helpers/translation';
+import { useTranslation, getFullZoneName } from '../../helpers/translation';
 import { formatCo2, formatPower } from '../../helpers/formatting';
 import { flagUri } from '../../helpers/flags';
 import { getRatioPercent } from '../../helpers/math';
@@ -23,6 +23,7 @@ const CountryPanelExchangeTooltip = ({
   zoneData,
   onClose,
 }) => {
+  const { __ } = useTranslation();
   if (!zoneData) return null;
 
   const co2Intensity = getExchangeCo2Intensity(exchangeKey, zoneData, electricityMixMode);

web/src/components/tooltips/countrypanelproductiontooltip.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { isFinite } from 'lodash';
 
-import { __, getFullZoneName } from '../../helpers/translation';
+import { useTranslation, getFullZoneName } from '../../helpers/translation';
 import { formatCo2, formatPower } from '../../helpers/formatting';
 import { flagUri } from '../../helpers/flags';
 import { getRatioPercent } from '../../helpers/math';
@@ -26,6 +26,7 @@ const CountryPanelProductionTooltip = ({
   zoneData,
   onClose,
 }) => {
+  const { __ } = useTranslation();
   if (!zoneData) return null;
 
   const co2Intensity = getProductionCo2Intensity(mode, zoneData);

web/src/layout/leftpanel/countrypanel.js
@@ -35,7 +35,7 @@ import { dispatchApplication } from '../../store';
 import { useCurrentZoneData } from '../../hooks/redux';
 import { useTrackEvent } from '../../hooks/tracking';
 import { flagUri } from '../../helpers/flags';
-import { getFullZoneName, __ } from '../../helpers/translation';
+import { useTranslation, getFullZoneName } from '../../helpers/translation';
 import EstimatedLabel from '../../components/countryestimationlabel';
 
 // TODO: Move all styles from styles.css to here
@@ -167,11 +167,11 @@ const EstimatedDataInfoBox = styled.p`
   margin: 1rem 0;
 `;
 
-const EstimatedDataInfo = () => (
+const EstimatedDataInfo = (text) => (
   <React.Fragment>
     <EstimatedDataInfoBox
       dangerouslySetInnerHTML={{
-        __html: __('country-panel.dataIsEstimated'),
+        __html: text,
       }}
     />
     <hr />
@@ -185,6 +185,7 @@ const CountryPanel = ({
   zones,
 }) => {
   const [tooltip, setTooltip] = useState(null);
+  const {__} = useTranslation();
 
   const isLoadingHistories = useSelector(state => state.data.isLoadingHistories);
 
@@ -320,7 +321,7 @@ const CountryPanel = ({
             <CountryTable />
 
             <hr />
-            {isDataEstimated && <EstimatedDataInfo />}
+            {isDataEstimated && <EstimatedDataInfo text={__('country-panel.dataIsEstimated')} />}
             <div className="country-history">
               <CountryHistoryTitle>
                 {__(tableDisplayEmissions ? 'country-history.emissions24h' : 'country-history.carbonintensity24h')}

electricitymaps/electricitymaps-contrib

web/src/components/tooltips/countrypanelexchangetooltip.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { __, getZoneNameWithCountry } from '../../helpers/translation';
+import { useTranslation, getZoneNameWithCountry } from '../../helpers/translation';
 import { formatCo2, formatPower } from '../../helpers/formatting';
 import { flagUri } from '../../helpers/flags';
 import { getRatioPercent } from '../../helpers/math';
@@ -23,6 +23,7 @@ const CountryPanelExchangeTooltip = ({
   zoneData,
   onClose,
 }) => {
+  const { __ } = useTranslation();
   if (!zoneData) return null;
 
   const co2Intensity = getExchangeCo2Intensity(exchangeKey, zoneData, electricityMixMode);

web/src/components/tooltips/countrypanelproductiontooltip.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { isFinite } from 'lodash';
 
-import { __, getZoneNameWithCountry } from '../../helpers/translation';
+import { useTranslation, getZoneNameWithCountry } from '../../helpers/translation';
 import { formatCo2, formatPower } from '../../helpers/formatting';
 import { flagUri } from '../../helpers/flags';
 import { getRatioPercent } from '../../helpers/math';
@@ -26,6 +26,7 @@ const CountryPanelProductionTooltip = ({
   zoneData,
   onClose,
 }) => {
+  const { __ } = useTranslation();
   if (!zoneData) return null;
 
   const co2Intensity = getProductionCo2Intensity(mode, zoneData);

web/src/layout/leftpanel/countrypanel.js
@@ -35,7 +35,7 @@ import { dispatchApplication } from '../../store';
 import { useCurrentZoneData } from '../../hooks/redux';
 import { useTrackEvent } from '../../hooks/tracking';
 import { flagUri } from '../../helpers/flags';
-import { getZoneNameWithCountry, __ } from '../../helpers/translation';
+import { useTranslation, getZoneNameWithCountry } from '../../helpers/translation';
 import EstimatedLabel from '../../components/countryestimationlabel';
 
 // TODO: Move all styles from styles.css to here
@@ -167,11 +167,11 @@ const EstimatedDataInfoBox = styled.p`
   margin: 1rem 0;
 `;
 
-const EstimatedDataInfo = () => (
+const EstimatedDataInfo = (text) => (
   <React.Fragment>
     <EstimatedDataInfoBox
       dangerouslySetInnerHTML={{
-        __html: __('country-panel.dataIsEstimated'),
+        __html: text,
       }}
     />
     <hr />
@@ -185,6 +185,7 @@ const CountryPanel = ({
   zones,
 }) => {
   const [tooltip, setTooltip] = useState(null);
+  const {__} = useTranslation();
 
   const isLoadingHistories = useSelector(state => state.data.isLoadingHistories);
 
@@ -320,7 +321,7 @@ const CountryPanel = ({
             <CountryTable />
 
             <hr />
-            {isDataEstimated && <EstimatedDataInfo />}
+            {isDataEstimated && <EstimatedDataInfo text={__('country-panel.dataIsEstimated')} />}
             <div className="country-history">
               <CountryHistoryTitle>
                 {__(tableDisplayEmissions ? 'country-history.emissions24h' : 'country-history.carbonintensity24h')}