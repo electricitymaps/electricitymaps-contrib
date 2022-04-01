2bdd50a6d6859a6561da619766a9f3a75be7514a
Markus Killendahl <markus.killendahl@tmrow.com>
2022-04-01 13:52:22 +0200

    revert mockserver change

--- mockserver/server.js
+++ mockserver/server.js
@@ -11,7 +11,7 @@ app.get("/v4/history", (req, res, next) => {
   const { countryCode } = req.query;
   if (countryCode && fs.existsSync(`./public/v4/history_${countryCode}`)) {
     // we alter the URL to search for the specific history file if available
-    res.redirect(`/v4/history_${countryCode}_min`);
+    res.redirect(`/v4/history_${countryCode}`);
   } else {
     next();
   }
