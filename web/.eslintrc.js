d43a703b8055d092fc4cba6bab480d24da983cf4
VIKTORVAV99 <30777521+VIKTORVAV99@users.noreply.github.com>
2022-04-20 11:14:59 +0200

    error on all lodash rules

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   parser: 'babel-eslint',
   extends: [
     'eslint:recommended',
-    'plugin:you-dont-need-lodash-underscore/compatible',
+    'plugin:you-dont-need-lodash-underscore/all',
     'plugin:react/recommended',
     'plugin:jest/recommended',
     'plugin:import/errors',
