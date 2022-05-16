8ce36f739693c2ce58eb3f145dd603ece06035d9
Mads Nedergaard <nedergaardmads@gmail.com>
2022-05-16 17:17:55 +0200

    Fixes comments in layers

--- web/src/hooks/layers.js
+++ web/src/hooks/layers.js
@@ -39,20 +39,22 @@ export function useInterpolatedWindData() {
       return null;
     }
 
+    // eslint-disable-next-line no-console
     console.log(
       `#1 wind forecast target ${formatDistance(tBefore, new Date(), { addSuffix: true })} made ${formatDistance(
         getRefTime(gribs1[0]),
         new Date(),
         { addSuffix: true }
       )}`
-    ); // eslint-disable-line no-console
+    );
+    // eslint-disable-next-line no-console
     console.log(
       `#2 wind forecast target ${formatDistance(tAfter, new Date(), { addSuffix: true })} made ${formatDistance(
         getRefTime(gribs2[0]),
         new Date(),
         { addSuffix: true }
       )}`
-    ); // eslint-disable-line no-console
+    );
 
     return [
       { ...gribs1[0], data: gribs1[0].data.map((d, i) => interpolate(d, gribs2[0].data[i])(k)) },
@@ -83,20 +85,22 @@ export function useInterpolatedSolarData() {
       return null;
     }
 
+    // eslint-disable-next-line no-console
     console.log(
       `#1 solar forecast target ${formatDistance(tBefore, new Date(), { addSuffix: true })} made ${formatDistance(
         getRefTime(grib1),
         new Date(),
         { addSuffix: true }
       )}`
-    ); // eslint-disable-line no-console
+    );
+    // eslint-disable-next-line no-console
     console.log(
       `#2 solar forecast target ${formatDistance(tAfter, new Date(), { addSuffix: true })} made ${formatDistance(
         getRefTime(grib2),
         new Date(),
         { addSuffix: true }
       )}`
-    ); // eslint-disable-line no-console
+    );
 
     return { ...grib1, data: grib1.data.map((d, i) => interpolate(d, grib2.data[i])(k)) };
   }, [solarData, customDatetime]);
