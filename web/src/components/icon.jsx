07e3da0adc9f795d89e8c8e0980e9f9b5f464c77
Mads Nedergaard <nedergaardmads@gmail.com>
2022-05-30 09:33:12 +0200

    Moves env further down

--- web/src/components/icon.jsx
+++ web/src/components/icon.jsx
@@ -16,7 +16,10 @@ const IconContainer = styled.svg`
  *     <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z"></path>
  *   </symbol>
  */
+
 const Icon = ({ iconName }) => {
+  // eslint-disable-next-line
+  console.log('test');
   return (
     <IconContainer>
       <use href={resolvePath(`images/material-icon-sprite.svg#${iconName}`)} />
