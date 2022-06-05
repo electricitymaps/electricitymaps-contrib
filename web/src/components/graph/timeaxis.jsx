aa0a35cd8c5b321e91e5e84e42950ff6bba0dce0
Markus Killendahl <markus.killendahl@tmrow.com>
2022-06-05 19:17:47 +0200

    Ensure proper timeaxis frequency

--- web/src/components/graph/timeaxis.jsx
+++ web/src/components/graph/timeaxis.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 import { formatDateTick } from '../../helpers/formatting';
 import { useTranslation } from '../../helpers/translation';
 
-const TICK_VALUE_FREQUENCY = 6; // Frequency at which values are displayed for a tick
+// Frequency at which values are displayed for a tick
+const TIME_TO_TICK_FREQUENCY = {
+  HOURLY: 6,
+  DAILY: 6,
+  MONTHLY: 1,
+  YEARLY: 1,
+};
 
 const renderTickValue = (v, idx, displayLive, lang, selectedTimeAggregate) => {
   const shouldDisplayLive = idx === 24 && displayLive; // TODO: change this for other aggregations
@@ -42,7 +48,7 @@ const getTicksValuesFromTimeScale = (scale, count) => {
 };
 
 const renderTick = (scale, val, idx, displayLive, lang, selectedTimeAggregate) => {
-  const shouldShowValue = idx % TICK_VALUE_FREQUENCY === 0;
+  const shouldShowValue = idx % TIME_TO_TICK_FREQUENCY[selectedTimeAggregate.toUpperCase()] === 0;
   return (
     <g key={`timeaxis-tick-${idx}`} className="tick" opacity={1} transform={`translate(${scale(val)},0)`}>
       <line stroke="currentColor" y2="6" opacity={shouldShowValue ? 0.5 : 0.2} />
@@ -51,20 +57,18 @@ const renderTick = (scale, val, idx, displayLive, lang, selectedTimeAggregate) =
   );
 };
 
-const TimeAxis = React.memo(
-  ({ className, scale, transform, displayLive, tickCount = 25, selectedTimeAggregate = 'hourly' }) => {
-    const [x1, x2] = scale.range();
-    const { i18n } = useTranslation();
+const TimeAxis = React.memo(({ className, scale, transform, displayLive, tickCount, selectedTimeAggregate }) => {
+  const [x1, x2] = scale.range();
+  const { i18n } = useTranslation();
 
-    return (
-      <g className={className} transform={transform} fill="none" textAnchor="middle" style={{ pointerEvents: 'none' }}>
-        <path className="domain" stroke="currentColor" d={`M${x1 + 0.5},6V0.5H${x2 + 0.5}V6`} />
-        {getTicksValuesFromTimeScale(scale, tickCount).map((v, idx) =>
-          renderTick(scale, v, idx, displayLive, i18n.language, selectedTimeAggregate)
-        )}
-      </g>
-    );
-  }
-);
+  return (
+    <g className={className} transform={transform} fill="none" textAnchor="middle" style={{ pointerEvents: 'none' }}>
+      <path className="domain" stroke="currentColor" d={`M${x1 + 0.5},6V0.5H${x2 + 0.5}V6`} />
+      {getTicksValuesFromTimeScale(scale, tickCount).map((v, idx) =>
+        renderTick(scale, v, idx, displayLive, i18n.language, selectedTimeAggregate)
+      )}
+    </g>
+  );
+});
 
 export default TimeAxis;
