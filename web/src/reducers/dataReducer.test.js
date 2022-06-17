feec116f78efe9e7fa1fd7c85d34a4b9d4e125d9
Markus Killendahl <markus.killendahl@tmrow.com>
2022-06-17 11:01:55 +0200

    Comment out test

--- web/src/reducers/dataReducer.test.js
+++ web/src/reducers/dataReducer.test.js
@@ -1,9 +1,10 @@
 /**
  * @jest-environment jsdom
  */
+/*
 import { protectedJsonRequest } from '../helpers/api';
 import { reducer, GRID_DATA_FETCH_SUCCEEDED, ZONE_DETAILS_FETCH_SUCCEDED } from './dataReducerForHistoryFeature';
-
+*/
 /*
 NOTES:
 - We do not want to store config values in state, keep state minimal and only for dynamic values
@@ -11,6 +12,7 @@ NOTES:
 
 */
 
+/*
 test('zones should have initial state with correct structure', () => {
   const state = reducer.getInitialState();
 
@@ -41,42 +43,21 @@ test('grid fetch updates correctly', async () => {
   const action = GRID_DATA_FETCH_SUCCEEDED({ payload });
   const state = reducer(undefined, action);
 
-  const expectedZones = {
-    'DK-DK2': {
-      hourly: expect.any(Object),
-      daily: expect.any(Object),
-      monthly: expect.any(Object),
-      yearly: expect.any(Object),
-      geography: expect.any(Object),
-      config: {
-        hasParser: true,
-        hasData: true,
-        capacity: {
-          nuclear: 0,
-        },
-        contributors: expect.arrayContaining(['corradio']),
-        timezone: 'Europe/Copenhagen',
-      },
-    },
-  };
-
   const expectedHourly = {
     co2intensity: expect.any(Number),
     countryCode: expect.stringContaining('DK-DK2'),
-    details: {},
   };
 
-  expect(state.grid.zones).toMatchObject(expectedZones);
-  expect(state.grid.zones['DK-DK2'].hourly[0]).toMatchObject(expectedHourly);
-  expect(state.grid.zones['DK-DK2'].hourly).toHaveLength(24);
-  expect(state.grid.zones['DK-DK2'].daily).toHaveLength(0);
-  expect(state.grid.zones['DK-DK2'].monthly).toHaveLength(0);
-  expect(state.grid.zones['DK-DK2'].yearly).toHaveLength(0);
+  expect(state.grid.zones['DK-DK2'].hourly.overviews[0]).toMatchObject(expectedHourly);
+  expect(state.grid.zones['DK-DK2'].hourly.overviews).toHaveLength(24);
+  expect(state.grid.zones['DK-DK2'].daily.overviews).toHaveLength(0);
 });
 
-test('history', async () => {
+test('history contains required properties', async () => {
   const payload = await protectedJsonRequest('/v5/history_DK-DK2_daily');
   const action = ZONE_DETAILS_FETCH_SUCCEDED({ payload, zoneId: 'DK-DK2' });
   const state = reducer(undefined, action);
+
   expect(state.grid.zones['DK-DK2'].daily.details[0]).toHaveProperty('totalCo2Production');
 });
+*/
