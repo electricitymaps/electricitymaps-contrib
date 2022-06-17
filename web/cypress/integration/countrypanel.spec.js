0da11c330858626dc59c6a801cc5c653095f8086
Mads Nedergaard <nedergaardmads@gmail.com>
2022-06-17 16:35:43 +0200

    Adds general test that interacts with the map

--- web/cypress/integration/countrypanel.spec.js
+++ web/cypress/integration/countrypanel.spec.js
@@ -1,6 +1,18 @@
 describe('Country Panel', () => {
+  it('interacts with details', () => {
+    cy.visit('/zone/DK-DK2?skip-onboarding=true');
+    cy.contains('East Denmark');
+    cy.contains('Carbon Intensity');
+    cy.get('#country-lowcarbon-gauge').trigger('mousemove');
+    cy.contains('Includes renewables and nuclear');
+    cy.get('#country-lowcarbon-gauge').trigger('mouseout');
+    cy.contains('Carbon emissions').should('not.have.class', 'selected');
+    cy.contains('Carbon emissions').click().should('have.class', 'selected');
+    cy.contains('0 t/min');
+  });
+
   it('asserts countryPanel contains no parser message', () => {
-    cy.visit('/zone/CN?remote=true');
+    cy.visit('/zone/CN');
     cy.get('[data-test-id=no-parser-message]').should('exist');
   });
 });
