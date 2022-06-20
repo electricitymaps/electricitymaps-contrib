86fffdd838b8f61629beadb401d55ed259cc96a7
Felix Qvist <felix.qvist@gmail.com>
2022-06-20 13:53:20 +0200

    Added tests for validators

---
+++ tests/validators/test_sanity_checks.py
@@ -0,0 +1,21 @@
+from electricitymap.contrib.validators.sanity_checks import (
+    validate_positive_production,
+    validate_production_one_non_nan_value,
+)
+from .lib.load_fixture import load_fixture
+import pandas as pd
+
+
+def test_validate_positive_production():
+    events = load_fixture("production_negative_values")
+    res = validate_positive_production(events)
+    assert res["2022-01-01 00:00:00+00:00"] == 1
+    assert res["2022-01-01 01:00:00+00:00"] == 0
+    assert res["2022-01-01 02:00:00+00:00"] == 1
+
+def test_validate_production_one_non_nan_value():
+    events = load_fixture("production_with_no_values")
+    res = validate_production_one_non_nan_value(events)
+    assert res["2022-01-01 00:00:00+00:00"] == 1
+    assert res["2022-01-01 01:00:00+00:00"] == 0
+    assert res["2022-01-01 02:00:00+00:00"] == 1
