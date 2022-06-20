ea7a7b8f34d47c246bc9f4d5140274f5e83e0dd6
Mads Nedergaard <nedergaardmads@gmail.com>
2022-06-20 07:36:47 +0200

    Tests slider

--- web/cypress/integration/countrypanel.spec.js
+++ web/cypress/integration/countrypanel.spec.js
@@ -9,10 +9,18 @@ describe('Country Panel', () => {
     cy.contains('Carbon emissions').should('not.have.class', 'selected');
     cy.contains('Carbon emissions').click().should('have.class', 'selected');
     cy.contains('0 t/min');
+
+    cy.get('.country-col').contains('182');
+    cy.get('input.time-slider-input').should('have.class', 'day');
+    cy.get('input.time-slider-input').setSliderValue('1641243600000');
+    cy.get('input.time-slider-input').should('have.class', 'night');
+    cy.get('.country-col').contains('124');
+
+    cy.get('.left-panel-back-button').click();
   });
 
-  it('asserts countryPanel contains no parser message', () => {
-    cy.visit('/zone/CN');
+  it('asserts countryPanel contains no parser message when zone has no data', () => {
+    cy.visit('/zone/CN?remote=true');
     cy.get('[data-test-id=no-parser-message]').should('exist');
   });
 });
