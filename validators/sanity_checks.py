09398baca03295d4f6ac9fe93a7d3625333e0148
Felix Qvist <felix.qvist@gmail.com>
2022-06-20 15:19:13 +0200

    Added tests for validators (#4268)
    * Add pytest
    
    * Invert so 1=valid and 0=invalid
    
    * Added tests for validators
    
    * format
    
    * Rename fixture file

--- validators/sanity_checks.py
+++ validators/sanity_checks.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-from validators.lib.config import validator
+from electricitymap.contrib.validators.lib.config import validator
 
 
 @validator(kind="production")
@@ -9,7 +9,7 @@ def validate_positive_production(events: pd.DataFrame) -> pd.Series:
     Validate that the production is positive. (Allows nan values)
     """
     production_cols = [col for col in events if col.startswith("production")]
-    res = (events[production_cols] < 0).any(axis=1).astype(int)
+    res = 1 - (events[production_cols] < 0).any(axis=1).astype(int)
     return res
 
 
@@ -19,5 +19,5 @@ def validate_production_one_non_nan_value(events: pd.DataFrame) -> pd.Series:
     Validate that the production has at least one non-nan value.
     """
     production_cols = [col for col in events if col.startswith("production")]
-    res = 1 - events[production_cols].notnull().any(axis=1).astype(int)
+    res = events[production_cols].notnull().any(axis=1).astype(int)
     return res
