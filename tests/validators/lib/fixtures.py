d7cacc2814cffdee66351d9ea58323eedf0b3f2f
Felix Qvist <felix.qvist@gmail.com>
2022-06-20 16:35:03 +0200

    Added all old quality checks to new rule system

--- tests/validators/lib/fixtures.py
+++ tests/validators/lib/fixtures.py
@@ -5,6 +5,6 @@ import pandas as pd
 
 def load_fixture(name):
     path = os.path.join(os.path.dirname(__file__), "..", "fixtures", f"{name}.csv")
-    df = pd.read_csv(path, index_col="datetime")
+    df = pd.read_csv(path, index_col="datetime", parse_dates=True)
     df = df.apply(pd.to_numeric, errors="coerce")
     return df
--- validators/sanity_checks.py
+++ validators/sanity_checks.py
@@ -1,6 +1,9 @@
+from datetime import datetime
+
 import pandas as pd
 
 from electricitymap.contrib.validators.lib.config import validator
+from electricitymap.contrib.config import EXCHANGES_CONFIG
 
 
 @validator(kind="production")
@@ -20,4 +23,64 @@ def validate_production_one_non_nan_value(events: pd.DataFrame) -> pd.Series:
     """
     production_cols = [col for col in events if col.startswith("production")]
     res = events[production_cols].notnull().any(axis=1).astype(int)
+    return res
+
+@validator(kind="production")
+def validate_production_is_plausible(events: pd.DataFrame) -> pd.Series:
+    """
+    Validates that the production doesn't exceed 500GW
+    """
+    production_cols = [col for col in events if col.startswith("production")]
+    res = (events.fillna(0)[production_cols] < 500000).all(axis=1).astype(int)
+    return res
+
+@validator(kind="production")
+def validate_reasonable_time_production(events: pd.DataFrame) -> pd.Series:
+    """
+    Validates that the datetime is > year 2000 and not in the future
+    """
+    evaluation = (events.index > "2000-01-01") & (
+        events.index < datetime.now().isoformat()
+    )
+    res = pd.Series(evaluation, index=events.index).astype(int)
+    return res
+
+
+@validator(kind="exchange")
+def validate_reasonable_time_exchange(events: pd.DataFrame) -> pd.Series:
+    """
+    (Same test as `validate_reasonable_time_production`, but for exchange)
+    Validates that the datetime is > year 2000 and not in the future
+    """
+    evaluation = (events.index > "2000-01-01") & (
+        events.index < datetime.now().isoformat()
+    )
+    res = pd.Series(evaluation, index=events.index).astype(int)
+    return res
+
+
+@validator(kind="exchange")
+def validate_exchange_netflow_is_plausible(events: pd.DataFrame) -> pd.Series:
+    """
+    Validates that exchanges doesn't exceed 100GW
+    """
+    res = (abs(events["netFlow"]) < 100000).astype(int)
+    return res
+
+
+@validator(kind="exchange")
+def validate_exchange_netflow_doesnt_exceed_capacity(
+    events: pd.DataFrame, zone_key: str
+) -> pd.Series:
+    """
+    Validates that exchanges doesn't exceed the interconnector capacity by more than 10%
+    """
+    ALLOWED_MARGIN = 1.1
+    interconnector_capacities = EXCHANGES_CONFIG[zone_key]["capacity"]
+
+    res = (
+        (min(interconnector_capacities) * ALLOWED_MARGIN <= events["netFlow"])
+        & (events["netFlow"] <= max(interconnector_capacities) * ALLOWED_MARGIN)
+    ).astype(int)
+
     return res
