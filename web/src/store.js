129eb11b7c95856590cd2a9b565be0bd6fe21fe4
Mads Nedergaard <nedergaardmads@gmail.com>
2022-06-20 17:08:55 +0200

    Makes createStore simpler

--- web/src/store.js
+++ web/src/store.js
@@ -1,22 +1,11 @@
 import createSagaMiddleware from 'redux-saga';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 
 import reducer from './reducers';
-import { isProduction } from './helpers/environment';
 
 export const sagaMiddleware = createSagaMiddleware();
-
-export const store = isProduction()
-  ? createStore(
-      reducer,
-      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-      applyMiddleware(sagaMiddleware)
-    )
-  : createStore(
-      reducer,
-      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-      applyMiddleware(sagaMiddleware)
-    );
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+export const store = createStore(reducer, composeEnhancers(applyMiddleware(sagaMiddleware)));
 
 // TODO: Deprecate and use actioncreators instead
 export const dispatchApplication = (key, value) => {
