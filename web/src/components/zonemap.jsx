2d1aa041b06d29c69a7cbf5bbe4cbb921b7a01ad
Mads Nedergaard <nedergaardmads@gmail.com>
2022-06-21 11:27:30 +0200

    Creates ZoomControls component

--- web/src/components/zonemap.jsx
+++ web/src/components/zonemap.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useMemo, useRef, useEffect } from 'react';
 import { Portal } from 'react-portal';
-import ReactMapGL, { NavigationControl, Source, Layer } from 'react-map-gl';
+import ReactMapGL, { Source, Layer } from 'react-map-gl';
 import { noop } from '../helpers/noop';
 import { isEmpty } from '../helpers/isEmpty';
 import { debounce } from '../helpers/debounce';
+import { ZoomControls } from './zoomcontrols';
 
 const interactiveLayerIds = ['zones-clickable-layer'];
 const mapStyle = { version: 8, sources: {}, layers: [] };
@@ -35,8 +36,6 @@ const ZoneMap = ({
   },
   zones = {},
   zoneHistories = {},
-  zoomInLabel = '',
-  zoomOutLabel = '',
 }) => {
   const ref = useRef(null);
   const wrapperRef = useRef(null);
@@ -271,9 +270,7 @@ const ZoneMap = ({
           hovering over zoom buttons doesn't fire hover events on the map.
         */}
         <Portal node={wrapperRef.current}>
-          <div className="mapboxgl-zoom-controls">
-            <NavigationControl showCompass={false} zoomInLabel={zoomInLabel} zoomOutLabel={zoomOutLabel} />
-          </div>
+          <ZoomControls />
         </Portal>
         {/* Layers */}
         <Layer id="ocean" type="background" paint={styles.ocean} />
