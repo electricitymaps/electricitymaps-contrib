2478e33f57544a3389f1457da8652d246426913b
Mads Nedergaard <nedergaardmads@gmail.com>
2022-06-22 11:28:40 +0200

    Fixes api test

--- web/cypress/integration/api/history.spec.js
+++ web/cypress/integration/api/history.spec.js
@@ -1,6 +1,6 @@
 describe('History', () => {
   it('Asserts visiting zone requests history data succesfully', () => {
-    cy.intercept('http://localhost:8001/v4/history?countryCode=DK-DK2').as('fetchHistory');
+    cy.intercept('http://localhost:8001/v5/history/hourly?countryCode=DK-DK2').as('fetchHistory');
     cy.visit('/zone/DK-DK2');
     cy.wait('@fetchHistory').its('response.statusCode').should('eq', 302); // mockserver redirects content
   });
