1bd7d5531fa7560bcc8f098d951bbc5367b813bc
Mads Nedergaard <nedergaardmads@gmail.com>
2022-06-22 13:10:33 +0200

    Updates countrypanel test after his-view changes

--- web/cypress/integration/countrypanel.spec.js
+++ web/cypress/integration/countrypanel.spec.js
@@ -10,13 +10,18 @@ describe('Country Panel', () => {
     cy.contains('Carbon emissions').click().should('have.class', 'selected');
     cy.contains('0 t/min');
 
-    cy.get('.country-col').contains('182');
-    cy.get('input.time-slider-input').should('have.class', 'day');
-    cy.get('input.time-slider-input').setSliderValue('1641243600000');
-    cy.get('input.time-slider-input').should('have.class', 'night');
-    cy.get('.country-col').contains('124');
+    cy.get('.country-col').contains('152');
+    cy.get('input.time-slider-input-new').setSliderValue('1655823600000');
+    cy.get('.country-col').contains('84');
 
     cy.get('.left-panel-back-button').click();
+  });
+
+  it('asserts countryPanel contains "no-recent-data" message', () => {
+    cy.visit('/zone/UA');
+    cy.get('.no-data-overlay-message')
+      .should('exist')
+      .contains('Data is temporarily unavailable for the selected time');
   });
 
   it('asserts countryPanel contains no parser message when zone has no data', () => {
