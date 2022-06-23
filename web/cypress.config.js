bc2a5645898d2ab6cccb8fb0fbb72e5d81a49658
Mads Nedergaard <nedergaardmads@gmail.com>
2022-06-23 20:55:26 +0200

    Reverts switch to new config file. It only changed in cypress v10 and that is a larger migration

--- web/cypress.config.js
+++
@@ -1,8 +0,0 @@
-const { defineConfig } = require('cypress');
-
-module.exports = defineConfig({
-  projectId: '9z8tgk',
-  baseUrl: 'http://localhost:8080',
-  video: false,
-  fixturesFolder: '../mockserver/public',
-});
