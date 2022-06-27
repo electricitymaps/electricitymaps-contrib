65534e80fae9cff3befea4b60c99ef3c1c0c31d0
Mads Nedergaard <nedergaardmads@gmail.com>
2022-06-27 09:43:49 +0200

    Updates styling for timeslider input. This add the icon back and fixes bugs on Firefox

--- web/src/components/timeslider.jsx
+++ web/src/components/timeslider.jsx
@@ -6,61 +6,12 @@ import TimeAxis from './graph/timeaxis';
 import { useRefWidthHeightObserver } from '../hooks/viewport';
 import TimeSliderTooltip from './tooltips/timeslidertooltip';
 import TimeControls from './timeControls';
-import styled from 'styled-components';
 import { useTranslation } from '../helpers/translation';
 import { TIME } from '../helpers/constants';
 import { useSelector } from 'react-redux';
+import { TimeSliderInput } from './TimeSliderInput';
 
 const AXIS_HORIZONTAL_MARGINS = 12;
-
-const StyledInput = styled.input`
-  -webkit-appearance: none;
-  width: 100%;
-  margin-top: 5px;
-  height: 26px;
-  background: transparent;
-  padding: 0;
-  overflow: visible;
-  border: none;
-  margin: 5px 0;
-
-  &:focus {
-    outline: none;
-  }
-
-  &::-webkit-slider-thumb {
-    -webkit-appearance: none;
-    height: 26px;
-    width: 26px;
-    border-radius: 17px;
-    background: #ffffff;
-    cursor: pointer;
-    margin-top: -8px;
-    transition: box-shadow 0.4s;
-    box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.15);
-    border: 1px solid lightgray;
-    background-position: center center;
-    background-size: 12px;
-    background-repeat: no-repeat;
-    border: none;
-    box-shadow: 0.1px 0.1px 6px rgba(0, 0, 0, 0.16);
-    // TODO: background-image is set in .scss file
-
-    &:hover {
-      box-shadow: 0 0 15px 0 rgba(0, 0, 0, 0.15);
-    }
-  }
-
-  ::-webkit-slider-runnable-track {
-    width: 100%;
-    height: 12px;
-    border-radius: 4px;
-    cursor: pointer;
-    background: #f0f0f0;
-    background-size: 100% 100%;
-    background-repeat: no-repeat;
-  }
-`;
 
 const getTimeScale = (rangeEnd, datetimes, startTime, endTime) =>
   scaleTime()
@@ -141,7 +92,7 @@ const TimeSlider = ({
         selectedTimeAggregate={selectedTimeAggregate}
         handleTimeAggregationChange={handleTimeAggregationChange}
       />
-      <StyledInput
+      <TimeSliderInput
         className="time-slider-input"
         type="range"
         onChange={handleChangeAndInput}
