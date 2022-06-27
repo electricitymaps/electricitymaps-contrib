934ebcc89f0945347da19e3d2365478d884a6938
Mads Nedergaard <nedergaardmads@gmail.com>
2022-06-27 10:26:09 +0200

    Adds timeslider icon and fixes styling on Firefox (#4297)
    * Moves styling to SC
    
    * Updates styling for timeslider input.
    This add the icon back and fixes bugs on Firefox

--- web/src/layout/timeController.jsx
+++ web/src/layout/timeController.jsx
@@ -37,12 +37,6 @@ const StyledTimeSlider = styled(TimeSlider)`
   position: fixed;
   box-shadow: 0px 10px 30px rgba(0, 0, 0, 0.3);
 
-  .time-slider-axis-container {
-    width: 100%;
-    height: 20px;
-    overflow: visible;
-  }
-
   .domain {
     display: none;
   }
