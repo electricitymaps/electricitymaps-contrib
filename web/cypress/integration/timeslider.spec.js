15d45122c504d4da9622d64a3bfe3bd3eb30d7c2
Markus Killendahl <markus.killendahl@tmrow.com>
2022-06-30 18:30:50 +0200

    fix timeslider test

--- web/cypress/integration/timeslider.spec.js
+++ web/cypress/integration/timeslider.spec.js
@@ -17,21 +17,21 @@ describe('TimeController', () => {
     cy.waitForAPISuccess(`v5/state/hourly`);
     cy.waitForAPISuccess(`v5/history/hourly?countryCode=DK-DK2`);
     cy.contains('LIVE');
-    cy.get('[data-test-id=co2-square-value').should('have.text', '151');
-    cy.get('[data-test-id=date-display').should('have.text', '27 June 2022 at 11:00');
-    cy.get('input.time-slider-input-new').setSliderValue('1656255600000');
-    cy.get('[data-test-id=date-display').should('have.text', '26 June 2022 at 17:00');
-    cy.get('[data-test-id=co2-square-value').should('have.text', '103');
+    cy.get('[data-test-id=co2-square-value').should('have.text', '150');
+    cy.get('[data-test-id=date-display').should('have.text', '30 June 2022 at 15:00');
+    cy.get('input.time-slider-input').setSliderValue('1656255600000');
+    cy.get('[data-test-id=date-display').should('have.text', '29 June 2022 at 15:00');
+    cy.get('[data-test-id=co2-square-value').should('have.text', '170');
 
     // Monthly
     cy.get('[data-test-id="time-controls-daily-btn"]').click();
     cy.waitForAPISuccess(`v5/state/daily`);
     cy.waitForAPISuccess(`v5/history/daily?countryCode=DK-DK2`);
-    cy.get('[data-test-id=co2-square-value').should('have.text', '369');
-    cy.get('[data-test-id=date-display').should('have.text', '26 June 2022');
-    cy.get('input.time-slider-input-new').setSliderValue('1653782400000');
-    cy.get('[data-test-id=date-display').should('have.text', '29 May 2022');
-    cy.get('[data-test-id=co2-square-value').should('have.text', '316');
+    cy.get('[data-test-id=co2-square-value').should('have.text', '392');
+    cy.get('[data-test-id=date-display').should('have.text', '29 June 2022');
+    cy.get('input.time-slider-input').setSliderValue('1653782400000');
+    cy.get('[data-test-id=date-display').should('have.text', '30 May 2022');
+    cy.get('[data-test-id=co2-square-value').should('have.text', '388');
 
     // Yearly
     cy.get('[data-test-id="time-controls-monthly-btn"]').click();
@@ -39,7 +39,7 @@ describe('TimeController', () => {
     cy.waitForAPISuccess(`v5/history/monthly?countryCode=DK-DK2`);
     cy.get('[data-test-id=co2-square-value').should('have.text', '328');
     cy.get('[data-test-id=date-display').should('have.text', 'May 2022');
-    cy.get('input.time-slider-input-new').setSliderValue('1640995200000');
+    cy.get('input.time-slider-input').setSliderValue('1640995200000');
     cy.get('[data-test-id=date-display').should('have.text', 'January 2022');
     cy.get('[data-test-id=co2-square-value').should('have.text', '360');
 
@@ -49,7 +49,7 @@ describe('TimeController', () => {
     cy.waitForAPISuccess(`v5/history/yearly?countryCode=DK-DK2`);
     cy.get('[data-test-id=co2-square-value').should('have.text', '331');
     cy.get('[data-test-id=date-display').should('have.text', '2021');
-    cy.get('input.time-slider-input-new').setSliderValue('1577836800000');
+    cy.get('input.time-slider-input').setSliderValue('1577836800000');
     cy.get('[data-test-id=date-display').should('have.text', '2020');
     cy.get('[data-test-id=co2-square-value').should('have.text', '295');
   });
