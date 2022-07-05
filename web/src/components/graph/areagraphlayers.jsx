5608fc8266c82e37ad9e1d8af9ac48f361bfe3e4
Olivier Corradi <olivier.corradi@gmail.com>
2022-07-05 12:16:56 +0200

    Fix graph bin width

--- web/src/components/graph/areagraphlayers.jsx
+++ web/src/components/graph/areagraphlayers.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { noop } from '../../helpers/noop';
-import { area, curveStep } from 'd3-shape';
+import { area, curveStepAfter } from 'd3-shape';
 
 import { detectHoveredDatapointIndex } from '../../helpers/graph';
 
@@ -14,7 +14,8 @@ const AreaGraphLayers = React.memo(
 
     // Generate layer paths
     const layerArea = area()
-      .curve(curveStep)
+      // see https://github.com/d3/d3-shape#curveStep
+      .curve(curveStepAfter)
       .x((d) => timeScale(d.data.datetime))
       .y0((d) => valueScale(d[0]))
       .y1((d) => valueScale(d[1]))
@@ -42,6 +43,17 @@ const AreaGraphLayers = React.memo(
         {layers.map((layer, ind) => {
           const isGradient = typeof layer.fill === 'function';
           const gradientId = `areagraph-gradient-${layer.key}`;
+          // The datapoint valid until the next point
+          // However, for the last point, we won't have a next point
+          // Therefore, we add one here in order to make sure
+          // the last point is visible for the following interval
+          const lastDataPoint = [...layer.datapoints.at(-1)];
+          lastDataPoint.data = {
+            ...layer.datapoints.at(-1)?.data,
+            datetime: datetimes[layer.datapoints.length],
+          };
+          const datapoints = [...layer.datapoints, lastDataPoint];
+
           return (
             <React.Fragment key={layer.key}>
               <path
@@ -49,7 +61,7 @@ const AreaGraphLayers = React.memo(
                 style={{ cursor: 'pointer' }}
                 stroke={layer.stroke}
                 fill={isGradient ? `url(#${gradientId})` : layer.fill}
-                d={layerArea(layer.datapoints)}
+                d={layerArea(datapoints)}
                 /* Support only click events in mobile mode, otherwise react to mouse hovers */
                 onClick={isMobile ? (ev) => handleLayerMouseMove(ev, ind) : noop}
                 onFocus={!isMobile ? (ev) => handleLayerMouseMove(ev, ind) : noop}
@@ -60,7 +72,7 @@ const AreaGraphLayers = React.memo(
               />
               {isGradient && (
                 <linearGradient id={gradientId} gradientUnits="userSpaceOnUse" x1={x1} x2={x2}>
-                  {layer.datapoints.map((d) => (
+                  {datapoints.map((d) => (
                     <stop
                       key={d.data.datetime}
                       offset={`${((timeScale(d.data.datetime) - x1) / (x2 - x1)) * 100.0}%`}
