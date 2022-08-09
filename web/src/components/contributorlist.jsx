413a7eb7a30e60110479c67a89791a34fc976918
Pierre Segonne <pierre.segonne@tmrow.com>
2022-08-09 21:38:18 +0200

    Add imports from yaml based config

--- web/src/components/contributorlist.jsx
+++ web/src/components/contributorlist.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
-import zonesConfig from '../../../config/zones.json';
+import { zonesConfig } from '../../generate-zones-config';
 
 const ContributorList = () => {
   const { zoneId } = useParams();
--- web/src/helpers/redux.js
+++ web/src/helpers/redux.js
@@ -1,7 +1,7 @@
 import { createAction } from '@reduxjs/toolkit';
 import { TIME } from './constants';
 import constructTopos from './topos';
-import zonesConfig from '../../../config/zones.json';
+import { zonesConfig } from '../../generate-zones-config';
 import exchangesConfig from '../../../config/exchanges.json';
 
 const GRID_DATA_FETCH_REQUESTED = createAction('data/grid-fetch-requested');
