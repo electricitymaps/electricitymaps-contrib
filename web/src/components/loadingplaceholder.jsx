135c267632a24ce6c90cab96e8dfeca1b8a8cc18
Zack Walton <ztwalto@gmail.com>
2022-08-09 23:31:34 -0400

    Hooks up animated SVG logo

--- web/src/components/loadingplaceholder.jsx
+++ web/src/components/loadingplaceholder.jsx
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 export default styled.div`
-  background: transparent url(${resolvePath('images/electricitymap-icon.svg')}) no-repeat center center;
-  background-size: 2rem;
+  background: transparent url(${resolvePath('images/electricitymap-loading.svg')}) no-repeat center center;
+  background-size: 10rem;
   display: inline-block;
   height: ${(props) => props.height || '100%'};
   width: 100%;
