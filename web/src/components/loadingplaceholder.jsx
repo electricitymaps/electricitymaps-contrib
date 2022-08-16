5d666bb054e941f0a53f24d34ac741e1a7db48fe
Markus Killendahl <markus.killendahl@tmrow.com>
2022-08-16 10:35:09 +0200

    change to icon

--- web/src/components/loadingplaceholder.jsx
+++ web/src/components/loadingplaceholder.jsx
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 export default styled.div`
-  background: transparent url(${resolvePath('images/electricitymap-loading.svg')}) no-repeat center center;
-  background-size: 10rem;
+  background: transparent url(${resolvePath('images/electricitymap-loading-icon.svg')}) no-repeat center center;
+  background-size: 2rem;
   display: inline-block;
   height: ${(props) => props.height || '100%'};
   width: 100%;
