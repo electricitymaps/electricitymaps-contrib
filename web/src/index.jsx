0e3588262b6f05ca89bab385e9e040e1591f9553
Tony <tonyvanswet@gmail.com>
2022-09-09 10:50:59 +0200

    update repo url references (#4551)
    * update repo url references
    
    * no cap for circle ci
    
    * dont pretty the readme
    
    * dont ignore the readme :)

--- web/src/index.jsx
+++ web/src/index.jsx
@@ -27,7 +27,7 @@ console.log(
 🌍 %cReady to work on fixing the climate full-time?
   https://electricitymaps.com/jobs
 🐙 Got comments or want to contribute?
-  https://github.com/electricitymap/electricitymap-contrib`,
+  https://github.com/electricityMaps/electricitymaps-contrib`,
   'color: green; font-weight: bold',
   'color: #666; font-style: italic'
 );
