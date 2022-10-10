36f5093daa7b1fe7c42dc6ac9d3bdbca371ae253
Pierre Segonne <32778266+pierresegonne@users.noreply.github.com>
2022-10-10 16:26:54 +0100

    Create config per zone (#4435)
    * Create prototype for feasability study
    
    * Generate zone config for js
    
    * Add imports from yaml based config
    
    * Add all individual configs
    
    * Remove script that generates config files
    
    * Add fs to web/
    
    * Run prettier on all yaml config
    
    * Move generate zones config to src
    
    * Update Prettier to also check and fix config files2
    
    * Extract generate-zone-config to script
    
    * Use newly generated json file within src/
    
    * Update exchange names to unicode arrow
    
    * Update exchange names to unicode arrow, filenames
    
    * Update generated json config in web
    
    * Update generate-zones-config to handle unicode arrow
    
    * Remove generate-yaml script
    
    * Finalised parse_yaml script in python to verify exact matching of previous config and new
    
    * Adopt new parsed objects as default config
    
    * Temporal test for equality between old config object and new
    
    * Make generation of zones config automatic
    
    * Add generate zones config where appropriate in scripts
    
    * Make scripts use post*
    
    * Add generate-zones-config.js to build
    
    * Fix signatures in builds
    
    * Provide inputs for web/BUILD.yaml
    
    * Fix builds
    
    * Format test_config
    
    * Update locks2
    
    * Update all config
    
    * Regenerate exchanges config files
    
    * Update error message for generate-zones-config.js
    
    * Add script used for splitting .json files into yaml
    
    * Remove script used for splitting .json files into yaml
    
    * Prettify all yaml files
    
    * Update generate-zone-bounding-boxes script
    
    * Remove js-sha256
    
    * Update config files
    
    * Remove generate config file
    
    Co-authored-by: Viktor Andersson <30777521+VIKTORVAV99@users.noreply.github.com>

--- web/src/helpers/redux.js
+++ web/src/helpers/redux.js
@@ -1,8 +1,8 @@
 import { createAction } from '@reduxjs/toolkit';
 import { TIME } from './constants';
 import constructTopos from './topos';
-import zonesConfig from '../../../config/zones.json';
-import exchangesConfig from '../../../config/exchanges.json';
+import zonesConfig from '../config/zones.json';
+import exchangesConfig from '../config/exchanges.json';
 
 const GRID_DATA_FETCH_REQUESTED = createAction('data/grid-fetch-requested');
 const GRID_DATA_FETCH_SUCCEEDED = createAction('data/grid-fetch-succeded');
