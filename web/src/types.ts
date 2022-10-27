bdce9e3ece1b75c9075faa560de9c2429a760ff9
Markus Killendahl <markus.killendahl@tmrow.com>
2022-10-27 15:48:17 +0200

    add initial types

--- web/src/types.ts
+++ web/src/types.ts
@@ -13,3 +13,42 @@ export interface IFruit {
     value: string;
   }[];
 }
+
+export interface GridState {
+  countries: Record<string, ZoneOverview[] | []>; // TODO: Can we change countries -> zones in app-backend?
+}
+
+export interface ZoneOverview {
+  countryCode: string;
+  co2intensity?: number;
+  stateDatetime: string;
+}
+
+export interface ZoneDetail extends ZoneOverview {
+  production: GenerationTypes;
+  capacity: GenerationTypes;
+}
+
+export interface ZoneDetails {
+  hasData: boolean;
+  stateAggregation: 'daily' | 'hourly' | 'monthly' | 'yearly';
+  zoneStates: ZoneDetail[];
+}
+
+export interface GenerationTypes {
+  biomass?: number;
+  coal?: number;
+  gas?: number;
+  geothermal?: number;
+  hydro?: number;
+  nuclear?: number;
+  oil?: number;
+  solar?: number;
+  wind?: number;
+  unknown?: number;
+}
+
+export interface StorageTypes {
+  battery: number;
+  hydro: number;
+}
