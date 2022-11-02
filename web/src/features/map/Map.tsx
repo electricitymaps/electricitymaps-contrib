25d81572d7b1ac64d77afe5488d2b1cbf510d364
Tony <tonyvanswet@gmail.com>
2022-11-02 18:43:42 +0100

    initial map setup (#13)

--- web/src/features/map/Map.tsx
+++ web/src/features/map/Map.tsx
@@ -3,6 +3,9 @@ import LoadingOrError from 'components/LoadingOrError';
 import type { ReactElement } from 'react';
 import useGetState from 'api/getState';
 import { TimeAverages } from 'types';
+import { Map } from 'react-map-gl';
+import maplibregl from 'maplibre-gl';
+import MapStyle from './MapStyle.json';
 
 export default function MapPage(): ReactElement {
   const { isLoading, isError, error, data } = useGetState(TimeAverages.HOURLY);
@@ -13,6 +16,17 @@ export default function MapPage(): ReactElement {
   return (
     <>
       <Head title="Vitamin" />
+      <Map
+        initialViewState={{
+          latitude: 37.8,
+          longitude: -122.4,
+          zoom: 1,
+        }}
+        mapLib={maplibregl}
+        style={{ width: 800, height: 600 }}
+        mapStyle={MapStyle as mapboxgl.Style}
+      ></Map>
+
       <div className="m-2 grid min-h-screen grid-cols-[minmax(0,384px)] place-content-center gap-2 md:m-0 md:grid-cols-[repeat(2,minmax(0,384px))] xl:grid-cols-[repeat(3,384px)]">
         {Object.entries(data.countries).map(([zoneKey, zoneOverviews]) =>
           zoneOverviews.length > 0 ? <li key={zoneKey}>{zoneOverviews[0].co2intensity}</li> : undefined
