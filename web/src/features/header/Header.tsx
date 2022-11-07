ad6f429c824f21067ab594ba9c31978eab723cc8
Tony <tonyvanswet@gmail.com>
2022-11-07 09:17:32 +0100

    Tonyvanswet/ele 1338 UI header component (#20)

--- web/src/features/header/Header.tsx
+++ web/src/features/header/Header.tsx
@@ -1,6 +1,4 @@
 import * as NavigationMenu from '@radix-ui/react-navigation-menu';
-import { classnames } from 'tailwindcss-classnames';
-
 interface MenuLinkProps {
   href: string;
   children: React.ReactNode;
@@ -10,11 +8,7 @@ function MenuLink({ children, href, active }: MenuLinkProps): JSX.Element {
   return (
     <div className="relative flex py-2 ">
       <NavigationMenu.Item asChild className="rounded-md transition-colors hover:bg-zinc-100">
-        <NavigationMenu.Link
-          active={active}
-          href={href}
-          className={classnames('px-2 py-2 text-sm', active && 'font-bold')}
-        >
+        <NavigationMenu.Link active={active} href={href} className={`px-2 py-2 text-sm ${active && 'font-bold'}`}>
           {children}
           {active && <div className="absolute left-0 bottom-0 h-[2px] w-full bg-green-500"></div>}
         </NavigationMenu.Link>
@@ -25,28 +19,30 @@ function MenuLink({ children, href, active }: MenuLinkProps): JSX.Element {
 
 export default function Header(): JSX.Element {
   return (
-    <div className="flex items-center justify-between pl-4 pr-8 shadow-md">
-      <img src="/electricity-maps-logo.svg" alt="" className="w-100 h-10" />
+    <div className="hidden md:block">
+      <div className="flex items-center justify-between pl-4 pr-8 shadow-md">
+        <img src="/electricity-maps-logo.svg" alt="" className="w-100 h-10" />
 
-      <NavigationMenu.Root>
-        <NavigationMenu.List className="flex space-x-2">
-          <MenuLink href="/" active>
-            Live
-          </MenuLink>
-          <MenuLink href="https://electricitymaps.com/open-source/?utm_source=app.electricitymaps.com&utm_medium=referral">
-            We&apos;re hiring!
-          </MenuLink>
-          <MenuLink href="https://electricitymaps.com/open-source/?utm_source=app.electricitymaps.com&utm_medium=referral">
-            Open Source
-          </MenuLink>
-          <MenuLink href="https://electricitymaps.com/blog/?utm_source=app.electricitymaps.com&utm_medium=referral">
-            Blog
-          </MenuLink>
-          <MenuLink href="https://electricitymaps.com?utm_source=app.electricitymaps.com&utm_medium=referral">
-            Get our data
-          </MenuLink>
-        </NavigationMenu.List>
-      </NavigationMenu.Root>
+        <NavigationMenu.Root>
+          <NavigationMenu.List className="flex space-x-2">
+            <MenuLink href="/" active>
+              Live
+            </MenuLink>
+            <MenuLink href="https://electricitymaps.com/open-source/?utm_source=app.electricitymaps.com&utm_medium=referral">
+              We&apos;re hiring!
+            </MenuLink>
+            <MenuLink href="https://electricitymaps.com/open-source/?utm_source=app.electricitymaps.com&utm_medium=referral">
+              Open Source
+            </MenuLink>
+            <MenuLink href="https://electricitymaps.com/blog/?utm_source=app.electricitymaps.com&utm_medium=referral">
+              Blog
+            </MenuLink>
+            <MenuLink href="https://electricitymaps.com?utm_source=app.electricitymaps.com&utm_medium=referral">
+              Get our data
+            </MenuLink>
+          </NavigationMenu.List>
+        </NavigationMenu.Root>
+      </div>
     </div>
   );
 }
