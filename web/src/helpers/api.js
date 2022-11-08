564a3e37fe543eb95afadb457da354acbf269192
Mads Nedergaard <nedergaardmads@gmail.com>
2022-11-08 20:17:56 +0100

    dbg

--- web/src/helpers/api.js
+++ web/src/helpers/api.js
@@ -25,7 +25,10 @@ function isUsingLocalEndpoint() {
 
 async function sha256(message) {
   try {
-    const hashBuffer = await crypto.subtle.digest('SHA-256', new TextEncoder().encode(message));
+    const msg = new TextEncoder().encode(message);
+    console.info(crypto);
+    console.info(crypto.subtle);
+    const hashBuffer = await crypto.subtle.digest('SHA-256', msg);
     return Array.from(new Uint8Array(hashBuffer))
       .map((b) => b.toString(16).padStart(2, '0'))
       .join('');
