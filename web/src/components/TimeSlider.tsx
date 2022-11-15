7ac65f7f8843c3a495bb5e01427b9064e550d8f2
Markus Killendahl <markus.killendahl@tmrow.com>
2022-11-15 17:39:33 +0100

    style and refactor timeslider

--- web/src/components/TimeSlider.tsx
+++ web/src/components/TimeSlider.tsx
@@ -1,28 +1,31 @@
 import * as SliderPrimitive from '@radix-ui/react-slider';
 
 interface TimeSliderProps {
-  datetimes: string[];
   onChange: (datetimeIndex: number) => void;
+  numberOfEntries: number;
+  selectedIndex?: number;
 }
 
-function TimeSlider({ datetimes, onChange }: TimeSliderProps) {
+function TimeSlider({ onChange, numberOfEntries, selectedIndex }: TimeSliderProps) {
   return (
     <SliderPrimitive.Root
       defaultValue={[0]}
-      max={datetimes.length - 1}
+      max={numberOfEntries}
       step={1}
+      value={selectedIndex && selectedIndex > 0 ? [selectedIndex] : [0]}
       onValueChange={(value) => onChange(value[0])}
       aria-label="value"
       className="relative mb-2 flex h-5 w-full touch-none items-center"
     >
-      <SliderPrimitive.Track className="relative h-2.5 w-full grow rounded-sm bg-gray-200 dark:bg-gray-800">
+      <SliderPrimitive.Track className="relative h-2.5 w-full grow rounded-sm bg-gray-100 dark:bg-gray-700">
         <SliderPrimitive.Range />
       </SliderPrimitive.Track>
       <SliderPrimitive.Thumb
-        className={
-          'block h-6 w-6 rounded-full bg-white shadow-xl focus:outline-none focus-visible:ring focus-visible:ring-gray-300 focus-visible:ring-opacity-75 dark:bg-white'
-        }
-      />
+        className={`block h-6 w-6 rounded-full bg-white bg-[url("/public/icons/slider-thumb.svg")]
+          bg-center bg-no-repeat shadow-3xl focus:outline-none
+          focus-visible:ring focus-visible:ring-green-900/10
+          focus-visible:ring-opacity-75 dark:bg-gray-400 dark:focus-visible:ring-white/70`}
+      ></SliderPrimitive.Thumb>
     </SliderPrimitive.Root>
   );
 }
