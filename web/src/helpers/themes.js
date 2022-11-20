60ed44b97ccb21803f60e234cb8444572b22010b
Stephen Brown-Bourne <sbrownbourne@gmail.com>
2022-11-20 13:14:04 -0500

    Add 6th color and step to CO2 scales

--- web/src/helpers/themes.js
+++ web/src/helpers/themes.js
@@ -1,8 +1,8 @@
 export const themes = {
   colorblindDark: {
     co2Scale: {
-      steps: [0, 200, 400, 600, 800],
-      colors: ['#FFFFB0', '#E0B040', '#A06030', '#602020', '#000010'],
+      steps: [0, 300, 600, 900, 1200, 1500],
+      colors: ['#FFFFB0', '#E3BF66', '#BB833C', '#8B4D2B', '#4E241F', '#000010'],
     },
     oceanColor: '#33414A',
     strokeWidth: 0.3,
@@ -12,8 +12,8 @@ export const themes = {
   },
   dark: {
     co2Scale: {
-      steps: [0, 150, 600, 750, 800],
-      colors: ['#2AA364', '#F5EB4D', '#9E4229', '#381D02', '#381D02'],
+      steps: [0, 300, 600, 900, 1200, 1500],
+      colors: ['#2AA364', '#F5EB4D', '#C6983A', '#885022', '#381D02', '#381D02'],
     },
     oceanColor: '#33414A',
     strokeWidth: 0.3,
@@ -23,7 +23,7 @@ export const themes = {
   },
   colorblindBright: {
     co2Scale: {
-      steps: [0, 200, 400, 600, 800],
+      steps: [0, 300, 600, 900, 1200, 1500],
       colors: ['#FFFFB0', '#E0B040', '#A06030', '#602020', '#000010'],
     },
     oceanColor: '#FAFAFA',
@@ -34,8 +34,8 @@ export const themes = {
   },
   bright: {
     co2Scale: {
-      steps: [0, 150, 600, 750, 800],
-      colors: ['#2AA364', '#F5EB4D', '#9E4229', '#381D02', '#381D02'],
+      steps: [0, 300, 600, 900, 1200, 1500],
+      colors: ['#2AA364', '#F5EB4D', '#C6983A', '#885022', '#381D02', '#381D02'],
     },
     oceanColor: '#FAFAFA',
     strokeWidth: 0.3,
