e72a720afc4be3773fe9f9385f36305156120ead
Viktor Andersson <30777521+VIKTORVAV99@users.noreply.github.com>
2022-12-12 18:22:05 +0100

    format data

--- parsers/AX_NEW.py
+++ parsers/AX_NEW.py
@@ -1,11 +1,17 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from logging import Logger, getLogger
-from typing import Optional
-from requests import Response, Session
-from bs4 import BeautifulSoup
 from re import findall
+from typing import List, Optional
+
+from bs4 import BeautifulSoup
+from pytz import timezone
+from requests import Response, Session
+
+from .lib.exceptions import ParserException
 
 IFRAME_URL = "https://grafik.kraftnat.ax/grafer/tot_inm_24h_15.php"
+TIME_ZONE = "Europe/Mariehamn"
+SOURCE = "kraftnat.ax"
 
 
 def fetch_data(session: Session, logger: Logger):
@@ -41,19 +47,146 @@ def fetch_data(session: Session, logger: Logger):
     return data_list
 
 
+def formated_data(
+    zone_key: Optional[str],
+    zone_key1: Optional[str],
+    zone_key2: Optional[str],
+    session: Session,
+    logger: Logger,
+    type: str,
+):
+    """Format data to Electricity Map standards."""
+    data_list = fetch_data(session, logger)
+    data_list.reverse()
+    date = datetime.now(timezone(TIME_ZONE)).replace(
+        hour=int(data_list[0]["time"].split(":")[0]),
+        minute=int(data_list[0]["time"].split(":")[1]),
+        second=0,
+        microsecond=0,
+    )
+    return_list = []
+    for data in data_list:
+        corrected_date = date - timedelta(minutes=15 * data_list.index(data))
+        # TODO: Check if the corrected date matches the expected time in the data.
+        if type == "production":
+            return_list.append(
+                {
+                    "zoneKey": zone_key,
+                    "production": {
+                        "wind": data["wind"],
+                        "oil": data["fossil"],
+                    },
+                    "datetime": corrected_date,
+                    "source": SOURCE,
+                }
+            )
+        elif type == "consumption":
+            return_list.append(
+                {
+                    "zoneKey": zone_key,
+                    "datetime": corrected_date,
+                    "consumption": data["consumption"],
+                    "source": SOURCE,
+                }
+            )
+        elif type == "exchange":
+            if zone_key1 == "AX" and zone_key2 == "SE":
+                return_list.append(
+                    {
+                        "sortedZoneKeys": "AX->SE",
+                        "datetime": corrected_date,
+                        "netFlow": data["sweden"],
+                        "source": SOURCE,
+                    }
+                )
+            elif zone_key1 == "AX" and zone_key2 == "FI":
+                return_list.append(
+                    {
+                        "sortedZoneKeys": "AX->FI",
+                        "datetime": corrected_date,
+                        "netFlow": data["alink"] + data["gustavs"],
+                        "source": SOURCE,
+                    }
+                )
+            else:
+                raise ParserException(
+                    "AX.py",
+                    "This parser can only fetch data between Åland <-> Sweden and Åland <-> Finland",
+                )
+        else:
+            raise ParserException(
+                "AX.py",
+                "The datasource currently implemented is only for production, consumption and exchange data",
+                zone_key,
+            )
+    return return_list
+
+
 def fetch_production(
     zone_key: str = "AX",
     session: Session = Session(),
     target_datetime: Optional[datetime] = None,
     logger: Logger = getLogger(__name__),
-) -> dict:
-    """Fetch production from the iFrame."""
+) -> List[dict]:
+    """Fetch production data."""
 
     if target_datetime is not None:
-        raise NotImplementedError(
-            "The datasource currently implemented is only real time"
+        raise ParserException(
+            "AX.py", "The datasource currently implemented is only for real time data"
         )
 
-    data = fetch_data(session, logger)
+    return formated_data(
+        zone_key=zone_key,
+        zone_key1=None,
+        zone_key2=None,
+        session=session,
+        logger=logger,
+        type="production",
+    )
 
-    return data
+
+def fetch_consumption(
+    zone_key: str = "AX",
+    session: Session = Session(),
+    target_datetime: Optional[datetime] = None,
+    logger: Logger = getLogger(__name__),
+) -> List[dict]:
+    """Fetch consumption data."""
+
+    if target_datetime is not None:
+        raise ParserException(
+            "AX.py", "The datasource currently implemented is only for real time data"
+        )
+
+    return formated_data(
+        zone_key=zone_key,
+        zone_key1=None,
+        zone_key2=None,
+        session=session,
+        logger=logger,
+        type="consumption",
+    )
+
+
+def fetch_exchange(
+    zone_key1: str,
+    zone_key2: str,
+    session: Session = Session(),
+    target_datetime: Optional[datetime] = None,
+    logger: Logger = getLogger(__name__),
+) -> List[dict]:
+    """Fetch exchange data."""
+
+    if target_datetime is not None:
+        raise ParserException(
+            "AX.py", "The datasource currently implemented is only for real time data"
+        )
+
+    return formated_data(
+        zone_key=None,
+        zone_key1=zone_key1,
+        zone_key2=zone_key2,
+        session=session,
+        logger=logger,
+        type="exchange",
+    )
