d1a87b0d701ebbe8172fd7704c6a0923c3fbf237
Viktor Andersson <30777521+VIKTORVAV99@users.noreply.github.com>
2022-12-20 14:52:06 +0100

    create MK wrapper

---
+++ parsers/MK_wrapper.py
@@ -0,0 +1,105 @@
+from datetime import datetime, timedelta
+from logging import Logger, getLogger
+from typing import List, Optional, Union
+
+from requests import Session
+from . import ENTSOE
+
+"""
+This wrapper is used to fetch data from ENTSOE for the country of North Macedonia.
+If no target_datetime is provided, the wrapper will provide a target datetime that is
+24 hours in the past due to intermittent data availability.
+"""
+
+def modify_target_datetime(target_datetime: Optional[datetime]) -> datetime:
+    if target_datetime is None:
+        return datetime.utcnow() - timedelta(hours=24)
+    else:
+        return target_datetime
+
+def fetch_consumption(
+    zone_key: str = "MK",
+    session: Session = Session(),
+    target_datetime: Optional[datetime] = None,
+    logger: Logger = getLogger(__name__),
+)   -> Union[List[dict], dict, None]:
+    target_datetime = modify_target_datetime(target_datetime)
+    return ENTSOE.fetch_consumption(
+        zone_key=zone_key,
+        session=session,
+        target_datetime=target_datetime,
+        logger=logger,
+    )
+
+def fetch_consumption_forecast(
+    zone_key: str = "MK",
+    session: Session = Session(),
+    target_datetime: Optional[datetime] = None,
+    logger: Logger = getLogger(__name__),
+)   -> list:
+    target_datetime = modify_target_datetime(target_datetime)
+    return ENTSOE.fetch_consumption_forecast(
+        zone_key=zone_key,
+        session=session,
+        target_datetime=target_datetime,
+        logger=logger,
+    )
+
+def fetch_generation_forecast(
+    zone_key: str = "MK",
+    session: Session = Session(),
+    target_datetime: Optional[datetime] = None,
+    logger: Logger = getLogger(__name__),
+)   -> list:
+    target_datetime = modify_target_datetime(target_datetime)
+    return ENTSOE.fetch_generation_forecast(
+        zone_key=zone_key,
+        session=session,
+        target_datetime=target_datetime,
+        logger=logger,
+    )
+
+def fetch_price(
+    zone_key: str = "MK",
+    session: Session = Session(),
+    target_datetime: Optional[datetime] = None,
+    logger: Logger = getLogger(__name__),
+)   -> list:
+    target_datetime = modify_target_datetime(target_datetime)
+    return ENTSOE.fetch_price(
+        zone_key=zone_key,
+        session=session,
+        target_datetime=target_datetime,
+        logger=logger,
+    )
+
+def fetch_production(
+    zone_key: str = "MK",
+    session: Session = Session(),
+    target_datetime: Optional[datetime] = None,
+    logger: Logger= getLogger(__name__),
+) -> list:
+    target_datetime = modify_target_datetime(target_datetime)
+    return ENTSOE.fetch_production(
+        zone_key=zone_key,
+        session=session,
+        target_datetime=target_datetime,
+        logger=logger,
+    )
+
+def fetch_wind_solar_forecasts(
+    zone_key: str = "MK",
+    session: Session = Session(),
+    target_datetime: Optional[datetime] = None,
+    logger: Logger = getLogger(__name__),
+)   -> list:
+    target_datetime = modify_target_datetime(target_datetime)
+    return ENTSOE.fetch_wind_solar_forecasts(
+        zone_key=zone_key,
+        session=session,
+        target_datetime=target_datetime,
+        logger=logger,
+    )
+
+
+
