dab3159b7163682aae34a777a90c8109c9fa73ae
Mads Nedergaard <nedergaardmads@gmail.com>
2023-01-05 09:20:08 +0100

    Fixes issue when adblocker is used + adds types (#117)

--- web/src/utils/analytics.ts
+++ web/src/utils/analytics.ts
@@ -1,7 +1,20 @@
+type PlausibleEventProps = { readonly [propName: string]: string | number | boolean };
+type PlausibleArguments = [string, { props: PlausibleEventProps }];
+
+// TODO: Consider moving this to it's own global file
+declare global {
+  const plausible: {
+    (...arguments: PlausibleArguments): void;
+    q?: PlausibleArguments[];
+  };
+
+  interface Window {
+    plausible?: typeof plausible | undefined;
+  }
+}
 export default function trackEvent(
   eventId: string,
-  additionalProps: { [key: string]: string | number | boolean } = {}
-) {
-  // @ts-ignore - plausible is defined in index.html
-  window.plausible(eventId, { props: additionalProps });
+  additionalProps: PlausibleEventProps = {}
+): void {
+  window.plausible && window.plausible(eventId, { props: additionalProps });
 }
