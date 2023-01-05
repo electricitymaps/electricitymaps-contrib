67d5f02d84a9050eda981d4ff58c39aaafcefb64
Viktor Andersson <30777521+VIKTORVAV99@users.noreply.github.com>
2023-01-05 12:40:18 +0100

    fixes the ZA parser Fixes the ZA parser by formatting the year in the URL as well.

--- parsers/ZA.py
+++ parsers/ZA.py
@@ -10,8 +10,10 @@ pp = PrettyPrinter(indent=4)
 
 TIMEZONE = "Africa/Johannesburg"
 
-curr_month = now().format("MM")
-PRODUCTION_URL = f"https://www.eskom.co.za/dataportal/wp-content/uploads/2022/{curr_month}/Station_Build_Up.csv"
+
+def get_url() -> str:
+    date = datetime.utcnow()
+    return f"https://www.eskom.co.za/dataportal/wp-content/uploads/{date.strftime('%Y')}/{date.strftime('%m')}/Station_Build_Up.csv"
 
 
 def fetch_production(
@@ -32,10 +34,10 @@ def fetch_production(
 
     r = session or Session()
 
-    res: Response = r.get(PRODUCTION_URL)
+    res: Response = r.get(get_url())
     assert res.status_code == 200, (
         "Exception when fetching production for "
-        "{}: error when calling url={}".format(zone_key, PRODUCTION_URL)
+        f"{zone_key}: error when calling url={get_url}"
     )
 
     csv_data = res.text.split("\r\n")
