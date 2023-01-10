c97693e9765363280062145a2f901b717081f28c
Mathilde Daugy <107848894+mathilde-daugy@users.noreply.github.com>
2023-01-10 11:53:12 +0100

    Update parsers/IE.py
    Co-authored-by: Viktor Andersson <30777521+VIKTORVAV99@users.noreply.github.com>

--- parsers/IE.py
+++ parsers/IE.py
@@ -168,7 +168,7 @@ def fetch_exchange(
 @refetch_frequency(timedelta(days=1))
 def fetch_consumption(
     zone_key: str,
-    session: Optional[Session] = None,
+    session: Session = Session(),
     target_datetime: Optional[datetime] = None,
     logger: Logger = getLogger(__name__),
 ) -> list:
