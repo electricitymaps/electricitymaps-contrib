f8a7c81991ad6536bff608d72a08ea66e09728a2
Viktor Andersson <30777521+VIKTORVAV99@users.noreply.github.com>
2023-01-16 13:36:22 +0100

    Round map to 4 decimals (#151)

--- web/geo/update-world.ts
+++ web/geo/update-world.ts
@@ -1,9 +1,10 @@
+import { coordEach } from '@turf/turf';
 import * as path from 'node:path';
 import { mergeZones } from '../scripts/generate-zones-config';
 import { generateAggregates } from './generate-aggregates';
 import { generateExchangesToIgnore } from './generate-exchanges-to-exclude';
 import { generateTopojson } from './generate-topojson';
-import { getJSON } from './utilities';
+import { getJSON, round } from './utilities';
 import { validateGeometry } from './validate';
 
 const config = {
@@ -27,6 +28,12 @@ const zoneConfig = mergeZones();
 const aggregates = generateAggregates(fc, zoneConfig);
 
 fc.features = aggregates;
+
+// Rounds coordinates to 4 decimals
+coordEach(fc, (coord) => {
+  coord[0] = round(coord[0], 4);
+  coord[1] = round(coord[1], 4);
+});
 
 validateGeometry(fc, config);
 generateTopojson(fc, config);
