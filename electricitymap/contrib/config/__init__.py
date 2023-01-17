7e6c448f5e6915ace330760b7546d29c9832e889
Mads Nedergaard <nedergaardmads@gmail.com>
2023-01-17 21:50:22 +0100

    Updates all files in config folder

--- electricitymap/contrib/config/__init__.py
+++ electricitymap/contrib/config/__init__.py
@@ -1,7 +1,11 @@
-import json
 from collections import defaultdict
+from copy import deepcopy
 from pathlib import Path
-from typing import Dict, List, NewType, Tuple
+from typing import Any, Dict, List, NewType, Tuple
+
+import yaml
+
+from electricitymap.contrib.config.constants import EXCHANGE_FILENAME_ZONE_SEPARATOR
 
 ZoneKey = NewType("ZoneKey", str)
 Point = NewType("Point", Tuple[float, float])
@@ -9,10 +13,68 @@ BoundingBox = NewType("BoundingBox", List[Point])
 
 CONFIG_DIR = Path(__file__).parent.parent.parent.parent.joinpath("config").resolve()
 
-# Read JOSN files
-ZONES_CONFIG = json.load(open(CONFIG_DIR.joinpath("zones.json")))
-EXCHANGES_CONFIG = json.load(open(CONFIG_DIR.joinpath("exchanges.json")))
-CO2EQ_PARAMETERS = json.load(open(CONFIG_DIR.joinpath("co2eq_parameters.json")))
+defaults = yaml.safe_load(open(CONFIG_DIR.joinpath("defaults.yaml"), encoding="utf-8"))
+
+zones_config: Dict[ZoneKey, Any] = {}
+for zone_path in CONFIG_DIR.joinpath("zones").glob("*.yaml"):
+    zone_key = ZoneKey(zone_path.stem)
+    zones_config[zone_key] = yaml.safe_load(open(zone_path, encoding="utf-8"))
+
+exchanges_config = {}
+for exchange_path in CONFIG_DIR.joinpath("exchanges").glob("*.yaml"):
+    _exchange_key_unicode = exchange_path.stem
+    _zone_keys = _exchange_key_unicode.split(EXCHANGE_FILENAME_ZONE_SEPARATOR)
+    assert len(_zone_keys) == 2
+    exchange_key = "->".join(_zone_keys)
+    exchanges_config[exchange_key] = yaml.safe_load(
+        open(exchange_path, encoding="utf-8")
+    )
+
+
+co2eq_parameters_all = {
+    k: {
+        "defaults": defaults[k],
+        "zoneOverrides": {},
+    }
+    for k in ["fallbackZoneMixes", "isLowCarbon", "isRenewable"]
+}
+co2eq_parameters_direct = {
+    "emissionFactors": {
+        "defaults": defaults["emissionFactors"]["direct"],
+        "zoneOverrides": {},
+    },
+}
+co2eq_parameters_lifecycle = {
+    "emissionFactors": {
+        "defaults": defaults["emissionFactors"]["lifecycle"],
+        "zoneOverrides": {},
+    },
+}
+# Populate zone overrides
+for zone_key, zone_config in zones_config.items():
+    for k in ["fallbackZoneMixes", "isLowCarbon", "isRenewable"]:
+        if k in zone_config:
+            co2eq_parameters_all[k]["zoneOverrides"][zone_key] = zone_config[k]
+            del zone_config[k]
+    if "emissionFactors" in zone_config:
+        for k in ["direct", "lifecycle"]:
+            if k in zone_config["emissionFactors"]:
+                if k == "direct":
+                    co2eq_parameters_direct["emissionFactors"]["zoneOverrides"][
+                        zone_key
+                    ] = zone_config["emissionFactors"][k]
+                elif k == "lifecycle":
+                    co2eq_parameters_lifecycle["emissionFactors"]["zoneOverrides"][
+                        zone_key
+                    ] = zone_config["emissionFactors"][k]
+        del zone_config["emissionFactors"]
+
+ZONES_CONFIG = deepcopy(zones_config)
+EXCHANGES_CONFIG = deepcopy(exchanges_config)
+CO2EQ_PARAMETERS_DIRECT = {**co2eq_parameters_all, **co2eq_parameters_direct}
+CO2EQ_PARAMETERS_LIFECYCLE = {**co2eq_parameters_all, **co2eq_parameters_lifecycle}
+CO2EQ_PARAMETERS = CO2EQ_PARAMETERS_LIFECYCLE  # Global LCA is the default
+
 
 # Prepare zone bounding boxes
 ZONE_BOUNDING_BOXES: Dict[ZoneKey, BoundingBox] = {}
@@ -27,9 +89,32 @@ for zone_id, zone_config in ZONES_CONFIG.items():
         for sub_zone_id in zone_config["subZoneNames"]:
             ZONE_PARENT[sub_zone_id] = zone_id
 
-# Prepare zone neighbours
-ZONE_NEIGHBOURS: Dict[ZoneKey, List[ZoneKey]] = {}
+# This object represents the edges of the flow-tracing graph
+def generate_zone_neighbours(
+    zones_config, exchanges_config
+) -> Dict[ZoneKey, List[ZoneKey]]:
+    zone_neighbours = defaultdict(set)
+    for k, v in exchanges_config.items():
+        if not v.get("parsers", {}).get("exchange", None):
+            # Interconnector config has no parser, and will therefore not be part
+            # of the flowtracing graph
+            continue
+        zone_1, zone_2 = k.split("->")
+        if zones_config[zone_1].get("subZoneNames") or zones_config[zone_2].get(
+            "subZoneNames"
+        ):
+            # Both zones must not have subzones
+            continue
+        pairs = [(zone_1, zone_2), (zone_2, zone_1)]
+        for zone_name_1, zone_name_2 in pairs:
+            zone_neighbours[zone_name_1].add(zone_name_2)
+    # Sort
+    return {k: sorted(v) for k, v in zone_neighbours.items()}
 
+
+ZONE_NEIGHBOURS: Dict[ZoneKey, List[ZoneKey]] = generate_zone_neighbours(
+    ZONES_CONFIG, EXCHANGES_CONFIG
+)
 
 # This object represents all neighbours regardless of granularity
 def generate_all_neighbours(exchanges_config) -> Dict[ZoneKey, List[ZoneKey]]:
@@ -46,20 +131,24 @@ def generate_all_neighbours(exchanges_config) -> Dict[ZoneKey, List[ZoneKey]]:
 ALL_NEIGHBOURS: Dict[ZoneKey, List[ZoneKey]] = generate_all_neighbours(EXCHANGES_CONFIG)
 
 
-for k, v in EXCHANGES_CONFIG.items():
-    zone_names = k.split("->")
-    pairs = [(zone_names[0], zone_names[1]), (zone_names[1], zone_names[0])]
-    for zone_name_1, zone_name_2 in pairs:
-        if zone_name_1 not in ZONE_NEIGHBOURS:
-            ZONE_NEIGHBOURS[zone_name_1] = set()
-        ZONE_NEIGHBOURS[zone_name_1].add(zone_name_2)
-# we want neighbors to always be in the same order
-for zone, neighbors in ZONE_NEIGHBOURS.items():
-    ZONE_NEIGHBOURS[zone] = sorted(neighbors)
-
-
-def emission_factors(zone_key: ZoneKey):
+def emission_factors(zone_key: ZoneKey) -> Dict[str, float]:
     override = CO2EQ_PARAMETERS["emissionFactors"]["zoneOverrides"].get(zone_key, {})
     defaults = CO2EQ_PARAMETERS["emissionFactors"]["defaults"]
+
+    def get_most_recent_value(emission_factors: Dict) -> Dict:
+        _emission_factors = deepcopy(emission_factors)
+        keys_with_yearly = [
+            k for (k, v) in _emission_factors.items() if isinstance(v, list)
+        ]
+        for k in keys_with_yearly:
+            _emission_factors[k] = max(
+                _emission_factors[k], key=lambda x: x["datetime"]
+            )
+        return _emission_factors
+
+    # Only use most recent yearly numbers from defaults & overrides
+    defaults = get_most_recent_value(defaults)
+    override = get_most_recent_value(override)
+
     merged = {**defaults, **override}
     return dict([(k, (v or {}).get("value")) for (k, v) in merged.items()])
