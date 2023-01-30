9a33e1c8e89db5b63174804811aff80e68f5895f
Mads Nedergaard <nedergaardmads@gmail.com>
2023-01-30 07:44:33 +0100

    Replaces map with a fallback shown for users on old ios versions (#175)

--- web/src/features/map/MapWrapper.tsx
+++ web/src/features/map/MapWrapper.tsx
@@ -1,12 +1,25 @@
 import MapControls from 'features/map-controls/MapControls';
+import { useAtom } from 'jotai';
+import { useEffect } from 'react';
 import Map from './Map';
+import MapFallback, { isOldIOSVersion } from './MapFallback';
 import MapTooltip from './MapTooltip';
+import { loadingMapAtom } from './mapAtoms';
 
 export default function MapWrapper() {
+  const shouldShowFallback = isOldIOSVersion();
+  const [_, setIsLoadingMap] = useAtom(loadingMapAtom);
+
+  useEffect(() => {
+    if (shouldShowFallback) {
+      setIsLoadingMap(false);
+    }
+  }, [shouldShowFallback]);
+
   return (
     <>
       <MapTooltip />
-      <Map />
+      {shouldShowFallback ? <MapFallback /> : <Map />}
       <MapControls />
     </>
   );
