2f05f12c2fef9d6f89b42673ba3f82914ee2c62d
Mads Nedergaard <nedergaardmads@gmail.com>
2023-02-07 23:27:07 +0100

    Removes fixed height to allow longer translations

--- web/src/features/map/MapTooltip.tsx
+++ web/src/features/map/MapTooltip.tsx
@@ -96,7 +96,7 @@ export default function MapTooltip() {
     return (
       <Portal.Root className="absolute left-0 top-0 h-0 w-0">
         <div
-          className="relative h-[176px] w-[300px] rounded border bg-zinc-50 p-3  text-sm shadow-lg dark:border-0 dark:bg-gray-900"
+          className="relative w-[300px] rounded border bg-zinc-50 p-3  text-sm shadow-lg dark:border-0 dark:bg-gray-900"
           style={{ left: tooltipWithDataPositon.x, top: tooltipWithDataPositon.y }}
         >
           <div>
