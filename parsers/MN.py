e8360894c64d70097df91fae016d5b67418293f7
Viktor Andersson <30777521+VIKTORVAV99@users.noreply.github.com>
2023-02-08 09:16:24 +0100

    final cleanup + formatting

--- parsers/MN.py
+++ parsers/MN.py
@@ -4,9 +4,9 @@ from datetime import datetime
 from logging import Logger, getLogger
 from typing import Optional
 
-import arrow
 from bs4 import BeautifulSoup
-from requests import Session, get
+from pytz import timezone
+from requests import Session
 
 #   MAIN_WEBSITE = https://ndc.energy.mn/
 NDC_GENERATION = "https://disnews.energy.mn/test/convert.php"
@@ -36,26 +36,20 @@ def fetch_production(
     # 'unknown' consists of 92.8% coal, 5.8% oil and 1.4% hydro as per 2020; sources: IEA and IRENA statistics.
     unknown_MW = round(consumption_MW - exchanges_MW - solar_MW - wind_MW, 2)
 
-    dataset_production = {
-        'unknown': unknown_MW,
-        'solar': solar_MW,
-        'wind': wind_MW
-    }
+    dataset_production = {"unknown": unknown_MW, "solar": solar_MW, "wind": wind_MW}
 
     date_raw = NDC_SOUP.find_all("td")[0].text.strip("Хоногийн ачаалал: ")
-    date_pretty = datetime.strptime(date_raw, '%Y-%m-%d')
+    date_pretty = datetime.strptime(date_raw, "%Y-%m-%d")
     time_raw = NDC_SOUP.find_all("td")[14].text.strip("Импортын чадал ")
-    time_time = datetime.strptime(time_raw, '%H:%M:%S')
+    time_time = datetime.strptime(time_raw, "%H:%M:%S")
     time_pretty = datetime.time(time_time)
 
     time_combined = datetime.combine(date_pretty, time_pretty)
-    time_arrow = arrow.get(time_combined, tzinfo='Asia/Ulaanbaatar')
 
     data = {
         "zoneKey": zone_key,
-        "datetime": time_arrow.datetime,
+        "datetime": time_combined.replace(tzinfo=timezone(TZ)),
         "production": dataset_production,
-        "storage": {},
         "source": "https://ndc.energy.mn/",
     }
 
@@ -76,26 +70,22 @@ def fetch_consumption(
     # Нийлбэр ачаалал
     consumption_MW = float(NDC_SOUP.find_all("td")[6].text.strip(" МВт"))
 
-
     date_raw = NDC_SOUP.find_all("td")[0].text.strip("Хоногийн ачаалал: ")
-    date_pretty = datetime.strptime(date_raw, '%Y-%m-%d')
+    date_pretty = datetime.strptime(date_raw, "%Y-%m-%d")
     time_raw = NDC_SOUP.find_all("td")[14].text.strip("Импортын чадал ")
-    time_time = datetime.strptime(time_raw, '%H:%M:%S')
+    time_time = datetime.strptime(time_raw, "%H:%M:%S")
     time_pretty = datetime.time(time_time)
 
     time_combined = datetime.combine(date_pretty, time_pretty)
-    time_arrow = arrow.get(time_combined, tzinfo='Asia/Ulaanbaatar')
 
     data = {
         "zoneKey": zone_key,
-        "datetime": time_arrow.datetime,
+        "datetime": time_combined.replace(tzinfo=timezone(TZ)),
         "consumption": consumption_MW,
-        "storage": {},
         "source": "https://ndc.energy.mn/",
     }
 
     return data
-
 
 
 if __name__ == "__main__":
