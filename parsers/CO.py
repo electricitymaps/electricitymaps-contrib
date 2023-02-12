b13498d1a86496e7b7a2e304ec581cda8a2eb1da
Viktor Andersson <30777521+VIKTORVAV99@users.noreply.github.com>
2023-02-12 18:59:16 +0100

    format + generate files

--- parsers/CO.py
+++ parsers/CO.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 import json
-import arrow
-
 from datetime import datetime
 from logging import Logger, getLogger
 from typing import Optional
+
+import arrow
 from requests import Session
 
 #   PARSER FOR COLOMBIA / DEMAND-ONLY as of 2023-02-11 / 5-minute-granularity / returns demand data of the recent day
@@ -13,6 +13,7 @@ colombia_demand_URL = "https://serviciosfacturacion.xm.com.co/XM.Portal.Indicado
 
 TZ = "America/Bogota"  # UTC-5
 demand_list = []
+
 
 def fetch_consumption(
     zone_key: str = "CO",
@@ -24,19 +25,20 @@ def fetch_consumption(
         raise NotImplementedError("This parser is not yet able to parse past dates.")
     with session.get(colombia_demand_URL, verify=False) as response:
         demand_json = json.loads(response.content)
-        demand_data = demand_json['Variables'][0]['Datos']
+        demand_data = demand_json["Variables"][0]["Datos"]
 
         for datapoint in demand_data:
             demand_list.append(
                 {
-                "zoneKey": zone_key,
-                "datetime": arrow.get(datapoint['Fecha'], tzinfo=TZ).datetime,
-                "consumption": round(datapoint['Valor'], 1),
-                "source": "xm.com.co"
+                    "zoneKey": zone_key,
+                    "datetime": arrow.get(datapoint["Fecha"], tzinfo=TZ).datetime,
+                    "consumption": round(datapoint["Valor"], 1),
+                    "source": "xm.com.co",
                 }
             )
 
     return demand_list
+
 
 if __name__ == "__main__":
     print("fetch_consumption() ->")
