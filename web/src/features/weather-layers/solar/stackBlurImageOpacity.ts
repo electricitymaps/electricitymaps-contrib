d3bbe2eb92b90fd03a5fbc7fc6f787298a47b187
Mads Nedergaard <nedergaardmads@gmail.com>
2023-02-23 15:15:52 +0100

    Disables typing of solar layer util

--- web/src/features/weather-layers/solar/stackBlurImageOpacity.ts
+++ web/src/features/weather-layers/solar/stackBlurImageOpacity.ts
@@ -1,3 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+/* eslint-disable unicorn/no-abusive-eslint-disable */
+/* eslint-disable */
+
 // TODO: remove once refactored
 
 const mul_table = [
