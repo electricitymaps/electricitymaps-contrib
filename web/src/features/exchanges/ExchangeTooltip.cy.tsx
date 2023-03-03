b6e3bb64d0e94aef6a6645ccb05c994ce9cac24f
Kathrin Siller <kathrinsiller@Kathrins-MacBook-Pro.local>
2023-03-03 11:01:33 +0100

    Added component test for exchange tooltip

---
+++ web/src/features/exchanges/ExchangeTooltip.cy.tsx
@@ -0,0 +1,15 @@
+import ExchangeTooltip from "./ExchangeTooltip";
+const data={ 
+    rotation: 90,
+    lonlat: [75, 75],
+    key: "DK-DK1->DK-DK2",
+    netFlow: 200,
+    co2intensity: 120,
+}
+
+it('mounts', () => {
+    cy.mount(<ExchangeTooltip exchangeData={data} />);
+    cy.contains('Denmark');
+  });
+
+
