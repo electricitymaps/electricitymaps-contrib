1ca5bb7562e937050b293830939eda034e5234d2
Frantisek12 <frantisek.komarek@gmail.com>
2023-03-07 22:03:46 +0100

    fetch exchange forecast implemented

--- parsers/CZ.py
+++ parsers/CZ.py
@@ -63,6 +63,74 @@ def make_request(session, payload, zone_key):
     return res
 
 
+def __get_exchange_data(
+    zone_key1: str = "CZ",
+    zone_key2: str = "DE",
+    session: Session = Session(),
+    target_datetime: Optional[datetime] = None,
+    logger: Logger = getLogger(__name__),
+    mode: str = "Actual",
+) -> Union[List[dict], dict]:
+
+    if not target_datetime:
+        target_datetime = datetime.now().replace(minute=0, second=0, microsecond=0)
+    from_datetime = target_datetime - timedelta(hours=48)
+
+    payload = """<?xml version="1.0" encoding="utf-8"?>
+    <soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
+      <soap12:Body>
+        <CrossborderPowerFlows xmlns="https://www.ceps.cz/CepsData/">
+          <dateFrom>{0}</dateFrom>
+          <dateTo>{1}</dateTo>
+          <agregation>{2}</agregation>
+          <function>{3}</function>
+          <version>{4}</version>
+        </CrossborderPowerFlows>
+      </soap12:Body>
+    </soap12:Envelope>""".format(
+        from_datetime.isoformat(), target_datetime.isoformat(), "QH", "AVG", "RT"
+    )
+
+    content = make_request(session, payload, zone_key1).content
+    xml = BeautifulSoup(content, "xml")
+    mapper = get_mapper(xml)
+
+    data_tag = xml.find("data")
+    data_list = []
+
+    if data_tag is not None:
+        for values in data_tag:
+            data = {
+                "sortedZoneKeys": f"{zone_key1}->{zone_key2}",
+                "datetime": datetime.fromisoformat(values["date"]),
+                "netFlow": 0.0,
+                "source": source,
+            }
+
+            for k, v in mapper.items():
+                country = "".join(
+                    [
+                        c
+                        for key, c in translate_table_dist.items()
+                        if key in k and mode in k
+                    ]
+                )
+                if country != "" and country in (zone_key1, zone_key2):
+                    data["netFlow"] += float(values[v])
+
+            data_list.append(data)
+
+    else:
+        zone_key = f"{zone_key1}->{zone_key2}"
+        ParserException(
+            "CZ.py",
+            f"There was no data returned for {zone_key1} and {zone_key2} at {target_datetime}",
+            zone_key,
+        )
+
+    return data_list
+
+
 def fetch_production(
     zone_key: str = "CZ",
     session: Session = Session(),
@@ -133,63 +201,21 @@ def fetch_exchange(
     target_datetime: Optional[datetime] = None,
     logger: Logger = getLogger(__name__),
 ) -> Union[List[dict], dict]:
-    if not target_datetime:
-        target_datetime = datetime.now().replace(minute=0, second=0, microsecond=0)
-    from_datetime = target_datetime - timedelta(hours=48)
-
-    payload = """<?xml version="1.0" encoding="utf-8"?>
-<soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
-  <soap12:Body>
-    <CrossborderPowerFlows xmlns="https://www.ceps.cz/CepsData/">
-      <dateFrom>{0}</dateFrom>
-      <dateTo>{1}</dateTo>
-      <agregation>{2}</agregation>
-      <function>{3}</function>
-      <version>{4}</version>
-    </CrossborderPowerFlows>
-  </soap12:Body>
-</soap12:Envelope>""".format(
-        from_datetime.isoformat(), target_datetime.isoformat(), "QH", "AVG", "RT"
+    return __get_exchange_data(
+        zone_key1, zone_key2, session, target_datetime, logger, mode="Actual"
     )
 
-    content = make_request(session, payload, zone_key1).content
-    xml = BeautifulSoup(content, "xml")
-    mapper = get_mapper(xml)
 
-    data_tag = xml.find("data")
-    data_list = []
-
-    if data_tag is not None:
-        for values in data_tag:
-            data = {
-                "sortedZoneKeys": f"{zone_key1}->{zone_key2}",
-                "datetime": datetime.fromisoformat(values["date"]),
-                "netFlow": 0.0,
-                "source": source,
-            }
-
-            for k, v in mapper.items():
-                country = "".join(
-                    [
-                        c
-                        for key, c in translate_table_dist.items()
-                        if key in k and "Actual" in k
-                    ]
-                )
-                if country != "" and country in (zone_key1, zone_key2):
-                    data["netFlow"] += float(values[v])
-
-            data_list.append(data)
-
-    else:
-        zone_key = f"{zone_key1}->{zone_key2}"
-        ParserException(
-            "CZ.py",
-            f"There was no data returned for {zone_key1} and {zone_key2} at {target_datetime}",
-            zone_key,
-        )
-
-    return data_list
+def fetch_exchange_forecast(
+    zone_key1: str = "CZ",
+    zone_key2: str = "DE",
+    session: Session = Session(),
+    target_datetime: Optional[datetime] = None,
+    logger: Logger = getLogger(__name__),
+) -> Union[List[dict], dict]:
+    return __get_exchange_data(
+        zone_key1, zone_key2, session, target_datetime, logger, mode="Planned"
+    )
 
 
 if __name__ == "__main__":
@@ -199,7 +225,7 @@ if __name__ == "__main__":
     print(fetch_production())
     # print("fetch_price() ->")
     # print(fetch_price())
-    # print("fetch_exchange_planned() ->")
-    # print(fetch_exchange_planned())
+    print("fetch_exchange_planned('AT', 'CZ') ->")
+    print(fetch_exchange_planned("AT", "CZ"))
     print("fetch_exchange('AT', 'CZ') ->")
     print(fetch_exchange("AT", "CZ"))
