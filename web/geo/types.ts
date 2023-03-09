81d169f2b409e4c315574dec1baf26cffa4ec19f
Mads Nedergaard <nedergaardmads@gmail.com>
2023-03-09 13:54:07 +0100

    Adds shared types

--- web/geo/types.ts
+++ web/geo/types.ts
@@ -1,6 +1,28 @@
+import { FeatureCollection, Feature, MultiPolygon, Polygon } from '@turf/turf';
+import { config } from './generateWorld';
+
+export type GeoConfig = typeof config;
+
 export interface ZoneConfig {
-  [key: string]: {
-    subZoneNames?: string[];
-    bounding_box: number[];
-  };
+  subZoneNames?: string[];
+  bounding_box: number[][];
+  timezone: string;
+  [key: string]: any;
+}
+
+export interface ZonesConfig {
+  [key: string]: ZoneConfig;
+}
+export declare type FeatureProperties = {
+  zoneName: string;
+  countryKey: string;
+  countryName: string;
+  isAggregatedView?: boolean;
+  isHighestGranularity?: boolean;
+  isCombined?: boolean;
+  [name: string]: any;
+};
+
+export interface WorldFeatureCollection extends FeatureCollection {
+  features: Feature<MultiPolygon | Polygon, FeatureProperties>[];
 }
