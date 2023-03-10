e0be55e9178e09b3099fa8470278649a22d4d21d
Mads Nedergaard <nedergaardmads@gmail.com>
2023-03-10 22:43:46 +0100

    Uses new selector for theme

--- web/src/features/map-controls/DarkModeSelector.tsx
+++ web/src/features/map-controls/DarkModeSelector.tsx
@@ -1,16 +1,12 @@
 import { useAtom } from 'jotai';
-import { ReactElement } from 'react';
 import { BsMoonStars } from 'react-icons/bs';
 import { HiOutlineComputerDesktop, HiOutlineSun } from 'react-icons/hi2';
-import { twMerge } from 'tailwind-merge';
 import { useTranslation } from 'translation/translation';
 import { ThemeOptions } from 'utils/constants';
 import { themeAtom } from 'utils/state/atoms';
 
-interface DarkModeSelectorProperties {
-  setDarkModeSelectorOpen: (isOpen: boolean) => void;
-  className?: string;
-}
+import MapButton from './MapButton';
+import MapOptionSelector from './MapOptionSelector';
 
 const ICONS = {
   light: <HiOutlineSun size={20} />,
@@ -23,44 +19,39 @@ const ICONS = {
   system: <HiOutlineComputerDesktop size={18} />,
 };
 
-export default function DarkModeSelector({
-  setDarkModeSelectorOpen,
-  className,
-}: DarkModeSelectorProperties): ReactElement {
+export default function DarkModeSelector() {
   const { __ } = useTranslation();
   const [selectedTheme, setSelectedTheme] = useAtom(themeAtom);
 
   const handleSetDarkMode = (mode: ThemeOptions) => {
     setSelectedTheme(mode);
-    setDarkModeSelectorOpen(false);
   };
-  const darkModeOptions = Object.values(ThemeOptions).map((option) => {
-    const icon = ICONS[option];
-    return (
-      <button
-        key={option}
-        onKeyDown={() => handleSetDarkMode(option)}
-        onClick={() => handleSetDarkMode(option)}
-        className={`w-full cursor-pointer px-2 py-1 text-start text-sm transition hover:bg-gray-100 dark:hover:bg-gray-600 ${
-          option === selectedTheme &&
-          'bg-gray-200 hover:bg-gray-200   dark:bg-gray-500 dark:hover:bg-gray-500'
-        }`}
-      >
-        <div className="flex">
-          <div className="mr-2"> {icon}</div>
-          {__(`darkModeButtons.${option}`)}
-        </div>
-      </button>
-    );
-  });
+
   return (
-    <div
-      className={twMerge(
-        'pointer-events-auto absolute top-[290px] right-10 h-[96px] w-[120px] overflow-auto rounded bg-white py-1 dark:bg-gray-900 dark:[color-scheme:dark]',
-        className
-      )}
+    <MapOptionSelector
+      trigger={
+        <MapButton
+          icon={<BsMoonStars size={14} style={{ strokeWidth: '0.2' }} />}
+          tooltipText={__('tooltips.toggleDarkMode')}
+          dataTestId="theme-selector-open-button"
+        />
+      }
     >
-      {darkModeOptions}
-    </div>
+      {Object.values(ThemeOptions).map((option) => (
+        <button
+          key={option}
+          onKeyDown={() => handleSetDarkMode(option)}
+          onClick={() => handleSetDarkMode(option)}
+          className={`flex w-full cursor-pointer px-2 py-2 text-start text-sm transition hover:bg-gray-200 dark:hover:bg-gray-700 ${
+            option === selectedTheme && 'bg-gray-100 font-bold dark:bg-gray-800'
+          }`}
+        >
+          <div className="flex items-center">
+            <div className="mr-2">{ICONS[option]}</div>
+            <span>{__(`darkModeButtons.${option}`)}</span>
+          </div>
+        </button>
+      ))}
+    </MapOptionSelector>
   );
 }
