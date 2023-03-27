89d8ce49eb002f22aa1bc33d8dc7db320ef2b8c0
Robin TROESCH <38283096+unitrium@users.noreply.github.com>
2023-03-27 17:22:36 +0200

    log warning and put back old adapter

--- parsers/lib/config.py
+++ parsers/lib/config.py
@@ -1,5 +1,6 @@
+from copy import deepcopy
 from datetime import timedelta
-from typing import Optional
+from logging import getLogger
 
 from requests import Session
 from requests.adapters import HTTPAdapter, Retry
@@ -32,10 +33,19 @@ def retry_policy(retry_policy: Retry):
     def wrap(f):
         def wrapped_f(*args, **kwargs):
             session = args[1] if len(args) > 2 else kwargs.get("session")
+            logger = kwargs.get("logger", getLogger(__name__))
             session = Session() if session is None else session
+            old_adapters = None
+            if "https://" in session.adapters or "http://" in session.adapters:
+                logger.warning("Session already has adapters, they will be overriden.")
+                old_adapters = deepcopy(session.adapters)
             session.mount("https://", HTTPAdapter(max_retries=retry_policy))
+            session.mount("http://", HTTPAdapter(max_retries=retry_policy))
             result = f(*args, **kwargs)
             del session.adapters["https://"]
+            del session.adapters["http://"]
+            if old_adapters is not None:
+                session.adapters.update(old_adapters)
             return result
 
         return wrapped_f
