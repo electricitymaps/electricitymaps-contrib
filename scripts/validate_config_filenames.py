ddd50933a3de708414bfa1f7b84d2ab09b374621
Quinten Yearsley <qyearsley@gmail.com>
2023-03-30 11:02:18 -0700

    Change conditional order and rename flag vars with has_

--- scripts/validate_config_filenames.py
+++ scripts/validate_config_filenames.py
@@ -14,8 +14,8 @@ import os
 def main():
     zone_files = os.listdir("config/zones")
     exchange_files = os.listdir("config/exchanges")
-    zone_error: bool = False
-    exchange_error: bool = False
+    has_zone_error: bool = False
+    has_exchange_error: bool = False
 
     print("Checking config files...")
     print("Checking if zone files are valid...")
@@ -25,12 +25,12 @@ def main():
         if file.endswith(".yaml") or file.endswith(".yml"):
             file = file.replace(".yaml", "") or file.replace(".yml", "")
             if file != file.upper():
-                zone_error = True
+                has_zone_error = True
                 print(f"ERROR: {file} is not uppercase")
-    if zone_error:
-        print("There are errors in the above zone filenames!")
-    else:
+    if not has_zone_error:
         print("All zone filenames are valid!")
+    else:
+        print("There are errors in the above zone filenames!")
 
     # Check if exchange files are valid. Exchange files must be sorted and
     # uppercase.
@@ -40,17 +40,17 @@ def main():
             exchange_keys = file.split("_")
             sorted_exchange_keys = sorted(exchange_keys)
             if file != file.upper():
-                exchange_error = True
+                has_exchange_error = True
                 print(f"ERROR: {file} is not uppercase")
             if exchange_keys != sorted_exchange_keys:
-                exchange_error = True
+                has_exchange_error = True
                 print(f"ERROR: {file} is not sorted")
-    if exchange_error:
-        print("There are errors in the above exchange filenames!")
-    else:
+    if not has_exchange_error:
         print("All exchange filenames are valid!")
+    else:
+        print("There are errors in the above exchange filenames!")
 
-    if zone_error or exchange_error:
+    if has_zone_error or has_exchange_error:
         exit(1)
 
 
