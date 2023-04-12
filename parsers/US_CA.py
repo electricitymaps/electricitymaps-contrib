996aa2e1d5852a1a9020fc685b574de52d32177a
Mathilde Daugy <mathilde.daugy@electricitymaps.com>
2023-04-12 16:05:31 +0200

    added fields to main

--- parsers/US_CA.py
+++ parsers/US_CA.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-from collections import defaultdict
 from datetime import datetime, timedelta
 from logging import Logger, getLogger
 from typing import List, Optional, Union
@@ -17,11 +16,18 @@ from parsers.lib.config import refetch_frequency
 from parsers.lib.validation import validate_consumption
 
 CAISO_PROXY = "https://us-ca-proxy-jfnx5klx2a-uw.a.run.app"
-PRODUCTION_URL_REAL_TIME = f"{CAISO_PROXY}/outlook/SP/fuelsource.csv?host=https://www.caiso.com"
-DEMAND_URL_REAL_TIME = f"{CAISO_PROXY}/outlook/SP/netdemand.csv?host=https://www.caiso.com"
+PRODUCTION_URL_REAL_TIME = (
+    f"{CAISO_PROXY}/outlook/SP/fuelsource.csv?host=https://www.caiso.com"
+)
+DEMAND_URL_REAL_TIME = (
+    f"{CAISO_PROXY}/outlook/SP/netdemand.csv?host=https://www.caiso.com"
+)
 
-HISTORICAL_URL_MAPPING = {"production":"fuelsource", "consumption":"netdemand"}
-REAL_TIME_URL_MAPPING = {"production":PRODUCTION_URL_REAL_TIME, "consumption":DEMAND_URL_REAL_TIME}
+HISTORICAL_URL_MAPPING = {"production": "fuelsource", "consumption": "netdemand"}
+REAL_TIME_URL_MAPPING = {
+    "production": PRODUCTION_URL_REAL_TIME,
+    "consumption": DEMAND_URL_REAL_TIME,
+}
 
 PRODUCTION_MODES_MAPPING = {
     "Solar": "solar",
@@ -40,13 +46,15 @@ STORAGE_MAPPING = {"Batteries": "battery"}
 
 MX_EXCHANGE_URL = "http://www.cenace.gob.mx/Paginas/Publicas/Info/DemandaRegional.aspx"
 
-def get_target_url(target_datetime: Optional[datetime], kind: str)->str:
+
+def get_target_url(target_datetime: Optional[datetime], kind: str) -> str:
     if target_datetime is None:
         target_datetime = datetime.now(tz=pytz.UTC)
         target_url = REAL_TIME_URL_MAPPING[kind]
     else:
         target_url = f"{CAISO_PROXY}/outlook/SP/History/{target_datetime.strftime('%Y%m%d')}/{HISTORICAL_URL_MAPPING[kind]}.csv?host=https://www.caiso.com"
     return target_url
+
 
 @refetch_frequency(timedelta(days=1))
 def fetch_production(
@@ -65,7 +73,7 @@ def fetch_production(
     csv = pandas.read_csv(target_url)
 
     # Filter out last row if timestamp is 00:00
-    if  csv.iloc[-1]["Time"] =="OO:OO":
+    if csv.iloc[-1]["Time"] == "OO:OO":
         df = csv.copy().iloc[:-1]
     else:
         df = csv.copy()
@@ -77,11 +85,13 @@ def fetch_production(
         production = {}
         storage = {}
 
-        row_datetime = target_datetime.replace(hour= int(row["Time"][:2]), minute=int(row["Time"][-2:]))
+        row_datetime = target_datetime.replace(
+            hour=int(row["Time"][:2]), minute=int(row["Time"][-2:])
+        )
         for mode in [mode for mode in PRODUCTION_MODES if mode in row]:
-            production_value =  float(row[mode])
+            production_value = float(row[mode])
             if production_value < 0 and (
-                mode in  ["solar", "nuclear", "coal","gas","unknown","biomass"]
+                mode in ["solar", "nuclear", "coal", "gas", "unknown", "biomass"]
             ):
                 logger.warn(
                     f" {mode} production for US_CA was reported as less than 0 and was clamped"
@@ -102,6 +112,7 @@ def fetch_production(
         all_data_points.append(data)
     return all_data_points
 
+
 @refetch_frequency(timedelta(days=1))
 def fetch_consumption(
     zone_key: str = "US-CAL-CISO",
@@ -116,30 +127,35 @@ def fetch_consumption(
     if target_datetime is None:
         target_datetime = arrow.now(tz="US/Pacific").floor("day").datetime
 
-
     # Get the demand from the CSV
     csv = pandas.read_csv(target_url)
 
     # Filter out last row if timestamp is 00:00
-    if  csv.iloc[-1]["Time"] =="OO:OO":
+    if csv.iloc[-1]["Time"] == "OO:OO":
         df = csv.copy().iloc[:-1]
     else:
         df = csv.copy()
 
     all_data_points = []
     for row in df.itertuples():
-        consumption= row._3
-        row_datetime = target_datetime.replace(hour= int(row.Time[:2]), minute=int(row.Time[-2:]))
+        consumption = row._3
+        row_datetime = target_datetime.replace(
+            hour=int(row.Time[:2]), minute=int(row.Time[-2:])
+        )
         if not np.isnan(consumption):
             data_point = {
                 "zoneKey": zone_key,
                 "consumption": consumption,
                 "source": "caiso.com",
-                "datetime":  arrow.get(row_datetime).replace(tzinfo="US/Pacific").datetime
+                "datetime": arrow.get(row_datetime)
+                .replace(tzinfo="US/Pacific")
+                .datetime,
             }
             all_data_points.append(data_point)
 
-    validated_data_points = [validate_consumption(datapoint, logger) for datapoint in all_data_points]
+    validated_data_points = [
+        validate_consumption(datapoint, logger) for datapoint in all_data_points
+    ]
     return validated_data_points
 
 
@@ -214,8 +230,14 @@ if __name__ == "__main__":
 
     from pprint import pprint
 
+
     print("fetch_production() ->")
+    pprint(fetch_production())
+
+    print('fetch_exchange("US-CA", "US") ->')
+    # pprint(fetch_exchange("US-CA", "US"))
+
+    print('fetch_exchange("MX-BC", "US-CA")')
+    pprint(fetch_exchange("MX-BC", "US-CA"))
     # pprint(fetch_production(target_datetime=datetime(2023,1,20)))s
-    pprint(fetch_consumption(target_datetime=datetime(2022,2,22)))
-
-
+    pprint(fetch_consumption(target_datetime=datetime(2022, 2, 22)))
