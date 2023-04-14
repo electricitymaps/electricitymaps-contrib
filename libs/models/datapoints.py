e25ff194323b3ef76e84e8d77aa2339db9f8609e
Robin TROESCH <38283096+unitrium@users.noreply.github.com>
2023-04-14 15:36:23 +0200

    add more validators

--- libs/models/datapoints.py
+++ libs/models/datapoints.py
@@ -1,81 +1,123 @@
 import datetime
 from abc import ABC
-from typing import Dict
+from typing import Any, Dict, Optional
 
-from electricitymap.contrib.libs.models.constants import VALID_CURRENCIES
-from pydantic import BaseModel, validator
+from pydantic import BaseModel, root_validator, validator
 
 from electricitymap.contrib.config import EXCHANGES_CONFIG, ZONES_CONFIG, ZoneKey
 from electricitymap.contrib.config.constants import PRODUCTION_MODES, STORAGE_MODES
+from electricitymap.contrib.libs.models.constants import VALID_CURRENCIES
+
+LOWER_DATETIME_BOUND = datetime.datetime(2000, 1, 1, tzinfo=datetime.timezone.utc)
+
+class ProductionMix(BaseModel):
+    biomass: Optional[float]
+    coal: Optional[float]
+    gas: Optional[float]
+    geothermal: Optional[float]
+    hydro: Optional[float]
+    nuclear: Optional[float]
+    oil: Optional[float]
+    solar: Optional[float]
+    unknown: Optional[float]
+    wind: Optional[float]
+    @root_validator
+    def _validate_production_mix(cls, values: Dict[str, Optional[float]]):
+        if all(v is None for v in values.values()):
+            raise ValueError("Production mix is completely empty")
+    @validator("*")
+    def _validate_production_mix_values(cls, v: Optional[float]):
+        if v is not None and v < 0:
+            raise ValueError(f"Production mix cannot be negative: {v}")
+        return v
+
+class StorageMix(BaseModel):
+    battery: Optional[float]
+    hydro: Optional[float]
+    @root_validator
+    def _validate_production_mix(cls, values: Dict[str, Optional[float]]):
+        if all(v is None for v in values.values()):
+            raise ValueError("Production mix is completely empty")
+
+
+
 
 
 class Datapoint(BaseModel, ABC):
     zoneKey: ZoneKey
     datetime: datetime
     source: dict
-    forecasted: bool
-    estimated: bool
-    consolidated: bool
+    forecasted: bool = False
 
     @validator("zoneKey")
-    def _validate_zone_key(self):
-        if self.zoneKey not in ZONES_CONFIG:
-            raise ValueError(f"Unknown zone: {self.zoneKey}")
+    def _validate_zone_key(cls, v):
+        if v not in ZONES_CONFIG:
+            raise ValueError(f"Unknown zone: {v}")
+        return v
+    @validator("datetime")
+    def _validate_datetime(cls, v, values: Dict[str, Any]):
+        if v.tzinfo is None:
+            raise ValueError(f"Missing timezone: {v}")
+        if v < LOWER_DATETIME_BOUND:
+            raise ValueError(f"Date is before 2000, this is not plausible: {v}")
+        if not values.get("forecasted", False) and v > datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(days=1):
+            raise ValueError(f"Date is in the future and this is not a forecasted point : {v}")
+        return v
 
 
 class Exchange(Datapoint):
     value: float
 
     @validator("zoneKey")
-    def _validate_zone_key(self):
-        if "->" not in self.zoneKey:
-            raise ValueError(f"Not an exchange key: {self.zoneKey}")
-        zone_keys = self.zoneKey.split("->")
+    def _validate_zone_key(cls, v: str):
+        if "->" not in v:
+            raise ValueError(f"Not an exchange key: {v}")
+        zone_keys = v.split("->")
         if zone_keys != sorted(zone_keys):
-            raise ValueError(f"Exchange key not sorted: {self.zoneKey}")
-        if self.zoneKey not in EXCHANGES_CONFIG:
-            raise ValueError(f"Unknown zone: {self.zoneKey}")
+            raise ValueError(f"Exchange key not sorted: {v}")
+        if v not in EXCHANGES_CONFIG:
+            raise ValueError(f"Unknown zone: {v}")
+        return v
+    @validator("value")
+    def _validate_value(cls, v: float):
+        if abs(v) > 100000:
+            raise ValueError(f"Exchange is implausibly high, above 100GW: {v}")
+        return v
 
 
 class Generation(Datapoint):
     value: float
 
     @validator("value")
-    def _validate_value(self):
-        if self.value < 0:
-            raise ValueError(f"Generation cannot be negative: {self.value}")
+    def _validate_value(cls, v: float):
+        if v < 0:
+            raise ValueError(f"Generation cannot be negative: {v}")
+        if v > 500000:
+            raise ValueError(f"Generation is implausibly high, above 500GW: {v}")
+        return v
 
 
 class ProductionBreakdown(Datapoint):
-    production: Dict[str, float]
-    storage: Dict[str, float]
-
-    @validator("production")
-    def _validate_production(self):
-        production_modes = set(self.production.keys())
-        if not production_modes.issubset(PRODUCTION_MODES):
-            raise ValueError(
-                f"Unknown production mode: {production_modes - set(PRODUCTION_MODES)}"
-            )
-
-    @validator("storage")
-    def _validate_storage(self):
-        storage_modes = set(self.storage.keys())
-        if not set(storage_modes).issubset(STORAGE_MODES):
-            raise ValueError(
-                f"Unknown storage mode: {storage_modes - set(STORAGE_MODES)}"
-            )
+    production: ProductionMix
+    storage: Optional[StorageMix]
 
 
 class Consumption(Datapoint):
     consumption: float
 
     @validator("consumption")
-    def _validate_consumption(self):
-        if self.consumption < 0:
-            raise ValueError(f"Consumption cannot be negative: {self.consumption}")
+    def _validate_consumption(cls, v: float):
+        if v < 0:
+            raise ValueError(f"Consumption cannot be negative: {v}")
+        if v > 500000:
+            raise ValueError(f"Consumption is implausibly high, above 500GW: {v}")
 
 
 class Price(Datapoint):
     price: float
     currency: str
+    @validator("currency")
+    def _validate_currency(cls, v: str):
+        if v not in VALID_CURRENCIES:
+            raise ValueError(f"Unknown currency: {v}")
+        return v
