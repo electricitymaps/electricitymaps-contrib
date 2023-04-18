a8e10608c9de1c67f09c2705f662672519abdf0b
Robin TROESCH <38283096+unitrium@users.noreply.github.com>
2023-04-18 15:06:35 +0200

    create remaining unit tests

--- electricitymap/contrib/libs/tests/test_datapoints.py
+++ electricitymap/contrib/libs/tests/test_datapoints.py
@@ -10,6 +10,14 @@ from electricitymap.contrib.libs.models.datapoints import (
     ConsumptionList,
     Exchange,
     ExchangeList,
+    Generation,
+    GenerationList,
+    Price,
+    PriceList,
+    ProductionBreakdown,
+    ProductionBreakdownList,
+    ProductionMix,
+    StorageMix,
 )
 
 
@@ -91,6 +99,7 @@ class TestExchange(unittest.TestCase):
                 value=1,
                 source="trust.me",
             )
+
     def test_append_to_list_logs_error(self):
         exchange_list = ExchangeList(logging.Logger("test"))
         with patch.object(exchange_list.logger, "error") as mock_error:
@@ -101,6 +110,7 @@ class TestExchange(unittest.TestCase):
                 source="trust.me",
             )
             mock_error.assert_called_once()
+
 
 class TestConsumption(unittest.TestCase):
     def test_create_consumption(self):
@@ -137,6 +147,7 @@ class TestConsumption(unittest.TestCase):
                 consumption=-1,
                 source="trust.me",
             )
+
     def test_consumption_list(self):
         consumption_list = ConsumptionList(logging.Logger("test"))
         consumption_list.append(
@@ -163,3 +174,202 @@ class TestConsumption(unittest.TestCase):
                 source="trust.me",
             )
             mock_error.assert_called_once()
+
+
+class TestPrice(unittest.TestCase):
+    def test_create_price(self):
+        price = Price(
+            zoneKey=ZoneKey("DE"),
+            datetime=datetime(2023, 1, 1, tzinfo=timezone.utc),
+            price=1,
+            source="trust.me",
+            currency="EUR",
+        )
+        assert price.zoneKey == ZoneKey("DE")
+        assert price.datetime == datetime(2023, 1, 1, tzinfo=timezone.utc)
+        assert price.price == 1
+        assert price.source == "trust.me"
+        assert price.currency == "EUR"
+
+    def test_invalid_price_raises(self):
+        with self.assertRaises(ValueError):
+            Price(
+                zoneKey=ZoneKey("ATT"),
+                datetime=datetime(2023, 1, 1, tzinfo=timezone.utc),
+                price=1,
+                source="trust.me",
+                currency="EUR",
+            )
+        with self.assertRaises(ValueError):
+            Price(
+                zoneKey=ZoneKey("AT"),
+                datetime=datetime(2023, 1, 1),
+                price=1,
+                source="trust.me",
+                currency="EUR",
+            )
+        with self.assertRaises(ValueError):
+            Price(
+                zoneKey=ZoneKey("AT"),
+                datetime=datetime(2023, 1, 1, tzinfo=timezone.utc),
+                price=1,
+                source="trust.me",
+                currency="EURO",
+            )
+
+    def test_price_list(self):
+        price_list = PriceList(logging.Logger("test"))
+        price_list.append(
+            zone_key=ZoneKey("AT"),
+            datetime=datetime(2023, 1, 1, tzinfo=timezone.utc),
+            price=1,
+            source="trust.me",
+            currency="EUR",
+        )
+        assert len(price_list.datapoints) == 1
+
+    def test_append_to_list_logs_error(self):
+        price_list = PriceList(logging.Logger("test"))
+        with patch.object(price_list.logger, "error") as mock_error:
+            price_list.append(
+                zone_key=ZoneKey("AT"),
+                datetime=datetime(2023, 1, 1, tzinfo=timezone.utc),
+                price=1,
+                source="trust.me",
+                currency="EURO",
+            )
+            mock_error.assert_called_once()
+
+
+class TestProductionBreakdown(unittest.TestCase):
+    def test_create_production_breakdown(self):
+        mix = ProductionMix(wind=10)
+        breakdown = ProductionBreakdown(
+            zoneKey=ZoneKey("DE"),
+            datetime=datetime(2023, 1, 1, tzinfo=timezone.utc),
+            production=mix,
+            source="trust.me",
+        )
+        assert breakdown.zoneKey == ZoneKey("DE")
+        assert breakdown.datetime == datetime(2023, 1, 1, tzinfo=timezone.utc)
+        assert breakdown.production.wind == 10
+        assert breakdown.source == "trust.me"
+
+    def test_create_production_breakdown_with_storage(self):
+        mix = ProductionMix(
+            wind=10,
+            hydro=20,
+        )
+        storage = StorageMix(
+            hydro=10,
+        )
+        breakdown = ProductionBreakdown(
+            zoneKey=ZoneKey("DE"),
+            datetime=datetime(2023, 1, 1, tzinfo=timezone.utc),
+            production=mix,
+            storage=storage,
+            source="trust.me",
+        )
+        assert breakdown.production.hydro == 20
+        assert breakdown.storage.hydro == 10
+
+    def test_invalid_breakdown_raises(self):
+        mix = ProductionMix(
+            wind=10,
+            hydro=20,
+        )
+        storage = StorageMix(
+            hydro=10,
+        )
+        with self.assertRaises(ValueError):
+            ProductionBreakdown(
+                zoneKey=ZoneKey("ATT"),
+                datetime=datetime(2023, 1, 1, tzinfo=timezone.utc),
+                production=mix,
+                source="trust.me",
+            )
+        with self.assertRaises(ValueError):
+            ProductionBreakdown(
+                zoneKey=ZoneKey("AT"),
+                datetime=datetime(2023, 1, 1),
+                production=mix,
+                source="trust.me",
+            )
+        with self.assertRaises(ValueError):
+            ProductionBreakdown(
+                zoneKey=ZoneKey("AT"),
+                datetime=datetime(2023, 1, 1, tzinfo=timezone.utc),
+                production=ProductionMix(wind=None),
+                storage=storage,
+                source="trust.me",
+            )
+
+    def test_negative_production_gets_corrected(self):
+        mix = ProductionMix(
+            wind=10,
+            hydro=-20,
+        )
+        logger = logging.Logger("test")
+        with patch.object(logger, "warning") as mock_warning:
+            breakdown = ProductionBreakdown.create(
+                logger=logger,
+                zone_key=ZoneKey("DE"),
+                datetime=datetime(2023, 1, 1, tzinfo=timezone.utc),
+                production=mix,
+                source="trust.me",
+            )
+            mock_warning.assert_called_once()
+            assert breakdown.production.hydro == None
+            assert breakdown.production.wind == 10
+
+    def test_production_list(self):
+        production_list = ProductionBreakdownList(logging.Logger("test"))
+        production_list.append(
+            zone_key=ZoneKey("AT"),
+            datetime=datetime(2023, 1, 1, tzinfo=timezone.utc),
+            production=ProductionMix(wind=10),
+            source="trust.me",
+        )
+        assert len(production_list.datapoints) == 1
+
+    def test_production_list_logs_error(self):
+        production_list = ProductionBreakdownList(logging.Logger("test"))
+        with patch.object(production_list.logger, "error") as mock_error:
+            production_list.append(
+                zone_key=ZoneKey("AT"),
+                datetime=datetime(2023, 1, 1),
+                production=ProductionMix(wind=10),
+                source="trust.me",
+            )
+            mock_error.assert_called_once()
+        with patch.object(production_list.logger, "warning") as mock_warning:
+            production_list.append(
+                zone_key=ZoneKey("AT"),
+                datetime=datetime(2023, 1, 1, tzinfo=timezone.utc),
+                production=ProductionMix(wind=-10),
+                source="trust.me",
+            )
+            mock_warning.assert_called_once()
+
+
+class TestGeneration(unittest.TestCase):
+    def test_create_generation(self):
+        generation = Generation(
+            zoneKey=ZoneKey("DE"),
+            datetime=datetime(2023, 1, 1, tzinfo=timezone.utc),
+            source="trust.me",
+            value=1,
+        )
+        assert generation.zoneKey == ZoneKey("DE")
+        assert generation.datetime == datetime(2023, 1, 1, tzinfo=timezone.utc)
+        assert generation.source == "trust.me"
+        assert generation.value == 1
+    def test_generation_list(self):
+        generation_list = GenerationList(logging.Logger("test"))
+        generation_list.append(
+            zone_key=ZoneKey("AT"),
+            datetime=datetime(2023, 1, 1, tzinfo=timezone.utc),
+            value=1,
+            source="trust.me",
+        )
+        assert len(generation_list.datapoints) == 1
