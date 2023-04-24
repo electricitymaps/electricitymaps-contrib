fcc1aaebc4ec3349ac2ed98cdb47ac9cc3be5c0c
Robin TROESCH <38283096+unitrium@users.noreply.github.com>
2023-04-24 15:47:05 +0200

    add test

---
+++ parsers/test/test_MX.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+
+import freezegun
+from requests import Session
+from requests_mock import ANY, Adapter
+
+from electricitymap.contrib.config import ZoneKey
+from electricitymap.contrib.parsers.MX import fetch_consumption
+
+
+class TestFetchConsumption(TestCase):
+    def setUp(self):
+        self.session = Session()
+        self.adapter = Adapter()
+        self.session.mount("https://", self.adapter)
+        data = open("parsers/test/mocks/MX/DemandaRegional.html", "rb")
+        self.adapter.register_uri(ANY, ANY, content=data.read())
+    @freezegun.freeze_time("2021-01-01 00:00:00")
+    def test_fetch_consumption(self):
+
+        data = fetch_consumption(ZoneKey('MX-OC'), self.session)
