1386d84633024ea4d8a76968cbd0c92da5517eea
Robin TROESCH <38283096+unitrium@users.noreply.github.com>
2023-06-12 12:43:53 +0200

    upgrade IN_EA to new EventList

--- parsers/IN_EA.py
+++ parsers/IN_EA.py
@@ -1,13 +1,13 @@
 from datetime import datetime, timedelta
 from logging import Logger, getLogger
-from typing import Optional
+from typing import Dict, List, Optional
 
 import arrow
 import pytz
 from requests import Response, Session
 
+from electricitymap.contrib.lib.models.event_lists import ExchangeList
 from parsers.lib.exceptions import ParserException
-from parsers.lib.quality import validate_datapoint_format
 
 IN_WE_PROXY = "https://in-proxy-jfnx5klx2a-el.a.run.app"
 IN_EA_TZ = pytz.timezone("Asia/Kolkata")
@@ -25,7 +25,7 @@ def fetch_exchange(
     session: Session = Session(),
     target_datetime: Optional[datetime] = None,
     logger: Logger = getLogger(__name__),
-) -> dict:
+) -> List[Dict]:
     """collects average daily exchanges for ERLC"""
     if target_datetime is None:
         # 1 day delay observed
@@ -36,27 +36,21 @@ def fetch_exchange(
     target_date = target_datetime.strftime("%Y-%m-%d")
     url_exchange = f"{IN_WE_PROXY}/api/pspreportpsp/Get/pspreport_psp_interregionalexchanges/GetByTwoDate?host=https://app.erldc.in&firstDate={target_date}&secondDate={target_date}"
     sorted_zone_keys = "->".join(sorted([zone_key1, zone_key2]))
-
+    exchanges = ExchangeList(logger)
     resp: Response = session.get(url=url_exchange)
-    if resp.status_code == 200:
-        data = resp.json()
-        zone_data = [
-            item for item in data if item["Type"] == EXCHANGE_MAPPING[zone_key2]
-        ]
-        imports = sum(float(item["ImportMW"]) for item in zone_data)
-        exports = sum(float(item["ExportMW"]) for item in zone_data)  # always negative
-        data_point = {
-            "datetime": target_datetime.replace(tzinfo=IN_EA_TZ),
-            "sortedZoneKeys": sorted_zone_keys,
-            "netFlow": imports + exports,
-            "source": "erldc.in",
-        }
-        validate_datapoint_format(
-            datapoint=data_point, zone_key=zone_key1, kind="exchange"
-        )
-        return data_point
-    else:
+    if not resp.ok:
         raise ParserException(
             parser="IN_EA.py",
             message=f"{target_datetime}: {sorted_zone_keys} data is not available : [{resp.status_code}]",
         )
+    data = resp.json()
+    zone_data = [item for item in data if item["Type"] == EXCHANGE_MAPPING[zone_key2]]
+    imports = sum(float(item["ImportMW"]) for item in zone_data)
+    exports = sum(float(item["ExportMW"]) for item in zone_data)  # always negative
+    exchanges.append(
+        sorted_zone_keys=sorted_zone_keys,
+        datetime=target_datetime.replace(tzinfo=IN_EA_TZ),
+        netFlow=imports + exports,
+        source="erldc.in",
+    )
+    return exchanges.to_list()
