8506ff52a310df4c62e4db897249add714df4170
tonypls <tonyvanswet@gmail.com>
2023-06-14 15:24:35 +0200

    extract out zone message component and use tailwind

--- web/src/features/panels/zone/ZoneHeader.tsx
+++ web/src/features/panels/zone/ZoneHeader.tsx
@@ -20,6 +20,51 @@ function LowCarbonTooltip() {
   );
 }
 
+function truncateString(string_: string, number_: number) {
+  return string_.length <= number_ ? string_ : string_.slice(0, number_) + '...';
+}
+
+function OutageMessage({
+  outageData: outageData,
+}: {
+  outageData: ZoneDetails['zoneMessage'];
+}) {
+  if (!outageData || !outageData.message) {
+    return null;
+  }
+  return (
+    <div className="pl-4">
+      <div className="w-full rounded-lg border border-yellow-300 bg-yellow-100 p-2 text-sm">
+        <p className="break-all">
+          ⚠️ {truncateString(outageData.message, 300)}{' '}
+          {outageData?.issue && outageData.issue != 'None' && (
+            <p className="inline-flex">
+              - read more{' '}
+              <a
+                className="inline-flex"
+                href={`https://github.com/electricitymaps/electricitymaps-contrib/issues/${outageData.issue}`}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="15"
+                  height="15"
+                  viewBox="0 0 24 24"
+                  className="ml-1"
+                >
+                  <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+                </svg>
+                <p className="pl-1 text-blue-600 underline">
+                  on this issue (#{outageData.issue.split('.')[0]})
+                </p>
+              </a>
+            </p>
+          )}
+        </p>
+      </div>
+    </div>
+  );
+}
+
 interface ZoneHeaderProps {
   zoneId: string;
   data: ZoneDetails | undefined;
@@ -69,39 +114,7 @@ export function ZoneHeader({ zoneId, data, isAggregated }: ZoneHeaderProps) {
         isAggregated={isAggregated}
       />
 
-      {outageData && (
-        <div
-          className="p-2 text-sm"
-          style={{
-            display: 'inline-flex',
-            backgroundColor: '#ffeb3b2b',
-            border: '1px #ffc107 solid',
-            borderRadius: '7px',
-          }}
-        >
-          <span>
-            ⚠️{' '}
-            {outageData.message || 'Data for this zone might contain some inaccuracies'} -
-            read more&nbsp;
-            <a
-              href={`https://github.com/electricitymaps/electricitymaps-contrib/issues/${outageData.issue}`}
-              style={{ color: 'blue' }}
-            >
-              <svg
-                style={{ display: 'inline' }}
-                xmlns="http://www.w3.org/2000/svg"
-                width="15"
-                height="15"
-                viewBox="0 0 24 24"
-              >
-                <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
-              </svg>
-              &nbsp;on this issue (#{outageData.issue})
-            </a>
-            .
-          </span>
-        </div>
-      )}
+      {outageData && <OutageMessage outageData={outageData} />}
 
       <div className="flex flex-row justify-evenly">
         <CarbonIntensitySquare
