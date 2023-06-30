be97fdc29333fde8412f79920e8a6fed01292eaf
Robin TROESCH <38283096+unitrium@users.noreply.github.com>
2023-06-30 11:53:56 +0200

    fix event list

--- electricitymap/contrib/lib/models/event_lists.py
+++ electricitymap/contrib/lib/models/event_lists.py
@@ -213,6 +213,7 @@ class ProductionBreakdownList(AggregatableEventList):
         if ProductionBreakdownList.is_completely_empty(
             ungrouped_production_breakdowns, logger
         ):
+            return production_breakdowns
         df = pd.concat(
             [
                 production_breakdowns.dataframe
@@ -224,11 +225,10 @@ class ProductionBreakdownList(AggregatableEventList):
 
         df = df.drop(columns=["source", "sourceType", "zoneKey"])
         df = df.groupby(level=0, dropna=False)["data"].apply(list)
-        result = ProductionBreakdownList(logger)
         for row in df:
             prod = ProductionBreakdown.aggregate(row)
-            result.events.append(prod)
-        return result
+            production_breakdowns.events.append(prod)
+        return production_breakdowns
 
 
 class TotalProductionList(EventList):
