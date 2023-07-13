2c2634358a23d1afd3480d0e930854e59898fb8a
Mathilde Daugy <mathilde.daugy@electricitymaps.com>
2023-07-13 13:24:26 +0200

    fit parser to parser validation scheme

--- parsers/MY_WM.py
+++ parsers/MY_WM.py
@@ -17,10 +17,18 @@ from typing import Optional
 import arrow
 from requests import Session
 
+from electricitymap.contrib.lib.models.event_lists import (
+    ExchangeList,
+    ProductionBreakdownList,
+    TotalConsumptionList,
+)
+from electricitymap.contrib.lib.models.events import ProductionMix
+from electricitymap.contrib.lib.types import ZoneKey
+
 # Local library imports
 from parsers.lib import config, validation
 
-DEFAULT_ZONE_KEY = "MY-WM"
+DEFAULT_ZONE_KEY = ZoneKey("MY-WM")
 DOMAIN = "www.gso.org.my"
 PRODUCTION_THRESHOLD = 10  # MW
 TIMEZONE = "Asia/Kuala_Lumpur"
@@ -29,65 +37,77 @@ EXCHANGE_URL = f"https://{DOMAIN}/SystemData/TieLine.aspx/GetChartDataSource"
 PRODUCTION_URL = f"https://{DOMAIN}/SystemData/CurrentGen.aspx/GetChartDataSource"
 SOLAR_URL = f"https://{DOMAIN}/SystemData/LargeScaleSolar.aspx/ForecastChart"
 
+PRODUCTION_BREAKDOWN = {
+    "Coal": "coal",
+    "Gas": "gas",
+    "CoGen": "unknown",
+    "Oil": "oil",
+    "Hydro": "hydro",
+    "Solar": "solar",
+}
+
 
 @config.refetch_frequency(timedelta(minutes=10))
 def fetch_consumption(
-    zone_key: str = DEFAULT_ZONE_KEY,
-    session: Optional[Session] = None,
+    zone_key: ZoneKey = DEFAULT_ZONE_KEY,
+    session: Session = Session(),
     target_datetime: Optional[datetime] = None,
     logger: Logger = getLogger(__name__),
 ) -> list:
     """Request the power consumption (in MW) of a given zone."""
     date_string = arrow.get(target_datetime).to(TIMEZONE).format("DD/MM/YYYY")
-    return [
+    consumption_data = get_api_data(
+        session or Session(),
+        CONSUMPTION_URL,
         {
-            "datetime": arrow.get(breakdown["DT"], tzinfo=TIMEZONE).datetime,
-            "consumption": breakdown["MW"],
-            "source": DOMAIN,
-            "zoneKey": zone_key,
-        }
-        for breakdown in get_api_data(
-            session or Session(),
-            CONSUMPTION_URL,
-            {
-                "Fromdate": date_string,
-                "Todate": date_string,
-            },
+            "Fromdate": date_string,
+            "Todate": date_string,
+        },
+    )
+    all_consumption_data = TotalConsumptionList(logger)
+
+    for item in consumption_data:
+        all_consumption_data.append(
+            zoneKey=zone_key,
+            datetime=arrow.get(item["DT"]).to(TIMEZONE).datetime,
+            consumption=item["MW"],
+            source=DOMAIN,
         )
-    ]
+    return all_consumption_data.to_list()
 
 
 @config.refetch_frequency(timedelta(minutes=10))
 def fetch_exchange(
-    zone_key1: str,
-    zone_key2: str,
-    session: Optional[Session] = None,
+    zone_key1: ZoneKey,
+    zone_key2: ZoneKey,
+    session: Session = Session(),
     target_datetime: Optional[datetime] = None,
     logger: Logger = getLogger(__name__),
 ) -> list:
     """Request the power exchange (in MW) between two zones."""
     date_string = arrow.get(target_datetime).to(TIMEZONE).format("DD/MM/YYYY")
-    session = session or Session()
+
     sorted_zone_keys = "->".join(sorted((zone_key1, zone_key2)))
+    all_exchange_data = ExchangeList(logger)
     if sorted_zone_keys == "MY-WM->SG":
         # The Singapore exchange is a PLTG tie.
-        return [
+        exchange_data = get_api_data(
+            session,
+            EXCHANGE_URL,
             {
-                "datetime": arrow.get(exchange["Tarikhmasa"], tzinfo=TIMEZONE).datetime,
-                "netFlow": exchange["MW"],
-                "sortedZoneKeys": sorted_zone_keys,
-                "source": DOMAIN,
-            }
-            for exchange in get_api_data(
-                session,
-                EXCHANGE_URL,
-                {
-                    "Fromdate": date_string,
-                    "Todate": date_string,
-                    "Line": "PLTG",
-                },
+                "Fromdate": date_string,
+                "Todate": date_string,
+                "Line": "PLTG",
+            },
+        )
+        for item in exchange_data:
+            all_exchange_data.append(
+                datetime=arrow.get(item["Tarikhmasa"], tzinfo=TIMEZONE).datetime,
+                netFlow=item["MW"],
+                zoneKey=ZoneKey(sorted_zone_keys),
+                source=DOMAIN,
             )
-        ]
+
     elif sorted_zone_keys == "MY-WM->TH":
         # The Thailand exchange is made up of EGAT and HVDC ties.
         egat_exchanges = get_api_data(
@@ -108,59 +128,49 @@ def fetch_exchange(
                 "Line": "HVDC",
             },
         )
-        exchanges = collections.defaultdict(float)
         for exchange in egat_exchanges + hvdc_exchanges:
-            exchanges[exchange["Tarikhmasa"]] += exchange["MW"]
-        return [
-            {
-                "datetime": arrow.get(timestamp, tzinfo=TIMEZONE).datetime,
-                "netFlow": power,
-                "sortedZoneKeys": sorted_zone_keys,
-                "source": DOMAIN,
-            }
-            for timestamp, power in exchanges.items()
-        ]
+            all_exchange_data.append(
+                datetime=arrow.get(exchange["Tarikhmasa"], tzinfo=TIMEZONE).datetime,
+                netFlow=exchange["MW"],
+                zoneKey=sorted_zone_keys,
+                source=DOMAIN,
+            )
     else:
         raise NotImplementedError("'{sorted_zone_keys}' is not implemented")
+    return all_exchange_data.to_list()
 
 
 @config.refetch_frequency(timedelta(minutes=10))
 def fetch_production(
-    zone_key: str = DEFAULT_ZONE_KEY,
-    session: Optional[Session] = None,
+    zone_key: ZoneKey = DEFAULT_ZONE_KEY,
+    session: Session = Session(),
     target_datetime: Optional[datetime] = None,
     logger: Logger = getLogger(__name__),
 ) -> list:
     """Request the production mix (in MW) of a given zone."""
     date_string = arrow.get(target_datetime).to(TIMEZONE).format("DD/MM/YYYY")
-    return [
-        validation.validate(
-            {
-                "datetime": arrow.get(breakdown["DT"], tzinfo=TIMEZONE).datetime,
-                "production": {
-                    "coal": breakdown["Coal"],
-                    "gas": breakdown["Gas"],
-                    "hydro": breakdown["Hydro"],
-                    "oil": breakdown["Oil"],
-                    "solar": breakdown["Solar"],
-                    "unknown": breakdown["CoGen"],
-                },
-                "source": DOMAIN,
-                "zoneKey": zone_key,
-            },
-            logger,
-            floor=PRODUCTION_THRESHOLD,
-            remove_negative=True,
+    all_production_data = ProductionBreakdownList(logger)
+
+    production_data = get_api_data(
+        session,
+        PRODUCTION_URL,
+        {
+            "Fromdate": date_string,
+            "Todate": date_string,
+        },
+    )
+    for item in production_data:
+        production_mix = ProductionMix()
+        item_datetime = arrow.get(item["DT"], tzinfo=TIMEZONE).datetime
+        for mode in [key for key in item if key != "DT"]:
+            production_mix.add_value(PRODUCTION_BREAKDOWN[mode], item[mode], True)
+        all_production_data.append(
+            zoneKey=zone_key,
+            production=production_mix,
+            source=DOMAIN,
+            datetime=item_datetime,
         )
-        for breakdown in get_api_data(
-            session or Session(),
-            PRODUCTION_URL,
-            {
-                "Fromdate": date_string,
-                "Todate": date_string,
-            },
-        )
-    ]
+    return all_production_data.to_list()
 
 
 @config.refetch_frequency(timedelta(minutes=10))
