dd57fa8472efb18a402411413e88708e4381918a
Viktor Andersson <30777521+VIKTORVAV99@users.noreply.github.com>
2023-07-28 11:41:19 +0200

    round values in the tooltip

--- web/src/features/charts/tooltips/NetExchangeChartTooltip.tsx
+++ web/src/features/charts/tooltips/NetExchangeChartTooltip.tsx
@@ -3,7 +3,7 @@ import { useTranslation } from 'translation/translation';
 import { timeAverageAtom } from 'utils/state/atoms';
 import { InnerAreaGraphTooltipProps } from '../types';
 import AreaGraphToolTipHeader from './AreaGraphTooltipHeader';
-import { getNetExchange } from 'utils/helpers';
+import { getNetExchange, round } from 'utils/helpers';
 import { scalePower } from 'utils/formatting';
 
 export default function PriceChartTooltip({ zoneDetail }: InnerAreaGraphTooltipProps) {
@@ -29,7 +29,7 @@ export default function PriceChartTooltip({ zoneDetail }: InnerAreaGraphTooltipP
         title={__('tooltips.netExchange')}
       />
       <p className="flex justify-center text-base">
-        {netExchange / formattingFactor} {unit}
+        {round(netExchange / formattingFactor)} {unit}
       </p>
     </div>
   );
