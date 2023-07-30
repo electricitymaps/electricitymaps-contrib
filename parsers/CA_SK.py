3304d46b6b97c561c9cab48706263620e5a514b7
nil-cipher <mmhomew@gmail.com>
2023-07-29 21:12:45 -0400

    Removed commented out portion of get request

--- parsers/CA_SK.py
+++ parsers/CA_SK.py
@@ -115,9 +115,7 @@ def fetch_consumption(
     # Set the headers to mimic a user browser as the API will return a 403 if not.
     headers = {"user-agent": USER_AGENT}
 
-    response: Response = session.get(
-        CONSUMPTION_URL, verify=False
-    )  # , headers=headers)
+    response: Response = session.get(CONSUMPTION_URL, verify=False)
 
     if not response.ok:
         raise ParserException(
