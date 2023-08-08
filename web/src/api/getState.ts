480637115486adec87f807dbbc55aaf2a40ffae0
Mads Nedergaard <nedergaardmads@gmail.com>
2023-08-08 22:50:11 +0200

    Renames endpoint to last_hour

--- web/src/api/getState.ts
+++ web/src/api/getState.ts
@@ -26,32 +26,32 @@ const getState = async (timeAverage: string): Promise<GridState> => {
 const useGetState = (): UseQueryResult<GridState> => {
   const [timeAverage] = useAtom(timeAverageAtom);
 
-  // First fetch hour zero
-  const hour_zero = useQuery<GridState>(
-    [QUERY_KEYS.STATE, { aggregate: 'hour_zero' }],
-    async () => getState('hour_zero'),
+  // First fetch last hour only
+  const last_hour = useQuery<GridState>(
+    [QUERY_KEYS.STATE, { aggregate: 'last_hour' }],
+    async () => getState('last_hour'),
     {
       enabled: timeAverage === TimeAverages.HOURLY,
     }
   );
 
-  const hourZeroWasSuccessful = Boolean(hour_zero.isLoading === false && hour_zero.data);
+  const hourZeroWasSuccessful = Boolean(last_hour.isLoading === false && last_hour.data);
 
   const shouldFetchFullState =
     timeAverage !== TimeAverages.HOURLY ||
     hourZeroWasSuccessful ||
-    hour_zero.isError === true;
+    last_hour.isError === true;
 
   // Then fetch the rest of the data
   const all_data = useQuery<GridState>(
     [QUERY_KEYS.STATE, { aggregate: timeAverage }],
     async () => getState(timeAverage),
     {
-      // The query should not execute until the hour_zero query is done
+      // The query should not execute until the last_hour query is done
       enabled: shouldFetchFullState,
     }
   );
-  return all_data.data || timeAverage != 'hourly' ? all_data : hour_zero;
+  return all_data.data || timeAverage != 'hourly' ? all_data : last_hour;
 };
 
 export default useGetState;
