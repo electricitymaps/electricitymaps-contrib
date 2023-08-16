abbf7fbbcf14cc3be27cebc760df198b737220b9
Robin TROESCH <38283096+unitrium@users.noreply.github.com>
2023-08-16 12:08:40 +0200

    Upgrade exchanges from CA_BC to new data format (#5739)
    * upgrade exchanges from CA_BC to new data format
    
    * remove depreciated zone

--- parsers/CA_BC.py
+++ parsers/CA_BC.py
@@ -3,7 +3,7 @@
 # The arrow library is used to handle datetimes
 from datetime import datetime, timedelta
 from logging import Logger, getLogger
-from typing import Optional
+from typing import Any, Dict, List, Optional
 
 import arrow
 import pandas as pd
@@ -11,7 +11,10 @@ import pytz
 from requests import Response, Session
 
 from electricitymap.contrib.config import ZONES_CONFIG
-from electricitymap.contrib.lib.models.event_lists import TotalConsumptionList
+from electricitymap.contrib.lib.models.event_lists import (
+    ExchangeList,
+    TotalConsumptionList,
+)
 from electricitymap.contrib.lib.types import ZoneKey
 from parsers.lib.config import refetch_frequency
 from parsers.lib.exceptions import ParserException
@@ -20,8 +23,15 @@ from parsers.lib.exceptions import ParserException
 # https://www.bchydro.com/energy-in-bc/our_system/transmission/transmission-system/actual-flow-data.html
 
 HISTORICAL_LOAD_REPORTS = "https://www.bchydro.com/content/dam/BCHydro/customer-portal/documents/corporate/suppliers/transmission-system/balancing_authority_load_data/Historical%20Transmission%20Data/BalancingAuthorityLoad{0}.xls"
+EXCHANGES_URL = "https://www.bchydro.com/bctc/system_cms/actual_flow/latest_values.txt"
 TIMEZONE = pytz.timezone(ZONES_CONFIG.get(ZoneKey("CA-BC")).get("timezone"))
+SOURCE = "bchydro.com"
 PUBLICATION_DELAY = timedelta(days=31)
+
+EXCHANGE_POSITION_MULTIPLIER = {
+    ZoneKey("CA-BC->US-NW-BPAT"): (1, 1),
+    ZoneKey("CA-AB->CA-BC"): (2, -1),
+}
 
 
 @refetch_frequency(timedelta(days=1))
@@ -63,7 +73,7 @@ def fetch_consumption(
             consumption_list.append(
                 zoneKey=zone_key,
                 datetime=row[0].to_pydatetime(),
-                source="bchydro.com",
+                source=SOURCE,
                 consumption=row[1]["Control Area Load"],
             )
     return consumption_list.to_list()
@@ -75,42 +85,36 @@ def fetch_exchange(
     session: Optional[Session] = None,
     target_datetime: Optional[datetime] = None,
     logger: Logger = getLogger(__name__),
-) -> dict:
+) -> List[Dict[str, Any]]:
     """Requests the last known power exchange (in MW) between two countries."""
     if target_datetime:
         raise NotImplementedError("This parser is not yet able to parse past dates")
 
     r = session or Session()
-    url = "https://www.bchydro.com/bctc/system_cms/actual_flow/latest_values.txt"
-    response = r.get(url)
+    response = r.get(EXCHANGES_URL)
     obj = response.text.split("\r\n")[1].replace("\r", "").split(",")
 
     datetime = arrow.get(
         arrow.get(obj[0], "DD-MMM-YY HH:mm:ss").datetime, TIMEZONE
     ).datetime
 
-    sortedZoneKeys = "->".join(sorted([zone_key1, zone_key2]))
-
-    if sortedZoneKeys == "CA-BC->US-BPA" or sortedZoneKeys == "CA-BC->US-NW-BPAT":
-        netFlow = float(obj[1])
-    elif sortedZoneKeys == "CA-AB->CA-BC":
-        netFlow = -1 * float(obj[2])
-    else:
+    sortedZoneKeys = ZoneKey("->".join(sorted([zone_key1, zone_key2])))
+    if sortedZoneKeys not in EXCHANGE_POSITION_MULTIPLIER:
         raise NotImplementedError("This exchange pair is not implemented")
+    position, multiplier = EXCHANGE_POSITION_MULTIPLIER[sortedZoneKeys]
 
-    return {
-        "datetime": datetime,
-        "sortedZoneKeys": sortedZoneKeys,
-        "netFlow": netFlow,
-        "source": "bchydro.com",
-    }
+    exchanges = ExchangeList(logger)
+    exchanges.append(
+        sortedZoneKeys,
+        datetime,
+        SOURCE,
+        float(obj[position]) * multiplier,
+    )
+    return exchanges.to_list()
 
 
 if __name__ == "__main__":
     """Main method, never used by the Electricity Map backend, but handy for testing."""
-
-    print("fetch_exchange(CA-BC, US-BPA) ->")
-    print(fetch_exchange("CA-BC", "US-BPA"))
     print("fetch_exchange(CA-AB, CA-BC) ->")
     print(fetch_exchange("CA-AB", "CA-BC"))
     print("fetch_consumption(CA-BC) ->")
