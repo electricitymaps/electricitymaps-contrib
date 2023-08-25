53113ae5675d9529662184c0316a6440b9a8f712
Mads Nedergaard <nedergaardmads@gmail.com>
2023-08-25 15:39:29 +0200

    Converts from const to function

--- web/src/features/charts/elements/ValueAxis.tsx
+++ web/src/features/charts/elements/ValueAxis.tsx
@@ -10,53 +10,51 @@ interface ValueAxisProps {
   formatTick: (value: number) => string | number;
 }
 
-const ValueAxis = React.memo(
-  ({ scale, label, width, height, formatTick }: ValueAxisProps) => {
-    const [y1, y2] = scale.range();
-    return (
-      <g
-        transform={`translate(${width - 1} -1)`}
-        fill="none"
-        fontSize="10"
-        fontFamily="sans-serif"
-        textAnchor="start"
-        className="opacity-50"
-        strokeWidth={0.5}
-        style={{ pointerEvents: 'none' }}
-      >
-        {label && (
-          <text
-            textAnchor="middle"
-            stroke="gray"
-            strokeWidth={0.5}
-            fontSize="0.6rem"
-            fill="gray"
-            transform={`translate(44, ${height / 2}) rotate(-90)`}
-          >
-            {label}
+function ValueAxis({ scale, label, width, height, formatTick }: ValueAxisProps) {
+  const [y1, y2] = scale.range();
+  return (
+    <g
+      transform={`translate(${width - 1} -1)`}
+      fill="none"
+      fontSize="10"
+      fontFamily="sans-serif"
+      textAnchor="start"
+      className="opacity-50"
+      strokeWidth={0.5}
+      style={{ pointerEvents: 'none' }}
+    >
+      {label && (
+        <text
+          textAnchor="middle"
+          stroke="gray"
+          strokeWidth={0.5}
+          fontSize="0.6rem"
+          fill="gray"
+          transform={`translate(44, ${height / 2}) rotate(-90)`}
+        >
+          {label}
+        </text>
+      )}
+      <path
+        className="domain"
+        stroke="currentColor"
+        d={`M6,${y1 + 0.5}H0.5V${y2 + 0.5}H6`}
+      />
+      {scale.ticks(5).map((v) => (
+        <g
+          key={`valueaxis-tick-${v}`}
+          className="tick"
+          opacity={1}
+          transform={`translate(0,${scale(v)})`}
+        >
+          <line stroke="currentColor" x2="6" />
+          <text fill="currentColor" x="6" y="3" dx="0.32em">
+            {formatTick(v)}
           </text>
-        )}
-        <path
-          className="domain"
-          stroke="currentColor"
-          d={`M6,${y1 + 0.5}H0.5V${y2 + 0.5}H6`}
-        />
-        {scale.ticks(5).map((v) => (
-          <g
-            key={`valueaxis-tick-${v}`}
-            className="tick"
-            opacity={1}
-            transform={`translate(0,${scale(v)})`}
-          >
-            <line stroke="currentColor" x2="6" />
-            <text fill="currentColor" x="6" y="3" dx="0.32em">
-              {formatTick(v)}
-            </text>
-          </g>
-        ))}
-      </g>
-    );
-  }
-);
+        </g>
+      ))}
+    </g>
+  );
+}
 
-export default ValueAxis;
+export default React.memo(ValueAxis);
