742243d049f8caffe02716f92ee8f474f71bae2f
Mads Nedergaard <nedergaardmads@gmail.com>
2023-09-04 10:55:28 +0200

    Updates look in light theme and aligns hover states

--- web/src/features/map-controls/MapButton.tsx
+++ web/src/features/map-controls/MapButton.tsx
@@ -25,7 +25,7 @@ export default function MapButton({
     <TooltipWrapper tooltipContent={tooltipText}>
       <Component
         onClick={onClick}
-        className={`pointer-events-auto flex h-8 w-8 items-center justify-center rounded bg-white/90 text-left shadow-lg backdrop-blur-sm transition hover:bg-gray-100 dark:bg-gray-800/80 dark:hover:bg-gray-900/90 ${className}`}
+        className={`pointer-events-auto flex h-8 w-8 items-center justify-center rounded bg-white/80 text-left shadow-lg backdrop-blur-sm transition  hover:bg-white dark:bg-gray-800/80 dark:hover:bg-gray-900/90 ${className}`}
         aria-label={ariaLabel}
         data-test-id={dataTestId}
         role="button"
