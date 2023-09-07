9ba3116815c94ff5a2da822b32a7c5d60b40b7f3
Robin TROESCH <38283096+unitrium@users.noreply.github.com>
2023-09-07 11:34:33 +0200

    Upgrade AR production parser to the new Event datastructure (#5838)
    * update AR parser
    
    * add testing for AR
    
    * matching timestamps only
    
    * add description
    
    * remove unused import
    
    * format
    
    * update snapshots
    
    * add info

--- parsers/test/test_AR.py
+++ parsers/test/test_AR.py
@@ -6,7 +6,12 @@ from requests_mock import ANY, GET, Adapter
 from snapshottest import TestCase
 
 from electricitymap.contrib.lib.types import ZoneKey
-from parsers.AR import fetch_exchange
+from parsers.AR import (
+    CAMMESA_DEMANDA_ENDPOINT,
+    CAMMESA_RENEWABLES_ENDPOINT,
+    fetch_exchange,
+    fetch_production,
+)
 
 
 class TestCammesaweb(TestCase):
@@ -63,5 +68,41 @@ class TestCammesaweb(TestCase):
                     "sourceType": element["sourceType"].value,
                 }
                 for element in exchange
+            ]
+        )
+
+    def test_fetch_production(self):
+        cammesa_conventional_production = resource_string(
+            "parsers.test.mocks.Cammesa", "conventional_production.json"
+        )
+        cammesa_renewable_production = resource_string(
+            "parsers.test.mocks.Cammesa", "renewable_production.json"
+        )
+        self.adapter.register_uri(
+            GET,
+            CAMMESA_DEMANDA_ENDPOINT,
+            json=loads(cammesa_conventional_production.decode("utf-8")),
+        )
+        self.adapter.register_uri(
+            GET,
+            CAMMESA_RENEWABLES_ENDPOINT,
+            json=loads(cammesa_renewable_production.decode("utf-8")),
+        )
+        production = fetch_production(
+            zone_key=ZoneKey("AR"),
+            session=self.session,
+        )
+
+        self.assertMatchSnapshot(
+            [
+                {
+                    "datetime": element["datetime"].isoformat(),
+                    "production": element["production"],
+                    "storage": element["storage"],
+                    "source": element["source"],
+                    "zoneKey": element["zoneKey"],
+                    "sourceType": element["sourceType"].value,
+                }
+                for element in production
             ]
         )
