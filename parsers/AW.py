9af7660b349f03ff678f415a5bf7106b80fa3449
Robin TROESCH <38283096+unitrium@users.noreply.github.com>
2023-09-12 09:43:16 +0200

    rewrite AW parser with production breakdown

--- parsers/AW.py
+++ parsers/AW.py
@@ -2,20 +2,33 @@
 
 from datetime import datetime
 from logging import Logger, getLogger
-from typing import Optional
+from typing import Any, Dict, List, Optional
 
-import arrow
+from pytz import timezone
 from requests import Session
+
+from electricitymap.contrib.lib.models.event_lists import ProductionBreakdownList
+from electricitymap.contrib.lib.models.events import ProductionMix
+from electricitymap.contrib.lib.types import ZoneKey
 
 from .lib.exceptions import ParserException
 
+FUEL_MAPPING = {
+    "Fossil": "oil",
+    "Wind": "wind",
+    "TotalSolar": "solar",
+    "total_bio_gas": "biomass",
+}
+TIMEZONE = timezone("America/Aruba")
+SOURCE = "webaruba.com"
+
 
 def fetch_production(
-    zone_key: str = "AW",
+    zone_key: ZoneKey = ZoneKey("AW"),
     session: Optional[Session] = None,
     target_datetime: Optional[datetime] = None,
     logger: Logger = getLogger(__name__),
-):
+) -> List[Dict[str, Any]]:
     if target_datetime:
         raise NotImplementedError("This parser is not yet able to parse past dates")
 
@@ -26,24 +39,16 @@ def fetch_production(
     response = r.get(url, headers=headers)
     aruba_json = response.json()
     top_data = aruba_json["dashboard_top_data"]
-
-    # Values currenlty used from service
-    fossil = top_data["Fossil"]
-    wind = top_data["Wind"]
-    solar = top_data["TotalSolar"]
-    # biogas live value is 0 MW all the time (2021)
-    biogas = top_data["total_bio_gas"]
-    total = top_data["TotalPower"]
+    mix = ProductionMix()
+    sources_total = 0
+    for fuel, mode in FUEL_MAPPING.items():
+        value = float(top_data[fuel]["value"])
+        mix.add_value(mode, value)
+        sources_total += value
     # "unknown" is when data reported in the categories above is less than total reported.
-    # If categories sum up to more than total, accept the datapoint, but only if it's less than 2% of total.
+    # If categories sum up to more than total, accept the datapoint, but only if it's less than 10% of total.
     # This helps avoid missing data when it's a little bit off, due to rounding or reporting
-    reported_total = float(total["value"])
-    sources_total = (
-        float(fossil["value"])
-        + float(wind["value"])
-        + float(solar["value"])
-        + float(biogas["value"])
-    )
+    reported_total = float(top_data["TotalPower"]["value"])
 
     if (sources_total / reported_total) > 1.1:
         raise ParserException(
@@ -52,27 +57,25 @@ def fetch_production(
             zone_key,
         )
 
-    missing_from_total = reported_total - sources_total
-    unknown = missing_from_total if missing_from_total > 0 else 0
+    mix.add_value(
+        "unknown", reported_total - sources_total, correct_negative_with_zero=True
+    )
     # We're using Fossil data to get timestamp in correct time zone
-    local_date_time = datetime.strptime(biogas["timestamp"], "%Y-%m-%d %H:%M:%S.%f")
-    zone_date_time = arrow.Arrow.fromdatetime(local_date_time, "America/Aruba")
+    local_date_time = TIMEZONE.localize(
+        datetime.strptime(
+            top_data["total_bio_gas"]["timestamp"], "%Y-%m-%d %H:%M:%S.%f"
+        )
+    )
 
-    data = {
-        "zoneKey": zone_key,
-        "datetime": zone_date_time.datetime,
-        "production": {
-            "oil": float(fossil["value"]),
-            "wind": float(wind["value"]),
-            "solar": float(solar["value"]),
-            "biomass": float(biogas["value"]),
-            "unknown": unknown,
-        },
-        "storage": {},
-        "source": "webaruba.com",
-    }
+    production_list = ProductionBreakdownList(logger)
+    production_list.append(
+        zone_key,
+        local_date_time,
+        SOURCE,
+        production=mix,
+    )
 
-    return data
+    return production_list.to_list()
 
 
 if __name__ == "__main__":
