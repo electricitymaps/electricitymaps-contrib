aed420ced6334081b5830882a792019ffba8003e
Mads Nedergaard <nedergaardmads@gmail.com>
2023-09-13 07:49:09 +0200

    Uses translation

--- web/src/features/charts/NotEnoughDataMessage.tsx
+++ web/src/features/charts/NotEnoughDataMessage.tsx
@@ -1,11 +1,13 @@
+import { useTranslation } from 'translation/translation';
 import { ChartTitle } from './ChartTitle';
 
 export function NotEnoughDataMessage({ title }: { title: string }) {
+  const { __ } = useTranslation();
   return (
     <div className="w-full">
       <ChartTitle translationKey={title} hasLink={false} />
       <div className="my-2 rounded bg-gray-200 py-4 text-center text-sm dark:bg-gray-800">
-        <p>Not enough data available to display chart</p>
+        <p>{__('country-history.not-enough-data')}</p>
       </div>
     </div>
   );
