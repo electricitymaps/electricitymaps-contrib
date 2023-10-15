718d99810b3c05ed7f9437b71d67f8b6d90e66da
Jonah Kruschke <jonah.kruschke@gmail.com>
2023-10-14 23:02:14 -0600

    refactor: upgrade CA-PE with event classes
    Upgrade the parser to use the ExchangeList, ProductionBreakdownList,
    ProductionMix, and ZoneKey classes. This should yield no functional
    change. Additionally:
    
    - Comply with Black's 88-column default line length limit.
    - Define global constants to replace local variables and literals.
    - Simplify the logic that retrieves event data from the source by
      transforming the response payload into a more convenient data
      structure.
    
    Refs: #6011

--- parsers/CA_PE.py
+++ parsers/CA_PE.py
@@ -1,160 +1,145 @@
 #!/usr/bin/env python3
-
 import json
 from datetime import datetime
 from logging import Logger, getLogger
 from typing import Any
+from zoneinfo import ZoneInfo
 
-# The arrow library is used to handle datetimes consistently with other parsers
-import arrow
 from requests import Session
 
-timezone = "Canada/Atlantic"
+from electricitymap.contrib.lib.models.event_lists import (
+    ExchangeList,
+    ProductionBreakdownList,
+)
+from electricitymap.contrib.lib.models.events import ProductionMix
+from electricitymap.contrib.lib.types import ZoneKey
+from parsers.lib.exceptions import ParserException
+
+SOURCE = "princeedwardisland.ca"
+TIMEZONE = ZoneInfo("Canada/Atlantic")
+# The dashboard associated with this API can be found at
+# https://www.princeedwardisland.ca/en/feature/pei-wind-energy.
+URL = "https://wdf.princeedwardisland.ca/api/workflow"  # API
+ZONE_KEY = ZoneKey("CA-PE")
 
 
-def _find_pei_key(pei_list, sought_key):
-    matching_item = [
-        item
-        for item in pei_list
-        if "header" in item["data"] and item["data"]["header"].startswith(sought_key)
-    ]
-
-    if not matching_item:
-        return None
-
-    return matching_item[0]["data"]["actualValue"]
+def _parse_power(text: str) -> float:
+    return float(text[: text.index(" MW")])
 
 
-def _get_pei_info(requests_obj):
-    url = "https://wdf.princeedwardisland.ca/api/workflow"
-    request = {"featureName": "WindEnergy", "queryName": "WindEnergy"}
-    headers = {"Content-Type": "application/json"}
-    response = requests_obj.post(url, data=json.dumps(request), headers=headers)
-
-    raw_data = response.json().get("data") or []
-
-    datetime_item = [
-        item["data"]["text"] for item in raw_data if "text" in item["data"]
-    ]
-    if not datetime_item:
-        # unable to get a timestamp, return empty
-        return None
-    datetime_text = datetime_item[0][len("Last updated ") :]
-    data_timestamp = arrow.get(datetime_text, "MMMM D, YYYY HH:mm A").replace(
-        tzinfo="Canada/Atlantic"
+def _get_event(session: Session):
+    # Request an event from the source.
+    event = session.post(
+        URL,
+        data=json.dumps({"featureName": "WindEnergy", "queryName": "WindEnergy"}),
+        headers={"Content-Type": "application/json"},
+    ).json()["data"]
+    # Collect the production modes and power values into a lookup table.
+    modes = dict(
+        (
+            object_["data"]["header"].split(": ", maxsplit=1)
+            for object_ in event
+            if object_["type"] == "GaugeChart"
+        )
     )
-
-    # see https://ruk.ca/content/new-api-endpoint-pei-wind for more info
-    data = {
-        "pei_load": _find_pei_key(raw_data, "Total On-Island Load"),
-        "pei_wind_gen": _find_pei_key(raw_data, "Total On-Island Wind Generation"),
-        "pei_fossil_gen": _find_pei_key(
-            raw_data, "Total On-Island Fossil Fuel Generation"
-        ),
-        "pei_wind_used": _find_pei_key(raw_data, "Wind Power Used On Island"),
-        "pei_wind_exported": _find_pei_key(raw_data, "Wind Power Exported Off Island"),
-        "datetime": data_timestamp.datetime,
+    # Extract the timestamp.
+    timestamp = datetime.strptime(
+        event[-1]["data"]["text"][len("Last updated ") :], "%B %d, %Y %I:%M %p"
+    ).replace(tzinfo=TIMEZONE)
+    # Return a new, more convenient lookup table.
+    return {
+        "datetime": timestamp,
+        "fossil": _parse_power(modes["Total On-Island Fossil Fuel Generation"]),
+        "load": _parse_power(modes["Total On-Island Load"]),
+        "wind": _parse_power(modes["Total On-Island Wind Generation"]),
     }
-
-    # the following keys are always required downstream, if we don't have them, no sense returning
-    if data["pei_wind_gen"] is None or data["pei_fossil_gen"] is None:
-        return None
-
-    return data
 
 
 def fetch_production(
-    zone_key: str = "CA-PE",
+    zone_key: ZoneKey = ZONE_KEY,
     session: Session | None = None,
     target_datetime: datetime | None = None,
     logger: Logger = getLogger(__name__),
-) -> dict[str, Any]:
+) -> list[dict[str, Any]]:
     """Requests the last known production mix (in MW) of a given country."""
+    if zone_key != ZONE_KEY:
+        raise ParserException("CA_PE.py", "Cannot parse zone '{zone_key}'", zone_key)
     if target_datetime:
-        raise NotImplementedError("This parser is not yet able to parse past dates")
+        raise ParserException("CA_PE.py", "Unable to fetch historical data", ZONE_KEY)
 
-    requests_obj = session or Session()
-    pei_info = _get_pei_info(requests_obj)
-
-    if pei_info is None:
-        return None
-
-    data = {
-        "datetime": pei_info["datetime"],
-        "zoneKey": zone_key,
-        "production": {
-            "wind": pei_info["pei_wind_gen"],
+    event = _get_event(session or Session())
+    production_breakdowns = ProductionBreakdownList(logger)
+    production_breakdowns.append(
+        datetime=event["datetime"],
+        production=ProductionMix(
+            # Specify some sources that definitely aren't present on PEI as
+            # zero; this allows the analyzer to better estimate CO2eq.
+            coal=0,
+            geothermal=0,
+            hydro=0,
+            nuclear=0,
             # These are oil-fueled ("heavy fuel oil" and "diesel") generators
-            # used as peakers and back-up
-            "oil": pei_info["pei_fossil_gen"],
-            # specify some sources that definitely aren't present on PEI as zero,
-            # this allows the analyzer to better estimate CO2eq
-            "coal": 0,
-            "hydro": 0,
-            "nuclear": 0,
-            "geothermal": 0,
-        },
-        "storage": {},
-        "source": "princeedwardisland.ca",
-    }
-
-    return data
+            # used as peakers and back-up.
+            oil=event["fossil"],
+            wind=event["wind"],
+        ),
+        source=SOURCE,
+        zoneKey=ZONE_KEY,
+    )
+    return production_breakdowns.to_list()
 
 
 def fetch_exchange(
-    zone_key1: str,
-    zone_key2: str,
+    zone_key1: ZoneKey,
+    zone_key2: ZoneKey,
     session: Session | None = None,
     target_datetime: datetime | None = None,
     logger: Logger = getLogger(__name__),
-) -> dict[str, Any]:
+) -> list[dict[str, Any]]:
     """Requests the last known power exchange (in MW) between two regions."""
     if target_datetime:
-        raise NotImplementedError("This parser is not yet able to parse past dates")
+        raise ParserException("CA_PE.py", "Unable to fetch historical data", ZONE_KEY)
 
-    sorted_zone_keys = "->".join(sorted([zone_key1, zone_key2]))
+    sorted_zone_keys = ZoneKey("->".join(sorted((zone_key1, zone_key2))))
+    if sorted_zone_keys != ZoneKey("CA-NB->CA-PE"):
+        raise ParserException(
+            "CA_PE.py",
+            "The exchange pair '{sorted_zone_keys}' is not implemented",
+            sorted_zone_keys,
+        )
 
-    if sorted_zone_keys != "CA-NB->CA-PE":
-        raise NotImplementedError("This exchange pair is not implemented")
-
-    requests_obj = session or Session()
-    pei_info = _get_pei_info(requests_obj)
-
-    if pei_info is None or pei_info["pei_load"] is None:
-        return None
-
-    # PEI imports most of its electricity. Everything not generated on island
-    # is imported from New Brunswick.
-    # In case of wind, some is paper-"exported" even if there is a net import,
-    # and 'pei_wind_used'/'data5' indicates their accounting of part of the load
-    # served by non-exported wind.
-    # https://www.princeedwardisland.ca/en/feature/pei-wind-energy says:
-    # "Wind Power Exported Off-Island is that portion of wind generation that is supplying
-    # contracts elsewhere. The actual electricity from this portion of wind generation
-    # may stay within PEI but is satisfying a contractual arrangement in another jurisdiction."
-    # We are ignoring these paper exports, as they are an accounting/legal detail
-    # that doesn't actually reflect what happens on the wires.
-    # (New Brunswick being the only interconnection with PEI, "exporting" wind power to NB
-    # then "importing" a balance of NB electricity likely doesn't actually happen.)
-    imported_from_nb = (
-        pei_info["pei_load"] - pei_info["pei_fossil_gen"] - pei_info["pei_wind_gen"]
+    # PEI imports most of its electricity. Everything not generated on the
+    # island is imported from New Brunswick.
+    #
+    # In the case of wind, some is "paper-exported" even if there is a net
+    # import, and the "Wind Power Exported Off Island" gauge on the dashboard
+    # indicates their accounting of these exports. The dashboard says:
+    #
+    #   Wind Power Exported Off-Island is that portion of wind generation that
+    #   is supplying contracts elsewhere. The actual electricity from this
+    #   portion of wind generation may stay within PEI but is satisfying a
+    #   contractual arrangement in another jurisdiction.
+    #
+    # We are ignoring these paper exports, as they are an accounting/legal
+    # detail that doesn't actually reflect what happens on the wires. Given
+    # that NB is the only interconnection with PEI, physically exporting wind
+    # power to NB while simultaneously importing the balance seems unlikely.
+    event = _get_event(session or Session())
+    exchanges = ExchangeList(logger)
+    exchanges.append(
+        datetime=event["datetime"],
+        # The sorted zones are CA-NB -> CA-PE, so a positive value represents
+        # export from NB / import into PE.
+        netFlow=event["load"] - event["fossil"] - event["wind"],
+        source=SOURCE,
+        zoneKey=sorted_zone_keys,
     )
 
-    # In expected result, "net" represents an export.
-    # We have sorted_zone_keys 'CA-NB->CA-PE', so it's export *from* NB,
-    # and import *to* PEI.
-    data = {
-        "datetime": pei_info["datetime"],
-        "sortedZoneKeys": sorted_zone_keys,
-        "netFlow": imported_from_nb,
-        "source": "princeedwardisland.ca",
-    }
-
-    return data
+    return exchanges.to_list()
 
 
 if __name__ == "__main__":
-    """Main method, never used by the Electricity Map backend, but handy for testing."""
+    # Never used by the Electricity Map backend, but handy for testing.
 
     print("fetch_production() ->")
     print(fetch_production())
