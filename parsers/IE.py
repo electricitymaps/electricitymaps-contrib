a1fa448a819b1ea8e5462c21a8cdbc8a843d9c05
Viktor Andersson <30777521+VIKTORVAV99@users.noreply.github.com>
2023-10-21 17:42:43 +0200

    WIP upgrade parser functions

--- parsers/IE.py
+++ parsers/IE.py
@@ -1,37 +1,59 @@
 from datetime import datetime, timedelta
 from logging import Logger, getLogger
+from zoneinfo import ZoneInfo
 
 import pytz
 from requests import Response, Session
+from electricitymap.contrib.lib.models.event_lists import (
+    ExchangeList,
+    ProductionBreakdownList,
+    TotalConsumptionList,
+    TotalProductionList,
+)
+from electricitymap.contrib.lib.models.events import (
+    EventSourceType,
+    ProductionBreakdown,
+    ProductionMix,
+)
+from electricitymap.contrib.lib.types import ZoneKey
 
 from parsers.lib.config import refetch_frequency
 from parsers.lib.exceptions import ParserException
 
-IE_TZ = pytz.timezone("Europe/Dublin")
+IE_TZ = ZoneInfo("Europe/Dublin")
 
-DEMAND_URL = "https://www.smartgriddashboard.com/DashboardService.svc/data?area=demandactual&region={zone}&datefrom={dt}+00%3A00&dateto={dt}+23%3A59"
-DEMAND_FORECAST_URL = "https://www.smartgriddashboard.com/DashboardService.svc/data?area=demandforecast&region={zone}&datefrom={dt}+00%3A00&dateto={dt}+23%3A59"
-WIND_URL = "https://www.smartgriddashboard.com/DashboardService.svc/data?area=windactual&region={zone}&datefrom={dt}+00%3A00&dateto={dt}+23%3A59"
-WIND_FORECAST_URL = "https://www.smartgriddashboard.com/DashboardService.svc/data?area=windforecast&region={zone}&datefrom={dt}+00%3A00&dateto={dt}+23%3A59"
-EXCHANGE_URL = "https://www.smartgriddashboard.com/DashboardService.svc/data?area=interconnection&region={zone}&datefrom={dt}+00%3A00&dateto={dt}+23%3A59"
+URL = "https://www.smartgriddashboard.com/DashboardService.svc/data"
 
-KINDS_URL = {
-    "demand": DEMAND_URL,
-    "demand_forecast": DEMAND_FORECAST_URL,
-    "wind": WIND_URL,
-    "wind_forecast": WIND_FORECAST_URL,
-    "exchange": EXCHANGE_URL,
+SOURCE = "eirgridgroup.com"
+
+KINDS_AREA_MAPPING = {
+    "demand": "demandactual",
+    "demand_forecast": "demandforecast",
+    "wind": "windactual",
+    "wind_forecast": "windforecast",
+    "exchange": "interconnection",
 }
 
-ZONE_MAPPING = {
-    "IE": {"key": "ROI", "exchange": "INTER_EWIC"},
-    "GB-NIR": {"key": "NI", "exchange": "INTER_MOYLE"},
+EXCHANGE_MAPPING = {
+    ZoneKey("GB->IE"): {"key": "ROI", "exchange": "INTER_EWIC", "direction": 1},
+    ZoneKey("GB->GB-NIR"): {"key": "NI", "exchange": "INTER_MOYLE", "direction": 1},
 }
+
+
+def get_datetime_params(datetime: datetime) -> dict:
+    return {
+        "datefrom": (datetime - timedelta(days=2)).strftime("%Y-%m-%d"),
+        "dateto": (datetime + timedelta(days=1)).strftime("%Y-%m-%d"),
+    }
+
+
+def parse_datetime(datetime_str: str) -> datetime:
+    return datetime.strptime(datetime_str, "%d-%b-%Y %H:%M:%S").replace(tzinfo=IE_TZ)
 
 
 def fetch_data(
     target_datetime: datetime,
-    zone_key: str,
+    zone_key: ZoneKey,
     kind: str,
     session: Session,
 ) -> list:
@@ -39,36 +61,73 @@ def fetch_data(
     Gets values and corresponding datetimes for the specified data kind in ROI.
     Removes any values that are in the future or don't have a datetime associated with them.
     """
-    assert type(target_datetime) == datetime
+    assert isinstance(target_datetime, datetime)
     assert kind != ""
     assert session is not None
 
     resp: Response = session.get(
-        KINDS_URL[kind].format(
-            dt=target_datetime.strftime("%d-%b-%Y"), zone=ZONE_MAPPING[zone_key]["key"]
-        )
+        url=URL,
+        params={
+            "area": KINDS_AREA_MAPPING[kind],
+            "region": "ROI" if zone_key == "IE" else EXCHANGE_MAPPING[zone_key]["key"],
+            **get_datetime_params(target_datetime),
+        },
     )
     try:
-        data = resp.json().get("Rows", {})
+        data = resp.json().get("Rows", [])
     except:
         raise ParserException(
             parser="IE.py",
             message=f"{target_datetime}: {kind} data is not available for {zone_key}",
         )
-    filtered_data = [item for item in data if item["Value"] is not None]
-    return filtered_data
+    return data
+
+
+def parse_consumption(
+    zone_key: ZoneKey,
+    session: Session | None,
+    target_datetime: datetime | None,
+    logger: Logger,
+    forecast: bool,
+) -> list:
+    """gets forecasted consumption values for ROI"""
+
+    session = session or Session()
+
+    if target_datetime is None:
+        target_datetime = datetime.now(tz=IE_TZ)
+
+    data = fetch_data(
+        target_datetime=target_datetime,
+        zone_key=zone_key,
+        kind="demand_forecast" if forecast else "demand",
+        session=session,
+    )
+
+    demandList = TotalConsumptionList(logger=logger)
+    for item in data:
+        demandList.append(
+            zoneKey=zone_key,
+            consumption=item["Value"],
+            datetime=parse_datetime(item["EffectiveTime"]),
+            source=SOURCE,
+            sourceType=EventSourceType.forecasted
+            if forecast
+            else EventSourceType.measured,
+        )
+    return demandList.to_list()
 
 
 @refetch_frequency(timedelta(days=1))
 def fetch_production(
-    zone_key: str,
+    zone_key: ZoneKey,
     session: Session = Session(),
     target_datetime: datetime | None = None,
     logger: Logger = getLogger(__name__),
 ) -> list:
     """Gets values for wind production and estimates unknwon production as demand - wind - exchange"""
     if target_datetime is None:
-        target_datetime = datetime.now().replace(tzinfo=IE_TZ)
+        target_datetime = datetime.now(tz=IE_TZ)
 
     demand_data = fetch_data(
         target_datetime=target_datetime,
@@ -119,9 +178,9 @@ def fetch_production(
 
 @refetch_frequency(timedelta(days=1))
 def fetch_exchange(
-    zone_key1: str,
-    zone_key2: str,
-    session: Session = Session(),
+    zone_key1: ZoneKey,
+    zone_key2: ZoneKey,
+    session: Session | None = None,
     target_datetime: datetime | None = None,
     logger: Logger = getLogger(__name__),
 ) -> list:
@@ -129,12 +188,14 @@ def fetch_exchange(
     Fetches exchanges values for the East-West (GB->IE) and Moyle (GB->GB-NIR)
     interconnectors.
     """
+    session = session or Session()
+
     if target_datetime is None:
-        target_datetime = datetime.now().replace(tzinfo=IE_TZ)
+        target_datetime = datetime.now(tz=IE_TZ)
 
-    sortedZoneKeys = "->".join(sorted([zone_key1, zone_key2]))
+    exchangeKey = ZoneKey("->".join(sorted([zone_key1, zone_key2])))
 
-    if sortedZoneKeys == "GB-NIR->IE":
+    if exchangeKey == "GB-NIR->IE":
         raise ParserException(
             parser="IE.py",
             message=f"the GB-NIR_IE interconnection is unsupported.",
@@ -142,112 +203,78 @@ def fetch_exchange(
 
     exchange_data = fetch_data(
         target_datetime=target_datetime,
-        zone_key=zone_key2,
+        zone_key=exchangeKey,
         kind="exchange",
         session=session,
     )
 
-    assert len(exchange_data) > 0
-    filtered_exchanges = [
-        item
-        for item in exchange_data
-        if item["FieldName"] == ZONE_MAPPING[zone_key2]["exchange"]
-    ]
-    exchange = []
-    for item in filtered_exchanges:
-        data_point = {
-            "netFlow": item["Value"],
-            "sortedZoneKeys": sortedZoneKeys,
-            "datetime": datetime.strptime(
-                item["EffectiveTime"], "%d-%b-%Y %H:%M:%S"
-            ).replace(tzinfo=pytz.timezone("Europe/Dublin")),
-            "source": "eirgridgroup.com",
-        }
-        exchange += [data_point]
-    return exchange
+    exchangeList = ExchangeList(logger=logger)
+    for exchange in exchange_data:
+        flow = (
+            exchange["Value"] * EXCHANGE_MAPPING[exchangeKey]["direction"]
+            if exchange["Value"]
+            else exchange["Value"]
+        )
+
+        exchangeList.append(
+            zoneKey=exchangeKey,
+            netFlow=flow,
+            datetime=parse_datetime(exchange["EffectiveTime"]),
+            source=SOURCE,
+        )
+
+    return exchangeList.to_list()
 
 
 @refetch_frequency(timedelta(days=1))
 def fetch_consumption(
-    zone_key: str,
-    session: Session = Session(),
+    zone_key: ZoneKey,
+    session: Session | None = None,
     target_datetime: datetime | None = None,
     logger: Logger = getLogger(__name__),
 ) -> list:
     """gets consumption values for ROI"""
-    if target_datetime is None:
-        target_datetime = datetime.now().replace(tzinfo=IE_TZ)
-
-    demand_data = fetch_data(
-        target_datetime=target_datetime,
+    return parse_consumption(
         zone_key=zone_key,
-        kind="demand",
         session=session,
+        target_datetime=target_datetime,
+        logger=logger,
+        forecast=False,
     )
-
-    assert len(demand_data) > 0
-
-    demand = []
-    for item in demand_data:
-        data_point = {
-            "zoneKey": zone_key,
-            "consumption": item["Value"],
-            "datetime": datetime.strptime(
-                item["EffectiveTime"], "%d-%b-%Y %H:%M:%S"
-            ).replace(tzinfo=pytz.timezone("Europe/Dublin")),
-            "source": "eirgridgroup.com",
-        }
-        demand += [data_point]
-    return demand
 
 
 @refetch_frequency(timedelta(days=1))
 def fetch_consumption_forecast(
-    zone_key: str,
-    session: Session = Session(),
+    zone_key: ZoneKey,
+    session: Session | None = None,
     target_datetime: datetime | None = None,
     logger: Logger = getLogger(__name__),
 ) -> list:
     """gets forecasted consumption values for ROI"""
-
-    if target_datetime is None:
-        target_datetime = datetime.now().replace(tzinfo=IE_TZ)
-
-    demand_forecast_data = fetch_data(
-        target_datetime=target_datetime,
+    return parse_consumption(
         zone_key=zone_key,
-        kind="demand_forecast",
         session=session,
+        target_datetime=target_datetime,
+        logger=logger,
+        forecast=True,
     )
-
-    assert len(demand_forecast_data) > 0
-
-    demand_forecast = []
-    for item in demand_forecast_data:
-        data_point = {
-            "zoneKey": zone_key,
-            "value": item["Value"],
-            "datetime": datetime.strptime(
-                item["EffectiveTime"], "%d-%b-%Y %H:%M:%S"
-            ).replace(tzinfo=pytz.timezone("Europe/Dublin")),
-            "source": "eirgridgroup.com",
-        }
-        demand_forecast += [data_point]
-    return demand_forecast
 
 
 @refetch_frequency(timedelta(days=1))
 def fetch_wind_forecasts(
-    zone_key: str,
-    session: Session = Session(),
+    zone_key: ZoneKey,
+    session: Session | None = None,
     target_datetime: datetime | None = None,
     logger: Logger = getLogger(__name__),
 ) -> list:
     """
     Gets values and corresponding datetimes for forecasted wind produciton.
     """
+
+    session = session or Session()
+
     if target_datetime is None:
-        target_datetime = datetime.now().replace(tzinfo=IE_TZ)
+        target_datetime = datetime.now(tz=IE_TZ)
 
     wind_forecast_data = fetch_data(
         target_datetime=target_datetime,
@@ -256,17 +283,15 @@ def fetch_wind_forecasts(
         session=session,
     )
 
-    assert len(wind_forecast_data) > 0
-
-    wind_forecast = []
+    wind_forecast = ProductionBreakdownList(logger=logger)
     for item in wind_forecast_data:
-        data_point = {
-            "zoneKey": zone_key,
-            "production": {"wind": item["Value"]},
-            "datetime": datetime.strptime(
-                item["EffectiveTime"], "%d-%b-%Y %H:%M:%S"
-            ).replace(tzinfo=pytz.timezone("Europe/Dublin")),
-            "source": "eirgridgroup.com",
-        }
-        wind_forecast += [data_point]
-    return wind_forecast
+        productionMix = ProductionMix()
+        productionMix.add_value("wind", item["Value"], correct_negative_with_zero=True)
+        wind_forecast.append(
+            zoneKey=zone_key,
+            production=productionMix,
+            datetime=parse_datetime(item["EffectiveTime"]),
+            source=SOURCE,
+            sourceType=EventSourceType.forecasted,
+        )
+    return wind_forecast.to_list()
