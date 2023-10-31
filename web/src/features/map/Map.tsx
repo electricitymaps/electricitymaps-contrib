7f0d7a03444ae25dbfb50760a435d739ec3d0f9c
tonypls <tonyvanswet@gmail.com>
2023-10-31 15:29:11 +0100

    1:1 dash array

--- web/src/features/map/Map.tsx
+++ web/src/features/map/Map.tsx
@@ -94,9 +94,9 @@ export default function MapPage(): ReactElement {
       } as mapboxgl.FillPaint,
       statesBorder: {
         'line-color': theme.stateBorderColor,
-        'line-width': 1,
+        'line-width': 1.4,
         'line-opacity': 0.9,
-        'line-dasharray': [4, 8],
+        'line-dasharray': [1, 1],
       } as mapboxgl.LinePaint,
     }),
     [theme]
