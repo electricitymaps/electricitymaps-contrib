86c5e2435544b5de5d4e9bcbb20650a6e1fb5a1b
Mathilde Daugy <mathilde.daugy@electricitymaps.com>
2023-10-31 12:52:19 +0100

    updated test

--- electricitymap/contrib/capacity_parsers/tests/test_ONS.py
+++ electricitymap/contrib/capacity_parsers/tests/test_ONS.py
@@ -5,23 +5,24 @@ import pandas as pd
 
 from electricitymap.contrib.capacity_parsers.ONS import filter_data_by_date
 
-datetime_tuple1 = (datetime(1980, 1, 1), datetime(2015, 1, 1))
-datetime_tuple2 = (datetime(2018, 10, 1), datetime(2023, 10, 1))
-datetime_tuple3 = (datetime(2023, 7, 1), None)
-datetime_tuple4 = (datetime(2010, 1, 1), datetime(2024, 10, 1))
-
-test_df = pd.DataFrame(
-    [
-        {"start": datetime_tuple1[0], "end": datetime_tuple1[1], "value": 1},
-        {"start": datetime_tuple2[0], "end": datetime_tuple2[1], "value": 1},
-        {"start": datetime_tuple3[0], "end": datetime_tuple3[1], "value": 1},
-        {"start": datetime_tuple4[0], "end": datetime_tuple4[1], "value": 1},
-    ]
-)
-
 
 class TestONS(unittest.TestCase):
     def test_filter_data_by_date(self):
+
+        datetime_tuple1 = (datetime(1980, 1, 1), datetime(2015, 1, 1))
+        datetime_tuple2 = (datetime(2018, 10, 1), datetime(2023, 10, 1))
+        datetime_tuple3 = (datetime(2023, 7, 1), None)
+        datetime_tuple4 = (datetime(2010, 1, 1), datetime(2024, 10, 1))
+
+        test_df = pd.DataFrame(
+            [
+                {"start": datetime_tuple1[0], "end": datetime_tuple1[1], "value": 1},
+                {"start": datetime_tuple2[0], "end": datetime_tuple2[1], "value": 1},
+                {"start": datetime_tuple3[0], "end": datetime_tuple3[1], "value": 1},
+                {"start": datetime_tuple4[0], "end": datetime_tuple4[1], "value": 1},
+            ]
+        )
+
         dt_1 = datetime(2019, 1, 1)
         dt_2 = datetime(2015, 1, 1)
         dt_3 = datetime(2023, 10, 31)
