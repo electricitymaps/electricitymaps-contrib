cf3c59f55db3c161583703033e7ecde40dad9c17
Mathilde Daugy <mathilde.daugy@electricitymaps.com>
2023-11-01 14:04:39 +0100

    update on session in fetch_production_capacity_for_all_zones

--- electricitymap/contrib/capacity_parsers/ENTSOE.py
+++ electricitymap/contrib/capacity_parsers/ENTSOE.py
@@ -123,10 +123,12 @@ def fetch_production_capacity(
 
 
 def fetch_production_capacity_for_all_zones(
-    target_datetime: datetime, session: Session
+    target_datetime: datetime, session: Session =None
 ) -> dict:
     capacity_dict = {}
-    session = Session()
+    if session is None:
+        session = Session()
+
     for zone in ENTSOE_ZONES:
         try:
             zone_capacity = fetch_production_capacity(zone, target_datetime, session)
