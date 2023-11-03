9c57938796dfdca67a2eb64168eebcb3ea47972f
Mathilde Daugy <mathilde.daugy@electricitymaps.com>
2023-11-03 10:51:58 +0100

    PR update

--- electricitymap/contrib/capacity_parsers/OPENNEM.py
+++ electricitymap/contrib/capacity_parsers/OPENNEM.py
@@ -85,7 +85,7 @@ def filter_capacity_data_by_datetime(
 def fetch_production_capacity_for_all_zones(
     target_datetime: datetime, session: Session | None = None
 ):
-session = session or Session()
+    session = session or Session()
     capacity_df = get_opennem_capacity_data(session)
     capacity_df = filter_capacity_data_by_datetime(capacity_df, target_datetime)
 
@@ -111,8 +111,9 @@ session = session or Session()
 
 
 def fetch_production_capacity(
-    zone_key: ZoneKey, target_datetime: datetime, session: Session
+    zone_key: ZoneKey, target_datetime: datetime, session: Session | None = None
 ):
+    session = session or Session()
     capacity = fetch_production_capacity_for_all_zones(target_datetime, session)[
         zone_key
     ]
