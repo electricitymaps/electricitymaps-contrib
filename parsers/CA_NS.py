6eb54093f0863eeaffe951836854e631f6f6fbfb
Jonah Kruschke <jonah.kruschke@gmail.com>
2023-11-02 20:48:56 -0600

    refactor: reliably correlate loads with mixes
    Reliably merge the source's base load array with the corresponding
    electricity mix array by correlating events based on their timestamps.
    
    Refs: #6011, #6050

--- parsers/CA_NS.py
+++ parsers/CA_NS.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-import time
 from datetime import datetime, timezone
 from logging import Logger, getLogger
 from typing import Any
@@ -21,33 +20,30 @@ SOURCE = "nspower.ca"
 ZONE_KEY = ZoneKey("CA-NS")
 
 
+def _parse_timestamp(timestamp: str) -> datetime:
+    """
+    Construct a datetime object from a date string formatted as, e.g.,
+    "/Date(1493924400000)/" by extracting the Unix timestamp 1493924400. Note
+    that the three trailing zeros are cut out as well).
+    """
+    return datetime.fromtimestamp(int(timestamp[6:-5]), tz=timezone.utc)
+
+
 def _get_ns_info(
     session: Session, logger: Logger
 ) -> tuple[ExchangeList, ProductionBreakdownList]:
-    # Request data from the endpoints until the timestamps between the two
-    # arrays agree or the retry limit is reached.
-    for _ in range(3):
-        base_loads = session.get(LOAD_URL).json()  # Base loads in MW
-        mixes = session.get(MIX_URL).json()  # Electricity mix breakdowns in %
-        if all(
-            base_load["datetime"] == mix["datetime"]
-            for base_load, mix in zip(base_loads, mixes)
-        ):
-            break
-        time.sleep(2)
-    else:
-        raise ParserException(PARSER, "source data is out of sync", ZONE_KEY)
+    # Request data from the source. Skip the first element of each JSON array
+    # because the reported base load is always 0 MW.
+    loads = {  # Lookup table mapping timestamps to base loads (in MW)
+        _parse_timestamp(load["datetime"]): load["Base Load"]
+        for load in session.get(LOAD_URL).json()[1:]
+    }
+    mixes = session.get(MIX_URL).json()[1:]  # Electricity mix breakdowns in %
 
     exchanges = ExchangeList(logger)
     production_breakdowns = ProductionBreakdownList(logger)
-    # Skip the first element of each JSON array because the reported base load
-    # is always 0 MW.
-    for base_load, mix in zip(base_loads[1:], mixes[1:]):
-        # The datetime key is in the format '/Date(1493924400000)/'; extract
-        # the timestamp 1493924400 (cutting out the last three zeros as well).
-        date_time = datetime.fromtimestamp(
-            int(base_load["datetime"][6:-5]), tz=timezone.utc
-        )
+    for mix in mixes:
+        timestamp = _parse_timestamp(mix["datetime"])
 
         # Ensure the provided percentages are within bounds, similarly to the
         # logic in https://www.nspower.ca/site/renewables/assets/js/site.js. In
@@ -66,33 +62,44 @@ def _get_ns_info(
             or 50 < mix["HFO/Natural Gas"] + mix["CT's"] + mix["LM 6000's"]
         ):
             logger.warning(
-                f"discarding datapoint at {date_time} because some mode's "
+                f"discarding datapoint at {timestamp} because some mode's "
                 f"share of the mix is infeasible: {mix}",
                 extra={"key": ZONE_KEY},
             )
             continue
 
-        load = base_load["Base Load"]
+        if timestamp in loads:
+            load = loads[timestamp]
+        else:
+            # If a base load corresponding with this timestamp is not found,
+            # assume 1244 MW based on the average yearly electricity available
+            # for use in 2014 and 2015 (Statistics Canada table 127-0008 for
+            # Nova Scotia).
+            load = 1244
+            logger.warning(
+                f"unable to find load for {timestamp}; assuming 1244 MW",
+                extra={"key": ZONE_KEY},
+            )
 
         # In this source, imports are positive. In the expected result for
         # CA-NB->CA-NS, "net" represents a flow from NB to NS, i.e., an import
         # to NS, so the value can be used directly. Note that this API only
         # specifies imports; when NS is exporting energy, the API returns 0.
         exchanges.append(
-            datetime=date_time,
+            datetime=timestamp,
             netFlow=load * mix["Imports"] / 100,
             source=SOURCE,
             zoneKey=ZoneKey("CA-NB->CA-NS"),
         )
 
         production_mix = ProductionMix()
-        production_mix.add_value("biomass", load * mix["Biomass"] / 100),
-        production_mix.add_value("coal", load * mix["Solid Fuel"] / 100),
-        production_mix.add_value("gas", load * mix["CT's"] / 100),
-        production_mix.add_value("gas", load * mix["HFO/Natural Gas"] / 100),
-        production_mix.add_value("gas", load * mix["LM 6000's"] / 100),
-        production_mix.add_value("hydro", load * mix["Hydro"] / 100),
-        production_mix.add_value("wind", load * mix["Wind"] / 100),
+        production_mix.add_value("biomass", load * mix["Biomass"] / 100)
+        production_mix.add_value("coal", load * mix["Solid Fuel"] / 100)
+        production_mix.add_value("gas", load * mix["CT's"] / 100)
+        production_mix.add_value("gas", load * mix["HFO/Natural Gas"] / 100)
+        production_mix.add_value("gas", load * mix["LM 6000's"] / 100)
+        production_mix.add_value("hydro", load * mix["Hydro"] / 100)
+        production_mix.add_value("wind", load * mix["Wind"] / 100)
         # Sanity checks: verify that reported production doesn't exceed listed
         # capacity by a lot. In particular, we've seen error cases where hydro
         # production ends up calculated as 900 MW which greatly exceeds known
@@ -105,13 +112,13 @@ def _get_ns_info(
             or 700 < production_mix.wind
         ):
             logger.warning(
-                f"discarding datapoint at {date_time} because some mode's "
+                f"discarding datapoint at {timestamp} because some mode's "
                 f"production is infeasible: {production_mix}",
                 extra={"key": ZONE_KEY},
             )
             continue
         production_breakdowns.append(
-            datetime=date_time,
+            datetime=timestamp,
             production=production_mix,
             source=SOURCE,
             zoneKey=ZONE_KEY,
@@ -166,10 +173,7 @@ def fetch_exchange(
 
 
 if __name__ == "__main__":
-    """
-    Main method, never used by the Electricity Map backend, but handy for
-    testing.
-    """
+    # Never used by the Electricity Map backend, but handy for testing.
     from pprint import pprint
 
     print("fetch_production() ->")
