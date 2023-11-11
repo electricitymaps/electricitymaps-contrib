8f6c49db2f78858138a374f51e76caaa87c739bc
silimotion <84802276+silimotion@users.noreply.github.com>
2023-11-11 19:35:02 +0100

    Review part 2

--- parsers/VN.py
+++ parsers/VN.py
@@ -25,50 +25,78 @@ LIVE_DATA = {
 
 HISTORICAL_DATA = "https://www.nldc.evn.vn/api/services/app/Dashboard/GetBieuDoTuongQuanPT"  # + dd/mm/yyyy
 
-ZONE_CODE = {ZoneKey("VN"): "HT"}
+ZONE_CODE = {
+    ZoneKey("VN"): "HT",
+    ZoneKey("VN-N"): "MB",
+    ZoneKey("VN-C"): "MT",
+    ZoneKey("VN-S"): "MN",
+}
 
 
 def fetch_consumption(
     zone_key: ZoneKey,
-    session: Session = Session(),
+    session: Session | None = None,
     target_datetime: datetime | None = None,
     logger: Logger = getLogger(__name__),
 ):
+    session = session or Session()
     if target_datetime is None:
-        live_consumption = fetch_live_consumption(session, logger)
-        # Live data is empty at 00:00, resort to historical from previous day (which includes 00:00)
+        live_consumption = fetch_live_consumption(zone_key, session, logger)
+        # Live data is empty at 00:00, resort to historical from previous day
         if len(live_consumption) > 0:
             return live_consumption
         else:
-            return fetch_historical_consumption(
-                datetime.now().astimezone(tz) - timedelta(days=1), session, logger
-            )
-    else:
+            if zone_key is ZoneKey("VN"):
+                return fetch_historical_consumption(
+                    datetime.now() - timedelta(days=1), session, logger
+                )
+            else:
+                raise ParserException(
+                    "VN.py",
+                    "No real time data found",
+                    zone_key,
+                )
+    elif target_datetime is not None and zone_key is ZoneKey("VN"):
         return fetch_historical_consumption(target_datetime, session, logger)
+    else:
+        raise NotImplementedError(
+            "This parser is not yet able to parse past dates for this zone"
+        )
 
 
 def fetch_price(
     zone_key: ZoneKey,
-    session: Session = Session(),
+    session: Session | None = None,
     target_datetime: datetime | None = None,
     logger: Logger = getLogger(__name__),
 ):
+    session = session or Session()
     if target_datetime is None:
-        live_consumption = fetch_live_price(session, logger)
-        # Live data is empty at 00:00, resort to historical from previous day (which includes 00:00)
+        live_consumption = fetch_live_price(zone_key, session, logger)
         if len(live_consumption) > 0:
             return live_consumption
         else:
-            return fetch_historical_price(
-                datetime.now().astimezone(tz) - timedelta(days=1), session, logger
-            )
-    else:
+            if zone_key is ZoneKey("VN"):
+                return fetch_historical_price(
+                    datetime.now() - timedelta(days=1), session, logger
+                )
+            else:
+                raise ParserException(
+                    "VN.py",
+                    "No real time data found",
+                    zone_key,
+                )
+    elif target_datetime is not None and zone_key is ZoneKey("VN"):
         return fetch_historical_price(target_datetime, session, logger)
+    else:
+        raise NotImplementedError(
+            "This parser is not yet able to parse past dates for this zone"
+        )
 
 
 def fetch_historical_consumption(
     target_datetime: datetime,
-    session: Session = Session(),
+    session: Session,
     logger: Logger = getLogger(__name__),
 ):
     data_list = fetch_historical_data(session=session, target_datetime=target_datetime)
@@ -76,7 +104,10 @@ def fetch_historical_consumption(
 
     for data in data_list:
         result_list.append(
-            datetime=datetime.fromisoformat(data["thoiGian"]).replace(tzinfo=tz),
+            datetime=datetime.fromisoformat(data["thoiGian"]).replace(tzinfo=tz)
+            - timedelta(
+                minutes=30
+            ),  # Substract 30 minutes to convert backwards looking timestamp to forward looking
             consumption=data["congSuat"],
             zoneKey=ZoneKey("VN"),
             source="nldc.evn.vn",
@@ -86,7 +117,7 @@ def fetch_historical_consumption(
 
 def fetch_historical_price(
     target_datetime: datetime,
-    session: Session = Session(),
+    session: Session,
     logger: Logger = getLogger(__name__),
 ):
     data_list = fetch_historical_data(session=session, target_datetime=target_datetime)
@@ -95,7 +126,8 @@ def fetch_historical_price(
 
     for data in data_list:
         result_list.append(
-            datetime=datetime.fromisoformat(data["thoiGian"]).replace(tzinfo=tz),
+            datetime=datetime.fromisoformat(data["thoiGian"]).replace(tzinfo=tz)
+            - timedelta(minutes=30),
             currency="VND",
             price=data["giaBan"],
             zoneKey=ZoneKey("VN"),
@@ -105,7 +137,8 @@ def fetch_historical_price(
 
 
 def fetch_live_consumption(
-    session: Session = Session(),
+    zone_key: ZoneKey,
+    session: Session,
     logger: Logger = getLogger(__name__),
 ):
     data_list = fetch_live_data("consumption", session)["phuTais"]
@@ -114,16 +147,18 @@ def fetch_live_consumption(
 
     for data in data_list:
         result_list.append(
-            datetime=datetime.fromisoformat(data["thoiGian"]).replace(tzinfo=tz),
-            consumption=data[f"congSuat{ZONE_CODE[ZoneKey('VN')]}"],
-            zoneKey=ZoneKey("VN"),
+            datetime=datetime.fromisoformat(data["thoiGian"]).replace(tzinfo=tz)
+            - timedelta(minutes=30),
+            consumption=data[f"congSuat{ZONE_CODE[zone_key]}"],
+            zoneKey=zone_key,
             source="nldc.evn.vn",
         )
     return result_list.to_list()
 
 
 def fetch_live_price(
-    session: Session = Session(),
+    zone_key: ZoneKey,
+    session: Session,
     logger: Logger = getLogger(__name__),
 ):
     data_list = fetch_live_data("price", session)["giaBiens"]
@@ -132,10 +167,11 @@ def fetch_live_price(
 
     for data in data_list:
         result_list.append(
-            datetime=datetime.fromisoformat(data["thoiGian"]).replace(tzinfo=tz),
-            price=data[f"giaBien{ZONE_CODE[ZoneKey('VN')]}"],
+            datetime=datetime.fromisoformat(data["thoiGian"]).replace(tzinfo=tz)
+            - timedelta(minutes=30),
+            price=data[f"giaBien{ZONE_CODE[zone_key]}"],
             currency="VND",
-            zoneKey=ZoneKey("VN"),
+            zoneKey=zone_key,
             source="nldc.evn.vn",
         )
     return result_list.to_list()
@@ -143,10 +179,10 @@ def fetch_live_price(
 
 def fetch_historical_data(
     target_datetime: datetime,
-    session: Session = Session(),
+    session: Session,
 ):
     res = session.get(
-        f"{HISTORICAL_DATA}?day={(target_datetime - timedelta(minutes=30)).strftime('%d/%m/%Y')}"
+        f"{HISTORICAL_DATA}?day={(target_datetime.astimezone(tz)).strftime('%d/%m/%Y')}"
     )
     if not res.ok:
         raise ParserException(
@@ -155,7 +191,7 @@ def fetch_historical_data(
     return json.loads(res.text)["result"]["data"]["phuTai"]
 
 
-def fetch_live_data(data_type=str, session: Session = Session()):
+def fetch_live_data(data_type: str, session: Session):
     res = session.get(LIVE_DATA[data_type])
     if not res.ok:
         raise ParserException(
