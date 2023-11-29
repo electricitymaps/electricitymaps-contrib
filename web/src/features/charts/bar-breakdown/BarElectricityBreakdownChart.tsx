7a95e6da6bfba8e37587882f1fae10d730a0866c
silimotion <84802276+silimotion@users.noreply.github.com>
2023-11-29 16:16:37 +0100

    BreakdownChart fix

--- web/src/features/charts/bar-breakdown/BarElectricityBreakdownChart.tsx
+++ web/src/features/charts/bar-breakdown/BarElectricityBreakdownChart.tsx
@@ -105,7 +105,7 @@ function BarElectricityBreakdownChart({
       const tickValue = isHourly ? formatPower(maxPower, 1) : formatEnergy(maxPower, 1);
       return tickValue.toString().replace(/[\d.]+/, '0');
     }
-    return isHourly ? formatPower(t) : formatEnergy(t);
+    return isHourly ? formatPower(t, 1) : formatEnergy(t, 1);
   };
   return (
     <svg className="w-full overflow-visible" height={height}>
