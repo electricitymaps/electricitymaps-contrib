283f8fa8923c6b476014787493a3c9908d6ad57e
Viktor Andersson <30777521+VIKTORVAV99@users.noreply.github.com>
2023-12-19 22:57:49 +0100

    fix exchange rendering issue when going back to hourly

--- web/src/hooks/arrows.ts
+++ web/src/hooks/arrows.ts
@@ -1,41 +1,22 @@
-/* eslint-disable unicorn/no-array-reduce */
 import useGetState from 'api/getState';
 import { useAtom } from 'jotai';
 import { useMemo } from 'react';
-import { ExchangeArrowData, GridState, StateExchangeData } from 'types';
-import { SpatialAggregate, TimeAverages } from 'utils/constants';
-import {
-  productionConsumptionAtom,
-  selectedDatetimeIndexAtom,
-  spatialAggregateAtom,
-  timeAverageAtom,
-} from 'utils/state/atoms';
+import { ExchangeArrowData, StateExchangeData } from 'types';
+import { SpatialAggregate } from 'utils/constants';
+import { selectedDatetimeIndexAtom, spatialAggregateAtom } from 'utils/state/atoms';
 
 import exchangesConfigJSON from '../../config/exchanges.json'; // do something globally
 import exchangesToExclude from '../../config/excluded_aggregated_exchanges.json'; // do something globally
 
-// TODO: set up proper typed method for retrieving config files.
 const exchangesConfig: Record<string, any> = exchangesConfigJSON;
 
 export function useExchangeArrowsData(): ExchangeArrowData[] {
-  const [timeAverage] = useAtom(timeAverageAtom);
   const [selectedDatetime] = useAtom(selectedDatetimeIndexAtom);
   const [viewMode] = useAtom(spatialAggregateAtom);
-  const { data, isError, isLoading } = useGetState();
-  const [mode] = useAtom(productionConsumptionAtom);
-  const isConsumption = mode === 'consumption';
-  const isHourly = timeAverage === TimeAverages.HOURLY;
+  const { data } = useGetState();
 
   const exchangesToUse: { [key: string]: StateExchangeData } = useMemo(() => {
-    if (!data) {
-      return {};
-    }
-    const grid = data as GridState;
-
-    const exchanges = grid?.data?.datetimes?.[selectedDatetime?.datetimeString]?.e;
-    if (!exchanges) {
-      return [];
-    }
+    const exchanges = data?.data?.datetimes?.[selectedDatetime?.datetimeString]?.e;
 
     const exchangesToExcludeZoneViewSet = new Set(
       exchangesToExclude.exchangesToExcludeZoneView
@@ -44,36 +25,30 @@ export function useExchangeArrowsData(): ExchangeArrowData[] {
       exchangesToExclude.exchangesToExcludeCountryView
     );
 
-    let zoneViewExchanges = {};
-    let countryViewExchanges = {};
+    const zoneViewExchanges: { [key: string]: StateExchangeData } = {};
+    const countryViewExchanges: { [key: string]: StateExchangeData } = {};
 
-    for (const key of Object.keys(exchanges)) {
+    for (const key of Object.keys(exchanges ?? {})) {
       if (!exchangesToExcludeZoneViewSet.has(key)) {
-        zoneViewExchanges = { ...zoneViewExchanges, [key]: exchanges[key] };
+        zoneViewExchanges[key] = exchanges?.[key] ?? {};
       }
       if (!exchangesToExcludeCountryViewSet.has(key)) {
-        countryViewExchanges = { ...countryViewExchanges, [key]: exchanges[key] };
+        countryViewExchanges[key] = exchanges?.[key] ?? {};
       }
     }
 
     return viewMode === SpatialAggregate.COUNTRY
       ? countryViewExchanges
       : zoneViewExchanges;
-  }, [viewMode, data]);
+  }, [data, selectedDatetime, viewMode]);
 
-  if (isError || isLoading || !isConsumption || !isHourly) {
-    return [];
-  }
-
-  const exchanges = data.data.datetimes[selectedDatetime.datetimeString].e;
-
-  const currentExchanges: ExchangeArrowData[] = Object.entries(exchangesToUse)
-    .filter(([key]) => exchanges[key] !== undefined)
-    .map(([key, value]) => ({
+  const currentExchanges: ExchangeArrowData[] = useMemo(() => {
+    return Object.entries(exchangesToUse).map(([key, value]) => ({
       ...value,
       ...exchangesConfig[key],
-      key: key,
+      key,
     }));
+  }, [exchangesToUse]);
 
   return currentExchanges;
 }
