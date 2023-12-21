e42f65a0384dce15621bbefdd9f27ab086de8d1f
Imran <imranbarissa@gmail.com>
2023-12-21 19:01:53 +0300

    refactor: remove arrow from CA-QC (#6192)
    * refactor: remove arrow from CA-QC
    - maintained parser functionality
    
    * update CA_QC
    
    ---------
    
    Co-authored-by: Viktor Andersson <30777521+VIKTORVAV99@users.noreply.github.com>

--- parsers/CA_QC.py
+++ parsers/CA_QC.py
@@ -4,8 +4,6 @@ from pprint import pprint
 from typing import Any
 from zoneinfo import ZoneInfo
 
-# The arrow library is used to handle datetimes
-import arrow
 from requests import Session
 
 from electricitymap.contrib.lib.models.event_lists import (
@@ -37,7 +35,8 @@ def fetch_production(
     now = datetime.now(tz=TIMEZONE)
     for elem in data:
         values = elem["valeurs"]
-        timestamp = arrow.get(elem["date"], tzinfo=TIMEZONE).datetime
+        if isinstance(elem["date"], str):
+            timestamp = datetime.fromisoformat(elem["date"]).replace(tzinfo=TIMEZONE)
         # The datasource returns future timestamps or recent with a 0.0 value, so we ignore them.
         if timestamp <= now and values.get("total", 0) > 0:
             production.append(
@@ -71,7 +70,7 @@ def fetch_consumption(
         if "demandeTotal" in elem["valeurs"]:
             consumption.append(
                 zoneKey=zone_key,
-                datetime=arrow.get(elem["date"], tzinfo=TIMEZONE).datetime,
+                datetime=datetime.fromisoformat(elem["date"]).replace(tzinfo=TIMEZONE),
                 consumption=elem["valeurs"]["demandeTotal"],
                 source=SOURCE,
             )
