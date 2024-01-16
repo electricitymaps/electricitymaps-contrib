e2ee846911e67f909d56d6660930a00771954348
Robin TROESCH <38283096+unitrium@users.noreply.github.com>
2024-01-16 11:21:09 +0100

    remove from tests

--- parsers/test/test_EIA.py
+++ parsers/test/test_EIA.py
@@ -8,7 +8,6 @@ from requests import Session
 from requests_mock import ANY, GET, Adapter
 
 from electricitymap.contrib.lib.models.events import EventSourceType
-from electricitymap.contrib.lib.models.event_lists import ProductionBreakdownList
 from electricitymap.contrib.lib.types import ZoneKey
 from parsers import EIA
 
