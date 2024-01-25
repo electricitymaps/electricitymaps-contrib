020c0db25bc1831b280009d19e9baa5a3dde68b4
Robin TROESCH <38283096+unitrium@users.noreply.github.com>
2024-01-25 15:07:33 +0100

    Update electricitymap/contrib/config/capacity.py

--- electricitymap/contrib/config/capacity.py
+++ electricitymap/contrib/config/capacity.py
@@ -19,7 +19,6 @@ for zone_id, zone_config in ZONES_CONFIG.items():
 ZONE_TO_CAPACITY_PARSER_SOURCE = {
     zone: source for source, zones in ZONES_CONFIG.items() for zone in zones
 }
-    for zone in zones:
 
 
 def get_capacity_data(capacity_config: dict, dt: datetime) -> dict[str, float]:
