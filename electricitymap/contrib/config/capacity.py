5bfa863475fc8319f83eec7954698ff56cb63940
Mathilde Daugy <107848894+mathilde-daugy@users.noreply.github.com>
2024-01-25 09:18:23 +0100

    Update electricitymap/contrib/config/capacity.py
    Co-authored-by: Robin TROESCH <38283096+unitrium@users.noreply.github.com>

--- electricitymap/contrib/config/capacity.py
+++ electricitymap/contrib/config/capacity.py
@@ -16,7 +16,11 @@ for zone_id, zone_config in ZONES_CONFIG.items():
         CAPACITY_PARSER_SOURCE_TO_ZONES[source] = []
     CAPACITY_PARSER_SOURCE_TO_ZONES[source].append(zone_id)
 
-ZONE_TO_CAPACITY_PARSER_SOURCE = {}
+ZONE_TO_CAPACITY_PARSER_SOURCE = {
+    zone: source
+    for source, zones in ZONES_CONFIG.items()
+    for zone in zones
+}
 for source, zones in CAPACITY_PARSER_SOURCE_TO_ZONES.items():
     for zone in zones:
         ZONE_TO_CAPACITY_PARSER_SOURCE[zone] = source
