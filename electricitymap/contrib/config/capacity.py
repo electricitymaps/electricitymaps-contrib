e713c4f6b247bf0242e2cb4781bc35079130040d
Mathilde Daugy <mathilde.daugy@electricitymaps.com>
2024-01-31 16:55:21 +0100

    update capacity

--- electricitymap/contrib/config/capacity.py
+++ electricitymap/contrib/config/capacity.py
@@ -32,7 +32,7 @@ def get_capacity_data(capacity_config: dict, dt: datetime) -> dict[str, float]:
             # TODO: This part is used for the old capacity format. It shoud be removed once all capacity configs are updated
             capacity[mode] = capacity_value
         else:
-            capacity[mode] = get_capacity_value_with_datetime(capacity_value, dt)
+            capacity[mode] = get_capacity_value_with_datetime(capacity_value, dt)[0]
     return capacity
 
 
@@ -47,23 +47,25 @@ def get_capacity_data_with_source(
             capacity[mode] = {"value": capacity_value, "source": None}
         else:
             capacity[mode] = {
-                "value": get_capacity_value_with_datetime(capacity_value, dt),
-                "source": capacity_value["source"],
+                "value": get_capacity_value_with_datetime(capacity_value, dt)[0],
+                "source": get_capacity_value_with_datetime(capacity_value, dt)[1],
             }
+
     return capacity
 
 
 def get_capacity_value_with_datetime(
     mode_capacity: list | dict, dt: datetime
-) -> float | None:
+) -> tuple(float, str) | float | None:
     if isinstance(mode_capacity, dict):
         return mode_capacity["value"]
     elif isinstance(mode_capacity, list):
-        capacity_tuples = [(d["datetime"], d["value"]) for d in mode_capacity]
+        capacity_tuples = [(d["datetime"], d["value"], d["source"]) for d in mode_capacity]
 
         if dt.isoformat() <= min(capacity_tuples)[0]:
-            return min(capacity_tuples)[1]
+            return (min(capacity_tuples)[1], min(capacity_tuples)[2])
         else:
             # valid datetime is the max datetime that is lower than the given datetime
             # In other words, it is the most recent value that is valid for the given dt
-            return max([(d, v) for d, v in capacity_tuples if d <= dt.isoformat()])[1]
+            max_tuple = max([(d, v) for d, v in capacity_tuples if d <= dt.isoformat()])
+            return (max_tuple[1], max_tuple[2])
