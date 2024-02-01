fa203926af1fedb0b93a04e758f036e9cb4a670b
Mathilde Daugy <mathilde.daugy@electricitymaps.com>
2024-02-01 10:11:08 +0100

    formt

--- electricitymap/contrib/config/capacity.py
+++ electricitymap/contrib/config/capacity.py
@@ -60,12 +60,17 @@ def get_capacity_from_dict_or_list(
     if isinstance(mode_capacity, dict):
         return (mode_capacity["value"], mode_capacity.get("source", "unknown source"))
     elif isinstance(mode_capacity, list):
-        capacity_tuples = [(d["datetime"], d["value"], d.get("source", "unknown source")) for d in mode_capacity]
+        capacity_tuples = [
+            (d["datetime"], d["value"], d.get("source", "unknown source"))
+            for d in mode_capacity
+        ]
 
         if dt.isoformat() <= min(capacity_tuples)[0]:
             return (min(capacity_tuples)[1], min(capacity_tuples)[2])
         else:
             # valid datetime is the max datetime that is lower than the given datetime
             # In other words, it is the most recent value that is valid for the given dt
-            max_tuple = max([(d, v, s) for d, v,s in capacity_tuples if d <= dt.isoformat()])
+            max_tuple = max(
+                [(d, v, s) for d, v, s in capacity_tuples if d <= dt.isoformat()]
+            )
             return (max_tuple[1], max_tuple[2])
