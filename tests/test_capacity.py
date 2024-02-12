c2d4d52d533165ed3c20685412a2c6c9ce0dc343
Mathilde Daugy <107848894+mathilde-daugy@users.noreply.github.com>
2024-02-12 09:35:30 +0100

    Revert-revert - update get_capacity_data function again (#6467)
    * update capacity
    
    * test + fix
    
    * rename function
    
    * formt
    
    * update get_capacity_data fct
    
    * format
    
    ---------
    
    Co-authored-by: Mathilde Daugy <mathilde.daugy@electricitymaps.com>

--- tests/test_capacity.py
+++ tests/test_capacity.py
@@ -2,34 +2,40 @@ from datetime import datetime
 
 import pytest
 
-from electricitymap.contrib.config.capacity import get_capacity_data
+from electricitymap.contrib.config.capacity import (
+    get_capacity_data,
+    get_capacity_data_with_source,
+)
 
 
 def test_get_capacity_data():
     capacity_data = {}
-    capacity_data_1 = {"solar": 3, "wind": 4}
+    capacity_data_1 = {"solar": 3, "wind": 4, "coal": None}
     capacity_data_2 = {
-        "coal": {"datetime": "2022-01-01", "value": 5},
-        "gas": {"datetime": "2022-01-01", "value": 6},
+        "coal": {"datetime": "2022-01-01", "value": 5, "source": "abc"},
+        "gas": {"datetime": "2022-01-01", "value": 6, "source": "abc"},
+        "solar": {"datetime": "2022-01-01", "value": None, "source": "abc"},
     }
     capacity_data_3 = {
         "coal": [
-            {"datetime": "2022-01-01", "value": 5},
-            {"datetime": "2023-06-01", "value": 8},
+            {"datetime": "2022-01-01", "value": 5, "source": "abc"},
+            {"datetime": "2023-06-01", "value": 8, "source": "abc"},
         ],
         "gas": [
-            {"datetime": "2022-01-01", "value": 6},
-            {"datetime": "2023-06-01", "value": 7},
+            {"datetime": "2022-01-01", "value": 6, "source": "abc"},
+            {"datetime": "2023-06-01", "value": 7, "source": "abc"},
         ],
     }
     assert get_capacity_data(capacity_data, datetime(2023, 1, 1)) == {}
     assert get_capacity_data(capacity_data_1, datetime(2023, 1, 1)) == {
         "solar": 3,
         "wind": 4,
+        "coal": None,
     }
     assert get_capacity_data(capacity_data_2, datetime(2022, 6, 1)) == {
         "coal": 5,
         "gas": 6,
+        "solar": None,
     }
     assert get_capacity_data(capacity_data_3, datetime(2023, 6, 1)) == {
         "coal": 8,
@@ -52,3 +58,27 @@ def test_get_capacity_from_list():
             capacity = item["value"]
 
     assert capacity == 3
+
+
+def test_get_capacity_with_source():
+    capacity_config = {
+        "solar": {"datetime": "2023-10-01", "value": 3, "source": "abc"},
+        "wind": [
+            {"datetime": "2023-10-01", "value": 4, "source": "abc"},
+            {"datetime": "2023-11-01", "value": 5, "source": "abc"},
+        ],
+    }
+
+    capacity_data = get_capacity_data_with_source(
+        capacity_config, datetime(2023, 10, 1)
+    )
+
+    assert capacity_data == {
+        "solar": {"value": 3, "source": "abc"},
+        "wind": {"value": 4, "source": "abc"},
+    }
+
+    assert get_capacity_data_with_source(capacity_config, datetime(2023, 11, 1)) == {
+        "solar": {"value": 3, "source": "abc"},
+        "wind": {"value": 5, "source": "abc"},
+    }
