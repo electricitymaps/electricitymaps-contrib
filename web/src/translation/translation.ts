2a236532083d5f294e1faff460a6212dab426315
madsnedergaard <nedergaardmads@gmail.com>
2024-02-18 23:23:35 +0100

    Removes unused function

--- web/src/translation/translation.ts
+++ web/src/translation/translation.ts
@@ -37,24 +37,6 @@ export const getZoneName = (zoneCode: string) =>
 export const getCountryName = (zoneCode: string) =>
   translateIfExists(`zoneShortName.${zoneCode}.countryName`);
 
-/**
- * Gets the full name of a zone with the country name in parentheses.
- * @param {string} zoneCode
- * @returns string
- */
-export function getZoneNameWithCountry(zoneCode: string) {
-  const zoneName = getZoneName(zoneCode);
-  if (!zoneName) {
-    return zoneCode;
-  }
-  const countryName = getCountryName(zoneCode);
-  if (!countryName) {
-    return zoneName;
-  }
-
-  return `${zoneName} (${countryName})`;
-}
-
 const DEFAULT_ZONE_NAME_LIMIT = 40;
 /**
  * Gets the name of a zone with the country name in parentheses and zone-name ellipsified if too long.
