7ca2e14d74b35461f0e7bc8150c85f574e7d7d4f
Viktor Andersson <30777521+VIKTORVAV99@users.noreply.github.com>
2024-02-24 15:17:39 +0100

    fix types

--- web/src/features/map/map-layers/ZonesLayer.tsx
+++ web/src/features/map/map-layers/ZonesLayer.tsx
@@ -9,69 +9,71 @@ export default function ZonesLayer() {
   const { worldGeometries } = useGetGeometries();
   const theme = useTheme();
 
-  const zoneLayerStyles = useMemo(
-    () => ({
-      zonesBorder: {
-        'line-color': [
-          'case',
-          ['boolean', ['feature-state', 'selected'], false],
-          'white',
-          theme.strokeColor,
-        ],
-        'line-width': [
-          'case',
-          ['boolean', ['feature-state', 'selected'], false],
-          theme.strokeWidth * 12,
-          ['boolean', ['feature-state', 'hover'], false],
-          theme.strokeWidth * 10,
-          theme.strokeWidth,
-        ],
-      } as mapboxgl.LinePaint,
-      zonesClickable: {
-        'fill-color': [
-          'coalesce',
-          ['feature-state', 'color'],
-          ['get', 'color'],
-          theme.clickableFill,
-        ],
-      } as mapboxgl.FillPaint,
-      zonesHover: {
-        'fill-color': '#FFFFFF',
-        'fill-opacity': ['case', ['boolean', ['feature-state', 'hover'], false], 0.3, 0],
-      } as mapboxgl.FillPaint,
-      zonesSelected: {
-        'fill-color': [
-          'coalesce',
-          ['feature-state', 'color'],
-          ['get', 'color'],
-          theme.clickableFill,
-        ],
-        'fill-opacity': [
-          'case',
-          ['boolean', ['feature-state', 'selected'], false],
-          0.7,
-          0,
-        ],
-      } as mapboxgl.FillPaint,
-    }),
-
-    [theme]
-  );
-
   return (
     <Source id={ZONE_SOURCE} promoteId={'zoneId'} type="geojson" data={worldGeometries}>
       <Layer
         id="zones-clickable-layer"
         type="fill"
-        paint={zoneLayerStyles.zonesClickable}
+        paint={{
+          'fill-color': [
+            'coalesce',
+            ['feature-state', 'color'],
+            ['get', 'color'],
+            theme.clickableFill,
+          ],
+        }}
       />
-      <Layer id="zones-hoverable-layer" type="fill" paint={zoneLayerStyles.zonesHover} />
+      <Layer
+        id="zones-hoverable-layer"
+        type="fill"
+        paint={{
+          'fill-color': '#FFFFFF',
+          'fill-opacity': [
+            'case',
+            ['boolean', ['feature-state', 'hover'], false],
+            0.3,
+            0,
+          ],
+        }}
+      />
       <Layer
         id="zones-selectable-layer"
         type="fill"
-        paint={zoneLayerStyles.zonesSelected}
+        paint={{
+          'fill-color': [
+            'coalesce',
+            ['feature-state', 'color'],
+            ['get', 'color'],
+            theme.clickableFill,
+          ],
+          'fill-opacity': [
+            'case',
+            ['boolean', ['feature-state', 'selected'], false],
+            0.7,
+            0,
+          ],
+        }}
       />
-      <Layer id="zones-border" type="line" paint={zoneLayerStyles.zonesBorder} />
+      <Layer
+        id="zones-border"
+        type="line"
+        paint={{
+          'line-color': [
+            'case',
+            ['boolean', ['feature-state', 'selected'], false],
+            'white',
+            theme.strokeColor,
+          ],
+          'line-width': [
+            'case',
+            ['boolean', ['feature-state', 'selected'], false],
+            theme.strokeWidth * 12,
+            ['boolean', ['feature-state', 'hover'], false],
+            theme.strokeWidth * 10,
+            theme.strokeWidth,
+          ],
+        }}
+      />
     </Source>
   );
 }
