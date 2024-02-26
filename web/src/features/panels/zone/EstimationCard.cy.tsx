4ca67516aaa94ac0eca6ba15a138abb3a399e4ed
Silke Bonnen <silke.bonnen@gmail.com>
2024-02-26 15:03:33 +0100

    merge test fix

--- web/src/features/panels/zone/EstimationCard.cy.tsx
+++ web/src/features/panels/zone/EstimationCard.cy.tsx
@@ -1,4 +1,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { I18nextProvider } from 'react-i18next';
+import i18n from 'translation/i18n';
 
 import EstimationCard from './EstimationCard';
 
@@ -7,13 +9,15 @@ const queryClient = new QueryClient();
 describe('EstimationCard', () => {
   beforeEach(() => {
     cy.mount(
-      <QueryClientProvider client={queryClient}>
-        <EstimationCard
-          cardType="estimated"
-          estimationMethod="ESTIMATED_CONSTRUCT_BREAKDOWN"
-          outageMessage={undefined}
-        />
-      </QueryClientProvider>
+      <I18nextProvider i18n={i18n}>
+        <QueryClientProvider client={queryClient}>
+          <EstimationCard
+            cardType="estimated"
+            estimationMethod="ESTIMATED_CONSTRUCT_BREAKDOWN"
+            outageMessage={undefined}
+          />
+        </QueryClientProvider>
+      </I18nextProvider>
     );
   });
 
--- web/src/features/panels/zone/FeedbackCard.cy.tsx
+++ web/src/features/panels/zone/FeedbackCard.cy.tsx
@@ -1,8 +1,15 @@
+import { I18nextProvider } from 'react-i18next';
+import i18n from 'translation/i18n';
+
 import FeedbackCard from './FeedbackCard';
 
 describe('FeedbackCard', () => {
   beforeEach(() => {
-    cy.mount(<FeedbackCard />);
+    cy.mount(
+      <I18nextProvider i18n={i18n}>
+        <FeedbackCard />
+      </I18nextProvider>
+    );
   });
 
   it('renders correctly', () => {
