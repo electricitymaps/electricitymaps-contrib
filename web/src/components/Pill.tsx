0927c6254596f7c52e112c85e97d5738f70ee500
Silke Bonnen <silke.bonnen@gmail.com>
2024-02-26 13:12:43 +0100

    remove unnecessary code in Pill

--- web/src/components/Pill.tsx
+++ web/src/components/Pill.tsx
@@ -6,19 +6,13 @@ export default function Pill({
 }: {
   classes?: string;
   text: string;
-  onClick?: (identifier: string) => void;
+  onClick?: () => void;
   identifier?: string;
 }): JSX.Element {
-  const handleClick = () => {
-    if (onClick) {
-      onClick(identifier);
-    }
-  };
-
   return (
     <button
       className={`flex h-9 w-full flex-row items-center justify-center rounded-full border border-black text-black hover:bg-neutral-200 disabled:border-neutral-200 disabled:text-neutral-200 dark:border-white dark:text-white dark:hover:bg-gray-700 disabled:dark:border-gray-700 disabled:dark:text-gray-700 ${classes}`}
-      onClick={handleClick}
+      onClick={onClick}
       data-test-id="pill"
     >
       <div className={`text-sm font-semibold`}>{text}</div>
