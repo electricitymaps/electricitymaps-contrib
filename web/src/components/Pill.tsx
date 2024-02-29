da87160851bcf5c4f2f64afdd0576ef2945f5599
Silke Bonnen <95029552+silkeholmebonnen@users.noreply.github.com>
2024-02-29 09:57:50 +0100

    Add estimation feedback card (#6464)
    * WIP
    
    * WIP
    
    * WIP
    
    * done with the basecard
    
    * only show once
    
    * Add some tests
    
    * add some test
    
    * add more test for feedback card
    
    * prettier
    
    * github suggestions
    
    * add zapier webhook
    
    * delete webhook
    
    * typo fix
    
    * add webhook and change font
    
    * add reference to webhook
    
    * merge fix
    
    * use switch instead of ifelse
    
    * fix based on tony's comments
    
    * update test to match new text
    
    * changes based on viktors comments
    
    * Wraps feedback card in LaunchDarkly
    
    * Makes component test work with feature-flag hook
    
    * feedback card is shown when expanding the estimation card
    
    * change state to feedbackState and use enum
    
    * pill changes
    
    * remove unnecessary code in Pill
    
    * change to use atoms
    
    * change: string array instead of number array
    
    * Update web/src/features/panels/zone/FeedbackCard.tsx
    
    Co-authored-by: Tony <tony.vanswet@electricitymaps.com>
    
    * merge fix
    
    * merge test fix
    
    ---------
    
    Co-authored-by: madsnedergaard <nedergaardmads@gmail.com>
    Co-authored-by: Tony <tony.vanswet@electricitymaps.com>

---
+++ web/src/components/Pill.tsx
@@ -0,0 +1,19 @@
+export default function Pill({
+  classes,
+  text,
+  onClick,
+}: {
+  classes?: string;
+  text: string;
+  onClick?: () => void;
+}): JSX.Element {
+  return (
+    <button
+      className={`flex h-9 w-full flex-row items-center justify-center rounded-full border border-black text-black hover:bg-neutral-200 disabled:border-neutral-200 disabled:text-neutral-200 dark:border-white dark:text-white dark:hover:bg-gray-700 disabled:dark:border-gray-700 disabled:dark:text-gray-700 ${classes}`}
+      onClick={onClick}
+      data-test-id="pill"
+    >
+      <div className={`text-sm font-semibold`}>{text}</div>
+    </button>
+  );
+}
