5eaab016e8d0f5dfb3bec193b2c2ba3f74274336
Silke Bonnen <silke.bonnen@gmail.com>
2024-03-11 13:13:33 +0100

    svg same size

--- web/src/features/charts/bar-breakdown/constants.ts
+++ web/src/features/charts/bar-breakdown/constants.ts
@@ -9,18 +9,3 @@ export const RECT_OPACITY = 0.8;
 export const X_AXIS_HEIGHT = 15;
 export const DEFAULT_FLAG_SIZE = 16;
 export const SCALE_TICKS = 4;
-
-export const iconHeight: { [mode in ElectricityModeType]: string } = {
-  solar: '10',
-  wind: '8',
-  hydro: '7',
-  'hydro storage': '8',
-  'battery storage': '6',
-  biomass: '9',
-  geothermal: '8',
-  nuclear: '8',
-  gas: '9',
-  coal: '9',
-  oil: '9',
-  unknown: '8',
-};
