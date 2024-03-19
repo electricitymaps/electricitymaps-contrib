5fc780e0f194ab88a7e436a9f8dc3f0d0940f95e
Silke Bonnen <silke.bonnen@gmail.com>
2024-03-19 18:04:24 +0100

    remove links above each graph

--- web/src/features/charts/ChartTitle.tsx
+++ web/src/features/charts/ChartTitle.tsx
@@ -22,24 +22,9 @@ export function ChartTitle({
   Use local for timeAverage if exists, otherwise use local default if exists. If no translation exists, use english
   */
   return (
-    <>
-      <div className="flex flex-row justify-between pb-0.5 pt-4">
+    <div className="flex flex-row justify-between pb-0.5 pt-4">
         <h3 className="text-md font-bold">{t(`${translationKey}.${timeAverage}`)}</h3>
         {badgeText != undefined && <EstimationBadge text={badgeText} />}
       </div>
-      {hasLink && (
-        <div className="flex flex-row items-center pb-2 text-center text-[0.7rem]">
-          <HiOutlineArrowDownTray className="min-w-[12px]" size={12} />
-          <a
-            href="https://electricitymaps.com/?utm_source=app.electricitymaps.com&utm_medium=referral&utm_campaign=country_panel"
-            target="_blank"
-            rel="noreferrer"
-            className="pl-0.5 text-left text-[#4178ac] no-underline hover:underline dark:invert"
-          >
-            {t('country-history.Getdata')}
-          </a>
-        </div>
-      )}
-    </>
   );
 }
