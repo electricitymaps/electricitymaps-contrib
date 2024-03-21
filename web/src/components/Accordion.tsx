4f7dd117c0d87443b04a458d8f52ab544fa258e5
Silke Bonnen <silke.bonnen@gmail.com>
2024-03-21 11:56:02 +0100

    WIP

---
+++ web/src/components/Accordion.tsx
@@ -0,0 +1,60 @@
+import { title } from 'node:process';
+
+import { useState } from 'react';
+import { HiChevronDown, HiChevronUp } from 'react-icons/hi2';
+
+export default function Accordion({
+  isCollapsedDefault = true,
+  onClick,
+  badge,
+  className,
+  icon,
+  children,
+}: {
+  isCollapsedDefault?: boolean;
+  onClick?: () => void;
+  badge?: React.ReactNode;
+  className?: string;
+  icon?: React.ReactNode;
+  children?: React.ReactNode;
+}) {
+  const [isCollapsed, setIsCollapsed] = useState(isCollapsedDefault);
+
+  const handleToggleCollapse = () => {
+    if (onClick != undefined) {onClick;}
+    setIsCollapsed((previous) => !previous);
+  };
+
+  return (
+    <div className="flex flex-col">
+      <button data-test-id="collapse-button" onClick={handleToggleCollapse}>
+        <div className={`flex flex-row items-center justify-between ${className}`}>
+          <div className="flex w-2/3 flex-initial flex-row gap-2">
+            <div className={`flex items-center justify-center`}>{icon}</div>
+            <h2
+              className={`self-center text-left text-sm font-semibold`}
+              data-test-id="title"
+            >
+              {title}
+            </h2>
+          </div>
+          <div className="flex h-fit flex-row gap-2 text-nowrap">
+            {badge}
+            <div className="text-lg">
+              {isCollapsed ? (
+                <div data-test-id="collapse-down">
+                  <HiChevronDown />
+                </div>
+              ) : (
+                <div data-test-id="collapse-up">
+                  <HiChevronUp />
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </button>
+      {!isCollapsed && children}
+    </div>
+  );
+}
--- web/src/features/panels/zone/EstimationCard.tsx
+++ web/src/features/panels/zone/EstimationCard.tsx
@@ -1,3 +1,4 @@
+import Accordion from 'components/Accordion';
 import Badge from 'components/Badge';
 import { useFeatureFlag } from 'features/feature-flags/api';
 import { useAtom } from 'jotai';
@@ -137,38 +138,15 @@ function BaseCard({
           : 'bg-neutral-100 dark:bg-gray-800'
       } mb-4 gap-2 border border-neutral-200 transition-all dark:border-gray-700`}
     >
-      <div className="flex flex-col">
-        <button data-test-id="collapse-button" onClick={handleToggleCollapse}>
-          <div className="flex flex-row items-center justify-between">
-            <div className="flex w-2/3 flex-initial flex-row gap-2">
-              <div className={`flex items-center justify-center`}>
-                <div className={`h-[16px] w-[16px] bg-center ${icon}`} />
-              </div>
-              <h2
-                className={`text-left text-sm font-semibold ${textColorTitle} self-center`}
-                data-test-id="title"
-              >
-                {title}
-              </h2>
-            </div>
-            <div className="flex h-fit flex-row gap-2 text-nowrap">
-              {showBadge && (
-                <Badge type={pillType} icon={iconPill} pillText={pillText}></Badge>
-              )}
-              <div className="text-lg">
-                {isCollapsed ? (
-                  <div data-test-id="collapse-down">
-                    <HiChevronDown />
-                  </div>
-                ) : (
-                  <div data-test-id="collapse-up">
-                    <HiChevronUp />
-                  </div>
-                )}
-              </div>
-            </div>
-          </div>
-        </button>
+      <Accordion
+        onClick={() => setFeedbackCardCollapsedNumber(feedbackCardCollapsedNumber + 1)}
+        isCollapsedDefault={estimationMethod == 'outage' ? false : true}
+        badge={
+          showBadge && <Badge type={pillType} icon={iconPill} pillText={pillText}></Badge>
+        }
+        className={textColorTitle}
+        icon={<div className={`h-[16px] w-[16px] bg-center ${icon}`} />}
+      >
         {!isCollapsed && (
           <div className="gap-2 pt-1.5">
             <div
@@ -195,7 +173,7 @@ function BaseCard({
             )}
           </div>
         )}
-      </div>
+      </Accordion>
     </div>
   );
 }
