111feb27100f22c902f90b1fd3818194297aca9f
madsnedergaard <nedergaardmads@gmail.com>
2024-04-02 23:33:35 +0200

    Styles flags similar to left panel

--- web/src/components/ZoneName.tsx
+++ web/src/components/ZoneName.tsx
@@ -6,7 +6,11 @@ import { CountryFlag } from './Flag';
 export function ZoneName({ zone, textStyle }: { zone: string; textStyle?: string }) {
   return (
     <div className="flex items-center">
-      <CountryFlag zoneId={zone} size={20} className="rounded-sm" />
+      <CountryFlag
+        zoneId={zone}
+        size={18}
+        className="rounded-sm shadow-[0_0px_3px_rgba(0,0,0,0.2)]"
+      />
       <p className={twMerge('truncate pl-1 text-base', textStyle)}>
         {getShortenedZoneNameWithCountry(zone)}
       </p>
