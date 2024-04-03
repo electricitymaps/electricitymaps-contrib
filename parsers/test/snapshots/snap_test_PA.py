50c4619b5af02df6d4cc171ee2ee4c2d02f0c8e9
amv213 <alvise@vianello.ai>
2024-04-03 08:53:04 +0100

    feat(pa): use json backend APIs

--- parsers/test/snapshots/snap_test_PA.py
+++ parsers/test/snapshots/snap_test_PA.py
@@ -4,7 +4,27 @@ from snapshottest import Snapshot
 
 snapshots = Snapshot()
 
-snapshots["TestPA::test_fetch_production 1"] = [
+snapshots["TestPA::test_fetch_consumption 1"] = [
+    {
+        "consumption": 1314.0,
+        "datetime": "2024-04-02T15:30:29-05:00",
+        "source": "sitr.cnd.com.pa",
+        "sourceType": "measured",
+        "zoneKey": "PA",
+    }
+]
+
+snapshots["TestPA::test_fetch_exchange_live 1"] = [
+    {
+        "datetime": "2024-04-02T15:31:45-05:00",
+        "netFlow": -78.4,
+        "sortedZoneKeys": "CR->PA",
+        "source": "sitr.cnd.com.pa",
+        "sourceType": "measured",
+    }
+]
+
+snapshots["TestPA::test_fetch_production_live 1"] = [
     {
         "correctedModes": [],
         "datetime": "2021-12-30T09:58:37-05:00",
@@ -15,8 +35,27 @@ snapshots["TestPA::test_fetch_production 1"] = [
             "hydro": 421.84,
             "oil": 238.2,
             "solar": 262.76,
-            "unknown": 0.0,
             "wind": 115.4,
+        },
+        "source": "sitr.cnd.com.pa",
+        "sourceType": "measured",
+        "storage": {},
+        "zoneKey": "PA",
+    }
+]
+
+snapshots["TestPA::test_fetch_production_live 2"] = [
+    {
+        "correctedModes": [],
+        "datetime": "2024-04-03T02:43:25-05:00",
+        "production": {
+            "biomass": 21.49,
+            "coal": 0.0,
+            "gas": 358.12,
+            "hydro": 554.19,
+            "oil": 278.72,
+            "solar": 0.0,
+            "wind": 17.8,
         },
         "source": "sitr.cnd.com.pa",
         "sourceType": "measured",
