3f26bb55031709eb3e614efcc0f105bdf4b905f0
Silke Bonnen <silke.bonnen@gmail.com>
2024-04-08 10:51:00 +0200

    move source component to its own file

--- web/src/features/charts/bar-breakdown/BarBreakdownChart.tsx
+++ web/src/features/charts/bar-breakdown/BarBreakdownChart.tsx
@@ -16,6 +16,7 @@ import BarBreakdownEmissionsChart from './BarBreakdownEmissionsChart';
 import BarElectricityBreakdownChart from './BarElectricityBreakdownChart';
 import BySource from './elements/BySource';
 import EmptyBarBreakdownChart from './EmptyBarBreakdownChart';
+import { Source } from './Source';
 
 const X_PADDING = 9;
 
@@ -217,30 +218,4 @@ function GetSourceArrayFromDictionary(sourceDict: {
     }
   }
   return sourcesWithoutDuplicates;
-}
-
-function Source({
-  title,
-  icon,
-  sources,
-}: {
-  title: string;
-  icon: React.ReactNode;
-  sources: string[];
-}) {
-  return (
-    <div className="flex flex-col py-2">
-      <div className="flex flex-row pb-2">
-        <div className="mr-1">{icon}</div>
-        <div className="text-md font-semibold">{title}</div>
-      </div>
-      <div className="flex flex-col gap-2 pl-5">
-        {sources.map((source, index) => (
-          <div key={index} className="text-xm">
-            {source}
-          </div>
-        ))}
-      </div>
-    </div>
-  );
 }
