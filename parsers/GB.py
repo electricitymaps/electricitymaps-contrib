f3f8ad8e85218239a04193185e614f53ad826415
amv213 <alvise@vianello.ai>
2024-04-09 11:56:25 +0200

    refactor(gb): upgrade parser to use event classes, replace arrow with datetime, add tests

--- parsers/GB.py
+++ parsers/GB.py
@@ -1,71 +1,99 @@
-#!/usr/bin/env python3
-
-"""
-Parser that uses the RTE-FRANCE API to return the following data type(s)
-fetch_price method copied from FR parser.
-Day-ahead Price
-"""
+"""Parser that uses the RTE-FRANCE API"""
 
 import xml.etree.ElementTree as ET
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from logging import Logger, getLogger
+from zoneinfo import ZoneInfo
 
-import arrow
 from requests import Session
 
+from electricitymap.contrib.lib.models.event_lists import PriceList
+from electricitymap.contrib.lib.models.events import EventSourceType
+from electricitymap.contrib.lib.types import ZoneKey
 from parsers.lib.config import refetch_frequency
+from parsers.lib.exceptions import ParserException
+
+PARSER = "GB.py"
+TIMEZONE = ZoneInfo("Europe/London")
+ZONE_KEY = ZoneKey("GB")
 
 
 @refetch_frequency(timedelta(days=1))
 def fetch_price(
-    zone_key: str,
+    zone_key: ZoneKey = ZONE_KEY,
     session: Session | None = None,
     target_datetime: datetime | None = None,
     logger: Logger = getLogger(__name__),
-) -> list:
-    if target_datetime:
-        now = arrow.get(target_datetime, tz="Europe/Paris")
-    else:
-        now = arrow.now(tz="Europe/London")
+) -> list[dict]:
+    """Returns one-hourly prices for the requested day."""
 
-    r = session or Session()
-    formatted_from = now.shift(days=-1).format("DD/MM/YYYY")
-    formatted_to = now.format("DD/MM/YYYY")
+    now = datetime.now(timezone.utc)
+    target_datetime = (
+        now if target_datetime is None else target_datetime.astimezone(timezone.utc)
+    )
 
-    url = f"http://eco2mix.rte-france.com/curves/getDonneesMarche?dateDeb={formatted_from}&dateFin={formatted_to}&mode=NORM"
+    # API works in UTC timestamps
+    day = target_datetime.strftime("%d/%m/%Y")
+    url = f"http://eco2mix.rte-france.com/curves/getDonneesMarche?dateDeb={day}&dateFin={day}&mode=NORM"
 
-    response = r.get(url)
-    obj = ET.fromstring(response.content)
-    datas = {}
-
-    for donnesMarche in obj:
-        if donnesMarche.tag != "donneesMarche":
-            continue
-
-        start_date = arrow.get(
-            arrow.get(donnesMarche.attrib["date"]).datetime, "Europe/Paris"
+    session = session or Session()
+    response = session.get(url)
+    if not response.ok:
+        raise ParserException(
+            PARSER,
+            f"Exception when fetching price error code: {response.status_code}: {response.text}",
+            zone_key,
         )
 
-        for item in donnesMarche:
-            if item.get("granularite") != "Global":
-                continue
-            country_c = item.get("perimetre")
-            if zone_key != country_c:
-                continue
-            value = None
-            for value in item:
-                if value.text == "ND":
-                    continue
-                period = int(value.attrib["periode"])
-                datetime = start_date.shift(hours=+period).datetime
-                if datetime not in datas:
-                    datas[datetime] = {
-                        "zoneKey": zone_key,
-                        "currency": "EUR",
-                        "datetime": datetime,
-                        "source": "rte-france.com",
-                    }
-                data = datas[datetime]
-                data["price"] = float(value.text)
+    xml_tree = ET.fromstring(response.content)
 
-    return list(datas.values())
+    price_list = PriceList(logger=logger)
+    for daily_market_data in xml_tree.iterfind("donneesMarche"):
+        day = datetime.strptime(daily_market_data.get("date"), "%Y-%m-%d").replace(
+            tzinfo=timezone.utc
+        )
+        for daily_zone_data in daily_market_data:
+            zone_code = daily_zone_data.get("perimetre")
+
+            # Data for Germany / Luxembourg is not set / reported as aggregate region
+            if zone_code in {"DE", "DL"}:
+                continue
+
+            if zone_key != zone_code:
+                continue
+
+            granularity = daily_zone_data.get("granularite")
+            if granularity != "Global":
+                continue
+
+            for value in daily_zone_data:
+                price = None if value.text == "ND" else float(value.text)
+                if price is None:
+                    continue
+
+                period_number = int(value.attrib["periode"])
+                dt = day + timedelta(hours=period_number)
+                is_future = dt > now
+
+                price_list.append(
+                    zoneKey=zone_key,
+                    datetime=dt,
+                    source="rte-france.com",
+                    price=price,
+                    currency="EUR",
+                    sourceType=EventSourceType.forecasted
+                    if is_future
+                    else EventSourceType.measured,
+                )
+
+    return price_list.to_list()
+
+
+if __name__ == "__main__":
+    for zone_key in ["BE", "CH", "AT", "ES", "FR", "GB", "IT", "NL", "PT"]:
+        print(f"fetch_price({zone_key}) ->")
+        print(fetch_price(ZoneKey(zone_key)))
+
+    historical_datetime = datetime(2022, 7, 16, 12, tzinfo=timezone.utc)
+    print(f"fetch_price(target_datetime={historical_datetime.isoformat()}) ->")
+    print(fetch_price(target_datetime=historical_datetime))
