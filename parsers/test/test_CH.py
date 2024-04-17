032ccc07865b425ecda2b4aed32e453aa7dba04d
amv213 <alvise@vianello.ai>
2024-04-17 11:37:13 +0100

    feat(ch): add 2023 and 2024 solar capacities, update historical ones

--- parsers/test/test_CH.py
+++ parsers/test/test_CH.py
@@ -1,20 +1,24 @@
-import unittest
+from datetime import datetime
 
-import arrow
+import pytest
 
 from parsers.CH import get_solar_capacity_at
 
 
-class TestCH(unittest.TestCase):
-    def test_get_solar_capacity(self):
-        assert get_solar_capacity_at(arrow.get("2018-01-02").datetime) == 2090
-        assert (
-            get_solar_capacity_at(arrow.get("2018-01-01 00:00:00+00:00").datetime)
-            == 2090
-        )
-        assert get_solar_capacity_at(arrow.get("2014-02-01").datetime) == 1393
-        assert get_solar_capacity_at(arrow.get("2022-01-01").datetime) == 3904
-
-
-if __name__ == "__main__":
-    unittest.main()
+@pytest.mark.parametrize(
+    "dt,expected",
+    [
+        ("2018-01-02", 2107.374),
+        ("2014-02-01", 995.055),
+        ("2022-01-01", 4080.415),
+        ("2023-01-01", 5094.375),
+        ("2024-01-01", 5108.034),
+        # make sure past date for which we do not have historical capacity returns earliest available data
+        ("1999-02-01", 6.213),
+        # make sure future date for which we do not have historical capacity returns latest available data
+        ("2024-02-01", 5108.034),
+    ],
+)
+def test_get_solar_capacity(dt, expected):
+    target_datetime = datetime.fromisoformat(dt)
+    assert get_solar_capacity_at(target_datetime) == expected
