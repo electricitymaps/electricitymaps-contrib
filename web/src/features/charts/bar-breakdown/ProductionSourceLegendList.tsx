26ab07db25a906fd71ed8f24e1bcbb44a6694b14
Silke Bonnen <95029552+silkeholmebonnen@users.noreply.github.com>
2024-04-22 09:59:22 +0200

    Add data source accordions to graphs (#6663)
    * add accordion to breakdownchart
    
    * add accordion to emission and carbon chart
    
    * use

--- web/src/features/charts/bar-breakdown/ProductionSourceLegendList.tsx
+++ web/src/features/charts/bar-breakdown/ProductionSourceLegendList.tsx
@@ -1,14 +1,17 @@
+import { twMerge } from 'tailwind-merge';
 import { ElectricityModeType } from 'types';
 
 import ProductionSourceLegend from './ProductionSourceLegend';
 
 export default function ProductionSourceLegendList({
   sources,
+  className,
 }: {
   sources: ElectricityModeType[];
+  className?: string;
 }) {
   return (
-    <div className="flex flex-row gap-1.5 py-1">
+    <div className={twMerge('flex flex-row gap-1.5 py-1', className)}>
       {sources.map((source, index) => (
         <ProductionSourceLegend key={index} electricityType={source} />
       ))}
