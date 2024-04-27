340c8d1cb3a05841b0c63450dda13394066c9d4a
Viktor Andersson <30777521+VIKTORVAV99@users.noreply.github.com>
2024-04-27 10:21:00 +0200

    remove unused import

--- web/src/features/feature-flags/api.ts
+++ web/src/features/feature-flags/api.ts
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
-import { getBasePath, getHeaders, QUERY_KEYS } from 'api/helpers';
+import { getBasePath, QUERY_KEYS } from 'api/helpers';
 
 import { FeatureFlags } from './types';
 
