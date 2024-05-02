739c7bcf20df45d65362adb5cebd615654897bf2
tonypls <tonyvanswet@gmail.com>
2024-05-02 14:46:33 +0200

    feat(web): update conditional rendering

--- web/src/components/MapCardContainer.tsx
+++ web/src/components/MapCardContainer.tsx
@@ -1,3 +1,4 @@
+import { useFeatureFlag } from 'features/feature-flags/api';
 import FeatureFlagsManager from 'features/feature-flags/FeatureFlagsManager';
 import { Suspense } from 'react';
 
@@ -5,6 +6,8 @@ import SurveyCard from './app-survey/SurveyCard';
 import LegendContainer from './legend/LegendContainer';
 
 export default function MapCardContainer() {
+  const surveyEnabled = useFeatureFlag('feedback-micro-survey');
+
   return (
     <div className="fixed bottom-4 right-4 z-20 flex-col space-y-3">
       <Suspense>
@@ -13,9 +16,11 @@ export default function MapCardContainer() {
       <Suspense>
         <LegendContainer />
       </Suspense>
-      <Suspense>
-        <SurveyCard />
-      </Suspense>
+      {surveyEnabled && (
+        <Suspense>
+          <SurveyCard />
+        </Suspense>
+      )}
     </div>
   );
 }
--- web/src/components/app-survey/SurveyCard.tsx
+++ web/src/components/app-survey/SurveyCard.tsx
@@ -1,10 +1,6 @@
 import { useFeatureFlag } from 'features/feature-flags/api';
 
 export default function SurveyCard() {
-  const surveyEnabled = useFeatureFlag('feedback-micro-survey');
-  if (!surveyEnabled) {
-    return null;
-  }
   return (
     <div className="invisible  z-20 flex w-[224px] flex-col rounded bg-white/90 px-1 py-2 shadow-xl backdrop-blur-sm sm:visible dark:bg-gray-800">
       this is a survey card
