eef0594807954f167e96e8d57a28441061332252
tonypls <tonyvanswet@gmail.com>
2024-05-13 09:39:50 +0200

    feat(web): draft state

--- web/src/components/MapCardContainer.tsx
+++ web/src/components/MapCardContainer.tsx
@@ -9,7 +9,7 @@ export default function MapCardContainer() {
   const surveyEnabled = useFeatureFlag('feedback-micro-survey');
 
   return (
-    <div className="fixed bottom-4 right-4 z-20 flex-col space-y-3">
+    <div className="fixed bottom-4 right-4 z-20 flex flex-col items-end space-y-3">
       <Suspense>
         <FeatureFlagsManager />
       </Suspense>
--- web/src/components/app-survey/SurveyCard.tsx
+++ web/src/components/app-survey/SurveyCard.tsx
@@ -1,9 +1,9 @@
-import { useFeatureFlag } from 'features/feature-flags/api';
+import FeedbackCard from 'features/panels/zone/FeedbackCard';
 
 export default function SurveyCard() {
   return (
-    <div className="invisible  z-20 flex w-[224px] flex-col rounded bg-white/90 px-1 py-2 shadow-xl backdrop-blur-sm sm:visible dark:bg-gray-800">
-      this is a survey card
+    <div className="invisible  z-20  sm:visible ">
+      <FeedbackCard estimationMethod="no" />
     </div>
   );
 }
