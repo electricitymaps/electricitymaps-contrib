ff95f67b554251ab73eaa93bb7b85f923fbca7cd
silkeholmebonnen <silke.bonnen@gmail.com>
2024-05-13 13:30:04 +0200

    applied methodology card into a component

--- web/src/features/panels/zone/ZoneDetails.tsx
+++ web/src/features/panels/zone/ZoneDetails.tsx
@@ -25,6 +25,7 @@ import AreaGraphContainer from './AreaGraphContainer';
 import Attribution from './Attribution';
 import DisplayByEmissionToggle from './DisplayByEmissionToggle';
 import EstimationCard from './EstimationCard';
+import MethodologyCard from './MethodologyCard';
 import NoInformationMessage from './NoInformationMessage';
 import { getHasSubZones, getZoneDataStatus, ZoneDataStatus } from './util';
 import { ZoneHeaderGauges } from './ZoneHeaderGauges';
@@ -116,41 +117,7 @@ export default function ZoneDetails(): JSX.Element {
               displayByEmissions={displayByEmissions}
             />
           )}
-          <RoundedCard>
-            <Accordion
-              icon={<EmapsIcon />}
-              title={t('left-panel.applied-methodologies.title')}
-              className="pb-1 pt-3 text-md"
-              onOpen={() => trackEvent('AppliedMethodologies Expanded')}
-            >
-              <div className="flex flex-col gap-2 pb-1 text-emerald-800 underline underline-offset-4 dark:text-emerald-500">
-                <a
-                  href="https://www.electricitymaps.com/methodology#missing-data"
-                  target="_blank"
-                  rel="noreferrer"
-                  className={`text-sm font-semibold `}
-                >
-                  <span>{t('left-panel.applied-methodologies.estimations')}</span>
-                </a>
-                <a
-                  href="https://www.electricitymaps.com/methodology#data-collection-and-processing"
-                  target="_blank"
-                  rel="noreferrer"
-                  className={`text-sm font-semibold `}
-                >
-                  <span>{t('left-panel.applied-methodologies.flowtracing')}</span>
-                </a>
-                <a
-                  href="https://www.electricitymaps.com/methodology#carbon-intensity-and-emission-factors"
-                  target="_blank"
-                  rel="noreferrer"
-                  className={`text-sm font-semibold `}
-                >
-                  <span>{t('left-panel.applied-methodologies.carbonintensity')}</span>
-                </a>
-              </div>
-            </Accordion>
-          </RoundedCard>
+          <MethodologyCard />
           <Attribution zoneId={zoneId} />
           {isMobile ? (
             <Button
