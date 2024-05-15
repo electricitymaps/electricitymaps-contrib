c47e5a7a2b54ac3df8afdb54250779dab2e73bb2
silkeholmebonnen <silke.bonnen@gmail.com>
2024-05-15 15:03:32 +0200

    delete duplicate

--- web/src/features/charts/DisabledMessage.tsx
+++ web/src/features/charts/DisabledMessage.tsx
@@ -1,6 +1,6 @@
 export function DisabledMessage({ message }: { message: JSX.Element | string }) {
   return (
-    <div className="absolute left-1/2 top-1/2 z-10 -translate-x-1/2 -translate-y-1/2 rounded-lg border-neutral-200 bg-neutral-100 p-2 text-center text-center text-sm shadow-lg dark:bg-gray-800">
+    <div className="absolute left-1/2 top-1/2 z-10 -translate-x-1/2 -translate-y-1/2 rounded-lg border-neutral-200 bg-neutral-100 p-2 text-center text-sm shadow-lg dark:bg-gray-800">
       {message}
     </div>
   );
