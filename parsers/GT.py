da16ae32946f7ab810415eca2e907958da86ae13
madsnedergaard <nedergaardmads@gmail.com>
2024-05-29 16:33:58 +0200

    Removes "www" from source

--- parsers/GT.py
+++ parsers/GT.py
@@ -21,7 +21,7 @@ PARSER = "GT.py"
 DEFAULT_ZONE_KEY = ZoneKey("GT")
 TIMEZONE = ZoneInfo("America/Guatemala")
 
-SOURCE = "www.amm.org.gt"  # Source is slightly different than API URL
+SOURCE = "amm.org.gt"  # Source is slightly different than API URL
 URL = "https://wl12.amm.org.gt/GraficaPW/graficaCombustible"
 
 PRODUCTION_TYPE_TO_PRODUCTION_MODE = {
