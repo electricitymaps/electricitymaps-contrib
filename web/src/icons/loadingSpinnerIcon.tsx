546686caaf7fd582f8017e2c46a2059f3ad057bf
Viktor Andersson <30777521+VIKTORVAV99@users.noreply.github.com>
2024-06-10 16:17:32 +0200

    fix size and spin direction

--- web/src/icons/loadingSpinnerIcon.tsx
+++ web/src/icons/loadingSpinnerIcon.tsx
@@ -15,24 +15,28 @@ function AnimatedCircle({ cx, cy, begin }: { cx: string; cy: string; begin: stri
   );
 }
 
-export const LoadingSpinnerIcon = memo(function LoadingSpinnerIcon() {
+export const LoadingSpinnerIcon = memo(function LoadingSpinnerIcon({
+  size = 100,
+}: {
+  size?: number;
+}) {
   return (
     <div className="text-black dark:text-white">
       <svg
-        width="512"
-        height="512"
+        width={size}
+        height={size}
         viewBox="0 0 512 512"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
       >
-        <AnimatedCircle cx="420" cy="420" begin="0s" />
-        <AnimatedCircle cx="256" cy="420" begin="0.1s" />
-        <AnimatedCircle cx="92" cy="420" begin="0.2s" />
-        <AnimatedCircle cx="420" cy="256" begin="0.3s" />
-        <AnimatedCircle cx="92" cy="256" begin="0.4s" />
-        <AnimatedCircle cx="420" cy="92" begin="0.5s" />
-        <AnimatedCircle cx="256" cy="92" begin="0.6s" />
-        <AnimatedCircle cx="92" cy="92" begin="0.7s" />
+        <AnimatedCircle cx="420" cy="92" begin="0s" />
+        <AnimatedCircle cx="420" cy="256" begin="0.1s" />
+        <AnimatedCircle cx="420" cy="420" begin="0.2s" />
+        <AnimatedCircle cx="256" cy="420" begin="0.3s" />
+        <AnimatedCircle cx="92" cy="420" begin="0.4s" />
+        <AnimatedCircle cx="92" cy="256" begin="0.5s" />
+        <AnimatedCircle cx="92" cy="92" begin="0.6s" />
+        <AnimatedCircle cx="256" cy="92" begin="0.7s" />
         <path
           d="M344.736 227.405H267.066V159.846C267.066 152.384 257.933 149.152 253.53 155.059L161.28 272C157.44 277.12 160.979 284.614 167.258 284.614H244.934V352.154C244.934 359.616 254.067 362.848 258.47 356.941L350.72 240C354.56 234.88 351.014 227.405 344.736 227.405Z"
           fill="currentColor"
