5dc501b065319373694d677e1f319efa337e5f27
silkeholmebonnen <silke.bonnen@gmail.com>
2024-06-12 14:04:37 +0200

    fix: remove the noreferrer rel

--- web/src/components/Link.tsx
+++ web/src/components/Link.tsx
@@ -1,9 +1,17 @@
-export function Link({ href, children }: { href: string; children: React.ReactNode }) {
+export function Link({
+  href,
+  rel,
+  children,
+}: {
+  href: string;
+  rel?: string;
+  children: React.ReactNode;
+}) {
   return (
     <a
       href={href}
       target="_blank"
-      rel="noopener noreferrer"
+      rel={`noopener ${rel}`}
       className="text-sm font-semibold text-emerald-800 underline underline-offset-2 dark:text-emerald-500"
     >
       {children}
