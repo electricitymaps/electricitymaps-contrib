5d887df5b4fa094d4ad8fd93049303d2c0838fc3
Mads Nedergaard <nedergaardmads@gmail.com>
2024-06-13 11:18:31 +0200

    Removes rel property

--- web/src/components/Link.tsx
+++ web/src/components/Link.tsx
@@ -1,17 +1,15 @@
 export function Link({
   href,
-  rel,
   children,
 }: {
   href: string;
-  rel?: string;
   children: React.ReactNode;
 }) {
   return (
     <a
       href={href}
       target="_blank"
-      rel={`noopener ${rel}`}
+      rel="noopener"
       className="text-sm font-semibold text-emerald-800 underline underline-offset-2 dark:text-emerald-500"
     >
       {children}
