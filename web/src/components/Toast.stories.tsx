ecaa925e316fc8b5bd8ce9191c20d5d30c578a28
Viktor Andersson <30777521+VIKTORVAV99@users.noreply.github.com>
2024-06-13 15:57:21 +0200

    update story

--- web/src/components/Toast.stories.tsx
+++ web/src/components/Toast.stories.tsx
@@ -4,8 +4,41 @@ import { useEffect, useState } from 'react';
 
 import Toast from './Toast';
 
+function ToastWrapperComponent() {
+  const [showToast, setShowToast] = useState(true);
+
+  useEffect(() => {
+    let timerId: string | number | NodeJS.Timeout | undefined;
+    if (!showToast) {
+      timerId = setTimeout(() => setShowToast(true), 1000);
+    }
+    return () => clearTimeout(timerId); // Cleanup the timer
+  }, [showToast]);
+
+  return (
+    <ToastProvider>
+      {showToast && (
+        <Toast
+          title="Toast Title"
+          description="This is a toast description"
+          toastAction={() => {
+            console.log('Toast action clicked');
+            setShowToast(false); // Hide toast when action is clicked
+          }}
+          toastActionText="Click me"
+          toastClose={() => {
+            console.log('Toast closed');
+            setShowToast(false); // Hide toast when closed
+          }}
+          duration={Number.POSITIVE_INFINITY}
+        />
+      )}
+    </ToastProvider>
+  );
+}
+
 const meta: Meta<typeof Toast> = {
-  title: 'Modals/Toast',
+  title: 'Modal/Toast',
   component: Toast,
 };
 
@@ -14,38 +47,14 @@ type Story = StoryObj<typeof Toast>;
 
 export const All: Story = {
   render: () => {
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const [showToast, setShowToast] = useState(true);
-
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    useEffect(() => {
-      let timerId: string | number | NodeJS.Timeout | undefined;
-      if (!showToast) {
-        // Set a timer to show the toast after 5 seconds
-        timerId = setTimeout(() => setShowToast(true), 1000);
-      }
-      return () => clearTimeout(timerId); // Cleanup the timer
-    }, [showToast]);
-
     return (
-      <ToastProvider>
-        {showToast && (
-          <Toast
-            title="Toast Title"
-            description="This is a toast description"
-            toastAction={() => {
-              console.log('Toast action clicked');
-              setShowToast(false); // Hide toast when action is clicked
-            }}
-            toastActionText="Click me"
-            toastClose={() => {
-              console.log('Toast closed');
-              setShowToast(false); // Hide toast when closed
-            }}
-            duration={Number.POSITIVE_INFINITY}
-          />
-        )}
-      </ToastProvider>
+      <>
+        <p>
+          The toast will pop back up when clicked on in the story but not in production.
+        </p>
+        <br />
+        <ToastWrapperComponent />
+      </>
     );
   },
 };
