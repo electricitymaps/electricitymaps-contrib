3f8dd18e023a1ea2cf287748bcec7d41524da27b
madsnedergaard <nedergaardmads@gmail.com>
2024-06-17 08:51:07 +0200

    Adds support for skip-onboarding query parameter

--- web/src/features/modals/AnnouncementModal.tsx
+++ web/src/features/modals/AnnouncementModal.tsx
@@ -4,6 +4,7 @@ import { useAtom } from 'jotai';
 import { atomWithStorage } from 'jotai/utils';
 import { useEffect, useState } from 'react';
 import { HiXMark } from 'react-icons/hi2';
+import { useSearchParams } from 'react-router-dom';
 import { hasOnboardingBeenSeenAtom } from 'utils/state/atoms';
 
 const hasAnnouncementBeenSeenAtom = atomWithStorage(
@@ -18,6 +19,8 @@ export default function AnnouncementModal() {
   const [hasOnboardingBeenSeen] = useAtom(hasOnboardingBeenSeenAtom);
   const [hasOnboardingBeenSeenInSession, setHasOnboardingBeenSeenInSession] =
     useState(false);
+  const [searchParameters] = useSearchParams();
+  const skipOnboarding = searchParameters.get('skip-onboarding') === 'true';
   const isExpired = new Date() > new Date('2024-07-15');
 
   // Check if onboarding has been seen in the current session
@@ -38,6 +41,7 @@ export default function AnnouncementModal() {
   if (
     hasAnnouncementBeenSeen ||
     isExpired ||
+    skipOnboarding ||
     !hasOnboardingBeenSeen ||
     hasOnboardingBeenSeenInSession
   ) {
