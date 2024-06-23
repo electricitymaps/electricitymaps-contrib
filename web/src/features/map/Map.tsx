a6663e359e19f951fe6cfbfbd29a742eacff52b7
Viktor Andersson <30777521+VIKTORVAV99@users.noreply.github.com>
2024-06-23 12:24:30 +0200

    perf: use optional chaining everywhere

--- web/src/features/map/Map.tsx
+++ web/src/features/map/Map.tsx
@@ -86,10 +86,9 @@ export default function MapPage({ onMapLoad }: MapPageProps): ReactElement {
     const setSourceLoadedForMap = () => {
       setSourceLoaded(
         Boolean(
-          map &&
-            map.getSource(ZONE_SOURCE) !== undefined &&
-            map.getSource('states') !== undefined &&
-            map.isSourceLoaded('states')
+          map?.getSource(ZONE_SOURCE) !== undefined &&
+            map?.getSource('states') !== undefined &&
+            map?.isSourceLoaded('states')
         )
       );
     };
@@ -255,7 +254,7 @@ export default function MapPage({ onMapLoad }: MapPageProps): ReactElement {
       );
     }
     setHoveredZone(null);
-    if (feature && feature.properties) {
+    if (feature?.properties) {
       const zoneId = feature.properties.zoneId;
       navigate(createToWithState(`/zone/${zoneId}`));
     } else {
