79a192b2cff9987c026ad73ed4c69369e815d6c9
Viktor Andersson <30777521+VIKTORVAV99@users.noreply.github.com>
2024-06-26 11:50:52 +0200

    extract buttons

---
+++ web/src/components/buttons/LinkedinButton.tsx
@@ -0,0 +1,38 @@
+import { Button, ButtonProps } from 'components/Button';
+import { useTranslation } from 'react-i18next';
+import { FaLinkedin } from 'react-icons/fa6';
+import { DEFAULT_ICON_SIZE } from 'utils/constants';
+
+interface LinkedinButtonProps
+  extends Omit<
+    ButtonProps,
+    'icon' | 'children' | 'href' | 'onClick' | 'backgroundClasses' | 'foregroundClasses'
+  > {
+  iconSize?: number;
+  isIconOnly?: boolean;
+  isShareLink?: boolean;
+}
+
+export function LinkedinButton({
+  isIconOnly,
+  iconSize = DEFAULT_ICON_SIZE,
+  isShareLink,
+  ...restProps
+}: LinkedinButtonProps) {
+  const { t } = useTranslation();
+  return (
+    <Button
+      backgroundClasses="bg-[#0A66C2]"
+      foregroundClasses="text-white dark:text-white focus-visible:outline-[#0A66C2]"
+      href={
+        isShareLink
+          ? 'https://www.linkedin.com/shareArticle?mini=true&url=https://app.electricitymaps.com'
+          : 'https://www.linkedin.com/company/electricitymaps/'
+      }
+      icon={<FaLinkedin size={iconSize} />}
+      {...restProps}
+    >
+      {isIconOnly ? undefined : t('button.linkedin')}
+    </Button>
+  );
+}
---
+++ web/src/components/buttons/TwitterButton.tsx
@@ -0,0 +1,38 @@
+import { Button, ButtonProps } from 'components/Button';
+import { useTranslation } from 'react-i18next';
+import { FaXTwitter } from 'react-icons/fa6';
+import { DEFAULT_ICON_SIZE } from 'utils/constants';
+
+interface TwitterButtonProps
+  extends Omit<
+    ButtonProps,
+    'icon' | 'children' | 'href' | 'onClick' | 'backgroundClasses' | 'foregroundClasses'
+  > {
+  iconSize?: number;
+  isIconOnly?: boolean;
+  isShareLink?: boolean;
+}
+
+export function TwitterButton({
+  isIconOnly,
+  iconSize = DEFAULT_ICON_SIZE,
+  isShareLink,
+  ...restProps
+}: TwitterButtonProps) {
+  const { t } = useTranslation();
+  return (
+    <Button
+      backgroundClasses="bg-black"
+      foregroundClasses="text-white dark:text-white focus-visible:outline-black"
+      href={
+        isShareLink
+          ? 'https://twitter.com/intent/tweet?url=https://app.electricitymaps.com'
+          : undefined
+      }
+      icon={<FaXTwitter size={iconSize} />}
+      {...restProps}
+    >
+      {isIconOnly ? undefined : t('button.twitter')}
+    </Button>
+  );
+}
