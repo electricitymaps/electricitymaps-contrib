efe06897cc7378fe088cb5f8255b034c07dd44ed
Viktor Andersson <30777521+VIKTORVAV99@users.noreply.github.com>
2024-06-29 10:09:47 +0200

    small size changes to minimize jitter

--- web/src/features/panels/ranking-panel/ZoneList.tsx
+++ web/src/features/panels/ranking-panel/ZoneList.tsx
@@ -21,7 +21,7 @@ export interface ZoneRowType {
 function ZoneRow({ zoneId, color, ranking, countryName, zoneName }: ZoneRowType) {
   return (
     <InternalLink
-      className="group my-[0.3rem] flex h-11 w-full items-center overflow-hidden rounded bg-gray-100 pl-3 text-left transition hover:bg-gray-200 focus:border focus:border-gray-400/60 focus-visible:outline-none dark:border dark:border-gray-400/10 dark:bg-gray-800 dark:hover:bg-gray-700/70 dark:focus:border-gray-500/80"
+      className="group my-1 flex h-11 w-full items-center overflow-hidden rounded bg-gray-100 pl-3 text-left transition hover:bg-gray-200 focus:border focus:border-gray-400/60 focus-visible:outline-none dark:border dark:border-gray-400/10 dark:bg-gray-800 dark:hover:bg-gray-700/70 dark:focus:border-gray-500/80"
       key={ranking}
       to={`/zone/${zoneId}`}
       data-test-id="zone-list-link"
@@ -60,7 +60,7 @@ export function VirtualizedZoneList({ data }: ZonelistProperties) {
   const rowVirtualizer = useVirtualizer({
     count: data.length,
     getScrollElement: () => parentReference.current,
-    estimateSize: () => 46,
+    estimateSize: () => 48,
     overscan: 5,
   });
 
