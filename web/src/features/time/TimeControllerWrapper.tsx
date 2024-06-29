bac1f54e847bfa6d1b49645e9b7bb5ef9c9cbaf4
Viktor Andersson <30777521+VIKTORVAV99@users.noreply.github.com>
2024-06-29 15:25:37 +0200

    add transparent background

--- web/src/features/time/TimeControllerWrapper.tsx
+++ web/src/features/time/TimeControllerWrapper.tsx
@@ -1,13 +1,10 @@
-import { loadingMapAtom } from 'features/map/mapAtoms';
-import { useAtom } from 'jotai';
-import { hasOnboardingBeenSeenAtom } from 'utils/state/atoms';
 import { useBreakpoint } from 'utils/styling';
 
 import TimeController from './TimeController';
 
 function FloatingTimeController() {
   return (
-    <div className="fixed bottom-3 left-3 z-20 w-[calc(14vw_+_16rem)] rounded-xl bg-white px-4 py-3 shadow-xl drop-shadow-2xl min-[780px]:w-[calc((14vw_+_16rem)_-_30px)] xl:px-5  dark:bg-gray-800">
+    <div className="fixed bottom-3 left-3 z-20 w-[calc(14vw_+_16rem)] rounded-xl bg-white/80 px-4 py-3 shadow-xl drop-shadow-2xl backdrop-blur min-[780px]:w-[calc((14vw_+_16rem)_-_30px)] xl:px-5 dark:bg-gray-800/80">
       <TimeController />
     </div>
   );
@@ -15,7 +12,7 @@ function FloatingTimeController() {
 
 function FixedTimeController() {
   return (
-    <div className="fixed bottom-0 left-0 right-0 z-20 w-full bg-white px-4 py-3 shadow-xl drop-shadow-2xl dark:bg-gray-800">
+    <div className="fixed bottom-0 left-0 right-0 z-20 w-full bg-white/80 px-4 py-3 shadow-xl drop-shadow-2xl backdrop-blur-sm dark:bg-gray-800/80">
       <TimeController />
     </div>
   );
