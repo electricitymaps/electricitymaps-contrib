2a0f19be1ee13acc84310d0d7f7285116d8ef40f
Viktor Andersson <30777521+VIKTORVAV99@users.noreply.github.com>
2024-07-24 09:12:23 +0200

    add success version to badge + story (#6983)

--- web/src/components/Badge.tsx
+++ web/src/components/Badge.tsx
@@ -1,4 +1,4 @@
-export type PillType = 'default' | 'warning';
+export type PillType = 'default' | 'warning' | 'success';
 
 type BadgeProps = {
   pillText: string;
@@ -7,14 +7,27 @@ type BadgeProps = {
 };
 
 export default function Badge({ pillText, type, icon }: BadgeProps) {
-  const classes =
-    type == 'warning'
-      ? 'bg-amber-700/10 dark:bg-amber-500/10 text-amber-700 dark:text-amber-500'
-      : 'bg-neutral-200 dark:bg-gray-700 text-black dark:text-white';
+  let classes = '';
+
+  switch (type) {
+    case 'warning': {
+      classes =
+        'bg-warning/10 dark:bg-warning-dark/10 text-warning dark:text-warning-dark';
+      break;
+    }
+    case 'success': {
+      classes =
+        'bg-success/10 dark:bg-success-dark/10 text-success dark:text-success-dark';
+      break;
+    }
+    default: {
+      classes = 'bg-neutral-200 dark:bg-gray-700 text-black dark:text-white';
+    }
+  }
 
   return (
     <span
-      className={`ml-2 flex h-[22px] flex-row items-center gap-1 whitespace-nowrap rounded-full px-2 py-1 text-xs font-semibold ${classes}`}
+      className={`flex h-6 flex-row items-center gap-1 whitespace-nowrap rounded-full px-2 py-1 text-xs font-semibold ${classes}`}
       data-test-id="badge"
     >
       {icon != undefined && <div className={`${icon}`} />}
