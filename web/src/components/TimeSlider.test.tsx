d5bab26193b5f7c93b09e0e522adea5c2fdca2a8
Viktor Andersson <30777521+VIKTORVAV99@users.noreply.github.com>
2024-07-25 01:02:10 +0200

    correct size in tests

--- web/src/components/TimeSlider.test.tsx
+++ web/src/components/TimeSlider.test.tsx
@@ -26,35 +26,35 @@ describe('getTrackBackground', () => {
 });
 
 describe('getThumbIcon', () => {
-  it('returns "<MoveHorizontal size={14} />" when no index or sets are provided', () => {
+  it('returns "<MoveHorizontal size={16} />" when no index or sets are provided', () => {
     const { container: expected } = render(
-      <MoveHorizontal size={14} pointerEvents="none" />
+      <MoveHorizontal size={16} pointerEvents="none" />
     );
     const { container } = render(getThumbIcon());
     expect(container.innerHTML).toEqual(expected.innerHTML);
   });
 
   it.each([[10], [35]])(
-    'returns "<Moon size={14} />" when the index %i is within a night time set',
+    'returns "<Moon size={16} />" when the index %i is within a night time set',
     (index) => {
       const sets = [
         [7, 21],
         [30, 40],
       ];
-      const { container: expected } = render(<Moon size={14} pointerEvents="none" />);
+      const { container: expected } = render(<Moon size={16} pointerEvents="none" />);
       const { container } = render(getThumbIcon(index, sets));
       expect(container.innerHTML).toEqual(expected.innerHTML);
     }
   );
 
   it.each([[5], [25], [50]])(
-    'returns "<Sun size={14} />" when the index %i is not within a night time set',
+    'returns "<Sun size={16} />" when the index %i is not within a night time set',
     (index) => {
       const sets = [
         [7, 21],
         [30, 40],
       ];
-      const { container: expected } = render(<Sun size={14} pointerEvents="none" />);
+      const { container: expected } = render(<Sun size={16} pointerEvents="none" />);
       const { container } = render(getThumbIcon(index, sets));
       expect(container.innerHTML).toEqual(expected.innerHTML);
     }
