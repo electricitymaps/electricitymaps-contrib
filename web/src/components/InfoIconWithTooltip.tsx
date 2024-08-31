6a97f98ba71a0900e8ae00c0647271dab102fa41
Viktor Andersson <30777521+VIKTORVAV99@users.noreply.github.com>
2024-08-31 17:27:38 +0200

    format

--- web/src/components/InfoIconWithTooltip.tsx
+++ web/src/components/InfoIconWithTooltip.tsx
@@ -1,12 +1,17 @@
-import { Info } from "lucide-react";
+import { Info } from 'lucide-react';
 
-import TooltipWrapper from "./tooltips/TooltipWrapper";
+import TooltipWrapper from './tooltips/TooltipWrapper';
 
 interface InfoIconWithTooltipProps {
   tooltipContent: string | JSX.Element;
 }
 
-export default function InfoIconWithTooltip({tooltipContent}: InfoIconWithTooltipProps) {
+/** This component is only meant to be used with `CarbonIntensitySquare` and `CircularGauge`
+ * components. It is a small info icon that displays a tooltip when hovered over.
+ */
+export default function InfoIconWithTooltip({
+  tooltipContent,
+}: InfoIconWithTooltipProps) {
   return (
     <div className="absolute top-[58px] rounded-full bg-zinc-50 dark:bg-gray-900">
       <TooltipWrapper tooltipContent={tooltipContent} side="bottom">
