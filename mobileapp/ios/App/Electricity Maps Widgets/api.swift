63c834e3cce923981b24bf2a795072fceb7c4ee2
madsnedergaard <nedergaardmads@gmail.com>
2024-09-12 11:34:34 +0200

    Adds new func for fetching data from app-backend

--- mobileapp/ios/App/Electricity Maps Widgets/api.swift
+++ mobileapp/ios/App/Electricity Maps Widgets/api.swift
@@ -27,14 +27,44 @@ struct ElectricityUnits: Codable {
   let carbonIntensity: String
 }
 
-struct DataFetcher {
-  static func fetchIntensity(zone: String) async throws -> Int {
-    // TODO: Validate zone name here
-    let url = URL(string: "https://api.co2signal.com/v1/latest?countryCode=\(zone)")!
-    let (data, _) = try await URLSession.shared.data(from: url)
-    let result = try JSONDecoder().decode(
-      APIResponse.self, from: data)
+struct APIResponseDayAhead: Decodable {
+    let data: FuturePriceData
 
-    return Int(result.data.carbonIntensity)
-  }
+    struct FuturePriceData: Decodable {
+        let futurePrice: FuturePrice
+    }
+
+    struct FuturePrice: Decodable {
+        let currency: String
+        let entryCount: Int
+        let priceData: [String: Float]
+    }
+}
+
+struct DataFetcher {
+
+    static func fetchDayAhead(zone: String) async throws -> Int {
+        // TODO: Make this work with the actual backend with authentication
+        let url = URL(string: "http://localhost:8001/v8/details/hourly/\(zone)")!
+        // let url = URL(string: "https://app-backend.electricitymap.org/v8/details/hourly/\(zone)")!
+        // let path = ...
+        // let token = ...
+        // let timestamp = String(Date().timeIntervalSince1970)
+        // url.addValue("x-request-timestamp", forHTTPHeaderField: timestamp)
+        // url.addValue("x-signature", forHTTPHeaderField: getHeaders(path: "/v8/details/hourly/\(zone)", timestamp: timestamp, token: token))
+        let (data, _) = try await URLSession.shared.data(from: url)
+        let result = try JSONDecoder().decode(APIResponseDayAhead.self, from: data)
+
+        // TODO: Return the full list in an appropriate format for the view to show
+        return Int(result.data.futurePrice.entryCount)
+    }
+    static func fetchIntensity(zone: String) async throws -> Int {
+        // TODO: Validate zone name here
+        let url = URL(string: "https://api.co2signal.com/v1/latest?countryCode=\(zone)")!
+        let (data, _) = try await URLSession.shared.data(from: url)
+        let result = try JSONDecoder().decode(
+            APIResponse.self, from: data)
+
+        return Int(result.data.carbonIntensity)
+    }
 }
