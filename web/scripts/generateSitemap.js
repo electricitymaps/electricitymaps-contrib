3d31d2fdcb87f566c1225e9db2d9592052132840
shing1Sks <shreyashks02@gmail.com>
2024-10-07 14:11:44 +0530

    error fix broken path and permission error

--- web/scripts/generateSitemap.js
+++ web/scripts/generateSitemap.js
@@ -5,14 +5,28 @@ import path from 'node:path';
 
 import zonesConfig from '../config/zones.json' assert { type: 'json' };
 
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
+let dirname = path.dirname(new URL(import.meta.url).pathname);
+
+// Fix the paths for Windows/Linux consistency
+// Extra / or \\ in begenning gives a broken path
+if (dirname.startsWith('/') || dirname.startsWith('\\')) {
+  dirname = dirname.substring(1);
+}
+
+const __dirname = decodeURIComponent(dirname);
 const SITEMAP_DIR = path.join(__dirname, '..', 'public');
 const SITEMAP_PATH = path.join(SITEMAP_DIR, 'sitemap.xml');
 
 function generateSitemap() {
   // Ensure the directory exists
   if (!fs.existsSync(SITEMAP_DIR)) {
-    fs.mkdirSync(SITEMAP_DIR, { recursive: true });
+    try {
+      fs.mkdirSync(SITEMAP_DIR, { recursive: true });
+      console.log('Directory created:', SITEMAP_DIR);
+    } catch (error) {
+      console.error('Failed to create directory:', error);
+      return;
+    }
   }
 
   const zoneUrls = Object.keys(zonesConfig.zones)
