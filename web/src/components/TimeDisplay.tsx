8289815937b9fa98dd1d76c75e7c6de482d79ae9
Tony <tony.vanswet@electricitymaps.com>
2024-10-14 16:29:13 +0200

    feat(web): zone specific datetimes (#7316)
    * feat(web): zone specific datetimes
    
    * include router context too chartooltips tests
    
    * specify python version in workflow
    
    * revert last commit
    
    * use zone time zone for charts and time slider
    
    * update parameter types

--- web/src/components/TimeDisplay.tsx
+++ web/src/components/TimeDisplay.tsx
@@ -1,10 +1,17 @@
 import { useAtomValue } from 'jotai';
 import { useTranslation } from 'react-i18next';
+import { ZoneKey } from 'types';
 import { selectedDatetimeIndexAtom, timeAverageAtom } from 'utils/state/atoms';
 
 import { FormattedTime } from './Time';
 
-export function TimeDisplay({ className }: { className?: string }) {
+export function TimeDisplay({
+  className,
+  zoneId,
+}: {
+  className?: string;
+  zoneId?: ZoneKey;
+}) {
   const { i18n } = useTranslation();
   const timeAverage = useAtomValue(timeAverageAtom);
   const selectedDatetime = useAtomValue(selectedDatetimeIndexAtom);
@@ -15,6 +22,7 @@ export function TimeDisplay({ className }: { className?: string }) {
       language={i18n.languages[0]}
       timeAverage={timeAverage}
       className={className}
+      zoneId={zoneId}
     />
   );
 }
