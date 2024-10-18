9f8480fa326add2a7fb3eb893cb8d302f0e7e88e
Cadence Banulis <cadence.banulis@electricitymaps.com>
2024-10-18 10:29:40 +0200

    feat: add scroll to chart functionality

--- web/src/features/charts/NotEnoughDataMessage.tsx
+++ web/src/features/charts/NotEnoughDataMessage.tsx
@@ -2,11 +2,11 @@ import { useTranslation } from 'react-i18next';
 
 import { ChartTitle } from './ChartTitle';
 
-export function NotEnoughDataMessage({ title }: { title: string }) {
+export function NotEnoughDataMessage({ title, id }: { title: string; id: string }) {
   const { t } = useTranslation();
   return (
     <div className="w-full">
-      <ChartTitle translationKey={title} />
+      <ChartTitle translationKey={title} id={id} />
       <div className="my-2 rounded bg-gray-200 py-4 text-center text-sm dark:bg-gray-800">
         <p>{t('country-history.not-enough-data')}</p>
       </div>
