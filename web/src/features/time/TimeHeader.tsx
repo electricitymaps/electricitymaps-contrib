99987f09bf7618929f6bfb87cc5ad0a4cee0ab0a
silkeholmebonnen <silke.bonnen@gmail.com>
2024-10-21 15:49:53 +0200

    WIP

--- web/src/features/time/TimeHeader.tsx
+++ web/src/features/time/TimeHeader.tsx
@@ -1,32 +1,32 @@
 import useGetState from 'api/getState';
 import Badge from 'components/Badge';
 import { FormattedTime } from 'components/Time';
-import { useAtomValue } from 'jotai';
+import { useAtom, useAtomValue } from 'jotai';
 import { useTranslation } from 'react-i18next';
 import { selectedDatetimeIndexAtom, timeAverageAtom } from 'utils/state/atoms';
 
 export default function TimeHeader() {
   const { t, i18n } = useTranslation();
   const timeAverage = useAtomValue(timeAverageAtom);
-  const selectedDatetime = useAtomValue(selectedDatetimeIndexAtom);
+  const [selectedDatetime, setSelectedDatetime] = useAtom(selectedDatetimeIndexAtom);
   const { isLoading } = useGetState();
 
+  // TODO: Left and right arrows to change the date
   return (
     <div className="flex min-h-6 flex-row items-center">
       <h3 className="grow select-none text-left">
         {t(`time-controller.title.${timeAverage}`)}
       </h3>
       {!isLoading && (
-        <Badge
-          pillText={
-            <FormattedTime
-              datetime={selectedDatetime.datetime}
-              language={i18n.languages[0]}
-              timeAverage={timeAverage}
-            />
-          }
-          type="success"
-        />
+        <input
+          className="rounded-full border-none bg-success/10 py-1 pl-2 text-xs font-semibold text-success dark:bg-success-dark/10 dark:text-success-dark"
+          type="date" // use month or year
+          value={selectedDatetime.datetime.toISOString().slice(0, 10)}
+          onChange={(event) => {
+            setSelectedDatetime({ datetime: new Date(event.target.value), index: 0 });
+          }}
+          aria-label="Select date"
+        ></input>
       )}
     </div>
   );
