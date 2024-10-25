e87ca8c8e0d728e9f96e61e03343be0679f79098
tonypls <tonyvanswet@gmail.com>
2024-10-25 15:14:43 +0200

    make cypress work with new router

--- web/src/components/modals/OnboardingModal.cy.tsx
+++ web/src/components/modals/OnboardingModal.cy.tsx
@@ -1,6 +1,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { I18nextProvider } from 'react-i18next';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createMemoryRouter, RouterProvider } from 'react-router-dom';
 import i18n from 'translation/i18n';
 
 import { OnboardingModal } from './OnboardingModal';
@@ -15,18 +15,27 @@ describe('OnboardingModal', () => {
   });
 
   it('mounts', () => {
-    const router = createBrowserRouter([
+    function ModalWrapper() {
+      return (
+        <QueryClientProvider client={queryClient}>
+          <I18nextProvider i18n={i18n}>
+            <OnboardingModal />
+          </I18nextProvider>
+        </QueryClientProvider>
+      );
+    }
+
+    const router = createMemoryRouter(
+      [
+        {
+          path: '*',
+          element: <ModalWrapper />,
+        },
+      ],
       {
-        path: '/',
-        element: (
-          <QueryClientProvider client={queryClient}>
-            <I18nextProvider i18n={i18n}>
-              <OnboardingModal />
-            </I18nextProvider>
-          </QueryClientProvider>
-        ),
-      },
-    ]);
+        initialEntries: ['/'],
+      }
+    );
 
     cy.mount(<RouterProvider router={router} />);
     cy.contains('Electricity Maps');
--- web/src/features/map/Map.cy.tsx
+++ web/src/features/map/Map.cy.tsx
@@ -1,11 +1,34 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { atom, WritableAtom } from 'jotai';
+import { createMemoryRouter, RouterProvider } from 'react-router-dom';
 import { TestProvider } from 'testing/testUtils';
-import { selectedDatetimeIndexAtom } from 'utils/state/atoms';
 
 import Map from './Map';
 
 const zonesToTest = ['DE', 'FR', 'NL', 'NO-NO1', 'SE-SE1', 'CH', 'ES'];
+interface DatetimeState {
+  datetime: Date;
+  index: number;
+}
+
+type TestProviderValue = [
+  WritableAtom<DatetimeState, [DatetimeState], void>,
+  DatetimeState
+];
+
+export const selectedDatetimeIndexAtom: WritableAtom<
+  DatetimeState,
+  [DatetimeState],
+  void
+> = atom(
+  {
+    datetime: new Date(),
+    index: 0,
+  },
+  (get, set, newValue: DatetimeState) => {
+    set(selectedDatetimeIndexAtom, newValue);
+  }
+);
 
 type ZoneLayer = {
   id: string;
@@ -139,28 +162,36 @@ describe('Map Component', () => {
   it('should display loading state initially', () => {
     const queryClient = new QueryClient();
 
-    const router = createBrowserRouter([
+    const initialState: DatetimeState = {
+      datetime: new Date('2022-12-05T08:00:00+00:00'),
+      index: 0,
+    };
+
+    const initialValues: [TestProviderValue] = [
+      [selectedDatetimeIndexAtom, initialState],
+    ];
+
+    function TestComponent() {
+      return (
+        <TestProvider initialValues={initialValues as any}>
+          <QueryClientProvider client={queryClient}>
+            <Map onMapLoad={handleMapLoad} />
+          </QueryClientProvider>
+        </TestProvider>
+      );
+    }
+
+    const router = createMemoryRouter(
+      [
+        {
+          path: '*',
+          element: <TestComponent />,
+        },
+      ],
       {
-        path: '/',
-        element: (
-          <TestProvider
-            initialValues={[
-              [
-                selectedDatetimeIndexAtom,
-                {
-                  datetime: new Date('2022-12-05T08:00:00+00:00'),
-                  index: 0,
-                },
-              ],
-            ]}
-          >
-            <QueryClientProvider client={queryClient}>
-              <Map onMapLoad={handleMapLoad} />
-            </QueryClientProvider>
-          </TestProvider>
-        ),
-      },
-    ]);
+        initialEntries: ['/'],
+      }
+    );
 
     cy.mount(<RouterProvider router={router} />);
     cy.get('[data-test-id=exchange-layer]').should('be.visible');
