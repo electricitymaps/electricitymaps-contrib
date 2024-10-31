3c2cdb71ad09fb8345540dd16208e4044771da6f
Tony <tony.vanswet@electricitymaps.com>
2024-10-31 11:38:47 +0100

    Update web/src/api/getZone.ts
    Co-authored-by: Viktor Andersson <30777521+VIKTORVAV99@users.noreply.github.com>

--- web/src/api/getZone.ts
+++ web/src/api/getZone.ts
@@ -1,7 +1,7 @@
 import type { UseQueryResult } from '@tanstack/react-query';
 import { useQuery } from '@tanstack/react-query';
 import { RouteParameters } from 'App';
-import { useAtom } from 'jotai';
+import { useAtomValue } from 'jotai';
 import { useParams } from 'react-router-dom';
 import invariant from 'tiny-invariant';
 import type { ZoneDetails } from 'types';
