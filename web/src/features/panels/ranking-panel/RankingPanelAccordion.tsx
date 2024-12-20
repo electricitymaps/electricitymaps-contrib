886e787cb189cf56949075846cda6b9eea08df8c
tonypls <tonyvanswet@gmail.com>
2024-12-20 14:07:54 +1100

    remove unused imports

--- web/src/features/panels/ranking-panel/RankingPanelAccordion.tsx
+++ web/src/features/panels/ranking-panel/RankingPanelAccordion.tsx
@@ -1,8 +1,7 @@
 import Accordion from 'components/Accordion';
-import { Link } from 'components/Link';
 import InfoText from 'features/modals/InfoText';
 import { useAtom } from 'jotai';
-import { Trans, useTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 import { rankingPanelAccordionCollapsedAtom } from 'utils/state/atoms';
 
 export default function RankingPanelAccordion() {
