5b084365d417da79fa48f4367c8f995fdf90592f
Viktor Andersson <30777521+VIKTORVAV99@users.noreply.github.com>
2024-12-31 13:29:00 +0100

    perf: Memoize CapacityLegend.tsx

--- web/src/features/charts/bar-breakdown/elements/CapacityLegend.tsx
+++ web/src/features/charts/bar-breakdown/elements/CapacityLegend.tsx
@@ -1,10 +1,14 @@
-export default function CapacityLegend({ children }: { children?: React.ReactNode }) {
+import { memo } from 'react';
+
+function CapacityLegend({ text, unit }: { text: string; unit?: string }) {
   return (
     <div className="flex flex-row pt-2">
-      <span className="mt-0.5 h-3 w-3 rounded-full bg-black/10 text-xs dark:bg-white/10"></span>
+      <span className="mt-0.5 h-3 w-3 rounded-full bg-black/10 text-xs dark:bg-white/10" />
       <span className="pl-2 text-xs font-medium text-neutral-600 dark:text-gray-300">
-        {children}
+        {text} ({unit})
       </span>
     </div>
   );
 }
+
+export default memo(CapacityLegend);
