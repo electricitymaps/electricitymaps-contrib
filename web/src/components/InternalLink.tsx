6775a691b0f569e2d127eaa1f1828a0e85222041
Viktor Andersson <30777521+VIKTORVAV99@users.noreply.github.com>
2025-01-10 11:50:46 +0100

    fix ranking panel links

--- web/src/components/InternalLink.tsx
+++ web/src/components/InternalLink.tsx
@@ -1,11 +1,13 @@
+import { TIME_RANGE_TO_TIME_AVERAGE } from 'api/helpers';
 import { Link, LinkProps, useLocation, useParams } from 'react-router-dom';
 import { RouteParameters } from 'types';
 
 export default function InternalLink({ to, ...rest }: LinkProps) {
   const location = useLocation();
+
   const { urlTimeRange } = useParams<RouteParameters>();
-  const toWithState = `${to}${urlTimeRange ? `/${urlTimeRange}` : ''}${location.search}${
-    location.hash
-  }`;
+  const toWithState = `${to}${
+    urlTimeRange ? `/${TIME_RANGE_TO_TIME_AVERAGE[urlTimeRange]}/${urlTimeRange}` : ''
+  }${location.search}${location.hash}`;
   return <Link to={toWithState} {...rest} />;
 }
