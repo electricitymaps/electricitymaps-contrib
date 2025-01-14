0a3781b90a4c56b5098db6c4a8210fe087ce2c8d
Erik Sundell <erik.i.sundell@gmail.com>
2025-01-14 22:59:45 +0100

    tests(parsers, ESTADISTICO_UT): add snapshot test for historical prod

--- parsers/test/test_ESTADISTICO_UT.py
+++ parsers/test/test_ESTADISTICO_UT.py
@@ -1,32 +1,45 @@
+from datetime import datetime
 from importlib import resources
 
-import pytest
 from requests_mock import GET, POST
 
-from electricitymap.contrib.lib.types import ZoneKey
-from parsers.ESTADISTICO_UT import DAILY_OPERATION_URL, fetch_production
+from parsers.ESTADISTICO_UT import DAILY_OPERATION_URL, ZONE_INFO, fetch_production
 
 
-@pytest.fixture(autouse=True)
-def mock_response(adapter):
+def test_fetch_production_live(adapter, session, snapshot):
     adapter.register_uri(
         GET,
         DAILY_OPERATION_URL,
         text=resources.files("parsers.test.mocks.ESTADISTICO_UT")
-        .joinpath("get.html")
+        .joinpath("get_live.html")
         .read_text(),
     )
     adapter.register_uri(
         POST,
         DAILY_OPERATION_URL,
         text=resources.files("parsers.test.mocks.ESTADISTICO_UT")
-        .joinpath("post.html")
+        .joinpath("post_live.html")
+        .read_text(),
+    )
+    assert snapshot == fetch_production(session=session)
+
+
+def test_fetch_production_historical(adapter, session, snapshot):
+    dt = datetime.fromisoformat("2025-01-01").replace(tzinfo=ZONE_INFO)
+
+    adapter.register_uri(
+        GET,
+        DAILY_OPERATION_URL,
+        text=resources.files("parsers.test.mocks.ESTADISTICO_UT")
+        .joinpath("get_historical.html")
+        .read_text(),
+    )
+    adapter.register_uri(
+        POST,
+        DAILY_OPERATION_URL,
+        text=resources.files("parsers.test.mocks.ESTADISTICO_UT")
+        .joinpath("post_historical.html")
         .read_text(),
     )
 
-
-def test_fetch_production(session, snapshot):
-    assert snapshot == fetch_production(
-        zone_key=ZoneKey("SV"),
-        session=session,
-    )
+    assert snapshot == fetch_production(session=session, target_datetime=dt)
