b00f3059bb54a9b2970c6760fe41485d411e20ed
t-couery <tanguy.couery@gmail.com>
2025-01-25 23:46:54 +0100

    Update parser data

--- parsers/test/test_ES.py
+++ parsers/test/test_ES.py
@@ -9,73 +9,48 @@ from parsers import ES
 ### El Hierro
 # Consumption
 def test_fetch_consumption(adapter, session, snapshot):
-    mock_data = b'null({"valoresHorariosGeneracion":[{"ts":"2023-09-04 00:55","dem":5.5,"die":3.2,"gas":0.0,"eol":3.0,"cc":0.0,"vap":0.0,"fot":0.0,"hid":-0.5}]}'
+    mock_data = b'null({"valoresHorariosGeneracion":[{"ts":"2025-01-25 02:00","dem":4.8,"die":3.4,"gas":0.0,"eol":1.9,"cc":0.0,"vap":0.0,"fot":0.0,"hid":-0.2,"conb":-0.5,"turb":0.3,"gnhd":-0.2,"efl":0.0}]})'
     adapter.register_uri(ANY, ANY, content=mock_data)
 
     assert snapshot == ES.fetch_consumption(
-        ZoneKey("ES-CN-HI"), session, datetime.fromisoformat("2023-09-04")
+        ZoneKey("ES-CN-HI"), session, datetime.fromisoformat("2025-01-25")
     )
 
 
 ### El Hierro
 # Production
 def test_fetch_production_storage(adapter, session, snapshot):
-    mock_data = b'null({"valoresHorariosGeneracion":[{"ts":"2023-09-04 00:55","dem":5.5,"die":3.2,"gas":0.0,"eol":3.0,"cc":0.0,"vap":0.0,"fot":0.0,"hid":-0.5}]}'
+    mock_data = b'null({"valoresHorariosGeneracion":[{"ts":"2025-01-25 02:00","dem":4.8,"die":3.4,"gas":0.0,"eol":1.9,"cc":0.0,"vap":0.0,"fot":0.0,"hid":-0.2,"conb":-0.5,"turb":0.3,"gnhd":-0.2,"efl":0.0}]})'
     adapter.register_uri(ANY, ANY, content=mock_data)
 
     assert snapshot == ES.fetch_production(
         ZoneKey("ES-CN-HI"),
         session,
-        datetime.fromisoformat("2023-09-04"),
+        datetime.fromisoformat("2025-01-25")
     )
-
-
-# Test for DST change days
-def test_production_DST_CN(adapter, session, snapshot):
-    mock_data = b'null({"valoresHorariosGeneracion":[{"ts":"2021-10-31 1A:55","dem":4.8,"die":2.8,"gas":0.0,"eol":3.3,"cc":0.0,"vap":0.0,"fot":0.0,"hid":-1.2},{"ts":"2021-10-31 1B:00","dem":4.5,"die":2.5,"gas":0.0,"eol":3.2,"cc":0.0,"vap":0.0,"fot":0.0,"hid":-1.2}]}'
-    adapter.register_uri(ANY, ANY, content=mock_data)
-
-    assert snapshot == ES.fetch_production(
-        ZoneKey("ES-CN-HI"),
-        session,
-        datetime.fromisoformat("2021-10-31"),
-    )
-
 
 ### Menorca
 # Production
 def test_fetch_production(adapter, session, snapshot):
-    mock_data = b'null({"valoresHorariosGeneracion":[{"ts":"2023-09-02 21:00","dem":85.9,"car":0.0,"die":22.6,"gas":59.3,"cc":0.0,"cb":0.0,"fot":1.1,"tnr":0.0,"trn":0.0,"eol":0.2,"emm":2.7,"emi":-0.0,"otrRen":0.0,"resid":0.0,"genAux":0.0,"cogen":0.0,"eif":-0.0}]}'
+    mock_data = b'null({"valoresHorariosGeneracion":[{"ts":"2025-01-25 03:00","dem":31.1,"car":0.0,"die":0.0,"gas":20.6,"cc":0.0,"cb":0.0,"fot":0.0,"tnr":0.0,"trn":0.0,"eol":0.0,"emm":10.5,"emi":-0.0,"otrRen":0.0,"resid":0.0,"genAux":0.0,"cogen":0.0,"eif":-0.0,"residNr":0.0,"residRen":0.0}]});'
     adapter.register_uri(ANY, ANY, content=mock_data)
 
     assert snapshot == ES.fetch_production(
         ZoneKey("ES-IB-ME"),
         session,
-        datetime.fromisoformat("2023-09-03"),
+        datetime.fromisoformat("2025-01-25")
     )
 
 
 ### Mallorca
 # Exchange
 def test_fetch_exchange(adapter, session, snapshot):
-    mock_data = b'null({"valoresHorariosGeneracion":[{"ts":"2023-09-03 19:05","dem":670.3,"car":0.0,"die":0.0,"gas":0.0,"cc":416.4,"cb":309.8,"fot":19.7,"tnr":0.0,"trn":0.0,"eol":0.0,"emm":-20.6,"emi":-91.3,"otrRen":0.0,"resid":34.6,"genAux":0.0,"cogen":1.7,"eif":-0.0}]}'
+    mock_data = b'null({"valoresHorariosGeneracion":[{"ts":"2025-01-25 03:00","dem":314.1,"car":0.0,"die":0.0,"gas":0.0,"cc":298.4,"cb":82.1,"fot":0.0,"tnr":0.0,"trn":0.0,"eol":0.0,"emm":-10.5,"emi":-70.5,"otrRen":1.3,"resid":0.0,"genAux":0.0,"cogen":3.2,"eif":-0.0,"residNr":5.1,"residRen":5.1}]});'
     adapter.register_uri(ANY, ANY, content=mock_data)
 
     assert snapshot == ES.fetch_exchange(
         ZoneKey("ES"),
         ZoneKey("ES-IB-MA"),
         session,
-        datetime.fromisoformat("2023-09-03"),
-    )
-
-
-# Test for DST change days
-def test_production_DST_IB(adapter, session, snapshot):
-    mock_data = b'null({"valoresHorariosGeneracion":[{"ts":"2020-10-25 2A:55","dem":261.3,"car":73.3,"die":0.0,"gas":0.0,"cc":127.1,"cb":100.4,"fot":0.0,"tnr":0.0,"trn":0.0,"eol":0.0,"emm":-3.5,"emi":-58.1,"otrRen":0.0,"resid":19.2,"genAux":0.0,"cogen":2.9,"eif":-0.0},{"ts":"2020-10-25 2B:00","dem":261.7,"car":73.3,"die":0.0,"gas":0.0,"cc":128.0,"cb":100.4,"fot":0.0,"tnr":0.0,"trn":0.0,"eol":0.0,"emm":-3.3,"emi":-58.1,"otrRen":0.0,"resid":18.5,"genAux":0.0,"cogen":2.9,"eif":-0.0}]}'
-    adapter.register_uri(ANY, ANY, content=mock_data)
-
-    assert snapshot == ES.fetch_production(
-        ZoneKey("ES-IB-MA"),
-        session,
-        datetime.fromisoformat("2020-10-25"),
+        datetime.fromisoformat("2025-01-25")
     )
