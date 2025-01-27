40ab868451117210b8e6e3195973b39a5857530b
Zach Wegrzyniak <zwegrzyniak@gmail.com>
2025-01-27 13:50:37 -0500

    patch up types for tsconfig.node once more

--- web/geo/validate.test.ts
+++ web/geo/validate.test.ts
@@ -1,3 +1,5 @@
+import { describe, expect, it } from 'vitest';
+
 import { GEO_CONFIG } from './generateWorld';
 import { WorldFeatureCollection } from './types';
 import { zeroOverlaps } from './validate';
