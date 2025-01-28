c163023e1d9342034b86036d1824d98822949686
tonypls <tonyvanswet@gmail.com>
2025-01-28 14:04:52 +1300

    link to new datasets and memo

--- web/src/components/buttons/DownloadCsv.tsx
+++ web/src/components/buttons/DownloadCsv.tsx
@@ -1,26 +1,13 @@
 import { Button } from 'components/Button';
 import { Download } from 'lucide-react';
+import { memo } from 'react';
 import { useParams } from 'react-router-dom';
 import { DEFAULT_ICON_SIZE } from 'utils/constants';
 
-import { getCountryName, getZoneName } from '../../translation/translation';
-
-function getCountryToDownload(zoneId?: string) {
-  if (!zoneId) {
-    return '';
-  }
-
-  const zoneName = getZoneName(zoneId);
-  const zoneCountryName =
-    zoneId && zoneId.includes('-') && getCountryName(zoneId)?.toLowerCase();
-  return zoneCountryName || zoneName;
-}
-
-export function DownloadCsv() {
+function DownloadCsv() {
   const { zoneId } = useParams();
-  const countryToDownload = getCountryToDownload(zoneId);
 
-  const url = `https://www.electricitymaps.com/data-portal/${countryToDownload.toLowerCase()}?utm_source=app&utm_medium=download_button&utm_campaign=csv_download`;
+  const url = `https://portal.electricitymaps.com/datasets/${zoneId}?utm_source=app&utm_medium=download_button&utm_campaign=csv_download`;
 
   return (
     <Button
@@ -33,4 +20,4 @@ export function DownloadCsv() {
   );
 }
 
-export default DownloadCsv;
+export default memo(DownloadCsv);
