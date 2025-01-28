cba4e0628ad2f17cdf8b42c1f3f0bf455b85aa0d
t-couery <tanguy.couery@gmail.com>
2025-01-28 22:19:13 +0100

    fromat

--- parsers/test/test_ES.py
+++ parsers/test/test_ES.py
@@ -6,6 +6,7 @@ from requests_mock import GET
 from electricitymap.contrib.lib.types import ZoneKey
 from parsers import ES
 
+
 @pytest.fixture(autouse=True)
 def mock_response(adapter):
     # these json files are not in valid json format
@@ -22,17 +23,20 @@ def mock_response(adapter):
             content=data.read(),
         )
 
+
 ### Iberian Peninsula (ES) Consumption
 def test_es_consumption(adapter, session, snapshot):
     assert snapshot == ES.fetch_consumption(
         ZoneKey("ES"), session, datetime.fromisoformat("2024-10-26")
     )
 
+
 ### Iberian Peninsula (ES) Production with DST Tests
 def test_es_production(adapter, session, snapshot):
     assert snapshot == ES.fetch_production(
         ZoneKey("ES"), session, datetime.fromisoformat("2024-10-26")
     )
+
 
 ### ES->PT exchange
 def test_es_pt_exchange(adapter, session, snapshot):
@@ -43,17 +47,20 @@ def test_es_pt_exchange(adapter, session, snapshot):
         datetime.fromisoformat("2024-10-26"),
     )
 
+
 ### Mallorca Consumption
 def test_es_ib_ma_consumption(adapter, session, snapshot):
     assert snapshot == ES.fetch_consumption(
         ZoneKey("ES-IB-MA"), session, datetime.fromisoformat("2025-01-27")
     )
 
+
 ### Mallorca Production
 def test_es_ib_ma_production(adapter, session, snapshot):
     assert snapshot == ES.fetch_production(
         ZoneKey("ES-IB-MA"), session, datetime.fromisoformat("2025-01-27")
     )
+
 
 ### Ibiza->Mallorca exchange
 def test_es_ib_ma_exchange(adapter, session, snapshot):
