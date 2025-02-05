b3e32476ede9acc9eef2415e4778858a863bbad1
Viktor Andersson <30777521+VIKTORVAV99@users.noreply.github.com>
2025-02-05 17:37:44 +0100

    simplify tests

--- parsers/test/test_UY.py
+++ parsers/test/test_UY.py
@@ -8,7 +8,8 @@ from electricitymap.contrib.lib.types import ZoneKey
 from parsers.UY import fetch_consumption, fetch_exchange, fetch_production
 
 
-def test_fetch_production(adapter, session, snapshot):
+@pytest.fixture(autouse=True)
+def register_mock_uris(adapter):
     html = Path("parsers/test/mocks/UY/html.html")
     adapter.register_uri(
         GET,
@@ -21,25 +22,16 @@ def test_fetch_production(adapter, session, snapshot):
         re.compile(r"https://pronos\.adme\.com\.uy.*cache.*"),
         content=data.read_bytes(),
     )
+
+
+def test_fetch_production(session, snapshot):
     assert snapshot == fetch_production(
         zone_key=ZoneKey("UY"),
         session=session,
     )
 
 
-def test_fetch_consumption(adapter, session, snapshot):
-    html = Path("parsers/test/mocks/UY/html.html")
-    adapter.register_uri(
-        GET,
-        "https://pronos.adme.com.uy/gpf.php",
-        text=html.read_text(),
-    )
-    data = Path("parsers/test/mocks/UY/data.ods")
-    adapter.register_uri(
-        GET,
-        re.compile(r"https://pronos\.adme\.com\.uy.*cache.*"),
-        content=data.read_bytes(),
-    )
+def test_fetch_consumption(session, snapshot):
     assert snapshot == fetch_consumption(
         zone_key=ZoneKey("UY"),
         session=session,
@@ -47,19 +39,7 @@ def test_fetch_consumption(adapter, session, snapshot):
 
 
 @pytest.mark.parametrize("zone_key", [ZoneKey("AR"), ZoneKey("BR-S")])
-def test_fetch_exchange(adapter, session, snapshot, zone_key):
-    html = Path("parsers/test/mocks/UY/html.html")
-    adapter.register_uri(
-        GET,
-        "https://pronos.adme.com.uy/gpf.php",
-        text=html.read_text(),
-    )
-    data = Path("parsers/test/mocks/UY/data.ods")
-    adapter.register_uri(
-        GET,
-        re.compile(r"https://pronos\.adme\.com\.uy.*cache.*"),
-        content=data.read_bytes(),
-    )
+def test_fetch_exchange(session, snapshot, zone_key):
     assert snapshot == fetch_exchange(
         zone_key1=ZoneKey("UY"),
         zone_key2=zone_key,
