562d810829dc91f45457f2ec01c9a8a9426a2f36
Viktor Andersson <30777521+VIKTORVAV99@users.noreply.github.com>
2025-02-08 17:52:42 +0100

    chore(tests): Clean up python test to use global fixtures (#7814)
    * chore(tests): Clean up python test to use global fixtures
    
    * align grammar

--- parsers/test/test_TAIPOWER.py
+++ parsers/test/test_TAIPOWER.py
@@ -1,14 +1,10 @@
-from requests import Session
-from requests_mock import GET, Adapter
+from requests_mock import GET
 
 from electricitymap.contrib.lib.types import ZoneKey
 from parsers.TAIPOWER import PRODUCTION_URL, fetch_production
 
 
-def test_production(snapshot):
-    session = Session()
-    adapter = Adapter()
-    session.mount("http://", adapter)
+def test_production(adapter, session, snapshot):
     with open("parsers/test/mocks/TAIPOWER/genary.json", "rb") as mock_file:
         adapter.register_uri(
             GET,
