f6abdf94d35a9b92802d3dbdb2bbcbf42aeb2294
madsnedergaard <nedergaardmads@gmail.com>
2025-03-17 14:16:12 +0100

    Adds "no results" illustration

--- web/src/features/panels/search-panel/NoResults.tsx
+++ web/src/features/panels/search-panel/NoResults.tsx
@@ -1,9 +1,12 @@
 import { useTranslation } from 'react-i18next';
 
+import NoResultsIllustration from './NoResultsIllustration';
+
 export default function NoResults() {
   const { t } = useTranslation();
   return (
-    <div className="flex flex-col items-center justify-center gap-2 pb-6 pt-6">
+    <div className="flex flex-col items-center justify-center gap-2 pb-6">
+      <NoResultsIllustration />
       <p className="text-center text-sm text-neutral-500">
         {t('ranking-panel.no-results')}
       </p>
