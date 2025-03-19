ee5d83110f5c35ec544dc753ba0b45ebd80e0c2d
tonypls <tonyvanswet@gmail.com>
2025-03-19 14:48:55 +0100

    use badge and toggle badge when not flow-traced

--- web/src/components/FlowTracedBadge.tsx
+++ web/src/components/FlowTracedBadge.tsx
@@ -1,21 +1,39 @@
-import { ChevronsUp } from 'lucide-react';
+import Badge from 'components/Badge';
+import { useAtomValue } from 'jotai';
+import { ChevronsUp, TriangleAlert } from 'lucide-react';
 import { useTranslation } from 'react-i18next';
+import { isConsumptionAtom } from 'utils/state/atoms';
 
 import LabelTooltip from './tooltips/LabelTooltip';
 import TooltipWrapper from './tooltips/TooltipWrapper';
 
 export default function FlowTracedBadge(): JSX.Element {
   const { t } = useTranslation();
+  const isConsumption = useAtomValue(isConsumptionAtom);
+
   return (
     <TooltipWrapper
       side="bottom"
       tooltipContent={<LabelTooltip>{t('tooltips.flowTraced')}</LabelTooltip>}
     >
-      <div className="inline-flex h-6 min-w-[112px] items-center rounded-full bg-brand-green/10 px-2 py-1">
-        <div className="flex flex-col">
-          <ChevronsUp className="mr-1 h-4 w-4 font-semibold text-brand-green" />
-        </div>
-        <span className="text-xs font-semibold text-brand-green">Flow-traced</span>
+      <div>
+        <Badge
+          pillText={
+            isConsumption ? (
+              'Flow-traced'
+            ) : (
+              <p className=" text-xs font-semibold line-through">Flow-traced</p>
+            )
+          }
+          type={isConsumption ? 'success' : 'default'}
+          icon={
+            isConsumption ? (
+              <ChevronsUp className="mr-1 h-4 w-4" />
+            ) : (
+              <TriangleAlert className="mr-1 h-4 w-4 " />
+            )
+          }
+        />
       </div>
     </TooltipWrapper>
   );
