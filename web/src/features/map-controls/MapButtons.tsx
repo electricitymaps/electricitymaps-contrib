d732201b58a4829386c7c117637a898e7d368c85
tonypls <tonyvanswet@gmail.com>
2025-03-26 14:41:57 +0100

    feat(app): remove old modals and buttons

---
+++ web/src/features/map-controls/MapButtons.tsx
@@ -0,0 +1,47 @@
+import {
+  isLayersModalOpenAtom,
+  isSettingsModalOpenAtom,
+} from 'features/modals/modalAtoms';
+import { useSetAtom } from 'jotai';
+import { LayersIcon, SettingsIcon } from 'lucide-react';
+import { ReactElement } from 'react';
+import { useTranslation } from 'react-i18next';
+
+import MapButton from './MapButton';
+
+export default function MapButtons(): ReactElement {
+  const { t } = useTranslation();
+  const setIsSettingsModalOpen = useSetAtom(isSettingsModalOpenAtom);
+  const setIsLayersModalOpen = useSetAtom(isLayersModalOpenAtom);
+
+  const handleToggleSettingsModal = (event: React.MouseEvent) => {
+    event.stopPropagation();
+    setIsSettingsModalOpen();
+  };
+
+  const handleToggleLayersModal = (event: React.MouseEvent) => {
+    event.stopPropagation();
+    setIsLayersModalOpen();
+  };
+
+  return (
+    <div className="pointer-events-none absolute right-3 top-2 z-20 mt-[env(safe-area-inset-top)] flex flex-col items-end">
+      <div className="pointer-events-auto flex flex-col gap-2">
+        <MapButton
+          icon={<SettingsIcon size={20} />}
+          tooltipText={t('tooltips.settings')}
+          onClick={handleToggleSettingsModal}
+          dataTestId="settings-button"
+          ariaLabel={t('aria.label.settings')}
+        />
+        <MapButton
+          icon={<LayersIcon size={20} />}
+          tooltipText={t('tooltips.layers')}
+          onClick={handleToggleLayersModal}
+          dataTestId="layers-button"
+          ariaLabel={t('aria.label.layers')}
+        />
+      </div>
+    </div>
+  );
+}
--- web/src/features/modals/modalAtoms.ts
+++ web/src/features/modals/modalAtoms.ts
@@ -1,5 +1,15 @@
 import { atom } from 'jotai';
 
-export const isFAQModalOpenAtom = atom(false);
-export const isInfoModalOpenAtom = atom(false);
-export const isSettingsModalOpenAtom = atom(false);
+type ModalType = 'settings' | 'layers' | null;
+
+const activeModalAtom = atom<ModalType>(null);
+
+export const isSettingsModalOpenAtom = atom(
+  (get) => get(activeModalAtom) === 'settings',
+  (_, set) => set(activeModalAtom, 'settings')
+);
+
+export const isLayersModalOpenAtom = atom(
+  (get) => get(activeModalAtom) === 'layers',
+  (_, set) => set(activeModalAtom, 'layers')
+);
