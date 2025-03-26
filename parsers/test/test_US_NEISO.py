2fb6ec67747ebc92d38306f96a37ef408cd7ec37
Alejandra Navarro Castillo <84456428+alejandranavcas@users.noreply.github.com>
2025-03-26 20:28:53 +0100

    US_NEISO fetch consumption forecast parser (#7933)
    * US_NEISO fetch consumption forecast parser
    
    Build fetch_consumption_forecast parser
    Create test for fetch_consumption_forecast
    Upload mock file for test
    Create snapshot from test
    
    * datetime timezone fix
    
    update snapshot as well
    
    * Include session and fix test
    
    ---------
    
    Co-authored-by: Viktor Andersson <30777521+VIKTORVAV99@users.noreply.github.com>

--- parsers/test/test_US_NEISO.py
+++ parsers/test/test_US_NEISO.py
@@ -1,12 +1,28 @@
 import re
 from pathlib import Path
 
-from requests_mock import GET
+from requests_mock import GET, POST
 
 from electricitymap.contrib.lib.types import ZoneKey
-from parsers.US_NEISO import fetch_wind_solar_forecasts
+from parsers.US_NEISO import fetch_consumption_forecast, fetch_wind_solar_forecasts
 
 base_path_to_mock = Path("parsers/test/mocks/US_NEISO")
+
+
+def test_fetch_consumption_forecast(adapter, session, snapshot):
+    # Mock url request
+    data = Path(base_path_to_mock, "day_ahead_load_forecast_20250317.xml")
+    adapter.register_uri(
+        POST,
+        "https://www.iso-ne.com/ws/wsclient",
+        text=data.read_text(),
+    )
+
+    # Run function under test
+    assert snapshot == fetch_consumption_forecast(
+        zone_key=ZoneKey("US-NE-ISNE"),
+        session=session,
+    )
 
 
 def test_fetch_wind_solar_forecasts(adapter, session, snapshot):
