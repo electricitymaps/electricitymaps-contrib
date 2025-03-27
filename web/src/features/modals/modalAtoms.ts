488f4eaa5f2b5ab85411fc2fd6addc0b3ef597d0
tonypls <tonyvanswet@gmail.com>
2025-03-27 13:37:43 +0100

    enum for modal types

--- web/src/features/modals/modalAtoms.ts
+++ web/src/features/modals/modalAtoms.ts
@@ -1,27 +1,37 @@
 import { atom } from 'jotai';
 
 type ModalType = 'settings' | 'layers' | null;
+enum MODAL_TYPES {
+  SETTINGS = 'settings',
+  LAYERS = 'layers',
+}
 
 const activeModalAtom = atom<ModalType>(null);
 
 export const isSettingsModalOpenAtom = atom(
-  (get) => get(activeModalAtom) === 'settings',
+  (get) => get(activeModalAtom) === MODAL_TYPES.SETTINGS,
   (get, set, value?: boolean) => {
     if (value === false) {
       set(activeModalAtom, null);
     } else {
-      set(activeModalAtom, get(activeModalAtom) === 'settings' ? null : 'settings');
+      set(
+        activeModalAtom,
+        get(activeModalAtom) === MODAL_TYPES.SETTINGS ? null : MODAL_TYPES.SETTINGS
+      );
     }
   }
 );
 
 export const isLayersModalOpenAtom = atom(
-  (get) => get(activeModalAtom) === 'layers',
+  (get) => get(activeModalAtom) === MODAL_TYPES.LAYERS,
   (get, set, value?: boolean) => {
     if (value === false) {
       set(activeModalAtom, null);
     } else {
-      set(activeModalAtom, get(activeModalAtom) === 'layers' ? null : 'layers');
+      set(
+        activeModalAtom,
+        get(activeModalAtom) === MODAL_TYPES.LAYERS ? null : MODAL_TYPES.LAYERS
+      );
     }
   }
 );
