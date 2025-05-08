a853a21f75fcb60438bb73e7a66ca4f172fe546d
chriswasser <christian_wassermann@web.de>
2025-05-08 12:33:03 +0200

    feat(parsers): support > 9 days historic data retrieval in US-NY-NYIS

--- parsers/US_NY.py
+++ parsers/US_NY.py
@@ -4,16 +4,19 @@
 
 from collections import defaultdict
 from datetime import datetime, timedelta
+from io import BytesIO
 from logging import Logger, getLogger
 from operator import itemgetter
 from typing import Any
 from urllib.error import HTTPError
+from zipfile import ZipFile
 from zoneinfo import ZoneInfo
 
 import pandas as pd
 
 # Pumped storage is present but is not split into a separate category.
 from requests import Session
+from requests.exceptions import HTTPError as RequestsHTTPError
 
 from electricitymap.contrib.config import ZoneKey
 from electricitymap.contrib.lib.models.event_lists import (
@@ -47,10 +50,22 @@ mapping = {
 }
 
 
-def read_csv_data(url: str):
+def read_csv_data(url: str) -> pd.DataFrame:
     """Gets csv data from a url and returns a dataframe."""
 
     csv_data = pd.read_csv(url)
+
+    return csv_data
+
+
+def read_zip_data(session: Session, url: str, csv_file: str) -> pd.DataFrame:
+    """Gets zip data from a url (with a session), extracts a csv file and returns a dataframe."""
+
+    response = session.get(url)
+    response.raise_for_status()
+
+    zip_file = ZipFile(BytesIO(response.content))
+    csv_data = pd.read_csv(zip_file.open(csv_file))
 
     return csv_data
 
@@ -111,24 +126,33 @@ def fetch_production(
     logger: Logger = getLogger(__name__),
 ) -> list[dict[str, Any]]:
     """Requests the last known production mix (in MW) of a given zone."""
+    session = session or Session()
+
     if target_datetime is None:
         target_datetime = datetime.now(tz=TIMEZONE)
     else:
         # assume passed in correct timezone
         target_datetime = target_datetime.replace(tzinfo=TIMEZONE)
 
-    if (datetime.now(tz=TIMEZONE) - target_datetime).days > 9:
-        raise NotImplementedError(
-            "you can get data older than 9 days at the url http://mis.nyiso.com/public/"
-        )
-
     ny_date = target_datetime.strftime("%Y%m%d")
-    mix_url = f"http://mis.nyiso.com/public/csv/rtfuelmix/{ny_date}rtfuelmix.csv"
-    try:
-        raw_data = read_csv_data(mix_url)
-    except HTTPError:
-        # this can happen when target_datetime has no data available
-        return []
+    if (datetime.now(tz=TIMEZONE) - target_datetime).days <= 9:
+        mix_url = f"http://mis.nyiso.com/public/csv/rtfuelmix/{ny_date}rtfuelmix.csv"
+        try:
+            raw_data = read_csv_data(mix_url)
+        except HTTPError:
+            # this can happen when target_datetime has no data available
+            return []
+    else:
+        mix_csv = f"{ny_date}rtfuelmix.csv"
+        ny_zip_date = target_datetime.strftime("%Y%m01")
+        mix_zip_url = (
+            f"http://mis.nyiso.com/public/csv/rtfuelmix/{ny_zip_date}rtfuelmix_csv.zip"
+        )
+        try:
+            raw_data = read_zip_data(session, mix_zip_url, mix_csv)
+        except (RequestsHTTPError, KeyError):
+            # this can happen when target_datetime has no data available
+            return []
 
     clean_data = data_parser(raw_data, logger)
 
@@ -156,7 +180,7 @@ def fetch_exchange(
         "http://mis.nyiso.com/public/csv/ExternalLimitsFlows/{}ExternalLimitsFlows.csv"
     )
 
-    sorted_zone_keys = "->".join(sorted([zone_key1, zone_key2]))
+    sorted_zone_keys = ZoneKey("->".join(sorted([zone_key1, zone_key2])))
 
     # In the source CSV, positive is flow into NY, negative is flow out of NY.
     # In Electricity Map, A->B means flow to B is positive.
@@ -200,7 +224,7 @@ def fetch_exchange(
         exchange_data = read_csv_data(exchange_url)
     except HTTPError:
         # this can happen when target_datetime has no data available
-        return None
+        return []
 
     new_england_exs = exchange_data.loc[
         exchange_data["Interface Name"].isin(relevant_exchanges)
@@ -247,17 +271,25 @@ def fetch_consumption_forecast(
         # assume passed in correct timezone
         target_datetime = target_datetime.replace(tzinfo=TIMEZONE)
 
-    if (
-        datetime.now(tz=TIMEZONE) - target_datetime
-    ).days > 9:  # TODO: If datetime is previous to 9 days, it need to be implemented
-        raise NotImplementedError(
-            "you can get data older than 9 days at the url http://mis.nyiso.com/public/"
-        )
     ny_date = target_datetime.strftime("%Y%m%d")
-
-    # Request to the url
-    target_url = f"http://mis.nyiso.com/public/csv/isolf/{ny_date}isolf.csv"
-    df = pd.read_csv(target_url)
+    if (datetime.now(tz=TIMEZONE) - target_datetime).days <= 9:
+        target_url = f"http://mis.nyiso.com/public/csv/isolf/{ny_date}isolf.csv"
+        try:
+            df = read_csv_data(target_url)
+        except HTTPError:
+            # this can happen when target_datetime has no data available
+            return []
+    else:
+        target_csv = f"{ny_date}isolf.csv"
+        ny_zip_date = target_datetime.strftime("%Y%m01")
+        target_zip_url = (
+            f"http://mis.nyiso.com/public/csv/isolf/{ny_zip_date}isolf_csv.zip"
+        )
+        try:
+            df = read_zip_data(session, target_zip_url, target_csv)
+        except (RequestsHTTPError, KeyError):
+            # this can happen when target_datetime has no data available
+            return []
 
     # Add events consumption_list
     all_consumption_events = (
