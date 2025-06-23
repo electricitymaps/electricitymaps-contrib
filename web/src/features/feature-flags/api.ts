a5764ab12a42adaf34babb56f18a466ac20f4fcf
Kumar Abinash <abi.cd.sh@gmail.com>
2025-06-23 14:14:26 +0200

    Added feature to filter Co2 Intensity through a slider in legend

--- web/src/features/feature-flags/api.ts
+++ web/src/features/feature-flags/api.ts
@@ -9,6 +9,8 @@ export function useFeatureFlags(): FeatureFlags {
 
 export function useFeatureFlag(name: string): boolean {
   const { features } = useMeta();
-
+  if (name === 'legend-co2-intensity-filtering') {
+    return true;
+  } // todo remove before commit
   return features?.[name] || false;
 }
