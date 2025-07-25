59720404a93ad256d9c03052e90a7ad66eb858a6
Bastien Bigué <bastien.bigue@gmail.com>
2025-07-25 13:38:32 +0200

    format

--- scripts/zone_names.py
+++ scripts/zone_names.py
@@ -1,6 +1,8 @@
-import os
 import json
+import os
+
 import yaml
+
 
 def add_country_names():
     json_path = os.path.join(os.path.dirname(__file__), "../web/public/locales/en.json")
@@ -19,18 +21,19 @@ def add_country_names():
         if not os.path.exists(yaml_path):
             print(f"Zone {key} is in the JSON file but not in the zones directory")
             continue
-        with open(yaml_path, "r", encoding="utf-8") as f_yaml:
+        with open(yaml_path, encoding="utf-8") as f_yaml:
             try:
                 yaml_data = yaml.safe_load(f_yaml)
             except Exception:
                 print(f"Error parsing yaml file : {yaml_path}")
-                continue 
+                continue
         yaml_data["country_name"] = country_name
         yaml_data["zone_name"] = zone_name
         if zone_short_name:
             yaml_data["zone_short_name"] = zone_short_name
         with open(yaml_path, "w", encoding="utf-8") as f_yaml:
             yaml.dump(yaml_data, f_yaml, allow_unicode=True, sort_keys=False)
+
 
 def check_additional_attributes():
     zones_dir = os.path.join(os.path.dirname(__file__), "../config/zones")
@@ -41,7 +44,7 @@ def check_additional_attributes():
         if not filename.endswith(".yaml"):
             continue
         yaml_path = os.path.join(zones_dir, filename)
-        with open(yaml_path, "r", encoding="utf-8") as f:
+        with open(yaml_path, encoding="utf-8") as f:
             yaml_data = yaml.safe_load(f)
         if "country_name" not in yaml_data:
             zones_without_country_name.append(filename)
@@ -52,6 +55,7 @@ def check_additional_attributes():
     print(f"Zones without country_name: {zones_without_country_name}")
     print(f"Zones without zone_name: {zones_without_zone_name}")
     print(f"Zones with zone_short_name: {zones_with_zone_short_name}")
+
 
 if __name__ == "__main__":
     add_country_names()
