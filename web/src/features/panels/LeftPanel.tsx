df2e4d784ab1d9f1be2504d31a8865eec794eea3
Cadence Banulis <cadence.banulis@electricitymaps.com>
2025-08-27 14:06:50 +0200

    add capacitor plugin to handle edge to edge on android 15

--- web/src/features/panels/LeftPanel.tsx
+++ web/src/features/panels/LeftPanel.tsx
@@ -5,7 +5,7 @@ import { twMerge } from 'tailwind-merge';
 
 function MapMobileHeader() {
   return (
-    <div className="flex w-full items-center justify-between bg-gradient-to-b to-transparent pb-4 pl-3 pt-[max(0.75rem,env(safe-area-inset-top))] dark:from-black/60 sm:pt-3">
+    <div className="mt-2 flex w-full items-center justify-between bg-gradient-to-b to-transparent pb-4 pl-3 pt-[max(0.75rem,env(safe-area-inset-top))] dark:from-black/60 sm:pt-3">
       <Logo className=" h-8 w-28  fill-black dark:fill-white" />
     </div>
   );
