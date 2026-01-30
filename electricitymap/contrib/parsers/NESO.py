a61864e8adc388e78a2df62b7971de61b6df0224
silkeholmebonnen <silke.bonnen@gmail.com>
2026-01-30 15:38:04 +0100

    use add_value

--- electricitymap/contrib/parsers/NESO.py
+++ electricitymap/contrib/parsers/NESO.py
@@ -14,6 +14,22 @@ from electricitymap.contrib.parsers.lib.exceptions import ParserException
 NESO_API = "https://api.neso.energy/api/3/action/datastore_search_sql"
 NESO_GENERATION_DATASET_ID = "f93d1835-75bc-43e5-84ad-12472b180a98"
 
+NESO_TO_PRODUCTION_MIX_MAPPING = {
+    "BIOMASS": "biomass",
+    "COAL": "coal",
+    "GAS": "gas",
+    "HYDRO": "hydro",
+    "NUCLEAR": "nuclear",
+    "SOLAR": "solar",
+    "WIND": "wind",
+    "WIND_EMB": "wind",
+    "OTHER": "unknown",
+}
+
+NESO_TO_STORAGE_MIX_MAPPING = {
+    "STORAGE": "hydro",
+}
+
 
 def fetch_production(
     zone_key: ZoneKey,
@@ -21,6 +37,8 @@ def fetch_production(
     target_datetime: datetime | None = None,
     logger: Logger = getLogger(__name__),
 ) -> list[dict] | dict:
+    session = session or Session()
+
     if target_datetime is None:
         start_datetime = datetime.now(tz=ZoneInfo("UTC")) - timedelta(hours=24)
         sql_query = f"""SELECT * FROM "{NESO_GENERATION_DATASET_ID}" WHERE "DATETIME" >= '{start_datetime.strftime("%Y-%m-%d")}' ORDER BY "DATETIME" ASC"""
@@ -33,7 +51,7 @@ def fetch_production(
         sql_query = f"""SELECT * FROM "{NESO_GENERATION_DATASET_ID}" WHERE "DATETIME" >= '{start_datetime.strftime("%Y-%m-%d")}' AND "DATETIME" <= '{end_datetime.strftime("%Y-%m-%d")}' ORDER BY "DATETIME" ASC"""
     else:
         raise ParserException(
-            "example_parser.py",
+            "NESO.py",
             "This parser is not yet able to parse dates before 2009-01-01",
             zone_key,
         )
@@ -43,33 +61,35 @@ def fetch_production(
     res: Response = session.get(NESO_API, params=params)
     if not res.status_code == 200:
         raise ParserException(
-            "example_parser.py",
+            "NESO.py",
             f"Exception when fetching production error code: {res.status_code}: {res.text}",
             zone_key,
         )
 
     obj = res.json()["result"]["records"]
-    production_list = ProductionBreakdownList(logger=logger)
 
-    for item in obj:
+    for row in obj:
+        production_list = ProductionBreakdownList(logger=logger)
+        production_mix = ProductionMix()
+
+        for neso_key, emaps_key in NESO_TO_PRODUCTION_MIX_MAPPING.items():
+            production_mix.add_value(emaps_key, float(row[neso_key]))
+
+        storage_mix = StorageMix()
+        for neso_key, emaps_key in NESO_TO_STORAGE_MIX_MAPPING.items():
+            storage_mix.add_value(
+                emaps_key,
+                float(row[neso_key]) * -1 if float(row[neso_key]) != 0 else 0,
+            )
+
         production_list.append(
             zoneKey=zone_key,
-            datetime=datetime.fromisoformat(item["DATETIME"]).replace(
+            datetime=datetime.fromisoformat(row["DATETIME"]).replace(
                 tzinfo=ZoneInfo("UTC")
             ),
-            production=ProductionMix(
-                biomass=float(item["BIOMASS"]),
-                coal=float(item["COAL"]),
-                gas=float(item["GAS"]),
-                hydro=float(item["HYDRO"]),
-                nuclear=float(item["NUCLEAR"]),
-                solar=float(item["SOLAR"]),
-                wind=float(item["WIND"]) + float(item["WIND_EMB"]),
-                unknown=(float(item["OTHER"])),
-            ),
-            storage=StorageMix(
-                hydro=float(item["STORAGE"]) * -1 if float(item["STORAGE"]) != 0 else 0
-            ),  # Classify storage as hydro storage since ELEXON uses this mapping and because NESO demand data is also hydro storage
+            production=production_mix,
+            storage=storage_mix,
             source="neso.energy",
         )
+
     return production_list.to_list()
